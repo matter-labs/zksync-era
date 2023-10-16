7b736d29f8fbdafc3e03f9dbd5ab9ddc445c5a07
Emil <evl@matterlabs.dev>
2023-10-16 13:12:38 +0200

    Cleanup BWIP DAL and main.rs testing revert

--- core/bin/zksync_server/src/main.rs
+++ core/bin/zksync_server/src/main.rs
@@ -28,7 +28,7 @@ struct Cli {
     /// Comma-separated list of components to launch.
     #[arg(
         long,
-        default_value = "api,tree,eth,data_fetcher,state_keeper,housekeeper"
+        default_value = "api,tree,eth,data_fetcher,state_keeper,witness_generator,housekeeper"
     )]
     components: ComponentsToRun,
 }
