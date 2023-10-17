314d2ba8fd4412b35d288f916aa060ff09a4ea0c
Emil <evl@matterlabs.dev>
2023-10-17 12:17:53 +0200

    Rebase + fix linting issues

--- core/lib/dal/src/blocks_dal.rs
+++ core/lib/dal/src/blocks_dal.rs
@@ -12,8 +12,8 @@ use zksync_types::{
     aggregated_operations::AggregatedActionType,
     block::{BlockGasCount, L1BatchHeader, MiniblockHeader},
     commitment::{L1BatchMetadata, L1BatchWithMetadata},
-    Address, L1BatchNumber, LogQuery, MiniblockNumber, ProtocolVersionId, H256, MAX_GAS_PER_PUBDATA_BYTE,
-    U256,
+    Address, L1BatchNumber, LogQuery, MiniblockNumber, ProtocolVersionId, H256,
+    MAX_GAS_PER_PUBDATA_BYTE, U256,
 };
 use zksync_utils::be_bytes_to_safe_address;
 
