5e175ad36b8354561dbf18a04636ed5dbe2c07da
koloz <zach.kolodny@gmail.com>
2023-10-20 19:59:54 -0400

    added unit test for extracting events

--- core/lib/types/src/event.rs
+++ core/lib/types/src/event.rs
@@ -61,7 +61,7 @@ static L1_MESSAGE_EVENT_SIGNATURE: Lazy<H256> = Lazy::new(|| {
 //     bytes32 key;
 //     bytes32 value;
 // }
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Clone, PartialEq)]
 pub struct L1MessengerL2ToL1Log {
     l2_shard_id: u8,
     is_service: bool,
@@ -334,4 +334,115 @@ pub fn extract_published_bytecodes(all_generated_events: &[VmEvent]) -> Vec<H256
 pub struct VmEventGroupKey {
     pub address: Address,
     pub topic: (u32, H256),
+}
+
+#[cfg(test)]
+mod tests {
+    use zksync_basic_types::{
+        ethabi::{self, Token},
+        Address, L1BatchNumber, U256,
+    };
+    use zksync_system_constants::{
+        BOOTLOADER_ADDRESS, KNOWN_CODES_STORAGE_ADDRESS, L1_MESSENGER_ADDRESS, L2_ETH_TOKEN_ADDRESS,
+    };
+
+    use crate::VmEvent;
+
+    use super::{extract_l2tol1logs_from_l1_messenger, L1MessengerL2ToL1Log};
+
+    fn create_value(tx_number: U256, sender: Address, key: U256, value: U256) -> Vec<u8> {
+        let mut key_arr = [0u8; 32];
+        key.to_big_endian(&mut key_arr);
+
+        let mut val_arr = [0u8; 32];
+        value.to_big_endian(&mut val_arr);
+
+        let tokens = vec![
+            /*l2ShardId*/ Token::Uint(U256::from(0)),
+            /*isService*/ Token::Bool(true),
+            /*txNumberInBlock*/ Token::Uint(tx_number),
+            /*sender*/ Token::Address(sender),
+            /*key*/ Token::FixedBytes(key_arr.to_vec()),
+            /*value*/ Token::FixedBytes(val_arr.to_vec()),
+        ];
+
+        ethabi::encode(&tokens)
+    }
+
+    fn create_vm_event(
+        from: Address,
+        tx_number: U256,
+        sender: Address,
+        key: U256,
+        value: U256,
+    ) -> VmEvent {
+        let l1_messenger_l2_to_l1_log_event_signature = ethabi::long_signature(
+            "L2ToL1LogSent",
+            &[ethabi::ParamType::Tuple(vec![
+                ethabi::ParamType::Uint(8),
+                ethabi::ParamType::Bool,
+                ethabi::ParamType::Uint(16),
+                ethabi::ParamType::Address,
+                ethabi::ParamType::FixedBytes(32),
+                ethabi::ParamType::FixedBytes(32),
+            ])],
+        );
+
+        VmEvent {
+            location: (L1BatchNumber(1), 0u32),
+            address: from,
+            indexed_topics: vec![l1_messenger_l2_to_l1_log_event_signature],
+            value: create_value(tx_number, sender, key, value),
+        }
+    }
+
+    #[test]
+    fn test_extract_l2tol1logs_from_l1_messenger() {
+        let expected = vec![
+            L1MessengerL2ToL1Log {
+                l2_shard_id: 0u8,
+                is_service: true,
+                tx_number_in_block: 5u16,
+                sender: KNOWN_CODES_STORAGE_ADDRESS,
+                key: U256::from(11),
+                value: U256::from(19),
+            },
+            L1MessengerL2ToL1Log {
+                l2_shard_id: 0u8,
+                is_service: true,
+                tx_number_in_block: 7u16,
+                sender: L1_MESSENGER_ADDRESS,
+                key: U256::from(19),
+                value: U256::from(93),
+            },
+        ];
+
+        let events = vec![
+            create_vm_event(
+                L1_MESSENGER_ADDRESS,
+                U256::from(5),
+                KNOWN_CODES_STORAGE_ADDRESS,
+                U256::from(11),
+                U256::from(19),
+            ),
+            create_vm_event(
+                BOOTLOADER_ADDRESS,
+                U256::from(6),
+                L2_ETH_TOKEN_ADDRESS,
+                U256::from(6),
+                U256::from(8),
+            ),
+            create_vm_event(
+                L1_MESSENGER_ADDRESS,
+                U256::from(7),
+                L1_MESSENGER_ADDRESS,
+                U256::from(19),
+                U256::from(93),
+            ),
+        ];
+
+        let logs = extract_l2tol1logs_from_l1_messenger(&events);
+
+        assert_eq!(expected, logs);
+    }
 }
