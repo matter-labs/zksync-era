fa4005d0e74281c0da27fd94df64261d988193d8
Akash <112477155+akash-chandrakar@users.noreply.github.com>
2023-10-27 11:59:17 +0400

    Update 20231027074517_add_index_for_fri_prover_job_retry.up.sql

--- core/lib/dal/migrations/20231027074517_add_index_for_fri_prover_job_retry.up.sql
+++ core/lib/dal/migrations/20231027074517_add_index_for_fri_prover_job_retry.up.sql
@@ -1,4 +1,4 @@
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_prover_jobs_fri_status_processing_attempts_2
+CREATE INDEX IF NOT EXISTS idx_prover_jobs_fri_status_processing_attempts_2
     ON prover_jobs_fri (processing_started_at, attempts)
     WHERE status IN ('in_progress', 'failed', 'in_gpu_proof');
 
