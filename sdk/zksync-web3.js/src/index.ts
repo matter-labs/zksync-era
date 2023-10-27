0b7916d38de0a503ebaffd724badfd5c73372734
Nicolas Villanueva <nicolasvillanueva@msn.com>
2023-10-27 04:37:23 -0700

    chore: remove zksync-web3.js stale project (#179)
    # What :computer: 
    * Remove majority of `sdk/zksync-web3.js` directory
    * Add `README.md` for `zksync-web3.js` with migration update
    * Update `link:` references to `file:` references in `package.json`
    * Upgrade version of `zksync-web3` used
    
    # Why :hand:
    * This repository was moved to https://github.com/zksync-sdk/zksync2-js
    , and this will reduce the number of Issues/Pull Requests in this repo
    that should be for another repo
    * This new README includes a warning that this code has been migrated.
    Just in case people are searching for the source of this code from
    pre-existing links
    * `link:` was deprecated and it wasn't pointing to the correct directory
    * Using the latest `0.15.5` version will bring consistency to the
    monorepo

--- sdk/zksync-web3.js/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * as utils from './utils';
-export * as types from './types';
-export { EIP712Signer, Signer, L1Signer, L1VoidSigner, L2VoidSigner } from './signer';
-export { Wallet } from './wallet';
-export { Web3Provider, Provider } from './provider';
-export { ContractFactory, Contract } from './contract';
