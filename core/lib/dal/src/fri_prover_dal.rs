29faa9a259f8a7939e1ee6459805ee95934f7a9c
Akash <a.chandrakar@matterlabs.dev>
2023-11-01 11:42:53 +0400

    update query

--- core/lib/dal/src/fri_prover_dal.rs
+++ core/lib/dal/src/fri_prover_dal.rs
@@ -113,15 +113,15 @@ impl FriProverDal<'_, '_> {
                     picked_by = $4
                 WHERE id = (
                     SELECT pj.id
-                    FROM prover_jobs_fri AS pj
-                    JOIN (
-                        SELECT unnest($1::smallint[]) AS circuit_id, unnest($2::smallint[]) AS aggregation_round
-                    ) AS unnested_data
-                    ON pj.circuit_id = unnested_data.circuit_id
-                    AND pj.aggregation_round = unnested_data.aggregation_round
+                    FROM prover_jobs_fri as pj
+                     join (SELECT *
+                            FROM unnest($1::smallint[], $2::smallint[])
+                     )
+                     as tuple (circuit_id, round)
+                     on tuple.circuit_id = pj.circuit_id and tuple.round = pj.aggregation_round
                     WHERE pj.status = 'queued'
                     AND pj.protocol_version = ANY($3)
-                    ORDER BY pj.aggregation_round DESC, pj.l1_batch_number ASC, pj.id ASC
+                    ORDER BY pj.l1_batch_number ASC, pj.aggregation_round DESC, pj.id ASC
                     LIMIT 1
                     FOR UPDATE
                     SKIP LOCKED
