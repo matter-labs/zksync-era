df49eed43366920130a2881cd9943c9e04281d51
Jean <148654781+oxJean@users.noreply.github.com>
2023-11-16 03:46:42 +0800

    chore: fixed typo in code notes
    chore: fixed typo in code notes

--- infrastructure/protocol-upgrade/src/transaction.ts
+++ infrastructure/protocol-upgrade/src/transaction.ts
@@ -3,8 +3,8 @@ import { BytesLike, ethers } from 'ethers';
 import { ForceDeployUpgraderFactory as ForceDeployUpgraderFactoryL2 } from 'l2-zksync-contracts/typechain';
 import { DefaultUpgradeFactory as DefaultUpgradeFactoryL1 } from 'l1-zksync-contracts/typechain';
 import { FacetCut } from 'l1-zksync-contracts/src.ts/diamondCut';
-import { IZkSyncFactory } from '../pre-boojum/IZkSyncFactory';
-import { ComplexUpgrader__factory } from '../../../etc/system-contracts/typechain-types';
+import { IZkSyncFactory } from 'l1-zksync-contracts/typechain/IZkSyncFactory';
+import { ComplexUpgraderFactory } from '../../../etc/system-contracts/typechain';
 import {
     getCommonDataFileName,
     getCryptoFileName,
@@ -56,7 +56,7 @@ export interface L2CanonicalTransaction {
     // is to be passed to account and any changes to its structure
     // would mean a breaking change to these accounts. In order to prevent this,
     // we should keep some fields as "reserved".
-    // It is also recommneded that their length is fixed, since
+    // It is also recommended that their length is fixed, since
     // it would allow easier proof integration (in case we will need
     // some special circuit for preprocessing transactions).
     reserved: [BigNumberish, BigNumberish, BigNumberish, BigNumberish];
@@ -146,7 +146,7 @@ export function forceDeploymentCalldata(forcedDeployments: ForceDeployment[]): B
 }
 
 export function prepareCallDataForComplexUpgrader(calldata: BytesLike, to: string): BytesLike {
-    const upgrader = new ComplexUpgrader__factory();
+    const upgrader = new ComplexUpgraderFactory();
     let finalCalldata = upgrader.interface.encodeFunctionData('upgrade', [to, calldata]);
     return finalCalldata;
 }
