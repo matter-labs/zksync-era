4f6ce4d7d22dbe8e539267502433075ff4bac629
Stanislav Breadless <stanislavbezkor@gmail.com>
2023-11-17 14:53:09 +0100

    new migration

--- core/lib/dal/migrations/20231019125310_storage-refunds.up.sql
+++ core/lib/dal/migrations/20231019125310_storage-refunds.up.sql
@@ -1,2 +1,6 @@
 ALTER TABLE l1_batches
     ADD COLUMN IF NOT EXISTS storage_refunds BIGINT[];
+
+-- DONT LET BE ME MERGED --
+ALTER TABLE miniblocks
+    ADD COLUMN IF NOT EXISTS pubdata_price BIGINT;
