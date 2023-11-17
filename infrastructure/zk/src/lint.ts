58206cbbe08b92725721516ea6e0ae473fc4cbf7
tomg10 <lemures64@gmail.com>
2023-11-17 13:02:53 +0100

    fix: fmt

--- infrastructure/zk/src/lint.ts
+++ infrastructure/zk/src/lint.ts
@@ -39,7 +39,7 @@ async function lintSystemContracts(check: boolean = false) {
 async function clippy() {
     process.chdir(process.env.ZKSYNC_HOME!);
     const { stdout: version } = await utils.exec('cargo clippy --version');
-    console.log(`linting using clippy: ${version}`)
+    console.log(`linting using clippy: ${version}`);
     await utils.spawn('cargo clippy --tests -- -D warnings');
 }
 
