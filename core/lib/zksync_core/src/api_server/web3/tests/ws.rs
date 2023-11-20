df8d824e7b1d531878a6de363280e93ab25fdcb9
Alex Ostrovski <aov@matterlabs.dev>
2023-11-20 15:21:41 +0200

    Test `get_filter_changes` RPC method

--- core/lib/zksync_core/src/api_server/web3/tests/ws.rs
+++ core/lib/zksync_core/src/api_server/web3/tests/ws.rs
@@ -11,14 +11,19 @@ use zksync_types::{
     api, block::MiniblockHeader, fee::TransactionExecutionMetrics, tx::IncludedTxLocation, Address,
     L1BatchNumber, ProtocolVersionId, VmEvent, H256, U64,
 };
+use zksync_web3_decl::types::Filter;
 use zksync_web3_decl::{
     jsonrpsee::{
-        core::client::{Subscription, SubscriptionClientT},
+        core::{
+            client::{Subscription, SubscriptionClientT},
+            Error as RpcError,
+        },
         rpc_params,
+        types::error::ErrorCode,
         ws_client::{WsClient, WsClientBuilder},
     },
     namespaces::{EthNamespaceClient, ZksNamespaceClient},
-    types::{BlockHeader, PubSubFilter},
+    types::{BlockHeader, FilterChanges, PubSubFilter},
 };
 
 use super::*;
@@ -108,20 +113,8 @@ async fn ws_server_can_start() {
 #[derive(Debug)]
 struct BasicSubscriptions;
 
-#[async_trait]
-impl WsTest for BasicSubscriptions {
-    async fn test(&self, client: &WsClient, pool: &ConnectionPool) {
-        let params = rpc_params!["newHeads"];
-        let mut blocks_subscription = client
-            .subscribe::<BlockHeader, _>("eth_subscribe", params, "eth_unsubscribe")
-            .await
-            .unwrap();
-        let params = rpc_params!["newPendingTransactions"];
-        let mut txs_subscription = client
-            .subscribe::<H256, _>("eth_subscribe", params, "eth_unsubscribe")
-            .await
-            .unwrap();
-
+impl BasicSubscriptions {
+    async fn update_storage(pool: &ConnectionPool) -> (MiniblockHeader, H256) {
         let mut storage = pool.access_storage().await.unwrap();
         let new_tx = create_l2_transaction(1, 2);
         let new_tx_hash = new_tx.hash();
@@ -137,7 +130,25 @@ impl WsTest for BasicSubscriptions {
             .insert_miniblock(&new_miniblock)
             .await
             .unwrap();
-        drop(storage);
+        (new_miniblock, new_tx_hash)
+    }
+}
+
+#[async_trait]
+impl WsTest for BasicSubscriptions {
+    async fn test(&self, client: &WsClient, pool: &ConnectionPool) {
+        let params = rpc_params!["newHeads"];
+        let mut blocks_subscription = client
+            .subscribe::<BlockHeader, _>("eth_subscribe", params, "eth_unsubscribe")
+            .await
+            .unwrap();
+        let params = rpc_params!["newPendingTransactions"];
+        let mut txs_subscription = client
+            .subscribe::<H256, _>("eth_subscribe", params, "eth_unsubscribe")
+            .await
+            .unwrap();
+
+        let (new_miniblock, new_tx_hash) = Self::update_storage(pool).await;
 
         let received_tx_hash = tokio::time::timeout(TEST_TIMEOUT, txs_subscription.next())
             .await
@@ -165,36 +176,8 @@ async fn basic_subscriptions() {
 #[derive(Debug)]
 struct LogSubscriptions;
 
-#[async_trait]
-impl WsTest for LogSubscriptions {
-    async fn test(&self, client: &WsClient, pool: &ConnectionPool) {
-        let params = rpc_params!["logs"];
-        let mut all_logs_subscription = client
-            .subscribe::<api::Log, _>("eth_subscribe", params, "eth_unsubscribe")
-            .await
-            .unwrap();
-        let address_filter = PubSubFilter {
-            address: Some(Address::repeat_byte(23).into()),
-            topics: None,
-        };
-        let params = rpc_params!["logs", address_filter];
-        let mut address_subscription = client
-            .subscribe::<api::Log, _>("eth_subscribe", params, "eth_unsubscribe")
-            .await
-            .unwrap();
-        let topic_filter = PubSubFilter {
-            address: None,
-            topics: Some(vec![Some(H256::repeat_byte(42).into())]),
-        };
-        let params = rpc_params!["logs", topic_filter];
-        let mut topic_subscription = client
-            .subscribe::<api::Log, _>("eth_subscribe", params, "eth_unsubscribe")
-            .await
-            .unwrap();
-
-        // Wait a little until subscriptions are fully registered.
-        tokio::time::sleep(Duration::from_millis(50)).await;
-
+impl LogSubscriptions {
+    async fn update_storage(pool: &ConnectionPool) -> (IncludedTxLocation, Vec<VmEvent>) {
         let mut storage = pool.access_storage().await.unwrap();
         let new_miniblock = create_miniblock(1);
         storage
@@ -237,12 +220,49 @@ impl WsTest for LogSubscriptions {
                 value: b"4".to_vec(),
             },
         ];
-        let events: Vec<_> = events.iter().collect();
         storage
             .events_dal()
-            .save_events(MiniblockNumber(1), &[(tx_location, events.clone())])
+            .save_events(
+                MiniblockNumber(1),
+                &[(tx_location, events.iter().collect())],
+            )
             .await;
-        drop(storage);
+        (tx_location, events)
+    }
+}
+
+#[async_trait]
+impl WsTest for LogSubscriptions {
+    async fn test(&self, client: &WsClient, pool: &ConnectionPool) {
+        let params = rpc_params!["logs"];
+        let mut all_logs_subscription = client
+            .subscribe::<api::Log, _>("eth_subscribe", params, "eth_unsubscribe")
+            .await
+            .unwrap();
+        let address_filter = PubSubFilter {
+            address: Some(Address::repeat_byte(23).into()),
+            topics: None,
+        };
+        let params = rpc_params!["logs", address_filter];
+        let mut address_subscription = client
+            .subscribe::<api::Log, _>("eth_subscribe", params, "eth_unsubscribe")
+            .await
+            .unwrap();
+        let topic_filter = PubSubFilter {
+            address: None,
+            topics: Some(vec![Some(H256::repeat_byte(42).into())]),
+        };
+        let params = rpc_params!["logs", topic_filter];
+        let mut topic_subscription = client
+            .subscribe::<api::Log, _>("eth_subscribe", params, "eth_unsubscribe")
+            .await
+            .unwrap();
+
+        // Wait a little until subscriptions are fully registered.
+        tokio::time::sleep(Duration::from_millis(50)).await;
+
+        let (tx_location, events) = Self::update_storage(pool).await;
+        let events: Vec<_> = events.iter().collect();
 
         let all_logs = collect_logs(&mut all_logs_subscription, 4).await;
         for (i, log) in all_logs.iter().enumerate() {
@@ -286,4 +306,102 @@ fn assert_logs_match(actual_logs: &[api::Log], expected_logs: &[&VmEvent]) {
 #[tokio::test]
 async fn log_subscriptions() {
     test_ws_server(LogSubscriptions).await;
+}
+
+#[derive(Debug)]
+struct BasicFilterChanges;
+
+#[async_trait]
+impl WsTest for BasicFilterChanges {
+    async fn test(&self, client: &WsClient, pool: &ConnectionPool) {
+        let block_filter_id = client.new_block_filter().await.unwrap();
+        let tx_filter_id = client.new_pending_transaction_filter().await.unwrap();
+
+        let (new_miniblock, new_tx_hash) = BasicSubscriptions::update_storage(pool).await;
+
+        let block_filter_changes = client.get_filter_changes(block_filter_id).await.unwrap();
+        assert_matches!(
+            block_filter_changes,
+            FilterChanges::Hashes(hashes) if hashes == [new_miniblock.hash]
+        );
+        let block_filter_changes = client.get_filter_changes(block_filter_id).await.unwrap();
+        assert_matches!(block_filter_changes, FilterChanges::Hashes(hashes) if hashes.is_empty());
+
+        let tx_filter_changes = client.get_filter_changes(tx_filter_id).await.unwrap();
+        assert_matches!(
+            tx_filter_changes,
+            FilterChanges::Hashes(hashes) if hashes == [new_tx_hash]
+        );
+        let tx_filter_changes = client.get_filter_changes(tx_filter_id).await.unwrap();
+        assert_matches!(tx_filter_changes, FilterChanges::Hashes(hashes) if hashes.is_empty());
+
+        // Check uninstalling the filter.
+        let removed = client.uninstall_filter(block_filter_id).await.unwrap();
+        assert!(removed);
+        let removed = client.uninstall_filter(block_filter_id).await.unwrap();
+        assert!(!removed);
+
+        let err = client
+            .get_filter_changes(block_filter_id)
+            .await
+            .unwrap_err();
+        assert_matches!(err, RpcError::Call(err) if err.code() == ErrorCode::InvalidParams.code());
+    }
+}
+
+#[tokio::test]
+async fn basic_filter_changes() {
+    test_ws_server(BasicFilterChanges).await;
+}
+
+#[derive(Debug)]
+struct LogFilterChanges;
+
+#[async_trait]
+impl WsTest for LogFilterChanges {
+    async fn test(&self, client: &WsClient, pool: &ConnectionPool) {
+        let all_logs_filter_id = client.new_filter(Filter::default()).await.unwrap();
+        let address_filter = Filter {
+            address: Some(Address::repeat_byte(23).into()),
+            ..Filter::default()
+        };
+        let address_filter_id = client.new_filter(address_filter).await.unwrap();
+        let topics_filter = Filter {
+            topics: Some(vec![Some(H256::repeat_byte(42).into())]),
+            ..Filter::default()
+        };
+        let topics_filter_id = client.new_filter(topics_filter).await.unwrap();
+
+        let (_, events) = LogSubscriptions::update_storage(pool).await;
+        let events: Vec<_> = events.iter().collect();
+
+        let all_logs = client.get_filter_changes(all_logs_filter_id).await.unwrap();
+        let FilterChanges::Logs(all_logs) = all_logs else {
+            panic!("Unexpected getFilterChanges output: {:?}", all_logs);
+        };
+        assert_logs_match(&all_logs, &events);
+
+        let address_logs = client.get_filter_changes(address_filter_id).await.unwrap();
+        let FilterChanges::Logs(address_logs) = address_logs else {
+            panic!("Unexpected getFilterChanges output: {:?}", address_logs);
+        };
+        assert_logs_match(&address_logs, &[events[0], events[3]]);
+
+        let topics_logs = client.get_filter_changes(topics_filter_id).await.unwrap();
+        let FilterChanges::Logs(topics_logs) = topics_logs else {
+            panic!("Unexpected getFilterChanges output: {:?}", topics_logs);
+        };
+        assert_logs_match(&topics_logs, &[events[1], events[3]]);
+
+        let new_all_logs = client.get_filter_changes(all_logs_filter_id).await.unwrap();
+        let FilterChanges::Logs(new_all_logs) = new_all_logs else {
+            panic!("Unexpected getFilterChanges output: {:?}", new_all_logs);
+        };
+        assert_eq!(new_all_logs, all_logs); // FIXME: is this expected?
+    }
+}
+
+#[tokio::test]
+async fn log_filter_changes() {
+    test_ws_server(LogFilterChanges).await;
 }
