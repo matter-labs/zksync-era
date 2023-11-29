c144b6c4dd9915cfab32513513b1bcc4087872cc
Danil <deniallugo@gmail.com>
2023-11-29 12:26:03 +0100

    Expose zk evm 1_4_0
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/lib/multivm/src/lib.rs
+++ core/lib/multivm/src/lib.rs
@@ -26,3 +26,6 @@ pub use versions::vm_m5;
 pub use versions::vm_m6;
 pub use versions::vm_refunds_enhancement;
 pub use versions::vm_virtual_blocks;
+pub use zk_evm_1_3_1;
+pub use zk_evm_1_3_3;
+pub use zk_evm_1_4_0;
