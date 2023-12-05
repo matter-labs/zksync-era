83d5a2a869177d4f512fe709b54f09c2269db477
pompon0 <pompon.pompon@gmail.com>
2023-12-05 18:06:51 +0300

    refactor: Removed protobuf encoding from zksync_types (#562)
    ## What ❔
    
    Removed protobuf encoding from zksync_types.
    
    ## Why ❔
    
    To make zksync_types have less dependencies.

--- core/lib/types/src/lib.rs
+++ core/lib/types/src/lib.rs
@@ -62,8 +62,6 @@ pub mod utils;
 pub mod vk_transform;
 pub mod vm_version;
 
-mod proto;
-
 /// Denotes the first byte of the special zkSync's EIP-712-signed transaction.
 pub const EIP_712_TX_TYPE: u8 = 0x71;
 
