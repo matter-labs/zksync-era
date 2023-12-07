6fefe6242e1be691330408925d70b624b8fe68d5
AnastasiiaVashchuk <72273339+AnastasiiaVashchuk@users.noreply.github.com>
2023-12-07 18:21:37 +0200

    chore: remove old witness generator (#619)

--- core/bin/external_node/src/main.rs
+++ core/bin/external_node/src/main.rs
@@ -193,14 +193,7 @@ async fn init_tasks(
         .build()
         .await
         .context("failed to build a tree_pool")?;
-    // todo: PLA-335
-    // Note: This pool isn't actually used by the metadata calculator, but it has to be provided anyway.
-    let prover_tree_pool = ConnectionPool::singleton(&config.postgres.database_url)
-        .build()
-        .await
-        .context("failed to build a prover_tree_pool")?;
-    let tree_handle =
-        task::spawn(metadata_calculator.run(tree_pool, prover_tree_pool, tree_stop_receiver));
+    let tree_handle = task::spawn(metadata_calculator.run(tree_pool, tree_stop_receiver));
 
     let consistency_checker_handle = tokio::spawn(consistency_checker.run(stop_receiver.clone()));
 
