9d8a57d55848607e56d3019bcfb879dc80da9f7e
Iván Litteri <ilitteri@fi.uba.ar>
2023-12-14 13:18:45 -0300

    Fix path to volume (relative path is not accepted in docker run -v)

--- infrastructure/zk/src/down.ts
+++ infrastructure/zk/src/down.ts
@@ -4,7 +4,7 @@ import * as utils from './utils';
 export async function down() {
     await utils.spawn('docker compose down -v');
     await utils.spawn('docker compose rm -s -f -v');
-    await utils.spawn('docker run --rm -v ./volumes:/volumes postgres:14 bash -c "rm -rf /volumes/*"');
+    await utils.spawn('docker run --rm -v volumes:/volumes postgres:14 bash -c "rm -rf /volumes/*"');
 }
 
 export const command = new Command('down').description('stop development containers').action(down);
