11a6e957da7cde43247fdff69c37bd1c25979dcf
Alex Ostrovski <aov@matterlabs.dev>
2023-12-19 13:18:36 +0200

    Add more tests for reorg detector

--- core/lib/zksync_core/src/reorg_detector/tests.rs
+++ core/lib/zksync_core/src/reorg_detector/tests.rs
@@ -2,6 +2,7 @@
 
 use std::collections::HashMap;
 
+use test_casing::{test_casing, Product};
 use tokio::sync::mpsc;
 use zksync_contracts::BaseSystemContractsHashes;
 use zksync_dal::StorageProcessor;
@@ -80,7 +81,6 @@ struct MockMaiNodeClient {
 #[async_trait]
 impl MainNodeClient for MockMaiNodeClient {
     async fn miniblock_hash(&self, number: MiniblockNumber) -> Result<Option<H256>, RpcError> {
-        dbg!(number);
         if let Some(response) = self.miniblock_responses.get(&number) {
             Ok(Some(*response))
         } else {
@@ -89,7 +89,6 @@ impl MainNodeClient for MockMaiNodeClient {
     }
 
     async fn l1_batch_root_hash(&self, number: L1BatchNumber) -> Result<Option<H256>, RpcError> {
-        dbg!(number);
         if let Some(response) = self.l1_batch_responses.get(&number) {
             Ok(Some(*response))
         } else {
@@ -117,11 +116,7 @@ async fn normal_reorg_function() {
         .await
         .unwrap();
 
-    let (stop_sender, stop_receiver) = watch::channel(false);
-    let (block_update_sender, mut block_update_receiver) =
-        mpsc::unbounded_channel::<(MiniblockNumber, L1BatchNumber)>();
     let mut client = MockMaiNodeClient::default();
-
     let miniblock_and_l1_batch_hashes = (1_u32..=10).map(|number| {
         let miniblock_hash = H256::from_low_u64_be(number.into());
         client
@@ -135,6 +130,9 @@ async fn normal_reorg_function() {
     });
     let miniblock_and_l1_batch_hashes: Vec<_> = miniblock_and_l1_batch_hashes.collect();
 
+    let (stop_sender, stop_receiver) = watch::channel(false);
+    let (block_update_sender, mut block_update_receiver) =
+        mpsc::unbounded_channel::<(MiniblockNumber, L1BatchNumber)>();
     let detector = ReorgDetector {
         client: Box::new(client),
         block_updater: Box::new(block_update_sender),
@@ -209,4 +207,135 @@ async fn detecting_reorg_by_batch_hash() {
     assert_eq!(last_correct_l1_batch, Some(L1BatchNumber(1)));
 }
 
-// FIXME: test mismatch by miniblock hash, transient and non-transient errors
+#[tokio::test]
+async fn detecting_reorg_by_miniblock_hash() {
+    let pool = ConnectionPool::test_pool().await;
+    let mut storage = pool.access_storage().await.unwrap();
+    ensure_genesis_state(&mut storage, L2ChainId::default(), &GenesisParams::mock())
+        .await
+        .unwrap();
+
+    let (_stop_sender, stop_receiver) = watch::channel(false);
+    let mut client = MockMaiNodeClient::default();
+    let miniblock_hash = H256::from_low_u64_be(23);
+    client
+        .miniblock_responses
+        .insert(MiniblockNumber(1), miniblock_hash);
+    client
+        .l1_batch_responses
+        .insert(L1BatchNumber(1), H256::repeat_byte(1));
+    client
+        .miniblock_responses
+        .insert(MiniblockNumber(2), miniblock_hash);
+    client
+        .miniblock_responses
+        .insert(MiniblockNumber(3), miniblock_hash);
+
+    let detector = ReorgDetector {
+        client: Box::new(client),
+        block_updater: Box::new(()),
+        pool: pool.clone(),
+        stop_receiver,
+        sleep_interval: Duration::from_millis(10),
+    };
+    let detector_task = tokio::spawn(detector.run());
+
+    store_miniblock(&mut storage, 1, miniblock_hash).await;
+    seal_l1_batch(&mut storage, 1, H256::repeat_byte(1)).await;
+    store_miniblock(&mut storage, 2, miniblock_hash).await;
+    store_miniblock(&mut storage, 3, H256::repeat_byte(42)).await;
+    // ^ Hash of the miniblock #3 differs from that on the main node.
+
+    let task_result = detector_task.await.unwrap();
+    let last_correct_l1_batch = task_result.unwrap();
+    assert_eq!(last_correct_l1_batch, Some(L1BatchNumber(1)));
+    // ^ All locally stored L1 batches should be correct.
+}
+
+#[derive(Debug, Clone, Copy)]
+enum StorageUpdateStrategy {
+    /// Prefill the local storage with all block data.
+    Prefill,
+    /// Sequentially add a new L1 batch after the previous one was checked.
+    Sequential,
+}
+
+impl StorageUpdateStrategy {
+    const ALL: [Self; 2] = [Self::Prefill, Self::Sequential];
+}
+
+#[test_casing(8, Product(([2, 3, 5, 8], StorageUpdateStrategy::ALL)))]
+#[tokio::test]
+async fn detecting_deep_reorg(
+    last_correct_batch: u32,
+    storage_update_strategy: StorageUpdateStrategy,
+) {
+    assert!(last_correct_batch < 10);
+
+    let pool = ConnectionPool::test_pool().await;
+    let mut storage = pool.access_storage().await.unwrap();
+    ensure_genesis_state(&mut storage, L2ChainId::default(), &GenesisParams::mock())
+        .await
+        .unwrap();
+
+    let mut client = MockMaiNodeClient::default();
+    let miniblock_and_l1_batch_hashes = (1_u32..=10).map(|number| {
+        let mut miniblock_hash = H256::from_low_u64_be(number.into());
+        client
+            .miniblock_responses
+            .insert(MiniblockNumber(number), miniblock_hash);
+        let mut l1_batch_hash = H256::repeat_byte(number as u8);
+        client
+            .l1_batch_responses
+            .insert(L1BatchNumber(number), l1_batch_hash);
+
+        if number > last_correct_batch {
+            miniblock_hash = H256::zero();
+            l1_batch_hash = H256::zero();
+        }
+        (number, miniblock_hash, l1_batch_hash)
+    });
+    let mut miniblock_and_l1_batch_hashes: Vec<_> = miniblock_and_l1_batch_hashes.collect();
+
+    if matches!(storage_update_strategy, StorageUpdateStrategy::Prefill) {
+        for &(number, miniblock_hash, l1_batch_hash) in &miniblock_and_l1_batch_hashes {
+            store_miniblock(&mut storage, number, miniblock_hash).await;
+            seal_l1_batch(&mut storage, number, l1_batch_hash).await;
+        }
+    }
+
+    let (_stop_sender, stop_receiver) = watch::channel(false);
+    let (block_update_sender, mut block_update_receiver) =
+        mpsc::unbounded_channel::<(MiniblockNumber, L1BatchNumber)>();
+    let detector = ReorgDetector {
+        client: Box::new(client),
+        block_updater: Box::new(block_update_sender),
+        pool: pool.clone(),
+        stop_receiver,
+        sleep_interval: Duration::from_millis(10),
+    };
+    let detector_task = tokio::spawn(detector.run());
+
+    if matches!(storage_update_strategy, StorageUpdateStrategy::Sequential) {
+        let mut last_number = 0;
+        while let Some((miniblock, l1_batch)) = block_update_receiver.recv().await {
+            if miniblock == MiniblockNumber(last_number) && l1_batch == L1BatchNumber(last_number) {
+                let (number, miniblock_hash, l1_batch_hash) =
+                    miniblock_and_l1_batch_hashes.remove(0);
+                assert_eq!(number, last_number + 1);
+                store_miniblock(&mut storage, number, miniblock_hash).await;
+                seal_l1_batch(&mut storage, number, l1_batch_hash).await;
+                last_number = number;
+            }
+        }
+    }
+
+    let task_result = detector_task.await.unwrap();
+    let last_correct_l1_batch = task_result.unwrap();
+    assert_eq!(
+        last_correct_l1_batch,
+        Some(L1BatchNumber(last_correct_batch))
+    );
+}
+
+// FIXME: test transient and non-transient errors
