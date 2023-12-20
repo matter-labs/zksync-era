d613e7486cd862c34bbfb9a918478972f2570f82
Alex Ostrovski <aov@matterlabs.dev>
2023-12-20 15:32:47 +0200

    Refactor `ConsistencyChecker::check_commitments()`

--- core/lib/zksync_core/src/consistency_checker/mod.rs
+++ core/lib/zksync_core/src/consistency_checker/mod.rs
@@ -1,13 +1,28 @@
 use std::time::Duration;
 
+use anyhow::Context as _;
 use zksync_contracts::PRE_BOOJUM_COMMIT_FUNCTION;
 use zksync_dal::ConnectionPool;
 use zksync_types::{
-    web3::{error, ethabi, transports::Http, types::TransactionId, Web3},
+    web3::{self, ethabi, transports::Http, types::TransactionId, Web3},
     L1BatchNumber,
 };
 
 use crate::metrics::{CheckerComponent, EN_METRICS};
+
+#[derive(Debug, thiserror::Error)]
+enum CheckError {
+    #[error("Web3 error communicating with L1")]
+    Web3(#[from] web3::Error),
+    #[error("Internal error")]
+    Internal(#[from] anyhow::Error),
+}
+
+impl From<zksync_dal::SqlxError> for CheckError {
+    fn from(err: zksync_dal::SqlxError) -> Self {
+        Self::Internal(err.into())
+    }
+}
 
 #[derive(Debug)]
 pub struct ConsistencyChecker {
@@ -33,113 +48,125 @@ impl ConsistencyChecker {
         }
     }
 
-    async fn check_commitments(&self, batch_number: L1BatchNumber) -> Result<bool, error::Error> {
-        let mut storage = self.db.access_storage().await.unwrap();
+    async fn check_commitments(&self, batch_number: L1BatchNumber) -> Result<bool, CheckError> {
+        let mut storage = self.db.access_storage().await?;
 
         let storage_l1_batch = storage
             .blocks_dal()
             .get_storage_l1_batch(batch_number)
-            .await
-            .unwrap()
-            .unwrap_or_else(|| panic!("L1 batch #{} not found in the database", batch_number));
+            .await?
+            .with_context(|| format!("L1 batch #{batch_number} not found in the database"))?;
 
         let commit_tx_id = storage_l1_batch
             .eth_commit_tx_id
-            .unwrap_or_else(|| panic!("Commit tx not found for L1 batch #{}", batch_number))
+            .with_context(|| format!("Commit tx not found for L1 batch #{batch_number}"))?
             as u32;
 
         let block_metadata = storage
             .blocks_dal()
             .get_l1_batch_with_metadata(storage_l1_batch)
-            .await
-            .unwrap()
-            .unwrap_or_else(|| {
-                panic!(
-                    "Metadata for L1 batch #{} not found in the database",
-                    batch_number
-                )
-            });
+            .await?
+            .with_context(|| {
+                format!("Metadata for L1 batch #{batch_number} not found in the database")
+            })?;
 
         let commit_tx_hash = storage
             .eth_sender_dal()
             .get_confirmed_tx_hash_by_eth_tx_id(commit_tx_id)
-            .await
-            .unwrap()
-            .unwrap_or_else(|| {
-                panic!(
-                    "Commit tx hash not found in the database. Commit tx id: {}",
-                    commit_tx_id
-                )
-            });
+            .await?
+            .with_context(|| {
+                format!("Commit tx hash not found in the database for tx id {commit_tx_id}")
+            })?;
 
-        tracing::info!(
-            "Checking commit tx {} for batch {}",
-            commit_tx_hash,
-            batch_number.0
-        );
+        tracing::info!("Checking commit tx {commit_tx_hash} for L1 batch #{batch_number}");
 
-        // we can't get tx calldata from db because it can be fake
+        // We can't get tx calldata from db because it can be fake.
         let commit_tx = self
             .web3
             .eth()
             .transaction(TransactionId::Hash(commit_tx_hash))
             .await?
-            .expect("Commit tx not found on L1");
+            .with_context(|| format!("Commit for tx {commit_tx_hash:?} not found on L1"))?;
 
         let commit_tx_status = self
             .web3
             .eth()
             .transaction_receipt(commit_tx_hash)
             .await?
-            .expect("Commit tx receipt not found on L1")
+            .with_context(|| format!("Receipt for tx {commit_tx_hash:?} not found on L1"))?
             .status;
 
-        assert_eq!(
-            commit_tx_status,
-            Some(1.into()),
-            "Main node gave us a failed commit tx"
-        );
+        if commit_tx_status != Some(1.into()) {
+            let err = anyhow::anyhow!("Main node gave us a failed commit tx");
+            return Err(err.into());
+        }
 
-        let commit_function = if block_metadata
+        let is_pre_boojum = block_metadata
             .header
             .protocol_version
-            .unwrap()
-            .is_pre_boojum()
-        {
-            PRE_BOOJUM_COMMIT_FUNCTION.clone()
+            .map_or(true, |ver| ver.is_pre_boojum());
+        let commit_function = if is_pre_boojum {
+            &*PRE_BOOJUM_COMMIT_FUNCTION
         } else {
-            self.contract.function("commitBatches").unwrap().clone()
+            self.contract
+                .function("commitBatches")
+                .context("L1 contract does not have `commitBatches` function")?
         };
 
-        let commitments = commit_function
+        let mut commit_input_tokens = commit_function
             .decode_input(&commit_tx.input.0[4..])
-            .unwrap()
+            .with_context(|| {
+                format!("Failed decoding L1 commit function for transaction {commit_tx_hash:?}")
+            })?;
+        let commitments = commit_input_tokens
             .pop()
-            .unwrap()
+            .context("Unexpected signature for L1 commit function")?
             .into_array()
-            .unwrap();
+            .context("Unexpected signature for L1 commit function")?;
 
         // Commit transactions usually publish multiple commitments at once, so we need to find
         // the one that corresponds to the batch we're checking.
-        let first_batch_number = match &commitments[0] {
-            ethabi::Token::Tuple(tuple) => tuple[0].clone().into_uint().unwrap().as_usize(),
-            _ => panic!("ABI does not match the expected one"),
+        let first_batch_commitment = commitments.first().with_context(|| {
+            format!("L1 batch commitment in transaction {commit_tx_hash:?} is empty")
+        })?;
+        let ethabi::Token::Tuple(first_batch_commitment) = first_batch_commitment else {
+            let err = anyhow::anyhow!("Unexpected signature for L1 commit function");
+            return Err(err.into());
         };
-        let commitment = &commitments[batch_number.0 as usize - first_batch_number];
+        let first_batch_number = first_batch_commitment
+            .first()
+            .context("Unexpected signature for L1 commit function")?;
+        let first_batch_number = first_batch_number
+            .clone()
+            .into_uint()
+            .context("Unexpected signature for L1 commit function")?;
+        let first_batch_number = usize::try_from(first_batch_number)
+            .map_err(|_| anyhow::anyhow!("Integer overflow for L1 batch number"))?;
+        // ^ `TryFrom` has `&str` error here, so we can't use `.context()`.
 
-        Ok(commitment == &block_metadata.l1_commit_data())
+        let commitment = (batch_number.0 as usize)
+            .checked_sub(first_batch_number)
+            .and_then(|offset| commitments.get(offset));
+        let commitment = commitment.with_context(|| {
+            let actual_range = first_batch_number..(first_batch_number + commitments.len());
+            format!(
+                "Malformed commitment data in transaction {commit_tx_hash:?}; it should prove L1 batch #{batch_number}, \
+                 but it actually proves batches #{actual_range:?}"
+            )
+        })?;
+
+        Ok(*commitment == block_metadata.l1_commit_data())
     }
 
-    async fn last_committed_batch(&self) -> L1BatchNumber {
-        self.db
+    async fn last_committed_batch(&self) -> anyhow::Result<L1BatchNumber> {
+        Ok(self
+            .db
             .access_storage()
-            .await
-            .unwrap()
+            .await?
             .blocks_dal()
             .get_number_of_last_l1_batch_committed_on_eth()
-            .await
-            .unwrap()
-            .unwrap_or(L1BatchNumber(0))
+            .await?
+            .unwrap_or(L1BatchNumber(0))) // FIXME: not always valid
     }
 
     pub async fn run(
@@ -148,13 +175,16 @@ impl ConsistencyChecker {
     ) -> anyhow::Result<()> {
         let mut batch_number: L1BatchNumber = self
             .last_committed_batch()
-            .await
+            .await?
             .0
             .saturating_sub(self.max_batches_to_recheck)
-            .max(1)
+            .max(1) // FIXME: not always valid
             .into();
 
-        tracing::info!("Starting consistency checker from batch {}", batch_number.0);
+        tracing::info!(
+            "Starting consistency checker from L1 batch #{}",
+            batch_number.0
+        );
 
         loop {
             if *stop_receiver.borrow() {
@@ -162,43 +192,47 @@ impl ConsistencyChecker {
                 break;
             }
 
-            let metadata = self
+            let batch_with_metadata = self
                 .db
                 .access_storage()
-                .await
-                .unwrap()
+                .await?
                 .blocks_dal()
                 .get_l1_batch_metadata(batch_number)
-                .await
-                .unwrap();
-            let batch_has_metadata = metadata
-                .map(|m| {
-                    m.metadata.bootloader_initial_content_commitment.is_some()
-                        && m.metadata.events_queue_commitment.is_some()
-                })
-                .unwrap_or(false);
+                .await?;
+            let batch_has_metadata = batch_with_metadata.map_or(false, |batch| {
+                batch
+                    .metadata
+                    .bootloader_initial_content_commitment
+                    .is_some()
+                    && batch.metadata.events_queue_commitment.is_some()
+            });
 
             // The batch might be already committed but not yet processed by the external node's tree
             // OR the batch might be processed by the external node's tree but not yet committed.
             // We need both.
-            if !batch_has_metadata || self.last_committed_batch().await < batch_number {
+            if !batch_has_metadata || self.last_committed_batch().await? < batch_number {
                 tokio::time::sleep(SLEEP_DELAY).await;
                 continue;
             }
 
             match self.check_commitments(batch_number).await {
                 Ok(true) => {
-                    tracing::info!("Batch {} is consistent with L1", batch_number.0);
+                    tracing::info!("L1 batch #{batch_number} is consistent with L1");
                     EN_METRICS.last_correct_batch[&CheckerComponent::ConsistencyChecker]
                         .set(batch_number.0.into());
-                    batch_number.0 += 1;
+                    batch_number += 1;
                 }
                 Ok(false) => {
-                    anyhow::bail!("Batch {} is inconsistent with L1", batch_number.0);
+                    anyhow::bail!("L1 Batch #{batch_number} is inconsistent with L1");
                 }
-                Err(e) => {
-                    tracing::warn!("Consistency checker error: {}", e);
+                Err(CheckError::Web3(err)) => {
+                    tracing::warn!("Error accessing L1; will retry after a delay: {err}");
                     tokio::time::sleep(SLEEP_DELAY).await;
+                }
+                Err(CheckError::Internal(err)) => {
+                    let context =
+                        format!("Failed verifying consistency of L1 batch #{batch_number}");
+                    return Err(err.context(context));
                 }
             }
         }
