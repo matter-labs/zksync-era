17e9e5a6d5dce9778e2830afd1d625005c5d3c76
Javier Chatruc <jrchatruc@gmail.com>
2023-12-21 13:07:51 -0300

    More changes

--- infrastructure/zk/src/init.ts
+++ infrastructure/zk/src/init.ts
@@ -55,13 +55,13 @@ export async function init(initArgs: InitArgs = DEFAULT_ARGS) {
         // TODO: Deploy and set native ERC20 token.
         await announced('Setting up native L2 ERC20 token', run.deployERC20('new', 'lambdacoin', 'LBC', '18'));
     }
+    await utils.spawn('yarn l1-contracts build');
     await announced('Deploying L1 verifier', contract.deployVerifier([]));
     await announced('Reloading env', env.reload());
     await announced('Running server genesis setup', server.genesisFromSources());
     await announced('Deploying L1 contracts', contract.redeployL1(governorPrivateKeyArgs));
     await announced('Initializing validator', contract.initializeValidator(governorPrivateKeyArgs));
     await announced('Initialize L1 allow list', contract.initializeL1AllowList(governorPrivateKeyArgs));
-    // THE FIRST DEPOSIT IS HERE
     await announced(
         'Deploying L2 contracts',
         contract.deployL2(
