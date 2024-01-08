eedd2a08d4034c8e46da9fa7034e9b6280ccc088
Dustin Brickwood <dustinbrickwood204@gmail.com>
2024-01-08 12:18:01 -0600

    fix: handle exit codes properly

--- infrastructure/zk/src/spellcheck.ts
+++ infrastructure/zk/src/spellcheck.ts
@@ -1,28 +1,34 @@
 import { Command } from 'commander';
 import * as utils from './utils';
 
-export async function runSpellCheck(pattern: string, config: string, useCargo: boolean, useCSpell: boolean) {
+export async function runSpellCheck(pattern: string, useCargo: boolean, useCSpell: boolean) {
     const cSpellCommand = `cspell ${pattern} --config=./spellcheck/cspell.json`;
     const cargoCommand = `cargo spellcheck --cfg=./spellcheck/era.cfg`;
 
     try {
+        let results = [];
         if (!useCargo && !useCSpell) {
-            await Promise.all([utils.spawn(cSpellCommand), utils.spawn(cargoCommand)]);
+            results = await Promise.all([utils.spawn(cSpellCommand), utils.spawn(cargoCommand)]);
         } else {
             // Run cspell if specified
-            // zk spellcheck --use-cspell
             if (useCSpell) {
-                await utils.spawn(cSpellCommand);
+                results.push(await utils.spawn(cSpellCommand));
             }
 
             // Run cargo spellcheck if specified
-            // zk spellcheck --use-cargo
             if (useCargo) {
-                await utils.spawn(cargoCommand);
+                results.push(await utils.spawn(cargoCommand));
             }
+        }
+
+        // Check results and exit with error code if any command failed
+        if (results.some((code) => code !== 0)) {
+            console.error('Spell check failed');
+            process.exit(1);
         }
     } catch (error) {
         console.error('Error occurred during spell checking:', error);
+        process.exit(1);
     }
 }
 
@@ -33,5 +39,5 @@ export const command = new Command('spellcheck')
     .option('--use-cspell', 'Use cspell')
     .description('Run spell check on specified files')
     .action((cmd) => {
-        runSpellCheck(cmd.pattern, cmd.config, cmd.useCargo, cmd.useCSpell);
+        runSpellCheck(cmd.pattern, cmd.useCargo, cmd.useCSpell);
     });
