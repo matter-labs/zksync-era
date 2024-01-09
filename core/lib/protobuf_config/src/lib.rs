3b7a0e25e59a07e598b3f194fddb4e2d5258e44a
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-01-09 12:13:21 +0100

    more conv

--- core/lib/protobuf_config/src/lib.rs
+++ core/lib/protobuf_config/src/lib.rs
@@ -17,6 +17,10 @@ mod fri_proof_compressor;
 mod fri_prover;
 mod fri_prover_gateway;
 mod fri_prover_group;
+mod fri_witness_generator;
+mod fri_witness_vector_generator;
+mod house_keeper;
+mod object_store;
 
 pub mod proto;
 mod repr;
