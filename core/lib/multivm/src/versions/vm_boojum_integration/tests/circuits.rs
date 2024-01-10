67519d00c3258b1b04c61206a016c9371c591b57
perekopskiy <mikeson.dp@gmail.com>
2024-01-10 13:50:19 +0200

    Copy test

--- core/lib/multivm/src/versions/vm_boojum_integration/tests/circuits.rs
+++ core/lib/multivm/src/versions/vm_boojum_integration/tests/circuits.rs
@@ -1,13 +1,9 @@
+use zkevm_test_harness_1_4_0::geometry_config::get_geometry_config;
 use zksync_types::{Address, Execute, U256};
 
 use crate::{
     interface::{TxExecutionMode, VmExecutionMode, VmInterface},
-    vm_boojum_integration::{
-        constants::BLOCK_GAS_LIMIT,
-        tests::tester::VmTesterBuilder,
-        tracers::circuits_capacity::{GEOMETRY_CONFIG, OVERESTIMATE_PERCENT},
-        HistoryEnabled,
-    },
+    vm_boojum_integration::{constants::BLOCK_GAS_LIMIT, tests::tester::VmTesterBuilder, HistoryEnabled},
 };
 
 // Checks that estimated number of circuits for simple transfer doesn't differ much
@@ -33,47 +29,37 @@ fn test_circuits() {
         None,
     );
     vm.vm.push_transaction(tx);
-    let res = vm
-        .vm
-        .inspect_with_circuit_statistic(Default::default(), VmExecutionMode::OneTx);
+    let res = vm.vm.inspect(Default::default(), VmExecutionMode::OneTx);
 
-    const EXPECTED_CIRCUITS_USED: f32 = 4.8685;
-    let delta =
-        (res.statistics.estimated_circuits_used - EXPECTED_CIRCUITS_USED) / EXPECTED_CIRCUITS_USED;
+    let statistic = res.statistics.circuit_statistic;
+    // Check `circuit_statistic`.
+    assert!(statistic.main_vm > f32::EPSILON);
+    assert!(statistic.ram_permutation > f32::EPSILON);
+    assert!(statistic.storage_application > f32::EPSILON);
+    assert!(statistic.storage_sorter > f32::EPSILON);
+    assert!(statistic.code_decommitter > f32::EPSILON);
+    assert!(statistic.code_decommitter_sorter > f32::EPSILON);
+    assert!(statistic.log_demuxer > f32::EPSILON);
+    assert!(statistic.events_sorter > f32::EPSILON);
+    assert!(statistic.keccak256 > f32::EPSILON);
+    // Single ecrecover should be used to validate tx signature.
+    assert_eq!(
+        statistic.ecrecover,
+        1.0 / get_geometry_config().cycles_per_ecrecover_circuit as f32
+    );
+    // SHA256 shouldn't be used.
+    assert_eq!(statistic.sha256, 0.0);
+
+    const EXPECTED_CIRCUITS_USED: f32 = 4.6363;
+    let delta = (statistic.total_f32() - EXPECTED_CIRCUITS_USED) / EXPECTED_CIRCUITS_USED;
+    println!("{}", statistic.total_f32());
 
     if delta.abs() > 0.1 {
         panic!(
-            "Estimation differs from expected result by too much: {}%, expected value: {}",
+            "Estimation differs from expected result by too much: {}%, expected value: {}, got {}",
             delta * 100.0,
-            res.statistics.estimated_circuits_used
+            EXPECTED_CIRCUITS_USED,
+            statistic.total_f32(),
         );
     }
-
-    // Check consistency between `estimated_circuits_used` and `circuit_statistic`.
-    let statistic = res.statistics.circuit_statistic.unwrap();
-    let delta = (statistic.total(&GEOMETRY_CONFIG) * OVERESTIMATE_PERCENT
-        - res.statistics.estimated_circuits_used)
-        / res.statistics.estimated_circuits_used;
-    if delta > 0.01 {
-        panic!(
-            "`circuit_statistic` and `estimated_circuits_used` are inconsistent, difference {}%",
-            delta * 100.0,
-        );
-    }
-
-    // Check `circuit_statistic` further.
-    assert!(statistic.main_vm > 0);
-    assert!(statistic.ram_permutation > 0);
-    assert!(statistic.storage_application_by_reads > 0);
-    assert!(statistic.storage_application_by_writes > 0);
-    assert!(statistic.storage_sorter > 0);
-    assert!(statistic.code_decommitter > 0);
-    assert!(statistic.code_decommitter_sorter > 0);
-    assert!(statistic.log_demuxer > 0);
-    assert!(statistic.events_sorter > 0);
-    assert!(statistic.keccak256 > 0);
-    // Ecrecover should be used to validata tx signature.
-    assert_eq!(statistic.ecrecover, 1);
-    // SHA256 shouldn't be used.
-    assert_eq!(statistic.sha256, 0);
 }
