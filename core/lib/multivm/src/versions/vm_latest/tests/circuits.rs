51b420607189a974713919fa0de23b6b6d8ffc00
perekopskiy <mikeson.dp@gmail.com>
2024-01-26 13:04:52 +0200

    Check each circuit fraction in the test

--- core/lib/multivm/src/versions/vm_latest/tests/circuits.rs
+++ core/lib/multivm/src/versions/vm_latest/tests/circuits.rs
@@ -1,4 +1,3 @@
-use zkevm_test_harness_1_4_1::geometry_config::get_geometry_config;
 use zksync_types::{Address, Execute, U256};
 
 use crate::{
@@ -31,34 +30,40 @@ fn test_circuits() {
     vm.vm.push_transaction(tx);
     let res = vm.vm.inspect(Default::default(), VmExecutionMode::OneTx);
 
-    let statistic = res.statistics.circuit_statistic;
+    let s = res.statistics.circuit_statistic;
     // Check `circuit_statistic`.
-    assert!(statistic.main_vm > f32::EPSILON);
-    assert!(statistic.ram_permutation > f32::EPSILON);
-    assert!(statistic.storage_application > f32::EPSILON);
-    assert!(statistic.storage_sorter > f32::EPSILON);
-    assert!(statistic.code_decommitter > f32::EPSILON);
-    assert!(statistic.code_decommitter_sorter > f32::EPSILON);
-    assert!(statistic.log_demuxer > f32::EPSILON);
-    assert!(statistic.events_sorter > f32::EPSILON);
-    assert!(statistic.keccak256 > f32::EPSILON);
-    // Single `ecrecover` should be used to validate tx signature.
-    assert_eq!(
-        statistic.ecrecover,
-        1.0 / get_geometry_config().cycles_per_ecrecover_circuit as f32
-    );
-    // `SHA256` shouldn't be used.
-    assert_eq!(statistic.sha256, 0.0);
-
-    const EXPECTED_CIRCUITS_USED: f32 = 4.6363;
-    let delta = (statistic.total_f32() - EXPECTED_CIRCUITS_USED) / EXPECTED_CIRCUITS_USED;
-
-    if delta.abs() > 0.1 {
-        panic!(
-            "Estimation differs from expected result by too much: {}%, expected value: {}, got {}",
-            delta * 100.0,
-            EXPECTED_CIRCUITS_USED,
-            statistic.total_f32(),
-        );
+    const EXPECTED: [f32; 11] = [
+        1.1979, 0.1390, 1.5455, 0.0031, 1.0573, 0.00059, 0.00226, 0.00077, 0.1195, 0.1429, 0.0,
+    ];
+    let actual = [
+        (s.main_vm, "main_vm"),
+        (s.ram_permutation, "ram_permutation"),
+        (s.storage_application, "storage_application"),
+        (s.storage_sorter, "storage_sorter"),
+        (s.code_decommitter, "code_decommitter"),
+        (s.code_decommitter_sorter, "code_decommitter_sorter"),
+        (s.log_demuxer, "log_demuxer"),
+        (s.events_sorter, "events_sorter"),
+        (s.keccak256, "keccak256"),
+        (s.ecrecover, "ecrecover"),
+        (s.sha256, "sha256"),
+    ];
+    for ((actual, name), expected) in actual.iter().zip(EXPECTED) {
+        if expected == 0.0 {
+            assert_eq!(
+                *actual, expected,
+                "Check failed for {}, expected {}, actual {}",
+                name, expected, actual
+            );
+        } else {
+            let diff = (actual - expected) / expected;
+            assert!(
+                diff.abs() < 0.1,
+                "Check failed for {}, expected {}, actual {}",
+                name,
+                expected,
+                actual
+            );
+        }
     }
 }
