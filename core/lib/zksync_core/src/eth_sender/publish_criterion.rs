5abeb4510107cc540bad7989760b2f4a6903f1a2
ilitteri <ilitteri@fi.uba.ar>
2024-01-30 14:52:58 -0300

    Add l1_batch_committer as a L1BatchPublishCriterion::last_l1_batch_to_publish parameter (only used by DataSizeCriterion)

--- core/lib/zksync_core/src/eth_sender/publish_criterion.rs
+++ core/lib/zksync_core/src/eth_sender/publish_criterion.rs
@@ -1,10 +1,11 @@
-use std::fmt;
+use std::{fmt, sync::Arc};
 
 use async_trait::async_trait;
 use chrono::Utc;
 use zksync_dal::StorageProcessor;
 use zksync_types::{
-    aggregated_operations::AggregatedActionType, commitment::L1BatchWithMetadata, L1BatchNumber,
+    aggregated_operations::AggregatedActionType, commitment::L1BatchWithMetadata,
+    l1_batch_committer::L1BatchCommitter, L1BatchNumber,
 };
 
 use super::metrics::METRICS;
@@ -22,6 +23,7 @@ pub trait L1BatchPublishCriterion: fmt::Debug + Send + Sync {
         storage: &mut StorageProcessor<'_>,
         consecutive_l1_batches: &[L1BatchWithMetadata],
         last_sealed_l1_batch: L1BatchNumber,
+        _l1_batch_committer: Arc<dyn L1BatchCommitter>,
     ) -> Option<L1BatchNumber>;
 }
 
@@ -43,6 +45,7 @@ impl L1BatchPublishCriterion for NumberCriterion {
         _storage: &mut StorageProcessor<'_>,
         consecutive_l1_batches: &[L1BatchWithMetadata],
         _last_sealed_l1_batch: L1BatchNumber,
+        _l1_batch_committer: Arc<dyn L1BatchCommitter>,
     ) -> Option<L1BatchNumber> {
         let mut batch_numbers = consecutive_l1_batches
             .iter()
@@ -89,6 +92,7 @@ impl L1BatchPublishCriterion for TimestampDeadlineCriterion {
         _storage: &mut StorageProcessor<'_>,
         consecutive_l1_batches: &[L1BatchWithMetadata],
         last_sealed_l1_batch: L1BatchNumber,
+        _l1_batch_committer: Arc<dyn L1BatchCommitter>,
     ) -> Option<L1BatchNumber> {
         let first_l1_batch = consecutive_l1_batches.iter().next()?;
         let last_l1_batch_number = consecutive_l1_batches.iter().last()?.header.number.0;
@@ -153,6 +157,7 @@ impl L1BatchPublishCriterion for GasCriterion {
         storage: &mut StorageProcessor<'_>,
         consecutive_l1_batches: &[L1BatchWithMetadata],
         _last_sealed_l1_batch: L1BatchNumber,
+        _l1_batch_committer: Arc<dyn L1BatchCommitter>,
     ) -> Option<L1BatchNumber> {
         let base_cost = agg_l1_batch_base_cost(self.op);
         assert!(
@@ -210,18 +215,18 @@ impl L1BatchPublishCriterion for DataSizeCriterion {
         _storage: &mut StorageProcessor<'_>,
         consecutive_l1_batches: &[L1BatchWithMetadata],
         _last_sealed_l1_batch: L1BatchNumber,
+        l1_batch_committer: Arc<dyn L1BatchCommitter>,
     ) -> Option<L1BatchNumber> {
         const STORED_BLOCK_INFO_SIZE: usize = 96; // size of `StoredBlockInfo` solidity struct
         let mut data_size_left = self.data_limit - STORED_BLOCK_INFO_SIZE;
 
         for (index, l1_batch) in consecutive_l1_batches.iter().enumerate() {
-            if data_size_left < l1_batch.l1_commit_data_size() {
+            let l1_commit_data_size = l1_batch_committer.l1_commit_data_size(l1_batch);
+            if data_size_left < l1_commit_data_size {
                 if index == 0 {
                     panic!(
                         "L1 batch #{} requires {} data, which is more than the range limit of {}",
-                        l1_batch.header.number,
-                        l1_batch.l1_commit_data_size(),
-                        self.data_limit
+                        l1_batch.header.number, l1_commit_data_size, self.data_limit
                     );
                 }
 
@@ -236,7 +241,7 @@ impl L1BatchPublishCriterion for DataSizeCriterion {
                 METRICS.block_aggregation_reason[&(self.op, "data_size").into()].inc();
                 return Some(output);
             }
-            data_size_left -= l1_batch.l1_commit_data_size();
+            data_size_left -= l1_commit_data_size;
         }
 
         None
