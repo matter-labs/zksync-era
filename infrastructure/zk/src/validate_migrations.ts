a57d1ad6a24c3a7c7ee1e26528f9889be381639f
tomg10 <lemures64@gmail.com>
2024-02-02 14:05:04 +0100

    zk fmt
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- infrastructure/zk/src/validate_migrations.ts
+++ infrastructure/zk/src/validate_migrations.ts
@@ -1,9 +1,9 @@
-import * as utils from "./utils";
-import fs from "fs";
+import * as utils from './utils';
+import fs from 'fs';
 
 async function validateMigration(filePath: string) {
-    const content = await fs.promises.readFile(filePath, {encoding: 'utf-8'});
-    const isBackwardsCompatible = !content.includes("RENAME") && !content.includes("DROP");
+    const content = await fs.promises.readFile(filePath, { encoding: 'utf-8' });
+    const isBackwardsCompatible = !content.includes('RENAME') && !content.includes('DROP');
     if (!isBackwardsCompatible) {
         console.warn(`Found not backwards-compatible migration in ${filePath}!`);
     }
@@ -11,18 +11,25 @@ async function validateMigration(filePath: string) {
 }
 
 async function getWhitelistedMigrations() {
-    const whitelistFilepath = "not-backwards-compatible-migrations-whitelist.txt"
-    const whitelistedFilesRaw = await fs.promises.readFile(whitelistFilepath, {encoding: 'utf-8'});
+    const whitelistFilepath = 'not-backwards-compatible-migrations-whitelist.txt';
+    const whitelistedFilesRaw = await fs.promises.readFile(whitelistFilepath, { encoding: 'utf-8' });
     //filtering out comments
-    return whitelistedFilesRaw.split("\n").filter(x => !x.trim().startsWith("//"));
+    return whitelistedFilesRaw.split('\n').filter((x) => !x.trim().startsWith('//'));
 }
 
 export async function validateMigrations() {
     const whitelistedMigrations = await getWhitelistedMigrations();
-    let {stdout: filesRaw} = await utils.exec('find core/lib/dal -type f -name "*up.sql"');
-    const files = filesRaw.trim().split('\n').sort().filter(filepath => !whitelistedMigrations.includes(filepath));
+    let { stdout: filesRaw } = await utils.exec('find core/lib/dal -type f -name "*up.sql"');
+    const files = filesRaw
+        .trim()
+        .split('\n')
+        .sort()
+        .filter((filepath) => !whitelistedMigrations.includes(filepath));
     const validateResults = await Promise.all(files.map((file) => validateMigration(file)));
     if (validateResults.includes(false)) {
-        throw new Error('A new migration that is not backwards-compatible was detected! As such migrations can easily cause ');
+        throw new Error(
+            'A new migration that is not backwards-compatible was detected! As such migrations can easily cause an outage,' +
+                'they have to be explicitly added to a whitelist: not-backwards-compatible-migrations-whitelist.txt'
+        );
     }
 }
