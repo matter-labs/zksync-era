aefba8a0e2fcf09bad31d9567f3bb371f92342e3
perekopskiy <mikeson.dp@gmail.com>
2024-02-07 12:38:02 +0200

    Remove migration

--- core/lib/dal/migrations/20240205114501_drop-l1_batches-unneeded-columns.down.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE blocks ADD COLUMN l2_l1_compressed_messages BYTEA;
-ALTER TABLE blocks ADD COLUMN compressed_write_logs BYTEA;
-ALTER TABLE blocks ADD COLUMN compressed_contracts BYTEA;
-ALTER TABLE blocks ADD COLUMN parent_hash BYTEA;
--- core/lib/dal/migrations/20240205114501_drop-l1_batches-unneeded-columns.up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE l1_batches DROP COLUMN l2_l1_compressed_messages;
-ALTER TABLE l1_batches DROP COLUMN compressed_write_logs;
-ALTER TABLE l1_batches DROP COLUMN compressed_contracts;
-ALTER TABLE l1_batches DROP COLUMN parent_hash;
