4f9a4281829c64a3fcfecfe20617b464c494f6bf
Igor Aleksanov <popzxc@yandex.ru>
2024-02-14 10:43:16 +0400

    Rework main node example

--- core/node/node_framework/examples/main_node.rs
+++ core/node/node_framework/examples/main_node.rs
@@ -21,6 +21,70 @@ use zksync_node_framework::{
     service::ZkStackService,
 };
 
+struct MainNodeBuilder {
+    node: ZkStackService,
+}
+
+impl MainNodeBuilder {
+    fn new() -> Self {
+        Self {
+            node: ZkStackService::new().expect("Failed to initialize the node"),
+        }
+    }
+
+    fn add_pools_layer(mut self) -> anyhow::Result<Self> {
+        let config = PostgresConfig::from_env()?;
+        let pools_layer = PoolsLayerBuilder::empty(config)
+            .with_master(true)
+            .with_replica(true)
+            .with_prover(true)
+            .build();
+        self.node.add_layer(pools_layer);
+        Ok(self)
+    }
+
+    fn add_metadata_calculator_layer(mut self) -> anyhow::Result<Self> {
+        let merkle_tree_env_config = DBConfig::from_env()?.merkle_tree;
+        let operations_manager_env_config = OperationsManagerConfig::from_env()?;
+        let metadata_calculator_config = MetadataCalculatorConfig::for_main_node(
+            &merkle_tree_env_config,
+            &operations_manager_env_config,
+        );
+        self.node
+            .add_layer(MetadataCalculatorLayer(metadata_calculator_config));
+        Ok(self)
+    }
+
+    fn add_state_keeper_layer(mut self) -> anyhow::Result<Self> {
+        let mempool_io_layer = MempoolIOLayer::new(
+            NetworkConfig::from_env()?,
+            ContractsConfig::from_env()?,
+            StateKeeperConfig::from_env()?,
+            MempoolConfig::from_env()?,
+        );
+        let main_node_batch_executor_builder_layer = MainNodeBatchExecutorBuilderLayer::new(
+            DBConfig::from_env()?,
+            StateKeeperConfig::from_env()?,
+        );
+        let state_keeper_layer = StateKeeperLayer;
+        self.node
+            .add_layer(mempool_io_layer)
+            .add_layer(main_node_batch_executor_builder_layer)
+            .add_layer(state_keeper_layer);
+        Ok(self)
+    }
+
+    fn add_healthcheck_layer(mut self) -> anyhow::Result<Self> {
+        let healthcheck_config = zksync_config::ApiConfig::from_env()?.healthcheck;
+        self.node.add_layer(HealthCheckLayer(healthcheck_config));
+        Ok(self)
+    }
+
+    fn build(self) -> ZkStackService {
+        self.node
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     #[allow(deprecated)] // TODO (QIT-21): Use centralized configuration approach.
     let log_format = vlog::log_format_from_env();
@@ -28,51 +92,13 @@ fn main() -> anyhow::Result<()> {
         .with_log_format(log_format)
         .build();
 
-    // Create the node with specified resource provider. We don't need to add any resources explicitly,
-    // the task will request what they actually need. The benefit here is that we won't instantiate resources
-    // that are not used, which would be complex otherwise, since the task set is often dynamic.
-    let mut node = ZkStackService::new()?;
-
-    // Add pools.
-    let postgres_config = PostgresConfig::from_env()?;
-    let pools_layer = PoolsLayerBuilder::empty(postgres_config)
-        .with_master(true)
-        .with_replica(true)
-        .with_prover(true)
-        .build();
-    node.add_layer(pools_layer);
-
-    // Add the metadata calculator task.
-    let merkle_tree_env_config = DBConfig::from_env()?.merkle_tree;
-    let operations_manager_env_config = OperationsManagerConfig::from_env()?;
-    let metadata_calculator_config = MetadataCalculatorConfig::for_main_node(
-        &merkle_tree_env_config,
-        &operations_manager_env_config,
-    );
-    node.add_layer(MetadataCalculatorLayer(metadata_calculator_config));
-
-    // Add the state keeper
-    let mempool_io_layer = MempoolIOLayer::new(
-        NetworkConfig::from_env()?,
-        ContractsConfig::from_env()?,
-        StateKeeperConfig::from_env()?,
-        MempoolConfig::from_env()?,
-    );
-    let main_node_batch_executor_builder_layer = MainNodeBatchExecutorBuilderLayer::new(
-        DBConfig::from_env()?,
-        StateKeeperConfig::from_env()?,
-    );
-    let state_keeper_layer = StateKeeperLayer;
-    node.add_layer(mempool_io_layer)
-        .add_layer(main_node_batch_executor_builder_layer)
-        .add_layer(state_keeper_layer);
-
-    // Add the healthcheck server.
-    let healthcheck_config = zksync_config::ApiConfig::from_env()?.healthcheck;
-    node.add_layer(HealthCheckLayer(healthcheck_config));
-
-    // Run the node until completion.
-    node.run()?;
+    MainNodeBuilder::new()
+        .add_pools_layer()?
+        .add_metadata_calculator_layer()?
+        .add_state_keeper_layer()?
+        .add_healthcheck_layer()?
+        .build()
+        .run()?;
 
     Ok(())
 }
