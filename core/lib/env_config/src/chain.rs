2f02dca3082b9b34d06f7a035f25b3c4114aba20
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-02-22 16:25:02 +0100

    Add unit test

--- core/lib/env_config/src/chain.rs
+++ core/lib/env_config/src/chain.rs
@@ -205,4 +205,12 @@ mod tests {
         let actual = CircuitBreakerConfig::from_env().unwrap();
         assert_eq!(actual, expected_circuit_breaker_config());
     }
+
+    #[test]
+    fn deafult_state_keeper_mode() {
+        assert_eq!(
+            StateKeeperConfig::default().l1_batch_commit_data_generator_mode,
+            L1BatchCommitDataGeneratorMode::Rollup
+        );
+    }
 }
