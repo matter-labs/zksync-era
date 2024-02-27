c99c61e94cc48390b1c4c8edcd3ea084f518ac84
Jordi <jordi.bonet.valiente@lambdaclass.com>
2024-02-27 11:28:31 +0100

    Stash

--- core/lib/protobuf_config/src/tests.rs
+++ core/lib/protobuf_config/src/tests.rs
@@ -1,8 +1,9 @@
 use pretty_assertions::assert_eq;
 use rand::Rng;
+use testonly::{Gen, RandomConfig};
 use zksync_config::testonly;
 
-use crate::{proto, repr::ProtoRepr};
+use crate::{proto, proto::FeeModelVersion::V1, repr::ProtoRepr};
 
 fn encode<P: ProtoRepr>(msg: &P::Type) -> Vec<u8> {
     let msg = P::build(msg);
@@ -89,4 +90,83 @@ fn test_encoding() {
     encode_decode::<proto::ProofDataHandler>(rng);
     encode_decode::<proto::SnapshotsCreator>(rng);
     encode_decode::<proto::WitnessGenerator>(rng);
+}
+
+#[test]
+fn test_encoding_state_keeper_mode() {
+    // Test Rollup mode
+    let mode_rollup = proto::L1BatchCommitDataGeneratorMode::Rollup;
+    encode_decode_mode::<proto::StateKeeper>(&mode_rollup);
+
+    // Test Validium mode
+    let mode_validium = proto::L1BatchCommitDataGeneratorMode::Validium;
+    encode_decode_mode::<proto::StateKeeper>(&mode_validium);
+}
+
+#[track_caller]
+fn encode_decode_mode<P: ProtoRepr>(mode: &proto::L1BatchCommitDataGeneratorMode)
+where
+    P::Type: PartialEq + std::fmt::Debug + testonly::RandomConfig,
+{
+    for required_only in [false, true] {
+        // Manually set the L1BatchCommitDataGeneratorMode
+        let mut state_keeper = generate_mode(mode);
+        dbg!(state_keeper);
+
+        let rng = &mut rand::thread_rng();
+        let want: P::Type = testonly::Gen {
+            rng,
+            required_only,
+            decimal_fractions: false,
+        }
+        .gen();
+        dbg!(want);
+        assert_eq!(1, 2);
+        //let got = decode::<P>(&encode::<P>(&want)).unwrap();
+
+        //     let got = decode::<P>(&encode::<P>(&state_keeper)).unwrap();
+        //     assert_eq!(&state_keeper, &got, "binary encoding");
+
+        //     let got = decode_json::<P>(&encode_json::<P>(&state_keeper)).unwrap();
+        //     assert_eq!(&state_keeper, &got, "json encoding");
+        //
+    }
+}
+
+fn generate_mode(mode: &proto::L1BatchCommitDataGeneratorMode) -> proto::StateKeeper {
+    proto::StateKeeper {
+        transaction_slots: Some(10926497003267881492),
+        block_commit_deadline_ms: Some(10947007445086036797),
+        miniblock_commit_deadline_ms: Some(9103314936350460727),
+        miniblock_seal_queue_capacity: Some(6906530626745786909),
+        max_single_tx_gas: Some(2785542050),
+        max_allowed_l2_tx_gas_limit: Some(1183074970),
+        reject_tx_at_geometry_percentage: Some(0.5296344133371791),
+        reject_tx_at_eth_params_percentage: Some(0.8773607433255183),
+        reject_tx_at_gas_percentage: Some(0.5289453936748325),
+        close_block_at_geometry_percentage: Some(0.6479326814884391),
+        close_block_at_eth_params_percentage: Some(0.21958980771195413),
+        close_block_at_gas_percentage: Some(0.5229693273318018),
+        fee_account_addr: Some(
+            [
+                153, 251, 1, 240, 243, 248, 139, 100, 180, 119, 34, 199, 146, 251, 165, 177, 250,
+                52, 209, 52,
+            ]
+            .into(),
+        ),
+        minimal_l2_gas_price: Some(2703211627494097776),
+        compute_overhead_part: Some(0.6857318990462495),
+        pubdata_overhead_part: Some(0.12953109152591813),
+        batch_overhead_l1_gas: Some(15518551973209220528),
+        max_gas_per_batch: Some(15859976163468884008),
+        max_pubdata_per_batch: Some(11012258501587398219),
+        fee_model_version: Some(V1.into()),
+        validation_computational_gas_limit: Some(2619723581),
+        save_call_traces: Some(true),
+        virtual_blocks_interval: Some(1113108050),
+        virtual_blocks_per_miniblock: Some(756446353),
+        upload_witness_inputs_to_gcs: Some(true),
+        enum_index_migration_chunk_size: Some(3905631822449257186),
+        l1_batch_commit_data_generator_mode: Some(mode.clone().into()),
+    }
 }
