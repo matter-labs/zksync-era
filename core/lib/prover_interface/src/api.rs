ca3178720c34aad90616e880d896e7623ccd37e3
Emil <evl@matterlabs.dev>
2024-02-27 06:12:09 +0000

    Enforce blob constraints through type system + invariants

--- core/lib/prover_interface/src/api.rs
+++ core/lib/prover_interface/src/api.rs
@@ -11,6 +11,7 @@ use crate::{inputs::PrepareBasicCircuitsJob, outputs::L1BatchProofForL1};
 
 const BLOB_CHUNK_SIZE: usize = 31;
 const ELEMENTS_PER_4844_BLOCK: usize = 4096;
+const MAX_4844_BLOBS_PER_BLOCK: usize = 2;
 
 pub const EIP_4844_BLOB_SIZE: usize = BLOB_CHUNK_SIZE * ELEMENTS_PER_4844_BLOCK;
 
@@ -18,13 +19,47 @@ pub type Blob = Vec<u8>;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Eip4844Blobs {
-    pub blobs: Vec<Blob>,
+    blobs: Vec<Blob>,
+}
+
+impl Eip4844Blobs {
+    pub fn blobs(self) -> Vec<Blob> {
+        self.blobs
+    }
+
+    fn get_rounded_blob_len(blobs: Vec<u8>) -> usize {
+        let rounded_down_blob_len = blobs.len() / EIP_4844_BLOB_SIZE * EIP_4844_BLOB_SIZE;
+
+        // if there's remainder, we will need to round it up
+        if blobs.len() != rounded_down_blob_len {
+            return rounded_down_blob_len + EIP_4844_BLOB_SIZE;
+        }
+        rounded_down_blob_len
+    }
+
+    fn check_blob_constraints(blobs_len: usize, rounded_blob_len: usize) {
+        assert!(rounded_blob_len % EIP_4844_BLOB_SIZE == 0);
+        if blobs_len == 0 {
+            panic!("cannot create EIP4844Blobs, received empty pubdata");
+        }
+
+        let blobs_received = rounded_blob_len / EIP_4844_BLOB_SIZE;
+
+        if blobs_received > MAX_4844_BLOBS_PER_BLOCK {
+            panic!(
+                "EIP4844 supports only {:?} blobs, received {:?} (from {:?} bytes of data)",
+                MAX_4844_BLOBS_PER_BLOCK, blobs_received, blobs_len
+            );
+        }
+    }
 }
 
 impl From<Vec<u8>> for Eip4844Blobs {
     fn from(mut blobs: Vec<u8>) -> Self {
-        let blob_size = blobs.len() / EIP_4844_BLOB_SIZE * EIP_4844_BLOB_SIZE;
-        blobs.resize(blob_size, 0u8);
+        let rounded_blob_len = Self::get_rounded_blob_len(blobs);
+        Self::check_blob_constraints(blobs.len(), rounded_blob_len);
+
+        blobs.resize(rounded_blob_len, 0u8);
         Self {
             blobs: blobs
                 .chunks(EIP_4844_BLOB_SIZE)
