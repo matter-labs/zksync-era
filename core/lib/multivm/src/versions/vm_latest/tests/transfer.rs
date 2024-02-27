cddf9654cf0b4675b4c6db6f957af22d13bacc7e
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-02-27 22:44:50 +0100

    send/transfer tests pass

--- core/lib/multivm/src/versions/vm_latest/tests/transfer.rs
+++ core/lib/multivm/src/versions/vm_latest/tests/transfer.rs
@@ -97,10 +97,7 @@ fn test_send_or_transfer(test_option: TestOptions) {
     );
 
     let batch_result = vm.vm.execute(VmExecutionMode::Batch);
-    assert!(
-        !batch_result.result.is_failed(),
-        "Transaction wasn't successful"
-    );
+    assert!(!batch_result.result.is_failed(), "Batch wasn't successful");
 
     let new_recipient_balance = get_balance(
         AccountTreeId::new(L2_ETH_TOKEN_ADDRESS),
@@ -117,4 +114,115 @@ fn test_send_and_transfer() {
     test_send_or_transfer(TestOptions::Send(U256::from(10).pow(18.into())));
     test_send_or_transfer(TestOptions::Transfer(U256::zero()));
     test_send_or_transfer(TestOptions::Transfer(U256::from(10).pow(18.into())));
+}
+
+fn test_reentrancy_protection_send_or_transfer(test_option: TestOptions) {
+    let test_bytecode = read_bytecode(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/TransferTest.json",
+    );
+    let reentrant_recipeint_bytecode = read_bytecode(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/ReentrantRecipient.json",
+    );
+    let test_abi = load_contract(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/TransferTest.json",
+    );
+    let reentrant_recipient_abi = load_contract(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/ReentrantRecipient.json",
+    );
+
+    let test_contract_address = Address::random();
+    let reentrant_recipeint_address = Address::random();
+
+    let (value, calldata) = match test_option {
+        TestOptions::Send(value) => (
+            value,
+            test_abi
+                .function("send")
+                .unwrap()
+                .encode_input(&[
+                    Token::Address(reentrant_recipeint_address),
+                    Token::Uint(value),
+                ])
+                .unwrap(),
+        ),
+        TestOptions::Transfer(value) => (
+            value,
+            test_abi
+                .function("transfer")
+                .unwrap()
+                .encode_input(&[
+                    Token::Address(reentrant_recipeint_address),
+                    Token::Uint(value),
+                ])
+                .unwrap(),
+        ),
+    };
+
+    let mut vm = VmTesterBuilder::new(HistoryEnabled)
+        .with_empty_in_memory_storage()
+        .with_execution_mode(TxExecutionMode::VerifyExecute)
+        .with_deployer()
+        .with_random_rich_accounts(1)
+        .with_custom_contracts(vec![
+            (test_bytecode, test_contract_address, false),
+            (
+                reentrant_recipeint_bytecode,
+                reentrant_recipeint_address,
+                false,
+            ),
+        ])
+        .build();
+
+    // First transaction, the job of which is to warm up the slots for balance of the recipient as well as its storage variable.
+    let account = &mut vm.rich_accounts[0];
+    let tx1 = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: reentrant_recipeint_address,
+            calldata: reentrant_recipient_abi
+                .function("setX")
+                .unwrap()
+                .encode_input(&[])
+                .unwrap(),
+            value: U256::from(1),
+            factory_deps: None,
+        },
+        None,
+    );
+
+    vm.vm.push_transaction(tx1);
+    let tx1_result = vm.vm.execute(VmExecutionMode::OneTx);
+    assert!(
+        !tx1_result.result.is_failed(),
+        "Transaction 1 wasn't successful"
+    );
+
+    let tx2 = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: test_contract_address,
+            calldata,
+            value,
+            factory_deps: None,
+        },
+        None,
+    );
+
+    vm.vm.push_transaction(tx2);
+    let tx2_result = vm.vm.execute(VmExecutionMode::OneTx);
+    assert!(
+        tx2_result.result.is_failed(),
+        "Transaction 2 should have failed, but it succeeded"
+    );
+
+    let batch_result = vm.vm.execute(VmExecutionMode::Batch);
+    assert!(!batch_result.result.is_failed(), "Batch wasn't successful");
+}
+
+#[test]
+fn test_reentrancy_protection_send_and_transfer() {
+    test_reentrancy_protection_send_or_transfer(TestOptions::Send(U256::zero()));
+    test_reentrancy_protection_send_or_transfer(TestOptions::Send(U256::from(10).pow(18.into())));
+    test_reentrancy_protection_send_or_transfer(TestOptions::Transfer(U256::zero()));
+    test_reentrancy_protection_send_or_transfer(TestOptions::Transfer(
+        U256::from(10).pow(18.into()),
+    ));
 }
--- etc/contracts-test-data/contracts/transfer/transfer.sol
+++ etc/contracts-test-data/contracts/transfer/transfer.sol
@@ -3,13 +3,18 @@
 pragma solidity ^0.8.0;
 
 contract TransferTest {
-    function transfer(address payable to, uint256 amount) public {
+    function transfer(address payable to, uint256 amount) public payable {
         to.transfer(amount);
     }
 
-    function send(address payable to, uint256 amount) public {
+    function send(address payable to, uint256 amount) public payable {
         bool success = to.send(amount);
-        require(success, "Transfer failed");
+
+        require(success, "Transaction failed");
+    }
+    
+    receive() external payable {
+
     }
 }
 
@@ -17,6 +22,29 @@ contract Recipient {
     event Received(address indexed sender, uint256 amount);
 
     receive() external payable {
+        require(gasleft() >= 2100, "Not enough gas");
+        require(gasleft() <= 2300, "Too much gas");
         emit Received(msg.sender, msg.value);
     }
 }
+
+contract ReentrantRecipient {
+    uint256 x;
+
+    event Received(uint256 tmp, uint256 amount);
+
+    function setX() external payable {
+        x = 1;
+    }
+
+    receive() external payable {
+        require(gasleft() >= 15000);
+        x = 1;
+    }
+}
+
+// contract GasTestingRecipient {
+//     receive() external payable {
+        
+//     }
+// }
