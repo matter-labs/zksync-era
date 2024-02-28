913cb4e144f03936a32184d60399d0f5fd96c6f4
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-02-28 13:56:08 +0100

    use remote compiler

--- etc/contracts-test-data/hardhat.config.ts
+++ etc/contracts-test-data/hardhat.config.ts
@@ -5,7 +5,8 @@ export default {
         // version: 'prerelease-0640c18-test-zkvm-v1.5.0',
         compilerSource: 'binary',
         settings: {
-            compilerPath: 'https://github.com/matter-labs/era-compiler-solidity/releases/download/prerelease-e880101-test-features/zksolc-macosx-arm64',
+            compilerPath:
+                'https://github.com/matter-labs/era-compiler-solidity/releases/download/prerelease-e880101-test-features/zksolc-macosx-arm64',
             isSystem: true
         }
     },
