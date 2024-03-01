8f98ea9a07d01086cca1e21cd5e78ca357b90846
Joonatan Saarhelo <joon.saar@gmail.com>
2024-03-01 19:36:04 +0100

    make CI report number of opcodes run changes

--- core/tests/vm-benchmark/src/compare_iai_results.rs
+++ core/tests/vm-benchmark/src/compare_iai_results.rs
@@ -1,32 +1,50 @@
-use std::{collections::HashMap, fs::File, io::BufReader};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 use vm_benchmark::parse_iai::parse_iai;
 
 fn main() {
-    let args: [String; 2] = std::env::args()
+    let [iai_before, iai_after, opcodes_before, opcodes_after] = std::env::args()
         .skip(1)
         .take(2)
         .collect::<Vec<_>>()
         .try_into()
-        .expect("expected two arguments");
+        .expect("expected four arguments");
 
-    let before = get_name_to_cycles(&args[0]);
-    let after = get_name_to_cycles(&args[1]);
+    let iai_before = get_name_to_cycles(&iai_before);
+    let iai_after = get_name_to_cycles(&iai_after);
+    let opcodes_before = get_name_to_opcodes(&opcodes_before);
+    let opcodes_after = get_name_to_opcodes(&opcodes_after);
 
-    let mut header_written = false;
+    let mut nonzero_diff = false;
 
-    for (name, cycles) in before {
-        if let Some(&cycles2) = after.get(&name) {
-            let change = ((cycles2 as f64) - (cycles as f64)) / (cycles as f64);
-            if change.abs() > 0.02 {
-                if !header_written {
-                    println!("Benchmark name | Difference in runtime\n--- | ---");
-                    header_written = true;
+    for (name, cycles) in iai_before {
+        if let Some(&cycles2) = iai_after.get(&name) {
+            let cycles_diff = ((cycles2 as f64) - (cycles as f64)) / (cycles as f64);
+            let opcodes_diff = opcodes_after.get(&name).cloned().unwrap_or_default()
+                - opcodes_before.get(&name).cloned().unwrap_or_default();
+            if cycles_diff.abs() > 0.02 {
+                // write the header before writing the first line of diff
+                if !nonzero_diff {
+                    println!("Benchmark name | change in estimated runtime | change in number of opcodes executed \n--- | --- | ---");
+                    nonzero_diff = true;
                 }
 
-                println!("{} | {:+.1}%", name, change * 100.0);
+                println!(
+                    "{} | {:+.1}% | {:+}",
+                    name,
+                    cycles_diff * 100.0,
+                    opcodes_diff
+                );
             }
         }
+    }
+
+    if nonzero_diff {
+        println!("\n Changes in number of opcodes executed indicate that the gas price of the benchmark has changed, which causes it run out of gas at a different time. Or that it is behaving completely differently.");
     }
 }
 
@@ -36,4 +54,18 @@ fn get_name_to_cycles(filename: &str) -> HashMap<String, u64> {
     ))
     .map(|x| (x.name, x.cycles))
     .collect()
+}
+
+fn get_name_to_opcodes(filename: &str) -> HashMap<String, u64> {
+    BufReader::new(File::open(filename).expect("failed to open file"))
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let mut it = line.split_whitespace();
+            (
+                it.next().unwrap().to_string(),
+                it.next().unwrap().parse().unwrap(),
+            )
+        })
+        .collect()
 }
