414e2f87c23caf8534ea7e4dddbeb441352c8ca3
Fedor Sakharov <fedor.sakharov@gmail.com>
2024-03-04 17:47:17 +0100

    fix a test

--- core/lib/env_config/src/eth_sender.rs
+++ core/lib/env_config/src/eth_sender.rs
@@ -26,9 +26,7 @@ impl FromEnv for GasAdjusterConfig {
 
 #[cfg(test)]
 mod tests {
-    use zksync_config::configs::eth_sender::{
-        ProofLoadingMode, ProofSendingMode, PubdataSendingMode,
-    };
+    use zksync_config::configs::eth_sender::{ProofLoadingMode, ProofSendingMode};
 
     use super::*;
     use crate::test_utils::{hash, EnvMutex};
@@ -56,7 +54,7 @@ mod tests {
                 l1_batch_min_age_before_execute_seconds: Some(1000),
                 max_acceptable_priority_fee_in_gwei: 100_000_000_000,
                 proof_loading_mode: ProofLoadingMode::OldProofFromDb,
-                pubdata_sending_mode: Some(PubdataSendingMode::Calldata),
+                pubdata_sending_mode: None,
             },
             gas_adjuster: GasAdjusterConfig {
                 default_priority_fee_per_gas: 20000000000,
