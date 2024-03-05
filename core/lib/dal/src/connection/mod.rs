b3b5229d26fcf27ae32e40ce97216a8502ecdb93
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-03-05 22:51:21 +0100

    increased retries in TestTemplate::connect_to

--- core/lib/dal/src/connection/mod.rs
+++ core/lib/dal/src/connection/mod.rs
@@ -117,7 +117,7 @@ impl TestTemplate {
 
     async fn connect_to(db_url: &url::Url) -> sqlx::Result<sqlx::PgConnection> {
         use sqlx::Connection as _;
-        let mut attempts = 10;
+        let mut attempts = 20;
         loop {
             match sqlx::PgConnection::connect(db_url.as_ref()).await {
                 Ok(conn) => return Ok(conn),
@@ -128,7 +128,7 @@ impl TestTemplate {
                     }
                 }
             }
-            tokio::time::sleep(Duration::from_millis(100)).await;
+            tokio::time::sleep(Duration::from_millis(200)).await;
         }
     }
 
