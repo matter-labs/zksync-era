7d5fb666802a6e7a78193c74f718e9124acef9e3
Alex Ostrovski <aov@matterlabs.dev>
2024-03-13 11:13:04 +0200

    Test `ManagedTasks`

--- core/lib/utils/src/wait_for_tasks.rs
+++ core/lib/utils/src/wait_for_tasks.rs
@@ -5,7 +5,12 @@ use tokio::task::JoinHandle;
 
 use crate::panic_extractor::try_extract_panic_message;
 
-#[must_use = "remaining tasks should be completed"]
+/// Container for fallible Tokio tasks with ability to track their shutdown.
+///
+/// The intended usage is [first waiting for a single task](Self::wait_single()) (perhaps in a `select!`
+/// with other futures, e.g. a termination signal listener), and then [completing](Self::complete())
+/// the remaining tasks.
+#[must_use = "Tasks should be `complete()`d"]
 #[derive(Debug)]
 pub struct ManagedTasks {
     task_handles: Vec<JoinHandle<anyhow::Result<()>>>,
@@ -13,6 +18,7 @@ pub struct ManagedTasks {
 }
 
 impl ManagedTasks {
+    /// Wraps the specified list of Tokio tasks.
     pub fn new(task_handles: Vec<JoinHandle<anyhow::Result<()>>>) -> Self {
         Self {
             task_handles,
@@ -20,6 +26,8 @@ impl ManagedTasks {
         }
     }
 
+    /// Specifies that the wrapped tasks can finish (by default, they are expected to run indefinitely).
+    /// This influences logging when a task finishes.
     pub fn allow_tasks_to_finish(mut self) -> Self {
         self.tasks_allowed_to_finish = true;
         self
@@ -79,4 +87,82 @@ impl ManagedTasks {
     }
 }
 
-// FIXME: test
+#[cfg(test)]
+mod tests {
+    use std::sync::{
+        atomic::{AtomicBool, AtomicUsize, Ordering},
+        Arc,
+    };
+
+    use tokio::sync::watch;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn managing_tasks_with_normal_tasks() {
+        let (shutdown_sender, shutdown_receiver) = watch::channel(false);
+        let counter = Arc::new(AtomicUsize::new(0));
+        let tasks = (0..5).map(|_| {
+            let mut shutdown_receiver = shutdown_receiver.clone();
+            let counter = counter.clone();
+            tokio::spawn(async move {
+                shutdown_receiver.changed().await.unwrap();
+                counter.fetch_add(1, Ordering::Relaxed);
+                Ok(())
+            })
+        });
+        let mut tasks = ManagedTasks::new(tasks.collect());
+        tokio::select! {
+            () = tasks.wait_single() => {
+                panic!("Tasks should not finish in this test");
+            }
+            () = tokio::time::sleep(Duration::from_millis(50)) => {
+                // Emulate shutdown after a delay.
+            }
+        }
+        shutdown_sender.send_replace(true);
+        tasks.complete(Duration::from_secs(1)).await;
+        assert_eq!(counter.load(Ordering::Relaxed), 5);
+    }
+
+    #[derive(Debug)]
+    enum TaskTermination {
+        Ok,
+        Error,
+        Panic,
+    }
+
+    impl TaskTermination {
+        const ALL: [Self; 3] = [Self::Ok, Self::Error, Self::Panic];
+    }
+
+    #[tokio::test]
+    async fn managing_tasks_with_terminating_task() {
+        for termination in TaskTermination::ALL {
+            println!("Testing termination outcome {termination:?}");
+
+            let is_finished = Arc::new(AtomicBool::new(false));
+            let (shutdown_sender, mut shutdown_receiver) = watch::channel(false);
+            let is_finished_for_task = is_finished.clone();
+            let mut tasks = ManagedTasks::new(vec![
+                tokio::spawn(async move {
+                    tokio::task::yield_now().await;
+                    match termination {
+                        TaskTermination::Ok => Ok(()),
+                        TaskTermination::Error => Err(anyhow::anyhow!("error")),
+                        TaskTermination::Panic => panic!("oops"),
+                    }
+                }),
+                tokio::spawn(async move {
+                    shutdown_receiver.changed().await.unwrap();
+                    is_finished_for_task.store(true, Ordering::Relaxed);
+                    Ok(())
+                }),
+            ]);
+            tasks.wait_single().await;
+            shutdown_sender.send_replace(true);
+            tasks.complete(Duration::from_secs(1)).await;
+            assert!(is_finished.load(Ordering::Relaxed));
+        }
+    }
+}
