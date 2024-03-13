9d8b17d293b5f236b5320a04b98dc0651c88d2f4
Lech <88630083+Artemka374@users.noreply.github.com>
2024-03-13 13:26:45 +0200

    fix dal test

--- core/lib/dal/src/events_web3_dal.rs
+++ core/lib/dal/src/events_web3_dal.rs
@@ -259,11 +259,11 @@ mod tests {
     use zksync_types::{Address, H256};
 
     use super::*;
-    use crate::connection::ConnectionPool;
+    use crate::{ConnectionPool, Server};
 
     #[tokio::test]
     async fn test_build_get_logs_where_clause() {
-        let connection_pool = ConnectionPool::test_pool().await;
+        let connection_pool = ConnectionPool::<Server>::test_pool().await;
         let storage = &mut connection_pool.access_storage().await.unwrap();
         let events_web3_dal = EventsWeb3Dal { storage };
         let filter = GetLogsFilter {
@@ -284,7 +284,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_build_get_logs_with_multiple_topics_where_clause() {
-        let connection_pool = ConnectionPool::test_pool().await;
+        let connection_pool = ConnectionPool::<Server>::test_pool().await;
         let storage = &mut connection_pool.access_storage().await.unwrap();
         let events_web3_dal = EventsWeb3Dal { storage };
         let filter = GetLogsFilter {
@@ -318,7 +318,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_build_get_logs_with_no_address_where_clause() {
-        let connection_pool = ConnectionPool::test_pool().await;
+        let connection_pool = ConnectionPool::<Server>::test_pool().await;
         let storage = &mut connection_pool.access_storage().await.unwrap();
         let events_web3_dal = EventsWeb3Dal { storage };
         let filter = GetLogsFilter {
