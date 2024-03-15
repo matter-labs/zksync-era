07e1c7e0cc764e64e35435c51dc7fc47da1f8498
Alex Ostrovski <aov@matterlabs.dev>
2024-03-15 11:34:47 +0200

    Fix typo

--- core/lib/zksync_core/src/sync_layer/mod.rs
+++ core/lib/zksync_core/src/sync_layer/mod.rs
@@ -17,6 +17,6 @@ pub use self::{
 /// Validation gas limit used by the external node.
 // This config value is used on the main node, and depending on these values certain transactions can
 // be *rejected* (that is, not included into the block). However, external node only mirrors what the main
-// node has already executed, so we can safely set thia value to the maximum possible values - if the main
+// node has already executed, so we can safely set this value to the maximum possible values – if the main
 // node has already executed the transaction, then the external node must execute it too.
 const VALIDATION_COMPUTATIONAL_GAS_LIMIT: u32 = u32::MAX;
