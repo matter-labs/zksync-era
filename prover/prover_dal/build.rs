fee6283f344ff3334efcb6415595206b452410f0
Lech <88630083+Artemka374@users.noreply.github.com>
2024-03-25 12:43:09 +0200

    use database url only if it exists

--- prover/prover_dal/build.rs
+++ prover/prover_dal/build.rs
@@ -1,5 +1,11 @@
 fn main() {
-    let database_url =
-        std::env::var("DATABASE_PROVER_URL").expect("Failed to load PROVER_DATABASE_URL");
-    println!("cargo:rustc-env=DATABASE_URL={}", database_url)
+    let database_url = std::env::var("DATABASE_PROVER_URL");
+    match database_url {
+        Ok(url) => {
+            println!("cargo:rustc-env=DATABASE_URL={}", url);
+        }
+        Err(_) => {
+            println!("cargo:warning=DATABASE_PROVER_URL is not set");
+        }
+    }
 }
