83d4c0f9d00aaa6ff01937bb409232856733e5cb
Lech <88630083+Artemka374@users.noreply.github.com>
2024-03-27 13:12:59 +0200

    update tests

--- core/lib/config/src/testonly.rs
+++ core/lib/config/src/testonly.rs
@@ -616,6 +616,7 @@ impl RandomConfig for configs::fri_prover_group::FriProverGroupConfig {
 impl RandomConfig for configs::FriWitnessGeneratorConfig {
     fn sample(g: &mut Gen<impl Rng>) -> Self {
         Self {
+            generation_timeout_in_secs: g.gen(),
             basic_generation_timeout_in_secs: g.gen(),
             leaf_generation_timeout_in_secs: g.gen(),
             node_generation_timeout_in_secs: g.gen(),
