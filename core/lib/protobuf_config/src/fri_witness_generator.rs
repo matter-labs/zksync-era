0cb320612cb5bbf4ade8dfc855c39bb8a8717827
Lech <88630083+Artemka374@users.noreply.github.com>
2024-03-27 13:43:35 +0200

    fix protobuf

--- core/lib/protobuf_config/src/fri_witness_generator.rs
+++ core/lib/protobuf_config/src/fri_witness_generator.rs
@@ -11,20 +11,26 @@ impl ProtoRepr for proto::FriWitnessGenerator {
             generation_timeout_in_secs: required(&self.generation_timeout_in_secs)
                 .and_then(|x| Ok((*x).try_into()?))
                 .context("basic_generation_timeout_in_secs")?,
-            basic_generation_timeout_in_secs: required(&self.basic_generation_timeout_in_secs)
-                .and_then(|x| Ok((*x).try_into()?))
+            basic_generation_timeout_in_secs: self
+                .basic_generation_timeout_in_secs
+                .map(|x| x.try_into())
+                .transpose()
                 .context("basic_generation_timeout_in_secs")?,
-            leaf_generation_timeout_in_secs: required(&self.leaf_generation_timeout_in_secs)
-                .and_then(|x| Ok((*x).try_into()?))
+            leaf_generation_timeout_in_secs: self
+                .leaf_generation_timeout_in_secs
+                .map(|x| x.try_into())
+                .transpose()
                 .context("leaf_generation_timeout_in_secs")?,
-            node_generation_timeout_in_secs: required(&self.node_generation_timeout_in_secs)
-                .and_then(|x| Ok((*x).try_into()?))
+            node_generation_timeout_in_secs: self
+                .node_generation_timeout_in_secs
+                .map(|x| x.try_into())
+                .transpose()
                 .context("node_generation_timeout_in_secs")?,
-            scheduler_generation_timeout_in_secs: required(
-                &self.scheduler_generation_timeout_in_secs,
-            )
-            .and_then(|x| Ok((*x).try_into()?))
-            .context("scheduler_generation_timeout_in_secs")?,
+            scheduler_generation_timeout_in_secs: self
+                .scheduler_generation_timeout_in_secs
+                .map(|x| x.try_into())
+                .transpose()
+                .context("scheduler_generation_timeout_in_secs")?,
             max_attempts: *required(&self.max_attempts).context("max_attempts")?,
             blocks_proving_percentage: self
                 .blocks_proving_percentage
@@ -42,12 +48,14 @@ impl ProtoRepr for proto::FriWitnessGenerator {
     fn build(this: &Self::Type) -> Self {
         Self {
             generation_timeout_in_secs: Some(this.generation_timeout_in_secs.into()),
-            basic_generation_timeout_in_secs: Some(this.basic_generation_timeout_in_secs.into()),
-            leaf_generation_timeout_in_secs: Some(this.leaf_generation_timeout_in_secs.into()),
-            node_generation_timeout_in_secs: Some(this.node_generation_timeout_in_secs.into()),
-            scheduler_generation_timeout_in_secs: Some(
-                this.scheduler_generation_timeout_in_secs.into(),
-            ),
+            basic_generation_timeout_in_secs: this
+                .basic_generation_timeout_in_secs
+                .map(|x| x.into()),
+            leaf_generation_timeout_in_secs: this.leaf_generation_timeout_in_secs.map(|x| x.into()),
+            node_generation_timeout_in_secs: this.node_generation_timeout_in_secs.map(|x| x.into()),
+            scheduler_generation_timeout_in_secs: this
+                .scheduler_generation_timeout_in_secs
+                .map(|x| x.into()),
             max_attempts: Some(this.max_attempts),
             blocks_proving_percentage: this.blocks_proving_percentage.map(|x| x.into()),
             dump_arguments_for_blocks: this.dump_arguments_for_blocks.clone(),
