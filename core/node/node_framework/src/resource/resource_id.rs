a8beb540078dca99d06f929581ffa30fcda798e9
Joonatan Saarhelo <joon.saar@gmail.com>
2024-03-28 17:58:47 +0100

    satisfy spellcheck

--- core/node/node_framework/src/resource/resource_id.rs
+++ core/node/node_framework/src/resource/resource_id.rs
@@ -2,7 +2,7 @@ use std::any::TypeId;
 
 /// A unique identifier of a resource.
 ///
-/// Internal representation is TypeId, which is a 64-bit hash.
+/// Internal representation is [TypeId], which is a 64-bit hash.
 /// That is sufficient for our purposes, as even when using 2^16 different resources,
 /// the chance of a hash collision occurring is about 1 in 2^32.
 /// See https://stackoverflow.com/a/62667633 for how to derive the likelihood.
