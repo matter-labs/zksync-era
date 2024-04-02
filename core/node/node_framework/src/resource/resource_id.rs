410574569adae3ec10fba8025b11847bdcfeebcd
Joonatan Saarhelo <joon.saar@gmail.com>
2024-04-02 15:01:18 +0100

    format link to appease spellchecker

--- core/node/node_framework/src/resource/resource_id.rs
+++ core/node/node_framework/src/resource/resource_id.rs
@@ -5,7 +5,7 @@ use std::any::TypeId;
 /// Internal representation is [`TypeId`], which is a 64-bit hash.
 /// That is sufficient for our purposes, as even when using 2^16 different resources,
 /// the chance of a hash collision occurring is about 1 in 2^32.
-/// See https://stackoverflow.com/a/62667633 for how to derive the likelihood.
+/// This [Stack overflow answer](https://stackoverflow.com/a/62667633) explains how to derive the likelihood.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct ResourceId(TypeId);
 
