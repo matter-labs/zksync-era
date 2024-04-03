b1fb7b79e33e04d313e49264474faf22d2d2e2ba
Joonatan Saarhelo <joon.saar@gmail.com>
2024-04-03 15:10:37 +0100

    refactor: TypeId instead of string-based ResourceId (#1518)
    `std::any::TypeId` solves the same problem as the current string
    identifiers, while using less memory. It eliminates errors that can
    occur due to copy-paste or similar naming.
    
    I'm not super happy with how building the name works but it is good
    enough in my opinion. I tried to add `const NAME: &'static str` to the
    interface but learned that constants can't depend on generic parameters
    of the trait impl. At least if the compiler is even a little bit smart
    it will concatenate the strings at compile time.

--- core/node/node_framework/src/implementations/resources/main_node_client.rs
+++ core/node/node_framework/src/implementations/resources/main_node_client.rs
@@ -2,13 +2,13 @@ use std::sync::Arc;
 
 use zksync_core::sync_layer::MainNodeClient;
 
-use crate::resource::{Resource, ResourceId};
+use crate::resource::Resource;
 
 #[derive(Debug, Clone)]
 pub struct MainNodeClientResource(pub Arc<dyn MainNodeClient>);
 
 impl Resource for MainNodeClientResource {
-    fn resource_id() -> ResourceId {
+    fn name() -> String {
         "external_node/main_node_client".into()
     }
 }
