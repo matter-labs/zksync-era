4ed836d84c9a2567190f85cac01410c5f1197fff
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-04-09 17:47:46 -0300

    Add xor opcode

--- core/lib/multivm/src/versions/vm_latest/tests/evm_simulator.rs
+++ core/lib/multivm/src/versions/vm_latest/tests/evm_simulator.rs
@@ -1151,9 +1151,10 @@ fn test_basic_eq_vectors() {
     assert_eq!(
         test_evm_vector(
             vec![
-                // push1 3
-                hex::decode("60").unwrap(),
-                hex::decode("03").unwrap(),
+                // push32 -3
+                hex::decode("7F").unwrap(),
+                hex::decode("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFD")
+                    .unwrap(),
                 // push1 13
                 hex::decode("60").unwrap(),
                 hex::decode("0D").unwrap(),
@@ -3925,114 +3926,6 @@ fn test_basic_revert_vectors() {
         .concat(),
     );
     // Result should be 0xFF01, maybe it includes the gas before?
-}
-
-#[test]
-fn test_basic_eq_vectors() {
-    // Here we just try to test some small EVM contracts and ensure that they work.
-    assert_eq!(
-        test_evm_vector(
-            vec![
-                // push1 3
-                hex::decode("60").unwrap(),
-                hex::decode("03").unwrap(),
-                // push1 13
-                hex::decode("60").unwrap(),
-                hex::decode("0D").unwrap(),
-                // eq
-                hex::decode("14").unwrap(),
-                // push0
-                hex::decode("5F").unwrap(),
-                // sstore
-                hex::decode("55").unwrap(),
-            ]
-            .into_iter()
-            .concat()
-        ),
-        0.into()
-    );
-    assert_eq!(
-        test_evm_vector(
-            vec![
-                // push1 7
-                hex::decode("60").unwrap(),
-                hex::decode("07").unwrap(),
-                // push1 8
-                hex::decode("60").unwrap(),
-                hex::decode("08").unwrap(),
-                // eq
-                hex::decode("14").unwrap(),
-                // push0
-                hex::decode("5F").unwrap(),
-                // sstore
-                hex::decode("55").unwrap(),
-            ]
-            .into_iter()
-            .concat()
-        ),
-        0.into()
-    );
-    assert_eq!(
-        test_evm_vector(
-            vec![
-                // push1 50
-                hex::decode("60").unwrap(),
-                hex::decode("3C").unwrap(),
-                // push1 50
-                hex::decode("60").unwrap(),
-                hex::decode("3C").unwrap(),
-                // eq
-                hex::decode("14").unwrap(),
-                // push0
-                hex::decode("5F").unwrap(),
-                // sstore
-                hex::decode("55").unwrap(),
-            ]
-            .into_iter()
-            .concat()
-        ),
-        1.into()
-    );
-}
-
-#[test]
-fn test_basic_iszero_vectors() {
-    // Here we just try to test some small EVM contracts and ensure that they work.
-    assert_eq!(
-        test_evm_vector(
-            vec![
-                // push1 3
-                hex::decode("60").unwrap(),
-                hex::decode("03").unwrap(),
-                // iszero
-                hex::decode("15").unwrap(),
-                // push0
-                hex::decode("5F").unwrap(),
-                // sstore
-                hex::decode("55").unwrap(),
-            ]
-            .into_iter()
-            .concat()
-        ),
-        0.into()
-    );
-    assert_eq!(
-        test_evm_vector(
-            vec![
-                // push0
-                hex::decode("5F").unwrap(),
-                // iszero
-                hex::decode("15").unwrap(),
-                // push0
-                hex::decode("5F").unwrap(),
-                // sstore
-                hex::decode("55").unwrap(),
-            ]
-            .into_iter()
-            .concat()
-        ),
-        1.into()
-    );
 }
 
 fn assert_deployed_hash<H: HistoryMode>(
