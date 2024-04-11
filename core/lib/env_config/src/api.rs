3f05e837ca80dca4f8f4636c11690f22f6a317a2
perekopskiy <53865202+perekopskiy@users.noreply.github.com>
2024-04-11 12:33:26 +0300

    chore(api): Deprecate `l1_to_l2_transactions_compatibility_mode` config (#1616)
    ## What ❔
    
    Deprecates `l1_to_l2_transactions_compatibility_mode` config variable
    
    ## Why ❔
    
    1.4.1 upgrade finished and this functionality is not needed
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zk fmt` and `zk lint`.
    - [ ] Spellcheck has been run via `zk spellcheck`.
    - [ ] Linkcheck has been run via `zk linkcheck`.

--- core/lib/env_config/src/api.rs
+++ core/lib/env_config/src/api.rs
@@ -74,7 +74,6 @@ mod tests {
                 estimate_gas_scale_factor: 1.0f64,
                 gas_price_scale_factor: 1.2,
                 estimate_gas_acceptable_overestimation: 1000,
-                l1_to_l2_transactions_compatibility_mode: true,
                 max_tx_size: 1000000,
                 vm_execution_cache_misses_limit: None,
                 vm_concurrency_limit: Some(512),
@@ -127,7 +126,6 @@ mod tests {
             API_WEB3_JSON_RPC_WHITELISTED_TOKENS_FOR_AA="0x0000000000000000000000000000000000000001,0x0000000000000000000000000000000000000002"
             API_WEB3_JSON_RPC_ESTIMATE_GAS_SCALE_FACTOR=1.0
             API_WEB3_JSON_RPC_ESTIMATE_GAS_ACCEPTABLE_OVERESTIMATION=1000
-            API_WEB3_JSON_RPC_L1_TO_L2_TRANSACTIONS_COMPATIBILITY_MODE=true
             API_WEB3_JSON_RPC_MAX_TX_SIZE=1000000
             API_WEB3_JSON_RPC_VM_CONCURRENCY_LIMIT=512
             API_WEB3_JSON_RPC_FACTORY_DEPS_CACHE_SIZE_MB=128
