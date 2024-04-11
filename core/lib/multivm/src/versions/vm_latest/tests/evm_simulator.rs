a3b550b55c86a401aeef71155dd77cf14a27b641
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-04-11 11:15:43 -0300

    Add callvalue opcode

--- core/lib/multivm/src/versions/vm_latest/tests/evm_simulator.rs
+++ core/lib/multivm/src/versions/vm_latest/tests/evm_simulator.rs
@@ -1435,6 +1435,29 @@ fn test_basic_caller_vectors() {
     );
 }
 
+#[test]
+fn test_basic_callvalue_vectors() {
+    // Here we just try to test some small EVM contracts and ensure that they work.
+    assert_eq!(
+        test_evm_vector(
+            vec![
+                // push1 0xFF
+                hex::decode("60").unwrap(),
+                hex::decode("FF").unwrap(),
+                // callvalue
+                hex::decode("34").unwrap(),
+                // push0
+                hex::decode("5F").unwrap(),
+                // sstore
+                hex::decode("55").unwrap(),
+            ]
+            .into_iter()
+            .concat()
+        ),
+        0.into()
+    );
+}
+
 fn assert_deployed_hash<H: HistoryMode>(
     tester: &mut VmTester<H>,
     address: Address,
