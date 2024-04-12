5659509efaa83126e8962e6bb6f5cbdecca115e3
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-04-12 17:01:49 -0300

    Fix tests

--- core/lib/multivm/src/versions/vm_latest/tests/evm_simulator.rs
+++ core/lib/multivm/src/versions/vm_latest/tests/evm_simulator.rs
@@ -2114,7 +2114,6 @@ fn test_basic_pc_vectors() {
 
 #[test]
 fn test_basic_gas_vectors() {
-    // This test will fail when push1 charge gas. Reduce expected value by 3 units.
     assert_eq!(
         test_evm_vector(
             vec![
@@ -2132,13 +2131,12 @@ fn test_basic_gas_vectors() {
             .concat()
         ),
         U256::from_dec_str(
-            "115792089237316195423570985008687907853269984665640564039457584007913129639933"
+            "115792089237316195423570985008687907853269984665640564039457584007913129639930"
         )
         .unwrap()
         .into()
     );
 
-    // This test will fail when push1 charge gas. Reduce expected value by 12 units.
     assert_eq!(
         test_evm_vector(
             vec![
@@ -2167,7 +2165,7 @@ fn test_basic_gas_vectors() {
             .concat()
         ),
         U256::from_dec_str(
-            "115792089237316195423570985008687907853269984665640564039457584007913129639931"
+            "115792089237316195423570985008687907853269984665640564039457584007913129639919"
         )
         .unwrap()
         .into()
