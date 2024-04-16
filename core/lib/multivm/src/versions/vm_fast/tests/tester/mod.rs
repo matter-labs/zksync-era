2643d7e32ebc604c444ab19bef7a0f4c92cf049a
Joonatan Saarhelo <joon.saar@gmail.com>
2024-04-16 15:05:42 +0200

    able to fail one test

---
+++ core/lib/multivm/src/versions/vm_fast/tests/tester/mod.rs
@@ -0,0 +1,8 @@
+pub(crate) use transaction_test_info::{ExpectedError, TransactionTestInfo, TxModifier};
+pub(crate) use vm_tester::{
+    default_l1_batch, get_empty_storage, InMemoryStorageView, VmTester, VmTesterBuilder,
+};
+pub(crate) use zksync_test_account::{Account, DeployContractsTx, TxType};
+
+mod transaction_test_info;
+mod vm_tester;
