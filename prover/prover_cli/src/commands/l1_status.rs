a29a9e825ff141eaf2ef077a94ff3bb090ea3209
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2024-04-18 12:16:58 -0300

    fix print

--- prover/prover_cli/src/commands/l1_status.rs
+++ prover/prover_cli/src/commands/l1_status.rs
@@ -12,7 +12,6 @@ use zksync_env_config::FromEnv;
 use zksync_eth_client::{clients::QueryClient, CallFunctionArgs, EthInterface};
 
 pub(crate) async fn run() -> anyhow::Result<()> {
-    println!(" ----------------------- ");
     println!(" ====== L1 Status ====== ");
     let postgres_config = PostgresConfig::from_env().context("PostgresConfig::from_env()")?;
     let contracts_config = ContractsConfig::from_env().context("ContractsConfig::from_env()")?;
