7cf62ff5031d43c6acb7eac4baf150f6bbe51f55
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2024-04-18 12:38:04 -0300

    fix status l1 command

--- prover/prover_cli/src/commands/status/mod.rs
+++ prover/prover_cli/src/commands/status/mod.rs
@@ -1,16 +1,19 @@
 use clap::Subcommand;
 
 pub(crate) mod batch;
+pub(crate) mod l1;
 
 #[derive(Subcommand)]
 pub enum StatusCommand {
     Batch(batch::Args),
+    L1,
 }
 
 impl StatusCommand {
     pub(crate) async fn run(self) -> anyhow::Result<()> {
         match self {
             StatusCommand::Batch(args) => batch::run(args).await,
+            StatusCommand::L1 => l1::run().await,
         }
     }
 }
