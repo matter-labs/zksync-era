4d9c767b7fd3e069b471c634fb414744025c4551
jayt106 <jay.tseng@crypto.com>
2024-04-19 12:39:43 -0400

    implement the deny_list_pool_sink

---
+++ core/lib/zksync_core/src/api_server/tx_sender/deny_list_pool_sink.rs
@@ -0,0 +1,79 @@
+use std::{collections::HashSet, str::FromStr};
+
+use zksync_dal::{transactions_dal::L2TxSubmissionResult, ConnectionPool};
+use zksync_types::{fee::TransactionExecutionMetrics, l2::L2Tx, Address};
+
+use super::{tx_sink::TxSink, SubmitTxError};
+use crate::metrics::{TxStage, APP_METRICS};
+
+/// Wrapper for the master DB pool that allows to submit transactions to the mempool.
+#[derive(Debug)]
+pub struct DenyListPoolSink {
+    deny_list_pool: ConnectionPool,
+    deny_list: HashSet<Address>,
+    inflight_requests: Mutex<HashMap<(Address, Nonce), H256>>,
+}
+
+impl DenyListPoolSink {
+    pub fn new(deny_list_pool: ConnectionPool, deny_list: HashSet<Address>) -> Self {
+
+        Self {
+            deny_list_pool,
+            deny_list,
+            inflight_requests: Mutex::new(HashMap::new()),
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl TxSink for DenyListPoolSink {
+    async fn submit_tx(
+        &self,
+        tx: L2Tx,
+        execution_metrics: TransactionExecutionMetrics,
+    ) -> Result<L2TxSubmissionResult, SubmitTxError> {
+        let address_and_nonce = (tx.initiator_account(), tx.nonce());
+        if self.deny_list.contains(address_and_nonce.0) {
+            return Err(SubmitTxError::SenderInDenyList);
+        }
+
+        let mut lock = self.inflight_requests.lock().await;
+        match lock.entry(address_and_nonce) {
+            Entry::Occupied(entry) => {
+                let submission_res_handle = if entry.get() == &tx.hash() {
+                    L2TxSubmissionResult::Duplicate
+                } else {
+                    L2TxSubmissionResult::InsertionInProgress
+                };
+                APP_METRICS.processed_txs[&TxStage::Mempool(submission_res_handle)].inc();
+                return Ok(submission_res_handle);
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(tx.hash());
+                API_METRICS.inflight_tx_submissions.inc_by(1);
+            }
+        };
+        drop(lock);
+
+        let result = match self.master_pool.connection_tagged("api").await {
+            Ok(mut connection) => connection
+                .transactions_dal()
+                .insert_transaction_l2(tx, execution_metrics)
+                .await
+                .map(|submission_res_handle| {
+                    APP_METRICS.processed_txs[&TxStage::Mempool(submission_res_handle)].inc();
+                    submission_res_handle
+                })
+                .map_err(|err| err.generalize().into()),
+            Err(err) => Err(err.generalize().into()),
+        };
+
+        self.inflight_requests
+            .lock()
+            .await
+            .remove(&address_and_nonce);
+        API_METRICS.inflight_tx_submissions.dec_by(1);
+
+        result
+    }
+}
