543f9e9397915e893d7b747ceccd9b76f9d571aa
Daniyar Itegulov <di@matterlabs.dev>
2024-04-23 23:03:24 +0500

    feat(vm-runner): implement VM runner storage layer (#1651)
    ## What ❔
    
    Adds an abstraction that can load unprocessed batches as well as
    implement `ReadStorageFactory`. Implementation largely reuses the
    existing primitives from state keeper and they had to be generalized
    slightly.
    
    ## Why ❔
    
    One of the components for the upcoming VM runner
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [x] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [x] Tests for the changes have been added / updated.
    - [x] Documentation comments have been added / updated.
    - [x] Code has been formatted via `zk fmt` and `zk lint`.
    - [x] Spellcheck has been run via `zk spellcheck`.
    - [x] Linkcheck has been run via `zk linkcheck`.

--- core/lib/zksync_core/src/state_keeper/state_keeper_storage.rs
+++ core/lib/zksync_core/src/state_keeper/state_keeper_storage.rs
@@ -1,75 +1,15 @@
-use std::{fmt::Debug, sync::Arc, time::Instant};
+use std::{fmt::Debug, sync::Arc};
 
 use anyhow::Context;
 use async_trait::async_trait;
 use once_cell::sync::OnceCell;
-use tokio::{runtime::Handle, sync::watch};
-use zksync_dal::{Connection, ConnectionPool, Core, CoreDal};
-use zksync_shared_metrics::{SnapshotRecoveryStage, APP_METRICS};
+use tokio::sync::watch;
+use zksync_dal::{ConnectionPool, Core};
 use zksync_state::{
-    PostgresStorage, ReadStorage, RocksdbStorage, RocksdbStorageBuilder, StateKeeperColumnFamily,
+    AsyncCatchupTask, PgOrRocksdbStorage, ReadStorageFactory, StateKeeperColumnFamily,
 };
 use zksync_storage::RocksDB;
-use zksync_types::{L1BatchNumber, L2BlockNumber};
-
-/// Factory that can produce a [`ReadStorage`] implementation on demand.
-#[async_trait]
-pub trait ReadStorageFactory: Debug + Send + Sync + 'static {
-    async fn access_storage(
-        &self,
-        stop_receiver: &watch::Receiver<bool>,
-    ) -> anyhow::Result<Option<PgOrRocksdbStorage<'_>>>;
-}
-
-/// A [`ReadStorage`] implementation that uses either [`PostgresStorage`] or [`RocksdbStorage`]
-/// underneath.
-#[derive(Debug)]
-pub enum PgOrRocksdbStorage<'a> {
-    Postgres(PostgresStorage<'a>),
-    Rocksdb(RocksdbStorage),
-}
-
-impl ReadStorage for PgOrRocksdbStorage<'_> {
-    fn read_value(&mut self, key: &zksync_types::StorageKey) -> zksync_types::StorageValue {
-        match self {
-            Self::Postgres(postgres) => postgres.read_value(key),
-            Self::Rocksdb(rocksdb) => rocksdb.read_value(key),
-        }
-    }
-
-    fn is_write_initial(&mut self, key: &zksync_types::StorageKey) -> bool {
-        match self {
-            Self::Postgres(postgres) => postgres.is_write_initial(key),
-            Self::Rocksdb(rocksdb) => rocksdb.is_write_initial(key),
-        }
-    }
-
-    fn load_factory_dep(&mut self, hash: zksync_types::H256) -> Option<Vec<u8>> {
-        match self {
-            Self::Postgres(postgres) => postgres.load_factory_dep(hash),
-            Self::Rocksdb(rocksdb) => rocksdb.load_factory_dep(hash),
-        }
-    }
-
-    fn get_enumeration_index(&mut self, key: &zksync_types::StorageKey) -> Option<u64> {
-        match self {
-            Self::Postgres(postgres) => postgres.get_enumeration_index(key),
-            Self::Rocksdb(rocksdb) => rocksdb.get_enumeration_index(key),
-        }
-    }
-}
-
-impl<'a> From<PostgresStorage<'a>> for PgOrRocksdbStorage<'a> {
-    fn from(value: PostgresStorage<'a>) -> Self {
-        Self::Postgres(value)
-    }
-}
-
-impl<'a> From<RocksdbStorage> for PgOrRocksdbStorage<'a> {
-    fn from(value: RocksdbStorage) -> Self {
-        Self::Rocksdb(value)
-    }
-}
+use zksync_types::L1BatchNumber;
 
 /// A [`ReadStorageFactory`] implementation that can produce short-lived [`ReadStorage`] handles
 /// backed by either Postgres or RocksDB (if it's caught up). Always initialized as a `Postgres`
@@ -83,78 +23,10 @@ pub struct AsyncRocksdbCache {
 }
 
 impl AsyncRocksdbCache {
-    /// Load latest sealed L2 block from the latest sealed L1 batch (ignores sealed L2 blocks
-    /// from unsealed batches).
-    async fn load_latest_sealed_l2_block(
-        connection: &mut Connection<'_, Core>,
-    ) -> anyhow::Result<Option<(L2BlockNumber, L1BatchNumber)>> {
-        let mut dal = connection.blocks_dal();
-        let Some(l1_batch_number) = dal.get_sealed_l1_batch_number().await? else {
-            return Ok(None);
-        };
-        let (_, l2_block_number) = dal
-            .get_l2_block_range_of_l1_batch(l1_batch_number)
-            .await?
-            .context("The latest sealed L1 batch does not have a L2 block range")?;
-        Ok(Some((l2_block_number, l1_batch_number)))
-    }
-
-    /// Returns a [`ReadStorage`] implementation backed by Postgres
-    pub(crate) async fn access_storage_pg(
-        pool: &ConnectionPool<Core>,
-    ) -> anyhow::Result<PgOrRocksdbStorage<'_>> {
-        let mut connection = pool.connection().await?;
-
-        let (l2_block_number, l1_batch_number) =
-            match Self::load_latest_sealed_l2_block(&mut connection).await? {
-                Some((l2_block_number, l1_batch_number)) => (l2_block_number, l1_batch_number),
-                None => {
-                    tracing::info!("Could not find latest sealed L2 block, loading from snapshot");
-                    let snapshot_recovery = connection
-                        .snapshot_recovery_dal()
-                        .get_applied_snapshot_status()
-                        .await?
-                        .context("Could not find snapshot, no state available")?;
-                    (
-                        snapshot_recovery.l2_block_number,
-                        snapshot_recovery.l1_batch_number,
-                    )
-                }
-            };
-
-        tracing::debug!(%l1_batch_number, %l2_block_number, "Using Postgres-based storage");
-        Ok(
-            PostgresStorage::new_async(Handle::current(), connection, l2_block_number, true)
-                .await?
-                .into(),
-        )
-    }
-
-    /// Catches up RocksDB synchronously (i.e. assumes the gap is small) and
-    /// returns a [`ReadStorage`] implementation backed by caught-up RocksDB.
-    async fn access_storage_rocksdb<'a>(
-        connection: &mut Connection<'_, Core>,
-        rocksdb: RocksDB<StateKeeperColumnFamily>,
-        stop_receiver: &watch::Receiver<bool>,
-    ) -> anyhow::Result<Option<PgOrRocksdbStorage<'a>>> {
-        tracing::debug!("Catching up RocksDB synchronously");
-        let rocksdb_builder = RocksdbStorageBuilder::from_rocksdb(rocksdb);
-        let rocksdb = rocksdb_builder
-            .synchronize(connection, stop_receiver)
-            .await
-            .context("Failed to catch up state keeper RocksDB storage to Postgres")?;
-        let Some(rocksdb) = rocksdb else {
-            tracing::info!("Synchronizing RocksDB interrupted");
-            return Ok(None);
-        };
-        let rocksdb_l1_batch_number = rocksdb.l1_batch_number().await.unwrap_or_default();
-        tracing::debug!(%rocksdb_l1_batch_number, "Using RocksDB-based storage");
-        Ok(Some(rocksdb.into()))
-    }
-
     async fn access_storage_inner(
         &self,
         stop_receiver: &watch::Receiver<bool>,
+        l1_batch_number: L1BatchNumber,
     ) -> anyhow::Result<Option<PgOrRocksdbStorage<'_>>> {
         if let Some(rocksdb) = self.rocksdb_cell.get() {
             let mut connection = self
@@ -162,12 +34,17 @@ impl AsyncRocksdbCache {
                 .connection_tagged("state_keeper")
                 .await
                 .context("Failed getting a Postgres connection")?;
-            Self::access_storage_rocksdb(&mut connection, rocksdb.clone(), stop_receiver)
-                .await
-                .context("Failed accessing RocksDB storage")
+            PgOrRocksdbStorage::access_storage_rocksdb(
+                &mut connection,
+                rocksdb.clone(),
+                stop_receiver,
+                l1_batch_number,
+            )
+            .await
+            .context("Failed accessing RocksDB storage")
         } else {
             Ok(Some(
-                Self::access_storage_pg(&self.pool)
+                PgOrRocksdbStorage::access_storage_pg(&self.pool, l1_batch_number)
                     .await
                     .context("Failed accessing Postgres storage")?,
             ))
@@ -179,11 +56,12 @@ impl AsyncRocksdbCache {
         state_keeper_db_path: String,
     ) -> (Self, AsyncCatchupTask) {
         let rocksdb_cell = Arc::new(OnceCell::new());
-        let task = AsyncCatchupTask {
-            pool: pool.clone(),
+        let task = AsyncCatchupTask::new(
+            pool.clone(),
             state_keeper_db_path,
-            rocksdb_cell: rocksdb_cell.clone(),
-        };
+            rocksdb_cell.clone(),
+            None,
+        );
         (Self { pool, rocksdb_cell }, task)
     }
 }
@@ -193,50 +71,9 @@ impl ReadStorageFactory for AsyncRocksdbCache {
     async fn access_storage(
         &self,
         stop_receiver: &watch::Receiver<bool>,
+        l1_batch_number: L1BatchNumber,
     ) -> anyhow::Result<Option<PgOrRocksdbStorage<'_>>> {
-        self.access_storage_inner(stop_receiver).await
-    }
-}
-
-#[derive(Debug)]
-pub struct AsyncCatchupTask {
-    pool: ConnectionPool<Core>,
-    state_keeper_db_path: String,
-    rocksdb_cell: Arc<OnceCell<RocksDB<StateKeeperColumnFamily>>>,
-}
-
-impl AsyncCatchupTask {
-    pub async fn run(self, stop_receiver: watch::Receiver<bool>) -> anyhow::Result<()> {
-        let started_at = Instant::now();
-        tracing::debug!("Catching up RocksDB asynchronously");
-
-        let mut rocksdb_builder = RocksdbStorage::builder(self.state_keeper_db_path.as_ref())
+        self.access_storage_inner(stop_receiver, l1_batch_number)
             .await
-            .context("Failed creating RocksDB storage builder")?;
-        let mut connection = self.pool.connection().await?;
-        let was_recovered_from_snapshot = rocksdb_builder
-            .ensure_ready(&mut connection, &stop_receiver)
-            .await
-            .context("failed initializing state keeper RocksDB from snapshot or scratch")?;
-        if was_recovered_from_snapshot {
-            let elapsed = started_at.elapsed();
-            APP_METRICS.snapshot_recovery_latency[&SnapshotRecoveryStage::StateKeeperCache]
-                .set(elapsed);
-            tracing::info!("Recovered state keeper RocksDB from snapshot in {elapsed:?}");
-        }
-
-        let rocksdb = rocksdb_builder
-            .synchronize(&mut connection, &stop_receiver)
-            .await
-            .context("Failed to catch up RocksDB to Postgres")?;
-        drop(connection);
-        if let Some(rocksdb) = rocksdb {
-            self.rocksdb_cell
-                .set(rocksdb.into_rocksdb())
-                .map_err(|_| anyhow::anyhow!("Async RocksDB cache was initialized twice"))?;
-        } else {
-            tracing::info!("Synchronizing RocksDB interrupted");
-        }
-        Ok(())
     }
 }
