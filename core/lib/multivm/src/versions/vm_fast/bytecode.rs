420f9cfe125dd4bf305ad0773f6fd8cc90964aa4
Joonatan Saarhelo <joon.saar@gmail.com>
2024-04-24 14:37:11 +0200

    deduplicate bytecodes based on latest storage changes

--- core/lib/multivm/src/versions/vm_fast/bytecode.rs
+++ core/lib/multivm/src/versions/vm_fast/bytecode.rs
@@ -1,13 +1,6 @@
 use itertools::Itertools;
-use zksync_state::{StoragePtr, WriteStorage};
-use zksync_types::U256;
-use zksync_utils::{
-    bytecode::{compress_bytecode, hash_bytecode, CompressedBytecodeInfo},
-    bytes_to_be_words,
-};
-
-use super::vm::Vm;
-use crate::interface::VmInterface;
+use zksync_types::H256;
+use zksync_utils::bytecode::{compress_bytecode, hash_bytecode, CompressedBytecodeInfo};
 
 /*impl Vm {
     /// Checks the last transaction has successfully published compressed bytecodes and returns `true` if there is at least one is still unknown.
@@ -24,16 +17,16 @@ use crate::interface::VmInterface;
     }
 }*/
 
-pub(crate) fn compress_bytecodes<S: WriteStorage>(
+pub(crate) fn compress_bytecodes(
     bytecodes: &[Vec<u8>],
-    storage: StoragePtr<S>,
+    is_bytecode_known: impl Fn(H256) -> bool,
 ) -> Vec<CompressedBytecodeInfo> {
     bytecodes
         .iter()
         .enumerate()
         .sorted_by_key(|(_idx, dep)| *dep)
         .dedup_by(|x, y| x.1 == y.1)
-        .filter(|(_idx, dep)| !storage.borrow_mut().is_bytecode_known(&hash_bytecode(dep)))
+        .filter(|(_idx, dep)| !is_bytecode_known(hash_bytecode(dep)))
         .sorted_by_key(|(idx, _dep)| *idx)
         .filter_map(|(_idx, dep)| {
             let compressed_bytecode = compress_bytecode(dep);
