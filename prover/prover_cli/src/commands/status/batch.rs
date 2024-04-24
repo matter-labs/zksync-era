58d47ad72c56d87e37d83c0cf0891720437f2baf
Ivan Litteri <ivanlitteri@Ivans-MacBook-Pro.local>
2024-04-24 15:38:58 -0300

    Setle status cmd baseline

--- prover/prover_cli/src/commands/status/batch.rs
+++ prover/prover_cli/src/commands/status/batch.rs
@@ -1,68 +1,46 @@
+use super::utils::BatchData;
+use crate::commands::status::utils::postgres_config;
 use anyhow::{ensure, Context as _};
 use clap::Args as ClapArgs;
-use prover_dal::{ConnectionPool, Prover, ProverDal};
-use zksync_basic_types::{prover_dal::JobCountStatistics, L1BatchNumber};
-use zksync_config::PostgresConfig;
-use zksync_env_config::FromEnv;
+
+use prover_dal::{ConnectionPool, Prover};
+use zksync_types::L1BatchNumber;
 
 #[derive(ClapArgs)]
 pub struct Args {
-    #[clap(short, long, num_args = 0..)]
-    batch: Vec<L1BatchNumber>,
+    #[clap(short = 'n', num_args = 1..)]
+    batches: Vec<L1BatchNumber>,
     #[clap(short, long, default_value("false"))]
     verbose: bool,
 }
 
 pub(crate) async fn run(args: Args) -> anyhow::Result<()> {
     ensure!(
-        !args.batch.is_empty(),
+        !args.batches.is_empty(),
         "At least one batch number should be provided"
     );
-    let postgres_config = PostgresConfig::from_env().context("PostgresConfig::from_env()")?;
 
-    let prover_connection_pool = ConnectionPool::<Prover>::builder(
-        postgres_config.prover_url()?,
-        postgres_config.max_connections()?,
-    )
-    .build()
-    .await
-    .context("failed to build a prover_connection_pool")?;
+    let batches_data = get_batches_data(args.batches).await?;
 
-    let mut conn = prover_connection_pool.connection().await.unwrap();
-    let stats = conn
-        .fri_prover_jobs_dal()
-        .get_prover_jobs_stats_for_batch(args.batch)
-        .await;
-    if stats.len() > 0 {
-        for row in &stats {
-            let ((l1_batch_number, aggregation_round), statistics) = row;
-            if *aggregation_round == 0 {
-                pretty_print_job_status(l1_batch_number, statistics, args.verbose)
-            }
-        }
-    } else {
-        println!("No batches found.")
+    for batch_data in batches_data {
+        println!("{batch_data:?}");
     }
+
     Ok(())
 }
 
-fn pretty_print_job_status(
-    l1_batch_number: &L1BatchNumber,
-    statistics: &JobCountStatistics,
-    verbose: bool,
-) {
-    let total_jobs =
-        statistics.queued + statistics.in_progress + statistics.failed + statistics.successful;
-    let progress = (statistics.successful as f32 / total_jobs as f32) * 100.0;
-    println!("Batch number: {}", l1_batch_number);
-    println!(
-        "Progress: {:.2}% ({}/{})",
-        progress, statistics.successful, total_jobs
-    );
-    if verbose {
-        println!("In progress: {}", statistics.in_progress);
-        println!("Queued: {}", statistics.in_progress);
-        println!("Successful: {}", statistics.in_progress);
-    }
-    println!("Failed: {}", statistics.failed);
+async fn get_batches_data(_batches: Vec<L1BatchNumber>) -> anyhow::Result<Vec<BatchData>> {
+    let config = postgres_config()?;
+
+    let prover_connection_pool =
+        ConnectionPool::<Prover>::builder(config.prover_url()?, config.max_connections()?)
+            .build()
+            .await
+            .context("failed to build a prover_connection_pool")?;
+
+    let _conn = prover_connection_pool.connection().await.unwrap();
+
+    // Queries here...
+
+    Ok(vec![BatchData::default()])
 }
