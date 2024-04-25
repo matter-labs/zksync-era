422b7235795064cc3fe9162d108e0757b04bd0a0
Gianbelinche <gianluca.belinche@lambdaclass.com>
2024-04-25 10:09:38 -0300

    Add environment variable for running with yul/solidity

--- core/lib/contracts/src/lib.rs
+++ core/lib/contracts/src/lib.rs
@@ -16,6 +16,7 @@ use ethabi::{
 };
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
+use std::env;
 use zksync_utils::{bytecode::hash_bytecode, bytes_to_be_words};
 
 pub mod test_contracts;
