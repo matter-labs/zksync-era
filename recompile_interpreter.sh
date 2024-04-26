d2982b22b4e1923f372dfb7592e84994b0770aa7
Javier Chatruc <jrchatruc@gmail.com>
2024-04-26 15:20:57 -0300

    Update to be compatible with old versions as well

--- recompile_interpreter.sh
+++ recompile_interpreter.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+VERSION_OUTPUT=$(zksolc --version)
+if [[ $VERSION_OUTPUT == *"1.4.1"* ]]; then
+    JUMP_TABLE_FLAG="--jump-table-density-threshold 5"
+else
+    JUMP_TABLE_FLAG=
+fi
+
 preprocess -f contracts/system-contracts/contracts/EvmInterpreter.template.yul -d contracts/system-contracts/contracts/EvmInterpreterPreprocessed.yul
-zksolc contracts/system-contracts/contracts/EvmInterpreterPreprocessed.yul --optimization 3 --jump-table-density-threshold 5 --yul --bin --overwrite -o contracts/system-contracts/contracts-preprocessed/artifacts/
-python hex_to_binary.py
-mv -f bytecode contracts/system-contracts/contracts-preprocessed/artifacts/EvmInterpreterPreprocessed.yul.zbin
+zksolc contracts/system-contracts/contracts/EvmInterpreterPreprocessed.yul --optimization 3 $JUMP_TABLE_FLAG --yul --bin --overwrite -o contracts/system-contracts/contracts-preprocessed/artifacts/
+
+VERSION_OUTPUT=$(zksolc --version)
+if [[ $VERSION_OUTPUT == *"1.4.1"* ]]; then
+    python hex_to_binary.py
+    mv -f bytecode contracts/system-contracts/contracts-preprocessed/artifacts/EvmInterpreterPreprocessed.yul.zbin
+fi
