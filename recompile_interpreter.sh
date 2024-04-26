3cbccbd8b3351ad664d4d4c3f1d9229010b00fc2
Javier Chatruc <jrchatruc@gmail.com>
2024-04-26 15:16:14 -0300

    Update script to handle zksolc 1.4.1

--- recompile_interpreter.sh
+++ recompile_interpreter.sh
@@ -2,4 +2,6 @@
 set -e
 
 preprocess -f contracts/system-contracts/contracts/EvmInterpreter.template.yul -d contracts/system-contracts/contracts/EvmInterpreterPreprocessed.yul
-zksolc contracts/system-contracts/contracts/EvmInterpreterPreprocessed.yul --optimization 3 --yul --bin --overwrite -o contracts/system-contracts/contracts-preprocessed/artifacts/
+zksolc contracts/system-contracts/contracts/EvmInterpreterPreprocessed.yul --optimization 3 --jump-table-density-threshold 5 --yul --bin --overwrite -o contracts/system-contracts/contracts-preprocessed/artifacts/
+python hex_to_binary.py
+mv -f bytecode contracts/system-contracts/contracts-preprocessed/artifacts/EvmInterpreterPreprocessed.yul.zbin
