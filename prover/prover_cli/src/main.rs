0fdddae88c7f745575cc415360e1a074f10d2769
ilitteri <ilitteri@fi.uba.ar>
2024-05-03 10:36:35 -0300

    Update deps

--- prover/prover_cli/src/main.rs
+++ prover/prover_cli/src/main.rs
@@ -2,16 +2,10 @@ use prover_cli::cli;
 
 #[tokio::main]
 async fn main() {
-    env_logger::builder()
-        .filter_module("zksync_db_connection", log::LevelFilter::Off)
-        .filter_module("sqlx", log::LevelFilter::Off)
-        .filter_level(log::LevelFilter::Debug)
-        .init();
-
     match cli::start().await {
         Ok(_) => {}
         Err(err) => {
-            log::error!("{err:?}");
+            tracing::error!("{err:?}");
             std::process::exit(1);
         }
     }
