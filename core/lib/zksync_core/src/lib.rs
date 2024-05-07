143d48c493008412753933d3a4e5c753a2c4319d
Juan Munoz <juanmunoz890@gmail.com>
2024-05-07 19:07:16 -0300

    prevent race condition

--- core/lib/zksync_core/src/lib.rs
+++ core/lib/zksync_core/src/lib.rs
@@ -361,6 +361,12 @@ pub async fn initialize_components(
     };
 
     let base_token_fetcher = if components.contains(&Component::BaseTokenFetcher) {
+        if components.contains(&Component::DevConversionRateApi) {
+            // If we are using the dev conversion rate API,
+            // we need to wait for it to start before initializing the base token fetcher.
+            // a more deterministic way should be used
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
         Arc::new(
             BaseTokenFetcher::new(
                 configs
