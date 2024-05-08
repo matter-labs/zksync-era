155f38e0507f5e9a15f1e67667006c0f176d8c06
tomg10 <lemures64@gmail.com>
2024-05-08 20:46:05 +0200

    prepare cache as a single command
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- infrastructure/zk/src/compiler.ts
+++ infrastructure/zk/src/compiler.ts
@@ -13,6 +13,14 @@ export async function compileSystemContracts() {
     await utils.spawn('yarn workspace system-contracts build');
 }
 
+export async function prepareCompilersCache() {
+    await utils.spawn("docker create --name compilers-temp-container matterlabs/compilers:latest")
+    await utils.spawn("mkdir -p /root/.cache/hardhat-nodejs/compilers-v2")
+    await utils.spawn("docker cp compilers-temp-container:./.cache/hardhat-nodejs/compilers-v2/ /root/.cache/hardhat-nodejs/")
+    await utils.spawn("chmod -R +x /root/.cache/hardhat-nodejs/")
+    await utils.spawn("docker rm compilers-temp-container")
+}
+
 export async function compileAll() {
     await compileSystemContracts();
     await compileTestContracts();
@@ -20,6 +28,7 @@ export async function compileAll() {
 
 export const command = new Command('compiler').description('compile contract');
 
+command.command('prepare-compilers-cache').description('').action(prepareCompilersCache);
 command.command('all').description('').action(compileAll);
 command.command('system-contracts').description('').action(compileSystemContracts);
 command.command('test-contracts').description('').action(compileTestContracts);
