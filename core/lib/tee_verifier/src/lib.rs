fd0f4f33995b9532f36cea219740d60cd0b05a3f
Harald Hoyer <harald@matterlabs.dev>
2024-05-14 12:35:07 +0200

    fixup! feat: tee_verifier_input_producer
    Signed-off-by: Harald Hoyer <harald@matterlabs.dev>

--- core/lib/tee_verifier/src/lib.rs
+++ core/lib/tee_verifier/src/lib.rs
@@ -3,24 +3,17 @@
 //! Verifies that a L1Batch has the expected root hash after
 //! executing the VM and verifying all the accessed memory slots by their
 //! merkle path.
-//!
-//! The crate/library shadows some structures to avoid polluting the global code
-//! base with a stable (De-)Serialize ABI.
-//! If additional components need to (De-)Serialize these structures, we can revisit this decision.
 
 use std::{cell::RefCell, rc::Rc};
 
 use anyhow::Context;
 use multivm::{
-    interface::{FinishedL1Batch, L1BatchEnv, L2BlockEnv, SystemEnv, TxExecutionMode, VmInterface},
+    interface::{FinishedL1Batch, L1BatchEnv, L2BlockEnv, SystemEnv, VmInterface},
     vm_latest::HistoryEnabled,
-    zk_evm_latest::ethereum_types::U256,
     VmInstance,
 };
 use serde::{Deserialize, Serialize};
 use vm_utils::execute_tx;
-use zksync_basic_types::{protocol_version::ProtocolVersionId, Address, L2BlockNumber, L2ChainId};
-use zksync_contracts::{BaseSystemContracts, SystemContractCode};
 use zksync_crypto::hasher::blake2::Blake2Hasher;
 use zksync_merkle_tree::{
     BlockOutputWithProofs, TreeInstruction, TreeLogEntry, TreeLogEntryWithProof,
@@ -29,373 +22,18 @@ use zksync_object_store::{serialize_using_bincode, Bucket, StoredObject};
 use zksync_prover_interface::inputs::{PrepareBasicCircuitsJob, StorageLogMetadata};
 use zksync_state::{InMemoryStorage, StorageView, WriteStorage};
 use zksync_types::{
-    block::L2BlockExecutionData,
-    ethabi::ethereum_types::BigEndianHash,
-    fee_model::{BatchFeeInput, L1PeggedBatchFeeModelInput, PubdataIndependentBatchFeeModelInput},
-    zk_evm_types::LogQuery,
-    AccountTreeId, L1BatchNumber, StorageKey, Transaction, H256,
+    block::L2BlockExecutionData, ethabi::ethereum_types::BigEndianHash, zk_evm_types::LogQuery,
+    AccountTreeId, L1BatchNumber, StorageKey, H256,
 };
 use zksync_utils::{bytecode::hash_bytecode, u256_to_h256};
-
-/// Shadowed struct of [`L2BlockExecutionData`]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct V1L2BlockExecutionData {
-    number: L2BlockNumber,
-    timestamp: u64,
-    prev_block_hash: H256,
-    virtual_blocks: u32,
-    txs: Vec<Transaction>,
-}
-
-impl From<V1L2BlockExecutionData> for L2BlockExecutionData {
-    fn from(value: V1L2BlockExecutionData) -> Self {
-        L2BlockExecutionData {
-            number: value.number,
-            timestamp: value.timestamp,
-            prev_block_hash: value.prev_block_hash,
-            virtual_blocks: value.virtual_blocks,
-            txs: value.txs,
-        }
-    }
-}
-
-impl From<L2BlockExecutionData> for V1L2BlockExecutionData {
-    fn from(value: L2BlockExecutionData) -> Self {
-        V1L2BlockExecutionData {
-            number: value.number,
-            timestamp: value.timestamp,
-            prev_block_hash: value.prev_block_hash,
-            virtual_blocks: value.virtual_blocks,
-            txs: value.txs,
-        }
-    }
-}
-
-/// Shadowed struct of [`L1PeggedBatchFeeModelInput`]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-struct V1L1PeggedBatchFeeModelInput {
-    /// Fair L2 gas price to provide
-    fair_l2_gas_price: u64,
-    /// The L1 gas price to provide to the VM.
-    l1_gas_price: u64,
-}
-impl From<V1L1PeggedBatchFeeModelInput> for L1PeggedBatchFeeModelInput {
-    fn from(value: V1L1PeggedBatchFeeModelInput) -> Self {
-        L1PeggedBatchFeeModelInput {
-            fair_l2_gas_price: value.fair_l2_gas_price,
-            l1_gas_price: value.l1_gas_price,
-        }
-    }
-}
-
-impl From<L1PeggedBatchFeeModelInput> for V1L1PeggedBatchFeeModelInput {
-    fn from(value: L1PeggedBatchFeeModelInput) -> Self {
-        V1L1PeggedBatchFeeModelInput {
-            fair_l2_gas_price: value.fair_l2_gas_price,
-            l1_gas_price: value.l1_gas_price,
-        }
-    }
-}
-/// Shadowed struct of [`PubdataIndependentBatchFeeModelInput`]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-struct V1PubdataIndependentBatchFeeModelInput {
-    fair_l2_gas_price: u64,
-    fair_pubdata_price: u64,
-    l1_gas_price: u64,
-}
-
-impl From<V1PubdataIndependentBatchFeeModelInput> for PubdataIndependentBatchFeeModelInput {
-    fn from(value: V1PubdataIndependentBatchFeeModelInput) -> Self {
-        PubdataIndependentBatchFeeModelInput {
-            fair_l2_gas_price: value.fair_l2_gas_price,
-            fair_pubdata_price: value.fair_pubdata_price,
-            l1_gas_price: value.l1_gas_price,
-        }
-    }
-}
-
-impl From<PubdataIndependentBatchFeeModelInput> for V1PubdataIndependentBatchFeeModelInput {
-    fn from(value: PubdataIndependentBatchFeeModelInput) -> Self {
-        V1PubdataIndependentBatchFeeModelInput {
-            fair_l2_gas_price: value.fair_l2_gas_price,
-            fair_pubdata_price: value.fair_pubdata_price,
-            l1_gas_price: value.l1_gas_price,
-        }
-    }
-}
-
-/// Shadowed struct of [`BatchFeeInput`]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-enum V1BatchFeeInput {
-    L1Pegged(V1L1PeggedBatchFeeModelInput),
-    PubdataIndependent(V1PubdataIndependentBatchFeeModelInput),
-}
-
-impl From<V1BatchFeeInput> for BatchFeeInput {
-    fn from(value: V1BatchFeeInput) -> Self {
-        match value {
-            V1BatchFeeInput::L1Pegged(input) => BatchFeeInput::L1Pegged(input.into()),
-            V1BatchFeeInput::PubdataIndependent(input) => {
-                BatchFeeInput::PubdataIndependent(input.into())
-            }
-        }
-    }
-}
-
-impl From<BatchFeeInput> for V1BatchFeeInput {
-    fn from(value: BatchFeeInput) -> Self {
-        match value {
-            BatchFeeInput::L1Pegged(input) => V1BatchFeeInput::L1Pegged(input.into()),
-            BatchFeeInput::PubdataIndependent(input) => {
-                V1BatchFeeInput::PubdataIndependent(input.into())
-            }
-        }
-    }
-}
-
-/// Shadowed struct of [`L2BlockEnv`]
-#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
-struct V1L2BlockEnv {
-    number: u32,
-    timestamp: u64,
-    prev_block_hash: H256,
-    max_virtual_blocks_to_create: u32,
-}
-
-impl From<V1L2BlockEnv> for L2BlockEnv {
-    fn from(value: V1L2BlockEnv) -> Self {
-        L2BlockEnv {
-            number: value.number,
-            timestamp: value.timestamp,
-            prev_block_hash: value.prev_block_hash,
-            max_virtual_blocks_to_create: value.max_virtual_blocks_to_create,
-        }
-    }
-}
-
-impl From<L2BlockEnv> for V1L2BlockEnv {
-    fn from(value: L2BlockEnv) -> Self {
-        V1L2BlockEnv {
-            number: value.number,
-            timestamp: value.timestamp,
-            prev_block_hash: value.prev_block_hash,
-            max_virtual_blocks_to_create: value.max_virtual_blocks_to_create,
-        }
-    }
-}
-
-/// Shadowed struct of [`L1BatchEnv`]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct V1L1BatchEnv {
-    previous_batch_hash: Option<H256>,
-    number: L1BatchNumber,
-    timestamp: u64,
-    fee_input: V1BatchFeeInput,
-    fee_account: Address,
-    enforced_base_fee: Option<u64>,
-    first_l2_block: V1L2BlockEnv,
-}
-
-impl From<V1L1BatchEnv> for L1BatchEnv {
-    fn from(value: V1L1BatchEnv) -> Self {
-        let V1L1BatchEnv {
-            previous_batch_hash,
-            number,
-            timestamp,
-            fee_input,
-            fee_account,
-            enforced_base_fee,
-            first_l2_block,
-        } = value;
-
-        L1BatchEnv {
-            previous_batch_hash,
-            number,
-            timestamp,
-            fee_input: fee_input.into(),
-            fee_account,
-            enforced_base_fee,
-            first_l2_block: first_l2_block.into(),
-        }
-    }
-}
-
-impl From<L1BatchEnv> for V1L1BatchEnv {
-    fn from(value: L1BatchEnv) -> Self {
-        let L1BatchEnv {
-            previous_batch_hash,
-            number,
-            timestamp,
-            fee_input,
-            fee_account,
-            enforced_base_fee,
-            first_l2_block,
-        } = value;
-
-        V1L1BatchEnv {
-            previous_batch_hash,
-            number,
-            timestamp,
-            fee_input: fee_input.into(),
-            fee_account,
-            enforced_base_fee,
-            first_l2_block: first_l2_block.into(),
-        }
-    }
-}
-
-/// Shadowed struct of [`SystemContractCode`]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct V1SystemContractCode {
-    code: Vec<U256>,
-    hash: H256,
-}
-
-impl From<V1SystemContractCode> for SystemContractCode {
-    fn from(value: V1SystemContractCode) -> Self {
-        let V1SystemContractCode { code, hash } = value;
-        SystemContractCode { code, hash }
-    }
-}
-
-impl From<SystemContractCode> for V1SystemContractCode {
-    fn from(value: SystemContractCode) -> Self {
-        let SystemContractCode { code, hash } = value;
-        V1SystemContractCode { code, hash }
-    }
-}
-
-/// Shadowed struct of [`BaseSystemContracts`]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct V1BaseSystemContracts {
-    bootloader: V1SystemContractCode,
-    default_aa: V1SystemContractCode,
-}
-
-impl From<V1BaseSystemContracts> for BaseSystemContracts {
-    fn from(value: V1BaseSystemContracts) -> Self {
-        let V1BaseSystemContracts {
-            bootloader,
-            default_aa,
-        } = value;
-
-        BaseSystemContracts {
-            bootloader: bootloader.into(),
-            default_aa: default_aa.into(),
-        }
-    }
-}
-
-impl From<BaseSystemContracts> for V1BaseSystemContracts {
-    fn from(value: BaseSystemContracts) -> Self {
-        let BaseSystemContracts {
-            bootloader,
-            default_aa,
-        } = value;
-
-        V1BaseSystemContracts {
-            bootloader: bootloader.into(),
-            default_aa: default_aa.into(),
-        }
-    }
-}
-
-/// Shadowed struct of [`SystemEnv`]
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct V1SystemEnv {
-    // Always false for VM
-    zk_porter_available: bool,
-    version: ProtocolVersionId,
-    base_system_smart_contracts: V1BaseSystemContracts,
-    bootloader_gas_limit: u32,
-    execution_mode: V1TxExecutionMode,
-    default_validation_computational_gas_limit: u32,
-    chain_id: L2ChainId,
-}
-
-impl From<V1SystemEnv> for SystemEnv {
-    fn from(value: V1SystemEnv) -> Self {
-        let V1SystemEnv {
-            zk_porter_available,
-            version,
-            base_system_smart_contracts,
-            bootloader_gas_limit,
-            execution_mode,
-            default_validation_computational_gas_limit,
-            chain_id,
-        } = value;
-
-        SystemEnv {
-            default_validation_computational_gas_limit,
-            chain_id,
-            zk_porter_available,
-            version,
-            execution_mode: execution_mode.into(),
-            base_system_smart_contracts: base_system_smart_contracts.into(),
-            bootloader_gas_limit,
-        }
-    }
-}
-
-impl From<SystemEnv> for V1SystemEnv {
-    fn from(value: SystemEnv) -> Self {
-        let SystemEnv {
-            zk_porter_available,
-            version,
-            base_system_smart_contracts,
-            bootloader_gas_limit,
-            execution_mode,
-            default_validation_computational_gas_limit,
-            chain_id,
-        } = value;
-
-        V1SystemEnv {
-            default_validation_computational_gas_limit,
-            chain_id,
-            zk_porter_available,
-            version,
-            execution_mode: execution_mode.into(),
-            base_system_smart_contracts: base_system_smart_contracts.into(),
-            bootloader_gas_limit,
-        }
-    }
-}
-
-/// Shadowed struct of [`TxExecutionMode`]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
-enum V1TxExecutionMode {
-    VerifyExecute,
-    EstimateFee,
-    EthCall,
-}
-
-impl From<V1TxExecutionMode> for TxExecutionMode {
-    fn from(value: V1TxExecutionMode) -> Self {
-        match value {
-            V1TxExecutionMode::VerifyExecute => TxExecutionMode::VerifyExecute,
-            V1TxExecutionMode::EstimateFee => TxExecutionMode::EstimateFee,
-            V1TxExecutionMode::EthCall => TxExecutionMode::EthCall,
-        }
-    }
-}
-
-impl From<TxExecutionMode> for V1TxExecutionMode {
-    fn from(value: TxExecutionMode) -> Self {
-        match value {
-            TxExecutionMode::VerifyExecute => V1TxExecutionMode::VerifyExecute,
-            TxExecutionMode::EstimateFee => V1TxExecutionMode::EstimateFee,
-            TxExecutionMode::EthCall => V1TxExecutionMode::EthCall,
-        }
-    }
-}
 
 /// Version 1 of the data used as input for the TEE verifier.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct V1TeeVerifierInput {
     prepare_basic_circuits_job: PrepareBasicCircuitsJob,
-    l2_blocks_execution_data: Vec<V1L2BlockExecutionData>,
-    l1_batch_env: V1L1BatchEnv,
-    system_env: V1SystemEnv,
+    l2_blocks_execution_data: Vec<L2BlockExecutionData>,
+    l1_batch_env: L1BatchEnv,
+    system_env: SystemEnv,
     used_contracts: Vec<(H256, Vec<u8>)>,
 }
 
@@ -419,12 +57,9 @@ impl TeeVerifierInput {
     ) -> Self {
         TeeVerifierInput::V1(V1TeeVerifierInput {
             prepare_basic_circuits_job,
-            l2_blocks_execution_data: l2_blocks_execution_data
-                .into_iter()
-                .map(|v| v.into())
-                .collect(),
-            l1_batch_env: l1_batch_env.into(),
-            system_env: system_env.into(),
+            l2_blocks_execution_data,
+            l1_batch_env,
+            system_env,
             used_contracts,
         })
     }
@@ -470,12 +105,7 @@ impl TeeVerifierInput {
 
         let storage_view = Rc::new(RefCell::new(StorageView::new(&raw_storage)));
 
-        let vm = VmInstance::new(l1_batch_env.into(), system_env.into(), storage_view);
-
-        let l2_blocks_execution_data = l2_blocks_execution_data
-            .into_iter()
-            .map(|v| v.into())
-            .collect();
+        let vm = VmInstance::new(l1_batch_env, system_env, storage_view);
 
         let vm_out = Self::execute_vm(l2_blocks_execution_data, vm)?;
 
@@ -659,6 +289,10 @@ impl StoredObject for TeeVerifierInput {
 
 #[cfg(test)]
 mod tests {
+    use multivm::interface::TxExecutionMode;
+    use zksync_basic_types::U256;
+    use zksync_contracts::{BaseSystemContracts, SystemContractCode};
+
     use super::*;
 
     #[test]
