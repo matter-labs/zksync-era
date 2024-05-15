4bb569e6ff198bfd5ec45624e54c8c4f21f42393
Lech <88630083+Artemka374@users.noreply.github.com>
2024-05-15 16:18:47 +0300

    remove typo

--- core/node/house_keeper/src/metrics.rs
+++ core/node/house_keeper/src/metrics.rs
@@ -105,7 +105,7 @@ pub(crate) struct ServerMetrics {
     pub fri_witness_generator_jobs_by_round: LabeledFamily<(&'static str, String), Gauge<u64>, 2>,
     pub leaf_fri_witness_generator_waiting_to_queued_jobs_transitions: Counter<u64>,
     pub node_fri_witness_generator_waiting_to_queued_jobs_transitions: Counter<u64>,
-    pub recursion_tip_witness_generator_waiting_to_queued_jobspostgres_transitions: Counter<u64>,
+    pub recursion_tip_witness_generator_waiting_to_queued_jobs_transitions: Counter<u64>,
     pub scheduler_witness_generator_waiting_to_queued_jobs_transitions: Counter<u64>,
 }
 
