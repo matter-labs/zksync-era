2947a7526a251359675e2344b7cf93152d303d38
Lech <88630083+Artemka374@users.noreply.github.com>
2024-05-15 16:39:25 +0300

    fix name

--- core/node/house_keeper/src/metrics.rs
+++ core/node/house_keeper/src/metrics.rs
@@ -102,7 +102,7 @@ pub(crate) struct ServerMetrics {
     pub prover_fri_requeued_jobs: Counter<u64>,
     pub requeued_jobs: Family<WitnessType, Counter<u64>>,
     #[metrics(labels = ["type", "round"])]
-    pub fri_witness_generator_jobs_by_round: LabeledFamily<(&'static str, String), Gauge<u64>, 2>,
+    pub fri_witness_generator_jobs: LabeledFamily<(&'static str, String), Gauge<u64>, 2>,
     pub leaf_fri_witness_generator_waiting_to_queued_jobs_transitions: Counter<u64>,
     pub node_fri_witness_generator_waiting_to_queued_jobs_transitions: Counter<u64>,
     pub recursion_tip_witness_generator_waiting_to_queued_jobs_transitions: Counter<u64>,
