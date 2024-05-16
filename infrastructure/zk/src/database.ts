aa0b46cd37b1db31f422c743522a3964fcc37fa2
tomg10 <lemures64@gmail.com>
2024-05-16 23:22:22 +0200

    zk fmt
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- infrastructure/zk/src/database.ts
+++ infrastructure/zk/src/database.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander';
 import * as utils from './utils';
-import path from "path";
+import path from 'path';
 
 export async function reset(opts: DbOpts) {
     await utils.confirmAction();
@@ -87,7 +87,7 @@ export async function drop(opts: DbOpts) {
 
 async function migrateForDal(dalPath: DalPath, dbUrl: string) {
     console.log(`Running migrations for ${dalPath}...`);
-    const migrationsDir = path.join(dalPath, "migrations")
+    const migrationsDir = path.join(dalPath, 'migrations');
     await utils.spawn(
         `cargo sqlx database create --source ${migrationsDir} --database-url ${dbUrl} && cargo sqlx migrate run  --source ${migrationsDir}--database-url ${dbUrl}`
     );
