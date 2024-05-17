0e247e505ee281f90413abb0e2c6c4697a9536c9
aon <21188659+aon@users.noreply.github.com>
2024-05-16 21:21:07 -0300

    feat: add ci and fix args

--- zk_toolbox/crates/zk_inception/src/commands/hyperchain/args/create.rs
+++ zk_toolbox/crates/zk_inception/src/commands/hyperchain/args/create.rs
@@ -17,7 +17,7 @@ use crate::{
 pub struct HyperchainCreateArgs {
     #[arg(long)]
     pub hyperchain_name: Option<String>,
-    #[arg(value_parser = clap::value_parser!(u32).range(1..))]
+    #[arg(long, value_parser = clap::value_parser!(u32).range(1..))]
     pub chain_id: Option<u32>,
     #[clap(long, help = "Prover options", value_enum)]
     pub prover_mode: Option<ProverMode>,
@@ -28,10 +28,10 @@ pub struct HyperchainCreateArgs {
     #[clap(long, help = "Commit data generation mode")]
     pub l1_batch_commit_data_generator_mode: Option<L1BatchCommitDataGeneratorMode>,
     #[clap(long, help = "Base token address")]
-    pub base_token_address: Option<String>,
-    #[clap(long, help = "Base token nominator")]
+    pub base_token_address: Option<H160>,
+    #[clap(long, help = "Base token nominator", value_parser = clap::value_parser!(u64).range(1..))]
     pub base_token_price_nominator: Option<u64>,
-    #[clap(long, help = "Base token denominator")]
+    #[clap(long, help = "Base token denominator", value_parser = clap::value_parser!(u64).range(1..))]
     pub base_token_price_denominator: Option<u64>,
     #[clap(long, help = "Set as default hyperchain", default_missing_value = "true", num_args = 0..=1)]
     pub set_as_default: Option<bool>,
@@ -49,20 +49,26 @@ impl HyperchainCreateArgs {
                 .ask()
         });
 
-        let wallet_creation = PromptSelect::new(
-            "Select how do you want to create the wallet",
-            WalletCreation::iter(),
-        )
-        .ask();
+        let prover_version = self.prover_mode.unwrap_or_else(|| {
+            PromptSelect::new("Select the prover version", ProverMode::iter()).ask()
+        });
 
-        let prover_version =
-            PromptSelect::new("Select the prover version", ProverMode::iter()).ask();
+        let l1_batch_commit_data_generator_mode =
+            self.l1_batch_commit_data_generator_mode.unwrap_or_else(|| {
+                PromptSelect::new(
+                    "Select the commit data generator mode",
+                    L1BatchCommitDataGeneratorMode::iter(),
+                )
+                .ask()
+            });
 
-        let l1_batch_commit_data_generator_mode = PromptSelect::new(
-            "Select the commit data generator mode",
-            L1BatchCommitDataGeneratorMode::iter(),
-        )
-        .ask();
+        let wallet_creation = self.wallet_creation.clone().unwrap_or_else(|| {
+            PromptSelect::new(
+                "Select how do you want to create the wallet",
+                WalletCreation::iter(),
+            )
+            .ask()
+        });
 
         let wallet_path: Option<PathBuf> = if self.wallet_creation == Some(WalletCreation::InFile) {
             Some(self.wallet_path.unwrap_or_else(|| {
@@ -78,31 +84,45 @@ impl HyperchainCreateArgs {
             None
         };
 
-        let base_token_selection =
-            PromptSelect::new("Select the base token to use", BaseTokenSelection::iter()).ask();
-        let base_token = match base_token_selection {
-            BaseTokenSelection::Eth => BaseToken::eth(),
-            BaseTokenSelection::Custom => {
-                let number_validator = |val: &String| -> Result<(), String> {
-                    let Ok(val) = val.parse::<u64>() else {
-                        return Err("Numer is not zero".to_string());
-                    };
-                    if val == 0 {
-                        return Err("Number should be greater than 0".to_string());
-                    }
-                    Ok(())
-                };
-                let address: H160 = Prompt::new("What is the base token address?").ask();
-                let nominator = Prompt::new("What is the base token price nominator?")
-                    .validate_with(number_validator)
-                    .ask();
-                let denominator = Prompt::new("What is the base token price denominator?")
-                    .validate_with(number_validator)
-                    .ask();
+        let base_token = {
+            if self.base_token_address.is_some()
+                && self.base_token_price_nominator.is_some()
+                && self.base_token_price_denominator.is_some()
+            {
                 BaseToken {
-                    address,
-                    nominator,
-                    denominator,
+                    address: self.base_token_address.unwrap(),
+                    nominator: self.base_token_price_nominator.unwrap(),
+                    denominator: self.base_token_price_denominator.unwrap(),
+                }
+            } else {
+                let base_token_selection =
+                    PromptSelect::new("Select the base token to use", BaseTokenSelection::iter())
+                        .ask();
+                match base_token_selection {
+                    BaseTokenSelection::Eth => BaseToken::eth(),
+                    BaseTokenSelection::Custom => {
+                        let number_validator = |val: &String| -> Result<(), String> {
+                            let Ok(val) = val.parse::<u64>() else {
+                                return Err("Numer is not zero".to_string());
+                            };
+                            if val == 0 {
+                                return Err("Number should be greater than 0".to_string());
+                            }
+                            Ok(())
+                        };
+                        let address: H160 = Prompt::new("What is the base token address?").ask();
+                        let nominator = Prompt::new("What is the base token price nominator?")
+                            .validate_with(number_validator)
+                            .ask();
+                        let denominator = Prompt::new("What is the base token price denominator?")
+                            .validate_with(number_validator)
+                            .ask();
+                        BaseToken {
+                            address,
+                            nominator,
+                            denominator,
+                        }
+                    }
                 }
             }
         };
