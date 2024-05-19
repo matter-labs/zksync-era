d3c3063a267e48b6969294ed630998777c335128
tomg10 <lemures64@gmail.com>
2024-05-19 17:36:59 +0200

    build zk-environment-full
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- bin/restore_yarn_cache.sh
+++ bin/restore_yarn_cache.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for node_dir in . etc/system-contracts etc/contracts-test-data contracts;
+for node_dir in . etc/contracts-test-data contracts;
 do
   mkdir -p $node_dir
   cp -r ../node_modules_backup/$node_dir $node_dir/node_modules
