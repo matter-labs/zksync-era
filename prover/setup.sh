e19ac67569ab3d7b2575d7fa2080640db2e15399
Lech <88630083+Artemka374@users.noreply.github.com>
2024-05-20 16:06:24 +0300

    update images

--- prover/setup.sh
+++ prover/setup.sh
@@ -18,7 +18,7 @@ sed -i.backup 's/^proof_loading_mode=.*$/proof_loading_mode="FriProofFromGcs"/' 
 rm ../etc/env/base/eth_sender.toml.backup
 sed -i.backup 's/^setup_data_path=.*$/setup_data_path="vk_setup_data_generator_server_fri\/data\/"/' ../etc/env/base/fri_prover.toml
 rm ../etc/env/base/fri_prover.toml.backup
-sed -i.backup 's/^universal_setup_path=.*$/universal_setup_path="..\/keys\/setup\/setup_2^26.key"/' ../etc/env/base/fri_proof_compressor.toml
+sed -i.backup 's/^universal_setup_path=.*$/universal_setup_path="..\/keys\/setup\/setup_2^24.key"/' ../etc/env/base/fri_proof_compressor.toml
 rm ../etc/env/base/fri_proof_compressor.toml.backup
 
 zk config compile dev
