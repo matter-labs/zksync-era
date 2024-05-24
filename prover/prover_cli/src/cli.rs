c6643be9f81882b901f296dee18778c005b9f46d
mm <mm@matterlabs.dev>
2024-05-24 19:55:32 +0200

    Tool for prover debugging

--- prover/prover_cli/src/cli.rs
+++ prover/prover_cli/src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{command, Args, Parser, Subcommand};
 use zksync_types::url::SensitiveUrl;
 
-use crate::commands::{self, delete, get_file_info, requeue, restart};
+use crate::commands::{self, debug_proof, delete, get_file_info, requeue, restart};
 
 pub const VERSION_STRING: &str = env!("CARGO_PKG_VERSION");
 
@@ -28,6 +28,7 @@ pub struct ProverCLIConfig {
 
 #[derive(Subcommand)]
 enum ProverCommand {
+    DebugProof(debug_proof::Args),
     FileInfo(get_file_info::Args),
     Delete(delete::Args),
     #[command(subcommand)]
@@ -44,6 +45,7 @@ pub async fn start() -> anyhow::Result<()> {
         ProverCommand::Status(cmd) => cmd.run(config).await?,
         ProverCommand::Requeue(args) => requeue::run(args, config).await?,
         ProverCommand::Restart(args) => restart::run(args).await?,
+        ProverCommand::DebugProof(args) => debug_proof::run(args).await?,
     };
 
     Ok(())
