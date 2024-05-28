9d496cfdcd0c13499e697368d4c2b54318e280f6
Danil <deniallugo@gmail.com>
2024-05-28 17:09:48 +0200

    Do not use weth as the main token
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/tests/ts-integration/src/env.ts
+++ core/tests/ts-integration/src/env.ts
@@ -80,9 +80,12 @@ async function loadTestEnvironmentFromFile(chain: string): Promise<TestEnvironme
     // wBTC is chosen because it has decimals different from ETH (8 instead of 18).
     // Using this token will help us to detect decimals-related errors.
     // but if it's not available, we'll use the first token from the list.
-    let token = tokens.tokens['wBTC'];
+    let token = tokens.tokens['WBTC'];
     if (token === undefined) {
         token = Object.values(tokens.tokens)[0];
+        if (token.symbol == 'WETH') {
+            token = Object.values(tokens.tokens)[1];
+        }
     }
     const weth = tokens.tokens['WETH'];
     let baseToken;
