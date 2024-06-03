1ce8c91b6f567e83c37b068912ac0cc770f9fbcf
Daniel Lumi <149794418+zk-Lumi@users.noreply.github.com>
2024-06-03 12:45:56 +0200

    fix(zk_toolbox): make ecosystem configuration not found error more clear

--- zk_toolbox/crates/config/src/ecosystem.rs
+++ zk_toolbox/crates/config/src/ecosystem.rs
@@ -201,7 +201,7 @@ impl EcosystemConfig {
 /// Result of checking if the ecosystem exists.
 #[derive(Error, Debug)]
 pub enum EcosystemConfigFromFileError {
-    #[error("Ecosystem configuration not found")]
+    #[error("Ecosystem configuration not found (make sure you have created one `zk_inception ecosystem create` + are in the ecosystem folder `cd path/to/ecosystem/name`)")]
     NotExists,
     #[error("Invalid ecosystem configuration")]
     InvalidConfig { source: anyhow::Error },
