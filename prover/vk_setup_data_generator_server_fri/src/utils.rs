87daf90a9c0a6aed70599e8eef4f0ae8bb20b5b5
mm <mm@matterlabs.dev>
2024-06-05 13:31:19 +0200

    clippy

--- prover/vk_setup_data_generator_server_fri/src/utils.rs
+++ prover/vk_setup_data_generator_server_fri/src/utils.rs
@@ -136,8 +136,7 @@ mod tests {
 
         for entry in std::fs::read_dir(path_to_input.clone()).unwrap().flatten() {
             if entry.metadata().unwrap().is_dir() {
-                let basepath =
-                    path_to_input.join(entry.file_name().into_string().unwrap().to_string());
+                let basepath = path_to_input.join(&entry.file_name().into_string().unwrap());
                 let keystore = Keystore::new_with_optional_setup_path(basepath.clone(), None);
 
                 let expected =
