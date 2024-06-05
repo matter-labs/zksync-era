0d21e6e9c99fd899f7ceb603301f623d16897f43
Patrick Beza <pab@matterlabs.dev>
2024-06-05 17:58:24 +0200

    Propagate errors instead of using unwrap()

--- core/node/proof_data_handler/src/tee_request_processor.rs
+++ core/node/proof_data_handler/src/tee_request_processor.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use axum::{extract::Path, Json};
 use zksync_config::configs::ProofDataHandlerConfig;
-use zksync_dal::{tee_proof_generation_dal::TeeType, ConnectionPool, Core, CoreDal};
+use zksync_dal::{tee_proof_generation_dal::TeeType, ConnectionPool, Core, CoreDal, SqlxError};
 use zksync_object_store::ObjectStore;
 use zksync_prover_interface::api::{
     GenericProofGenerationDataResponse, RegisterTeeAttestationRequest,
@@ -42,15 +42,16 @@ impl TeeRequestProcessor {
     ) -> Result<Json<TeeProofGenerationDataResponse>, RequestProcessorError> {
         tracing::info!("Received request for proof generation data: {:?}", request);
 
-        let l1_batch_number_result = self
+        let mut connection = self
             .pool
             .connection()
             .await
-            .unwrap()
+            .map_err(|_| RequestProcessorError::Sqlx(SqlxError::PoolClosed))?;
+
+        let l1_batch_number_result = connection
             .tee_proof_generation_dal()
             .get_next_block_to_be_proven(self.config.proof_generation_timeout())
             .await;
-
         let l1_batch_number = match l1_batch_number_result {
             Some(number) => number,
             None => return Ok(Json(TeeProofGenerationDataResponse::Success(None))),
@@ -73,7 +74,11 @@ impl TeeRequestProcessor {
         Json(payload): Json<SubmitTeeProofRequest>,
     ) -> Result<Json<SubmitProofResponse>, RequestProcessorError> {
         let l1_batch_number = L1BatchNumber(l1_batch_number);
-        let mut connection = self.pool.connection().await.unwrap();
+        let mut connection = self
+            .pool
+            .connection()
+            .await
+            .map_err(|_| RequestProcessorError::Sqlx(SqlxError::PoolClosed))?;
         let mut dal = connection.tee_proof_generation_dal();
 
         match payload {
@@ -113,7 +118,11 @@ impl TeeRequestProcessor {
     ) -> Result<Json<RegisterTeeAttestationResponse>, RequestProcessorError> {
         tracing::info!("Received attestation: {:?}", payload);
 
-        let mut connection = self.pool.connection().await.unwrap();
+        let mut connection = self
+            .pool
+            .connection()
+            .await
+            .map_err(|_| RequestProcessorError::Sqlx(SqlxError::PoolClosed))?;
         let mut dal = connection.tee_proof_generation_dal();
 
         dal.save_attestation(&payload.pubkey, &payload.attestation, &payload.valid_until)
