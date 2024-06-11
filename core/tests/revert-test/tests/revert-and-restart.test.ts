df170cef700953f204fecce5eb97610aae37a07e
Bence Haromi <bence.haromi@gmail.com>
2024-06-11 12:26:25 +0200

    chore(revert-test): zksync-ethers v6

--- core/tests/revert-test/tests/revert-and-restart.test.ts
+++ core/tests/revert-test/tests/revert-and-restart.test.ts
@@ -1,9 +1,10 @@
 import * as utils from 'utils';
 import { Tester } from './tester';
 import * as zksync from 'zksync-ethers';
-import { BigNumber, Contract, ethers } from 'ethers';
+import * as ethers from 'ethers';
 import { expect } from 'chai';
 import fs from 'fs';
+import { IZkSyncStateTransition } from 'zksync-ethers/build/typechain';
 
 // Parses output of "print-suggested-values" command of the revert block tool.
 function parseSuggestedValues(suggestedValuesString: string) {
@@ -51,13 +52,13 @@ function ignoreError(_err: any, context?: string) {
     console.info(message);
 }
 
-const depositAmount = ethers.utils.parseEther('0.001');
+const depositAmount = ethers.parseEther('0.001');
 
 describe('Block reverting test', function () {
     let tester: Tester;
     let alice: zksync.Wallet;
-    let mainContract: Contract;
-    let blocksCommittedBeforeRevert: number;
+    let mainContract: IZkSyncStateTransition;
+    let blocksCommittedBeforeRevert: bigint;
     let logs: fs.WriteStream;
     let operatorAddress = process.env.ETH_SENDER_SENDER_OPERATOR_COMMIT_ETH_ADDR;
 
@@ -133,19 +134,19 @@ describe('Block reverting test', function () {
         }
 
         const balance = await alice.getBalance();
-        expect(balance.eq(depositAmount.mul(2)), 'Incorrect balance after deposits').to.be.true;
+        expect(balance === depositAmount * 2n, 'Incorrect balance after deposits').to.be.true;
 
         // Check L1 committed and executed blocks.
         let blocksCommitted = await mainContract.getTotalBatchesCommitted();
         let blocksExecuted = await mainContract.getTotalBatchesExecuted();
         let tryCount = 0;
-        while (blocksCommitted.eq(blocksExecuted) && tryCount < 100) {
+        while (blocksCommitted === blocksExecuted && tryCount < 100) {
             blocksCommitted = await mainContract.getTotalBatchesCommitted();
             blocksExecuted = await mainContract.getTotalBatchesExecuted();
             tryCount += 1;
             await utils.sleep(1);
         }
-        expect(blocksCommitted.gt(blocksExecuted), 'There is no committed but not executed block').to.be.true;
+        expect(blocksCommitted > blocksExecuted, 'There is no committed but not executed block').to.be.true;
         blocksCommittedBeforeRevert = blocksCommitted;
 
         // Stop server.
@@ -178,7 +179,7 @@ describe('Block reverting test', function () {
         );
 
         let blocksCommitted = await mainContract.getTotalBatchesCommitted();
-        expect(blocksCommitted.eq(lastL1BatchNumber), 'Revert on contract was unsuccessful').to.be.true;
+        expect(blocksCommitted === lastL1BatchNumber, 'Revert on contract was unsuccessful').to.be.true;
     });
 
     step('execute transaction after revert', async () => {
@@ -190,7 +191,7 @@ describe('Block reverting test', function () {
         await utils.sleep(10);
 
         const balanceBefore = await alice.getBalance();
-        expect(balanceBefore.eq(depositAmount.mul(2)), 'Incorrect balance after revert').to.be.true;
+        expect(balanceBefore === depositAmount * 2n, 'Incorrect balance after revert').to.be.true;
 
         // Execute a transaction
         const depositHandle = await tester.syncWallet.deposit({
@@ -220,13 +221,12 @@ describe('Block reverting test', function () {
         expect(receipt.status).to.be.eql(1);
 
         const balanceAfter = await alice.getBalance();
-        expect(balanceAfter.eq(BigNumber.from(depositAmount).mul(3)), 'Incorrect balance after another deposit').to.be
-            .true;
+        expect(balanceAfter === depositAmount * 3n, 'Incorrect balance after another deposit').to.be.true;
     });
 
     step('execute transactions after simple restart', async () => {
         // Execute an L2 transaction
-        await checkedRandomTransfer(alice, BigNumber.from(1));
+        await checkedRandomTransfer(alice, 1n);
 
         // Stop server.
         await killServerAndWaitForShutdown(tester);
@@ -236,7 +236,7 @@ describe('Block reverting test', function () {
         await utils.sleep(10);
 
         // Trying to send a transaction from the same address again
-        await checkedRandomTransfer(alice, BigNumber.from(1));
+        await checkedRandomTransfer(alice, 1n);
     });
 
     after('Try killing server', async () => {
@@ -244,9 +244,10 @@ describe('Block reverting test', function () {
     });
 });
 
-async function checkedRandomTransfer(sender: zksync.Wallet, amount: BigNumber) {
+async function checkedRandomTransfer(sender: zksync.Wallet, amount: bigint) {
     const senderBalanceBefore = await sender.getBalance();
-    const receiver = zksync.Wallet.createRandom().connect(sender.provider);
+    const receiverHD = zksync.Wallet.createRandom();
+    const receiver = new zksync.Wallet(receiverHD.privateKey, sender.provider);
     const transferHandle = await sender.sendTransaction({
         to: receiver.address,
         value: amount,
@@ -263,9 +264,8 @@ async function checkedRandomTransfer(sender: zksync.Wallet, amount: BigNumber) {
     const senderBalance = await sender.getBalance();
     const receiverBalance = await receiver.getBalance();
 
-    expect(receiverBalance.eq(amount), 'Failed updated the balance of the receiver').to.be.true;
+    expect(receiverBalance === amount, 'Failed updated the balance of the receiver').to.be.true;
 
-    const spentAmount = txReceipt.gasUsed.mul(transferHandle.gasPrice!).add(amount);
-    expect(senderBalance.add(spentAmount).gte(senderBalanceBefore), 'Failed to update the balance of the sender').to.be
-        .true;
+    const spentAmount = txReceipt.gasUsed * transferHandle.gasPrice! + amount;
+    expect(senderBalance + spentAmount >= senderBalanceBefore, 'Failed to update the balance of the sender').to.be.true;
 }
--- core/tests/revert-test/tests/tester.ts
+++ core/tests/revert-test/tests/tester.ts
@@ -4,12 +4,12 @@ import * as zksync from 'zksync-ethers';
 import * as fs from 'fs';
 import * as path from 'path';
 
-const BASE_ERC20_TO_MINT = ethers.utils.parseEther('100');
+const BASE_ERC20_TO_MINT = ethers.parseEther('100');
 
 export class Tester {
-    public runningFee: Map<zksync.types.Address, ethers.BigNumber>;
+    public runningFee: Map<zksync.types.Address, bigint>;
     constructor(
-        public ethProvider: ethers.providers.Provider,
+        public ethProvider: ethers.Provider,
         public ethWallet: ethers.Wallet,
         public syncWallet: zksync.Wallet,
         public web3Provider: zksync.Provider,
@@ -22,19 +22,21 @@ export class Tester {
 
     // prettier-ignore
     static async init(l1_rpc_addr: string, l2_rpc_addr: string) : Promise<Tester> {
-        const ethProvider = new ethers.providers.JsonRpcProvider(l1_rpc_addr);
+        const ethProvider = new ethers.JsonRpcProvider(l1_rpc_addr);
         ethProvider.pollingInterval = 100;
 
         const testConfigPath = path.join(process.env.ZKSYNC_HOME!, `etc/test_config/constant`);
         const ethTestConfig = JSON.parse(fs.readFileSync(`${testConfigPath}/eth.json`, { encoding: 'utf-8' }));
-        let ethWallet = ethers.Wallet.fromMnemonic(
-            ethTestConfig.test_mnemonic as string,
+        const ethWalletHD = ethers.HDNodeWallet.fromMnemonic(
+            ethers.Mnemonic.fromPhrase(ethTestConfig.test_mnemonic),
             "m/44'/60'/0'/0/0"
-        ).connect(ethProvider);
-        let hyperchainAdmin = ethers.Wallet.fromMnemonic(
-            ethTestConfig.mnemonic as string,
+        );
+        const ethWallet = new ethers.Wallet(ethWalletHD.privateKey, ethProvider);
+        const hyperchainAdminHD = ethers.HDNodeWallet.fromMnemonic(
+            ethers.Mnemonic.fromPhrase(ethTestConfig.mnemonic),
             "m/44'/60'/0'/0/1"
-        ).connect(ethProvider);
+        );
+        const hyperchainAdmin = new ethers.Wallet(hyperchainAdminHD.privateKey, ethProvider);
         const web3Provider = new zksync.Provider(l2_rpc_addr);
         web3Provider.pollingInterval = 100; // It's OK to keep it low even on stage.
         const syncWallet = new zksync.Wallet(ethWallet.privateKey, web3Provider, ethProvider);
@@ -42,16 +44,16 @@ export class Tester {
 
         // Since some tx may be pending on stage, we don't want to get stuck because of it.
         // In order to not get stuck transactions, we manually cancel all the pending txs.
-        const latestNonce = await ethWallet.getTransactionCount('latest');
-        const pendingNonce = await ethWallet.getTransactionCount('pending');
+        const latestNonce = await ethWallet.getNonce('latest');
+        const pendingNonce = await ethWallet.getNonce('pending');
         const cancellationTxs = [];
         for (let nonce = latestNonce; nonce != pendingNonce; nonce++) {
             // For each transaction to override it, we need to provide greater fee. 
             // We would manually provide a value high enough (for a testnet) to be both valid
             // and higher than the previous one. It's OK as we'll only be charged for the bass fee
             // anyways. We will also set the miner's tip to 5 gwei, which is also much higher than the normal one.
-            const maxFeePerGas = ethers.utils.parseEther("0.00000025"); // 250 gwei
-            const maxPriorityFeePerGas = ethers.utils.parseEther("0.000000005"); // 5 gwei
+            const maxFeePerGas = ethers.parseEther("0.00000025"); // 250 gwei
+            const maxPriorityFeePerGas = ethers.parseEther("0.000000005"); // 5 gwei
             cancellationTxs.push(ethWallet.sendTransaction({ to: ethWallet.address, nonce, maxFeePerGas, maxPriorityFeePerGas }).then((tx) => tx.wait()));
         }
         if (cancellationTxs.length > 0) {
@@ -77,15 +79,12 @@ export class Tester {
         }
     }
 
-    async fundedWallet(
-        ethAmount: ethers.BigNumberish,
-        l1Token: zksync.types.Address,
-        tokenAmount: ethers.BigNumberish
-    ) {
-        const newWallet = zksync.Wallet.createRandom().connect(this.web3Provider).connectToL1(this.ethProvider);
+    async fundedWallet(ethAmount: bigint, l1Token: zksync.types.Address, tokenAmount: bigint) {
+        const newWalletHD = zksync.Wallet.createRandom();
+        const newWallet = new zksync.Wallet(newWalletHD.privateKey, this.web3Provider, this.ethProvider);
 
         let ethBalance = await this.syncWallet.getBalanceL1();
-        expect(ethBalance.gt(ethAmount), 'Insufficient eth balance to create funded wallet').to.be.true;
+        expect(ethBalance > ethAmount, 'Insufficient eth balance to create funded wallet').to.be.true;
 
         // To make the wallet capable of requesting priority operations,
         // send ETH to L1.
@@ -99,7 +98,7 @@ export class Tester {
         // Funds the wallet with L1 token.
 
         let tokenBalance = await this.syncWallet.getBalanceL1(l1Token);
-        expect(tokenBalance.gt(tokenAmount), 'Insufficient token balance to create funded wallet').to.be.true;
+        expect(tokenBalance > tokenAmount, 'Insufficient token balance to create funded wallet').to.be.true;
 
         const erc20ABI = ['function transfer(address to, uint256 amount)'];
         const erc20Contract = new ethers.Contract(l1Token, erc20ABI, this.ethWallet);
@@ -111,6 +110,7 @@ export class Tester {
     }
 
     emptyWallet() {
-        return zksync.Wallet.createRandom().connect(this.web3Provider).connectToL1(this.ethProvider);
+        const walletHD = zksync.Wallet.createRandom();
+        return new zksync.Wallet(walletHD.privateKey, this.web3Provider, this.ethProvider);
     }
 }
