0079f8297815ab3e957429e41eb6e945d2376df8
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-06-14 19:10:43 +0200

    use governance during init

--- infrastructure/zk/src/contract.ts
+++ infrastructure/zk/src/contract.ts
@@ -229,7 +229,8 @@ export async function registerHyperchain({
     const args = [
         privateKey ? `--private-key ${privateKey}` : '',
         baseTokenName ? `--base-token-name ${baseTokenName}` : '',
-        deploymentMode == DeploymentMode.Validium ? '--validium-mode' : ''
+        deploymentMode == DeploymentMode.Validium ? '--validium-mode' : '',
+        '--use-governance'
     ];
     await utils.spawn(`yarn l1-contracts register-hyperchain ${args.join(' ')} | tee registerHyperchain.log`);
     const deployLog = fs.readFileSync('registerHyperchain.log').toString();
