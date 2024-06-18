595e98fbfce65b81aebb06f202d24f69f0678d31
matias-gonz <maigonzalez@fi.uba.ar>
2024-06-18 11:20:59 +0200

    fmt

--- zk_toolbox/crates/zk_inception/src/commands/prover/generate_sk.rs
+++ zk_toolbox/crates/zk_inception/src/commands/prover/generate_sk.rs
@@ -3,9 +3,8 @@ use common::{cmd::Cmd, logger, spinner::Spinner};
 use config::EcosystemConfig;
 use xshell::{cmd, Shell};
 
-use crate::messages::{MSG_GENERATING_SK_SPINNER, MSG_SK_GENERATED};
-
 use super::utils::get_link_to_prover;
+use crate::messages::{MSG_GENERATING_SK_SPINNER, MSG_SK_GENERATED};
 
 pub(crate) async fn run(shell: &Shell) -> anyhow::Result<()> {
     let ecosystem_config = EcosystemConfig::from_file(shell)?;
