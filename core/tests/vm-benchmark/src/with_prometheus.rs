d9fce4576cb24d8b228c908fd336e9d5c2476fc7
Alex Ostrovski <aov@matterlabs.dev>
2024-06-21 11:50:22 +0300

    Update `vm-benchmark`

--- core/tests/vm-benchmark/src/with_prometheus.rs
+++ core/tests/vm-benchmark/src/with_prometheus.rs
@@ -1,23 +1,27 @@
 use std::time::Duration;
 
-use metrics_exporter_prometheus::PrometheusBuilder;
+use tokio::sync::watch;
+use zksync_prometheus_exporter::PrometheusExporterConfig;
 
 pub fn with_prometheus<F: FnOnce()>(f: F) {
+    tokio::runtime::Runtime::new()
+        .unwrap()
+        .block_on(with_prometheus_async(f));
+}
+
+async fn with_prometheus_async<F: FnOnce()>(f: F) {
     println!("Pushing results to Prometheus");
 
     let endpoint =
         "http://vmagent.stage.matterlabs.corp/api/v1/import/prometheus/metrics/job/vm-benchmark";
+    let (stop_sender, stop_receiver) = watch::channel(false);
+    let prometheus_config =
+        PrometheusExporterConfig::push(endpoint.to_owned(), Duration::from_millis(100));
+    tokio::spawn(prometheus_config.run(stop_receiver));
 
-    tokio::runtime::Runtime::new().unwrap().block_on(async {
-        PrometheusBuilder::new()
-            .with_push_gateway(endpoint, Duration::from_millis(100), None, None)
-            .unwrap()
-            .install()
-            .unwrap();
+    f();
 
-        f();
-
-        println!("Waiting for push to happen...");
-        tokio::time::sleep(Duration::from_secs(1)).await;
-    });
+    println!("Waiting for push to happen...");
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    stop_sender.send_replace(true);
 }
