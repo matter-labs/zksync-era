8d40dc7c975676317821726f2cd1cd565d6ff07a
anastasiiaVashchuk <anastacia.vashuck@gmail.com>
2024-06-24 17:37:04 +0300

    remove added migrations

--- core/lib/dal/migrations/20240616204246_remove-l2l1logs-column-for-l1-batches.down.sql
+++
@@ -1,2 +0,0 @@
--- Add down migration script here
-ALTER TABLE l1_batches ADD COLUMN IF NOT EXISTS l2_to_l1_logs;
--- core/lib/dal/migrations/20240616204246_remove-l2l1logs-column-for-l1-batches.up.sql
+++
@@ -1,2 +0,0 @@
--- Add up migration script here
-ALTER TABLE l1_batches DROP COLUMN IF EXISTS l2_to_l1_logs;
