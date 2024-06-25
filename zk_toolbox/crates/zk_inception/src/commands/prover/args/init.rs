19604d295a96da724becc7c34e4c26779cae051c
matias-gonz <maigonzalez@fi.uba.ar>
2024-06-25 11:00:20 +0200

    Fix fill_values_with_prompt

--- zk_toolbox/crates/zk_inception/src/commands/prover/args/init.rs
+++ zk_toolbox/crates/zk_inception/src/commands/prover/args/init.rs
@@ -44,24 +44,24 @@ pub struct CreateGCSBucketConfig {
     pub location: Option<String>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct ProofStorageFileBacked {
     pub proof_store_dir: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct ProofStorageGCS {
     pub bucket_base_url: String,
     pub credentials_file: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub struct ProofStorageGCSCreateBucket {
     pub bucket_name: String,
     pub location: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
 pub enum ProverInitArgsFinal {
     FileBacked(ProofStorageFileBacked),
     GCS(ProofStorageGCS),
@@ -70,13 +70,16 @@ pub enum ProverInitArgsFinal {
 
 impl ProverInitArgs {
     pub(crate) fn fill_values_with_prompt(&self) -> ProverInitArgsFinal {
-        if self.store_config_provided() {
-            return ProverInitArgsFinal::FileBacked(ProofStorageFileBacked {
-                proof_store_dir: self
-                    .proof_store_dir
-                    .clone()
-                    .expect("proof_store_dir not set"),
-            });
+        if self.proof_store_dir.is_some() {
+            return self.handle_file_backed_config();
+        }
+
+        if self.partial_gcs_config_provided() {
+            return self.ask_gcs_config();
+        }
+
+        if self.partial_create_gcs_bucket_config_provided() {
+            return self.handle_create_gcs_bucket();
         }
 
         let proof_store_config =
@@ -88,10 +91,9 @@ impl ProverInitArgs {
         }
     }
 
-    fn store_config_provided(&self) -> bool {
-        self.proof_store_dir.is_some()
-            || (self.proof_store_gcs_config.bucket_base_url.is_some()
-                && self.proof_store_gcs_config.credentials_file.is_some())
+    fn partial_create_gcs_bucket_config_provided(&self) -> bool {
+        self.create_gcs_bucket_config.bucket_name.is_some()
+            || self.create_gcs_bucket_config.location.is_some()
     }
 
     fn partial_gcs_config_provided(&self) -> bool {
@@ -100,7 +102,10 @@ impl ProverInitArgs {
     }
 
     fn handle_file_backed_config(&self) -> ProverInitArgsFinal {
-        let proof_store_dir = Prompt::new(MSG_PROOF_STORE_DIR_PROMPT).ask();
+        let proof_store_dir = self
+            .proof_store_dir
+            .clone()
+            .unwrap_or_else(|| Prompt::new(MSG_PROOF_STORE_DIR_PROMPT).ask());
 
         ProverInitArgsFinal::FileBacked(ProofStorageFileBacked { proof_store_dir })
     }
@@ -116,8 +121,16 @@ impl ProverInitArgs {
     }
 
     fn handle_create_gcs_bucket(&self) -> ProverInitArgsFinal {
-        let bucket_name = Prompt::new(MSG_CREATE_GCS_BUCKET_NAME_PROMTP).ask();
-        let location = Prompt::new(MSG_CREATE_GCS_BUCKET_LOCATION_PROMPT).ask();
+        let bucket_name = self
+            .create_gcs_bucket_config
+            .bucket_name
+            .clone()
+            .unwrap_or_else(|| Prompt::new(MSG_CREATE_GCS_BUCKET_NAME_PROMTP).ask());
+        let location = self
+            .create_gcs_bucket_config
+            .location
+            .clone()
+            .unwrap_or_else(|| Prompt::new(MSG_CREATE_GCS_BUCKET_LOCATION_PROMPT).ask());
 
         ProverInitArgsFinal::GCSCreateBucket(ProofStorageGCSCreateBucket {
             bucket_name,
