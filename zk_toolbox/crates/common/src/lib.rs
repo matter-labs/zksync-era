ec7d44667b11c1185ef0e5cdc0e33db09f950728
matias-gonz <maigonzalez@fi.uba.ar>
2024-06-27 10:13:41 +0200

    fmt

--- zk_toolbox/crates/common/src/lib.rs
+++ zk_toolbox/crates/common/src/lib.rs
@@ -1,5 +1,4 @@
-pub use prerequisites::check_general_prerequisites;
-pub use prerequisites::check_prover_prequisites;
+pub use prerequisites::{check_general_prerequisites, check_prover_prequisites};
 pub use prompt::{init_prompt_theme, Prompt, PromptConfirm, PromptSelect};
 pub use term::{logger, spinner};
 
