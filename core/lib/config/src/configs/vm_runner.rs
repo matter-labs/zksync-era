fcc4a341910085480928abb800ab6387cce8a6b3
Lech <88630083+Artemka374@users.noreply.github.com>
2024-06-27 14:55:31 +0300

    allow dead code

--- core/lib/config/src/configs/vm_runner.rs
+++ core/lib/config/src/configs/vm_runner.rs
@@ -30,6 +30,7 @@ pub struct BasicWitnessInputProducerConfig {
 }
 
 impl BasicWitnessInputProducerConfig {
+    #[allow(dead_code)]
     fn default_db_path() -> String {
         "./db/basic_witness_input_producer".to_owned()
     }
