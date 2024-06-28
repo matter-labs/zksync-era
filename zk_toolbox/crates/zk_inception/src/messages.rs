d2de4a11ad142be343e32fdcc1f49e81a7e4a990
Danil <deniallugo@gmail.com>
2024-06-28 12:16:57 +0200

    feat(zk_toolbox): Dev command
    Signed-off-by: Danil <deniallugo@gmail.com>

--- zk_toolbox/crates/zk_inception/src/messages.rs
+++ zk_toolbox/crates/zk_inception/src/messages.rs
@@ -36,6 +36,8 @@ pub(super) const MSG_LINK_TO_CODE_SELECTION_PATH: &str = "I have the code alread
 /// Ecosystem and chain init related messages
 pub(super) const MSG_L1_RPC_URL_HELP: &str = "L1 RPC URL";
 pub(super) const MSG_GENESIS_ARGS_HELP: &str = "Genesis options";
+pub(super) const MSG_DEV_ARG_HELP: &str =
+    "Deploy ecosystem  using all defaults. Suitable for local development";
 pub(super) const MSG_DEPLOY_ECOSYSTEM_PROMPT: &str =
     "Do you want to deploy ecosystem contracts? (Not needed if you already have an existing one)";
 pub(super) const MSG_L1_RPC_URL_PROMPT: &str = "What is the RPC URL of the L1 network?";
