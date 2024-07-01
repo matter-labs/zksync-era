2dd35dd1d2cac4ebeba8ddafbc7d8c97f2a11de2
Danil <deniallugo@gmail.com>
2024-07-01 18:45:53 +0200

    chore(zk_toolbox): satisfy clippy (#2359)
    ## What ❔
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zk fmt` and `zk lint`.
    
    ---------
    
    Signed-off-by: Danil <deniallugo@gmail.com>

--- zk_toolbox/crates/zk_inception/src/commands/prover/mod.rs
+++ zk_toolbox/crates/zk_inception/src/commands/prover/mod.rs
@@ -1,6 +1,7 @@
 use args::init::ProverInitArgs;
 use clap::Subcommand;
 use xshell::Shell;
+
 mod args;
 mod gcs;
 mod generate_sk;
@@ -10,14 +11,14 @@ mod utils;
 #[derive(Subcommand, Debug)]
 pub enum ProverCommands {
     /// Initialize prover
-    Init(ProverInitArgs),
+    Init(Box<ProverInitArgs>),
     /// Generate setup keys
     GenerateSK,
 }
 
 pub(crate) async fn run(shell: &Shell, args: ProverCommands) -> anyhow::Result<()> {
     match args {
-        ProverCommands::Init(args) => init::run(args, shell).await,
+        ProverCommands::Init(args) => init::run(*args, shell).await,
         ProverCommands::GenerateSK => generate_sk::run(shell).await,
     }
 }
