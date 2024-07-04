55d24f4793c8cf348a1cc6072e81a0353eb6a4f5
Igor Aleksanov <popzxc@yandex.ru>
2024-07-04 14:25:27 +0400

    Port binaries

--- core/lib/default_da_clients/src/no_da/wiring_layer.rs
+++ core/lib/default_da_clients/src/no_da/wiring_layer.rs
@@ -3,8 +3,8 @@ use std::fmt::Debug;
 use zksync_da_client::DataAvailabilityClient;
 use zksync_node_framework::{
     implementations::resources::da_client::DAClientResource,
-    service::ServiceContext,
     wiring_layer::{WiringError, WiringLayer},
+    IntoContext,
 };
 
 use crate::no_da::client::NoDAClient;
@@ -12,17 +12,25 @@ use crate::no_da::client::NoDAClient;
 #[derive(Debug, Default)]
 pub struct NoDAClientWiringLayer;
 
+#[derive(Debug, IntoContext)]
+pub struct Output {
+    pub client: DAClientResource,
+}
+
 #[async_trait::async_trait]
 impl WiringLayer for NoDAClientWiringLayer {
+    type Input = ();
+    type Output = Output;
+
     fn layer_name(&self) -> &'static str {
         "no_da_layer"
     }
 
-    async fn wire(self: Box<Self>, mut context: ServiceContext<'_>) -> Result<(), WiringError> {
+    async fn wire(self, _input: Self::Input) -> Result<Self::Output, WiringError> {
         let client: Box<dyn DataAvailabilityClient> = Box::new(NoDAClient);
 
-        context.insert_resource(DAClientResource(client))?;
-
-        Ok(())
+        Ok(Output {
+            client: DAClientResource(client),
+        })
     }
 }
