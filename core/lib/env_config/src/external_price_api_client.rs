ae1f5c822d3cc665fd8097489e7f7b175afb4842
Shahar Kaminsky <sk@matterlabs.dev>
2024-07-09 16:59:49 +0300

    config unit test

--- core/lib/env_config/src/external_price_api_client.rs
+++ core/lib/env_config/src/external_price_api_client.rs
@@ -19,14 +19,14 @@ mod tests {
 
     static MUTEX: EnvMutex = EnvMutex::new();
 
-    fn expected_external_price_api_client_config(api_key: String) -> ExternalPriceApiClientConfig {
+    fn expected_external_price_api_client_config() -> ExternalPriceApiClientConfig {
         ExternalPriceApiClientConfig {
             source: "no-op".to_string(),
             base_url: Some("https://pro-api.coingecko.com".to_string()),
-            api_key: Some(api_key),
+            api_key: Some("qwerty12345".to_string()),
             client_timeout_ms: DEFAULT_TIMEOUT_MS,
-            forced_numerator: None,
-            forced_denominator: None,
+            forced_numerator: Some(100),
+            forced_denominator: Some(1),
         }
     }
 
@@ -37,12 +37,12 @@ mod tests {
             EXTERNAL_PRICE_API_CLIENT_SOURCE=no-op
             EXTERNAL_PRICE_API_CLIENT_BASE_URL=https://pro-api.coingecko.com
             EXTERNAL_PRICE_API_CLIENT_API_KEY=qwerty12345
+            EXTERNAL_PRICE_API_CLIENT_FORCED_NUMERATOR=100
+            EXTERNAL_PRICE_API_CLIENT_FORCED_DENOMINATOR=1
         "#;
         lock.set_env(config);
 
-        let api_key = "qwerty12345".to_string();
-
         let actual = ExternalPriceApiClientConfig::from_env().unwrap();
-        assert_eq!(actual, expected_external_price_api_client_config(api_key));
+        assert_eq!(actual, expected_external_price_api_client_config());
     }
 }
