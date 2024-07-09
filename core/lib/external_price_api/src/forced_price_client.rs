5fa5294e71278c78e8ae36b4221a55d449106bf1
Shahar Kaminsky <sk@matterlabs.dev>
2024-07-09 16:15:58 +0300

    randomizing ratio

--- core/lib/external_price_api/src/forced_price_client.rs
+++ core/lib/external_price_api/src/forced_price_client.rs
@@ -1,6 +1,7 @@
 use std::num::NonZeroU64;
 
 use async_trait::async_trait;
+use rand::Rng;
 use zksync_config::configs::ExternalPriceApiClientConfig;
 use zksync_types::{base_token_ratio::BaseTokenAPIRatio, Address};
 
@@ -33,7 +34,29 @@ impl ForcedPriceClient {
 
 #[async_trait]
 impl PriceAPIClient for ForcedPriceClient {
+    // Returns a ratio which is 10% higher or lower than the configured forced ratio.
     async fn fetch_ratio(&self, _token_address: Address) -> anyhow::Result<BaseTokenAPIRatio> {
-        Ok(self.ratio)
+        let mut rng = rand::thread_rng();
+
+        let numerator_range = (
+            (self.ratio.numerator.get() as f64 * 0.9).round() as u64,
+            (self.ratio.numerator.get() as f64 * 1.1).round() as u64,
+        );
+
+        let denominator_range = (
+            (self.ratio.denominator.get() as f64 * 0.9).round() as u64,
+            (self.ratio.denominator.get() as f64 * 1.1).round() as u64,
+        );
+
+        let new_numerator = rng.gen_range(numerator_range.0..=numerator_range.1);
+        let new_denominator = rng.gen_range(denominator_range.0..=denominator_range.1);
+
+        let adjusted_ratio = BaseTokenAPIRatio {
+            numerator: NonZeroU64::new(new_numerator).unwrap_or(self.ratio.numerator),
+            denominator: NonZeroU64::new(new_denominator).unwrap_or(self.ratio.denominator),
+            ratio_timestamp: chrono::Utc::now(),
+        };
+
+        Ok(adjusted_ratio)
     }
 }
