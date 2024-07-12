6709f1d454e21b39b06eb95888c12619844ae65f
matias-gonz <maigonzalez@fi.uba.ar>
2024-07-12 11:33:23 +0200

    Show output from contract verifier

--- zk_toolbox/crates/zk_inception/src/commands/contract_verifier/run.rs
+++ zk_toolbox/crates/zk_inception/src/commands/contract_verifier/run.rs
@@ -20,8 +20,10 @@ pub(crate) async fn run(shell: &Shell) -> anyhow::Result<()> {
 
     logger::info(MSG_RUNNING_CONTRACT_VERIFIER);
 
-    Cmd::new(cmd!(
+    let mut cmd = Cmd::new(cmd!(
         shell,
         "cargo run --bin zksync_contract_verifier -- --config-path={config_path} --secrets-path={secrets_path}"
-    )).run().context(MSG_FAILED_TO_RUN_CONTRACT_VERIFIER_ERR)
+    ));
+    cmd = cmd.with_force_run();
+    cmd.run().context(MSG_FAILED_TO_RUN_CONTRACT_VERIFIER_ERR)
 }
