673a557f9ab5688f4bb467ab6f38f23d805b1d11
steph-mipt <steph.sinyakov@gmail.com>
2024-07-17 17:07:01 +0200

    feat: add custom error, update Cargo.toml

--- core/lib/via_btc_client/src/types.rs
+++ core/lib/via_btc_client/src/types.rs
@@ -1,7 +1,36 @@
-use std::error::Error;
+use thiserror::Error;
 
-pub type BitcoinClientResult<T> = Result<T, Box<dyn Error>>;
-pub type BitcoinSignerResult<T> = Result<T, Box<dyn Error>>;
-pub type BitcoinInscriberResult<T> = Result<T, Box<dyn Error>>;
-pub type BitcoinInscriptionIndexerResult<T> = Result<T, Box<dyn Error>>;
-pub type BitcoinTransactionBuilderResult<T> = Result<T, Box<dyn Error>>;
+#[derive(Debug, Error)]
+pub enum BitcoinError {
+    #[error("RPC error: {0}")]
+    Rpc(String),
+
+    #[error("Invalid address: {0}")]
+    InvalidAddress(String),
+
+    #[error("Invalid transaction: {0}")]
+    InvalidTransaction(String),
+
+    #[error("Signing error: {0}")]
+    SigningError(String),
+
+    #[error("Inscription error: {0}")]
+    InscriptionError(String),
+
+    #[error("Indexing error: {0}")]
+    IndexingError(String),
+
+    #[error("Transaction building error: {0}")]
+    TransactionBuildingError(String),
+
+    #[error("Other error: {0}")]
+    Other(String),
+}
+
+pub type Result<T> = std::result::Result<T, BitcoinError>;
+
+pub type BitcoinClientResult<T> = Result<T>;
+pub type BitcoinSignerResult<T> = Result<T>;
+pub type BitcoinInscriberResult<T> = Result<T>;
+pub type BitcoinInscriptionIndexerResult<T> = Result<T>;
+pub type BitcoinTransactionBuilderResult<T> = Result<T>;
