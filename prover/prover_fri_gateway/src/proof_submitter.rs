62c3326d6286faf79f1a28ee584d8ea8d5c2fadc
Igor Aleksanov <popzxc@yandex.ru>
2024-07-22 11:40:54 +0400

    refactor(prover_fri_gateway): Refactor gateway structures (#2451)
    ## What ❔
    
    Improves readability of prover fri gateway:
    
    - `PeriodicApi` is moved to a separate file. Generic argument was
    removed; instead now it has associated types for both request and
    response.
    - `PeriodicApiStruct` was renamed to `ProverApiClient`.
    `PeriodicApiStruct::run` was moved to `PeriodicApi` trait.
    - Dedicated types were created for `ProofSubmitter` and
    `ProofGenDataFetcher`.
    - A bit of doc comments.
    
    It can be refactored further, but I want to focus on incremental
    improvements for now.
    
    ## Why ❔
    
    - Previously the workflow responsibilities were split between
    `PeriodicApi` and `PeriodicApiStruct`. Now each type has its own area of
    responsiblity.
    - Using the same type for two different pollers was pretty confusing.
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zk fmt` and `zk lint`.

--- prover/prover_fri_gateway/src/proof_submitter.rs
+++ prover/prover_fri_gateway/src/proof_submitter.rs
@@ -1,13 +1,37 @@
+use std::sync::Arc;
+
 use async_trait::async_trait;
-use zksync_prover_dal::ProverDal;
+use zksync_object_store::ObjectStore;
+use zksync_prover_dal::{ConnectionPool, Prover, ProverDal};
 use zksync_prover_interface::api::{SubmitProofRequest, SubmitProofResponse};
 use zksync_types::{prover_dal::ProofCompressionJobStatus, L1BatchNumber};
 
-use crate::api_data_fetcher::{PeriodicApi, PeriodicApiStruct};
+use crate::{client::ProverApiClient, traits::PeriodicApi};
 
-impl PeriodicApiStruct {
+/// The path to the API endpoint that submits the proof.
+const SUBMIT_PROOF_PATH: &str = "/submit_proof";
+
+/// Poller structure that will periodically check the database for new proofs to submit.
+/// Once a new proof is detected, it will be sent to the prover API.
+#[derive(Debug)]
+pub struct ProofSubmitter(ProverApiClient);
+
+impl ProofSubmitter {
+    pub(crate) fn new(
+        blob_store: Arc<dyn ObjectStore>,
+        base_url: String,
+        pool: ConnectionPool<Prover>,
+    ) -> Self {
+        let api_url = format!("{base_url}{SUBMIT_PROOF_PATH}");
+        let inner = ProverApiClient::new(blob_store, pool, api_url);
+        Self(inner)
+    }
+}
+
+impl ProofSubmitter {
     async fn next_submit_proof_request(&self) -> Option<(L1BatchNumber, SubmitProofRequest)> {
         let (l1_batch_number, protocol_version, status) = self
+            .0
             .pool
             .connection()
             .await
@@ -19,6 +43,7 @@ impl PeriodicApiStruct {
         let request = match status {
             ProofCompressionJobStatus::Successful => {
                 let proof = self
+                    .0
                     .blob_store
                     .get((l1_batch_number, protocol_version))
                     .await
@@ -36,7 +61,8 @@ impl PeriodicApiStruct {
     }
 
     async fn save_successful_sent_proof(&self, l1_batch_number: L1BatchNumber) {
-        self.pool
+        self.0
+            .pool
             .connection()
             .await
             .unwrap()
@@ -47,8 +73,9 @@ impl PeriodicApiStruct {
 }
 
 #[async_trait]
-impl PeriodicApi<SubmitProofRequest> for PeriodicApiStruct {
+impl PeriodicApi for ProofSubmitter {
     type JobId = L1BatchNumber;
+    type Request = SubmitProofRequest;
     type Response = SubmitProofResponse;
     const SERVICE_NAME: &'static str = "ProofSubmitter";
 
@@ -62,8 +89,8 @@ impl PeriodicApi<SubmitProofRequest> for PeriodicApiStruct {
         job_id: Self::JobId,
         request: SubmitProofRequest,
     ) -> reqwest::Result<Self::Response> {
-        let endpoint = format!("{}/{job_id}", self.api_url);
-        self.send_http_request(request, &endpoint).await
+        let endpoint = format!("{}/{job_id}", self.0.api_url);
+        self.0.send_http_request(request, &endpoint).await
     }
 
     async fn handle_response(&self, job_id: L1BatchNumber, response: Self::Response) {
