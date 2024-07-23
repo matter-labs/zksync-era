48b2d9df36e390fb11443ba39ff8c728ef3f34ce
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-07-23 20:35:28 -0300

    Utils and aux structs to match with trait implementation

---
+++ core/lib/multivm/src/versions/era_vm/bytecode.rs
@@ -0,0 +1,27 @@
+use itertools::Itertools;
+use zksync_types::H256;
+use zksync_utils::bytecode::{compress_bytecode, hash_bytecode, CompressedBytecodeInfo};
+
+pub(crate) fn compress_bytecodes(
+    bytecodes: &[Vec<u8>],
+    is_bytecode_known: impl Fn(H256) -> bool,
+) -> Vec<CompressedBytecodeInfo> {
+    bytecodes
+        .iter()
+        .enumerate()
+        .sorted_by_key(|(_idx, dep)| *dep)
+        .dedup_by(|x, y| x.1 == y.1)
+        .filter(|(_idx, dep)| !is_bytecode_known(hash_bytecode(dep)))
+        .sorted_by_key(|(idx, _dep)| *idx)
+        .filter_map(|(_idx, dep)| {
+            let compressed_bytecode = compress_bytecode(dep);
+
+            compressed_bytecode
+                .ok()
+                .map(|compressed| CompressedBytecodeInfo {
+                    original: dep.clone(),
+                    compressed,
+                })
+        })
+        .collect()
+}
--- core/lib/multivm/src/versions/era_vm/mod.rs
+++ core/lib/multivm/src/versions/era_vm/mod.rs
@@ -1,1 +1,4 @@
 pub mod vm;
+mod initial_bootloader_memory;
+mod transaction_data;
+mod bytecode;
