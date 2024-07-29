466c55071c231254adb4a4f083ca8a6046ab996d
matias-gonz <maigonzalez@fi.uba.ar>
2024-07-29 17:57:32 +0200

    Add threads support for WVG

--- zk_toolbox/crates/zk_inception/src/messages.rs
+++ zk_toolbox/crates/zk_inception/src/messages.rs
@@ -284,6 +284,7 @@ pub(super) const MSG_BELLMAN_CUDA_ORIGIN_SELECT: &str =
 pub(super) const MSG_BELLMAN_CUDA_SELECTION_CLONE: &str = "Clone for me (recommended)";
 pub(super) const MSG_BELLMAN_CUDA_SELECTION_PATH: &str = "I have the code already";
 pub(super) const MSG_CLOUD_TYPE_PROMPT: &str = "Select the cloud type:";
+pub(super) const MSG_THREADS_PROMPT: &str = "Provide the number of threads:";
 
 pub(super) fn msg_bucket_created(bucket_name: &str) -> String {
     format!("Bucket created successfully with url: gs://{bucket_name}")
