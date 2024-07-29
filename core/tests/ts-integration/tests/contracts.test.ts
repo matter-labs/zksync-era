90848015f4d4a221d7425d5ea7d7bf6ee4fa45e1
perekopskiy <mikeson.dp@gmail.com>
2024-07-29 19:32:08 +0300

    Increase constant in test

--- core/tests/ts-integration/tests/contracts.test.ts
+++ core/tests/ts-integration/tests/contracts.test.ts
@@ -74,7 +74,7 @@ describe('Smart contract behavior checks', () => {
         const expensiveContract = await deployContract(alice, contracts.expensive, []);
 
         // First, check that the transaction that is too expensive would be rejected by the API server.
-        await expect(expensiveContract.expensive(3000)).toBeRejected();
+        await expect(expensiveContract.expensive(7000)).toBeRejected();
 
         // Second, check that processable transaction may fail with "out of gas" error.
         // To do so, we estimate gas for arg "1" and supply it to arg "20".
