bdce2ded9d7d2f50d554f539d75db301550793eb
Fedor Sakharov <fedor.sakharov@gmail.com>
2024-07-30 14:54:37 +0200

    infinite loop

--- core/tests/vm-benchmark/deployment_benchmarks_sources/heap_read_write.sol
+++ core/tests/vm-benchmark/deployment_benchmarks_sources/heap_read_write.sol
@@ -2,16 +2,22 @@ pragma solidity ^0.8.0;
 
 contract HeapBenchmark {
     constructor() {
+        uint256 i = 0;
         uint256 n = 1000000;
         uint256[] memory array = new uint256[](n);
         
-        for (uint i = 0; i < n; i++) {
+        while (true) { 
             uint256 previous = 0;
 
             if (i > 2) {
                 previous += array[i-1] + array[i-2];
             }
-            array[i] = previous + i + 999;
+            array[i] = previous;
+            i += 1;
+
+            if (i > n) {
+                i = 0;
+            }
         }
     }
 }
