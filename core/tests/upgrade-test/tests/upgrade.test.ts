8a84bc19e0ac82d05a2c8ba9127d31fa67195fd1
Manuel Mauro <manuel.mauro@protonmail.com>
2024-08-07 16:14:40 +0200

    fix: use correct chain in upgrade test

--- core/tests/upgrade-test/tests/upgrade.test.ts
+++ core/tests/upgrade-test/tests/upgrade.test.ts
@@ -132,7 +132,8 @@ describe('Upgrade test', function () {
             components: serverComponents,
             stdio: [null, logs, logs],
             cwd: pathToHome,
-            useZkInception: fileConfig.loadFromFile
+            useZkInception: fileConfig.loadFromFile,
+            chain: fileConfig.chain
         });
         // Server may need some time to recompile if it's a cold run, so wait for it.
         let iter = 0;
@@ -339,7 +340,8 @@ describe('Upgrade test', function () {
             components: serverComponents,
             stdio: [null, logs, logs],
             cwd: pathToHome,
-            useZkInception: fileConfig.loadFromFile
+            useZkInception: fileConfig.loadFromFile,
+            chain: fileConfig.chain
         });
         await utils.sleep(10);
 
