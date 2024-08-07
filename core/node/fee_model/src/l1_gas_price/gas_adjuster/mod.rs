c39be19ad088d64d62494a2e9e75ee47f7529a15
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-08-07 18:02:03 +0200

    cargo fix

--- core/node/fee_model/src/l1_gas_price/gas_adjuster/mod.rs
+++ core/node/fee_model/src/l1_gas_price/gas_adjuster/mod.rs
@@ -7,8 +7,8 @@ use std::{
 
 use tokio::sync::watch;
 use zksync_config::{configs::eth_sender::PubdataSendingMode, GasAdjusterConfig};
-use zksync_eth_client::{BaseFees, EnrichedClientResult, EthFeeInterface, EthInterface};
-use zksync_types::{commitment::L1BatchCommitmentMode, L1_GAS_PER_PUBDATA_BYTE, U256, U64};
+use zksync_eth_client::{EthFeeInterface, EthInterface};
+use zksync_types::{commitment::L1BatchCommitmentMode, L1_GAS_PER_PUBDATA_BYTE, U256};
 use zksync_web3_decl::client::{DynClient, ObjectSafeClient, L1, L2};
 
 use self::metrics::METRICS;
