d105dbcb48423929601dba331765343092840880
matias-gonz <maigonzalez@fi.uba.ar>
2024-08-09 09:47:04 +0300

    Add linter_rs

--- zk_toolbox/crates/zk_supervisor/src/commands/lint.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/lint.rs
@@ -1,7 +1,8 @@
 use clap::{Parser, ValueEnum};
-use common::logger;
+use common::{cmd::Cmd, logger};
+use config::EcosystemConfig;
 use strum::EnumIter;
-use xshell::Shell;
+use xshell::{cmd, Shell};
 
 use crate::messages::msg_running_linters_for_files;
 
@@ -28,6 +29,66 @@ pub enum Extension {
 }
 
 pub fn run(shell: &Shell, args: LintArgs) -> anyhow::Result<()> {
-    logger::info(&msg_running_linters_for_files(&args.extensions));
+    let extensions = if args.extensions.is_empty() {
+        vec![
+            Extension::Rs,
+            Extension::Md,
+            Extension::Sol,
+            Extension::Js,
+            Extension::Ts,
+        ]
+    } else {
+        args.extensions.clone()
+    };
+
+    logger::info(&msg_running_linters_for_files(&extensions));
+
+    let ecosystem = EcosystemConfig::from_file(shell)?;
+
+    for extension in extensions {
+        match extension {
+            Extension::Rs => linter_rs(shell, &ecosystem)?,
+            Extension::Md => linter_md(shell, &ecosystem)?,
+            Extension::Sol => linter_sol(shell, &ecosystem, args.check)?,
+            Extension::Js => linter_js(shell, &ecosystem)?,
+            Extension::Ts => linter_ts(shell, &ecosystem)?,
+        }
+    }
+
     Ok(())
+}
+
+fn linter_rs(shell: &Shell, ecosystem: &EcosystemConfig) -> anyhow::Result<()> {
+    let link_to_code = &ecosystem.link_to_code;
+    let lint_to_prover = &ecosystem.link_to_code.join("prover");
+    let link_to_toolbox = &ecosystem.link_to_code.join("zk_toolbox");
+    let paths = vec![link_to_code, lint_to_prover, link_to_toolbox];
+
+    for path in paths {
+        let _dir_guard = shell.push_dir(path);
+        Cmd::new(cmd!(
+            shell,
+            "cargo clippy --locked -- -D warnings -D unstable_features"
+        ))
+        .with_force_run()
+        .run()?;
+    }
+
+    Ok(())
+}
+
+fn linter_md(shell: &Shell, ecosystem: &EcosystemConfig) -> anyhow::Result<()> {
+    todo!()
+}
+
+fn linter_sol(shell: &Shell, ecosystem: &EcosystemConfig, check: bool) -> anyhow::Result<()> {
+    todo!()
+}
+
+fn linter_js(shell: &Shell, ecosystem: &EcosystemConfig) -> anyhow::Result<()> {
+    todo!()
+}
+
+fn linter_ts(shell: &Shell, ecosystem: &EcosystemConfig) -> anyhow::Result<()> {
+    todo!()
 }
