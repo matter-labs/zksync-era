931e4529d964d01268cb5965877f3d81d32c921e
Matías Ignacio González <maigonzalez@fi.uba.ar>
2024-08-12 15:31:21 +0300

    feat(zk_toolbox): Add zk_supervisor test l1 contracts command (#2613)
    ## What ❔
    Add zk_supervisor test l1 contracts command

--- zk_toolbox/crates/zk_supervisor/src/commands/test/mod.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/test/mod.rs
@@ -3,12 +3,13 @@ use clap::Subcommand;
 use xshell::Shell;
 
 use crate::messages::{
-    MSG_INTEGRATION_TESTS_ABOUT, MSG_PROVER_TEST_ABOUT, MSG_RECOVERY_TEST_ABOUT,
-    MSG_REVERT_TEST_ABOUT, MSG_UPGRADE_TEST_ABOUT,
+    MSG_INTEGRATION_TESTS_ABOUT, MSG_L1_CONTRACTS_ABOUT, MSG_PROVER_TEST_ABOUT,
+    MSG_RECOVERY_TEST_ABOUT, MSG_REVERT_TEST_ABOUT, MSG_UPGRADE_TEST_ABOUT,
 };
 
 mod args;
 mod integration;
+mod l1_contracts;
 mod prover;
 mod recovery;
 mod revert;
@@ -24,6 +25,8 @@ pub enum TestCommands {
     Recovery(RecoveryArgs),
     #[clap(about = MSG_UPGRADE_TEST_ABOUT, alias = "u")]
     Upgrade,
+    #[clap(about = MSG_L1_CONTRACTS_ABOUT, alias = "l1")]
+    L1Contracts,
     #[clap(about = MSG_PROVER_TEST_ABOUT, alias = "p")]
     Prover,
 }
@@ -34,6 +37,7 @@ pub fn run(shell: &Shell, args: TestCommands) -> anyhow::Result<()> {
         TestCommands::Revert(args) => revert::run(shell, args),
         TestCommands::Recovery(args) => recovery::run(shell, args),
         TestCommands::Upgrade => upgrade::run(shell),
+        TestCommands::L1Contracts => l1_contracts::run(shell),
         TestCommands::Prover => prover::run(shell),
     }
 }
