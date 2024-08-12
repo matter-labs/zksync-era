d090d1fe7bb2cbaf090cc3ad7f764baad4c4b164
Lech <88630083+Artemka374@users.noreply.github.com>
2024-08-12 16:41:40 +0300

    update env

--- prover/run.sh
+++ prover/run.sh
@@ -4,4 +4,4 @@ zk f cargo run --release --bin zksync_witness_vector_generator -- --threads 10
 
 zk f cargo run --features "gpu" --release --bin zksync_prover_fri
 
-zk f cargo run --features "gpu" --release --bin zksync_proof_fri_compressor
+BELLMAN_CUDA_DIR=$PWD/bellman-cuda zk f cargo run --features "gpu" --release --bin zksync_proof_fri_compressor
