d19d89764e40c4ed0d028b655c1c4adb8dd1fa64
Ivan Schasny <ivan.schasny@gmail.com>
2024-08-13 18:47:33 +0100

    lint

--- core/lib/protobuf_config/src/external_price_api_client.rs
+++ core/lib/protobuf_config/src/external_price_api_client.rs
@@ -13,29 +13,28 @@ impl ProtoRepr for proto::ExternalPriceApiClient {
                 client_timeout_ms: self.client_timeout_ms.expect("client_timeout_ms"),
                 base_url: self.base_url.clone(),
                 api_key: self.api_key.clone(),
-                forced: self.forced.clone().map(|forced| ForcedPriceClientConfig {
-                    numerator: forced.numerator,
-                    denominator: forced.denominator,
-                    fluctuation: forced.fluctuation,
+                forced: Some(ForcedPriceClientConfig {
+                    numerator: self.forced_numerator,
+                    denominator: self.forced_denominator,
+                    fluctuation: self.forced_fluctuation,
                 }),
             },
         )
     }
 
     fn build(this: &Self::Type) -> Self {
+        let numerator = this.forced.as_ref().and_then(|x| x.numerator);
+        let denominator = this.forced.as_ref().and_then(|x| x.denominator);
+        let fluctuation = this.forced.as_ref().and_then(|x| x.fluctuation);
+
         Self {
             source: Some(this.source.clone()),
             base_url: this.base_url.clone(),
             api_key: this.api_key.clone(),
             client_timeout_ms: Some(this.client_timeout_ms),
-            forced: this
-                .forced
-                .clone()
-                .map(|forced| proto::ForcedPriceClientConfig {
-                    numerator: forced.numerator,
-                    denominator: forced.denominator,
-                    fluctuation: forced.fluctuation,
-                }),
+            forced_numerator: numerator,
+            forced_denominator: denominator,
+            forced_fluctuation: fluctuation,
         }
     }
 }
