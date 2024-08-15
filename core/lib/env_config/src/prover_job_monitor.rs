4edc1f6a0226ff406e42f4808cb68ce578cf990d
EmilLuta <virgil.luta@gmail.com>
2024-08-15 12:36:09 +0200

    Fix env tests

--- core/lib/env_config/src/prover_job_monitor.rs
+++ core/lib/env_config/src/prover_job_monitor.rs
@@ -19,6 +19,18 @@ mod tests {
         ProverJobMonitorConfig {
             prometheus_port: 3317,
             max_db_connections: 9,
+            graceful_shutdown_timeout_ms: 5000,
+            gpu_prover_archiver_run_interval_ms: 86400000,
+            gpu_prover_archiver_archive_prover_after_secs: 172800,
+            prover_jobs_archiver_run_interval_ms: 1800000,
+            prover_jobs_archiver_archive_jobs_after_secs: 172800,
+            proof_compressor_job_requeuer_run_interval_ms: 10000,
+            prover_job_requeuer_run_interval_ms: 10000,
+            witness_generator_job_requeuer_run_interval_ms: 10000,
+            proof_compressor_queue_reporter_run_interval_ms: 10000,
+            prover_queue_reporter_run_interval_ms: 10000,
+            witness_generator_queue_reporter_run_interval_ms: 10000,
+            witness_job_queuer_run_interval_ms: 10000,
         }
     }
 
@@ -27,6 +39,18 @@ mod tests {
         let config = r#"
             PROVER_JOB_MONITOR_PROMETHEUS_PORT=3317
             PROVER_JOB_MONITOR_MAX_DB_CONNECTIONS=9
+            PROVER_JOB_MONITOR_GRACEFUL_SHUTDOWN_TIMEOUT_MS=5000
+            PROVER_JOB_MONITOR_GPU_PROVER_ARCHIVER_RUN_INTERVAL_MS=86400000
+            PROVER_JOB_MONITOR_GPU_PROVER_ARCHIVER_ARCHIVE_PROVER_AFTER_SECS=172800
+            PROVER_JOB_MONITOR_PROVER_JOBS_ARCHIVER_RUN_INTERVAL_MS=1800000
+            PROVER_JOB_MONITOR_PROVER_JOBS_ARCHIVER_ARCHIVE_JOBS_AFTER_SECS=172800
+            PROVER_JOB_MONITOR_PROOF_COMPRESSOR_JOB_REQUEUER_RUN_INTERVAL_MS=10000
+            PROVER_JOB_MONITOR_PROVER_JOB_REQUEUER_RUN_INTERVAL_MS=10000
+            PROVER_JOB_MONITOR_WITNESS_GENERATOR_JOB_REQUEUER_RUN_INTERVAL_MS=10000
+            PROVER_JOB_MONITOR_PROOF_COMPRESSOR_QUEUE_REPORTER_RUN_INTERVAL_MS=10000
+            PROVER_JOB_MONITOR_PROVER_QUEUE_REPORTER_RUN_INTERVAL_MS=10000
+            PROVER_JOB_MONITOR_WITNESS_GENERATOR_QUEUE_REPORTER_RUN_INTERVAL_MS=10000
+            PROVER_JOB_MONITOR_WITNESS_JOB_QUEUER_RUN_INTERVAL_MS=10000
         "#;
         let mut lock = MUTEX.lock();
         lock.set_env(config);
