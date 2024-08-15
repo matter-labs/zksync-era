cf013b173837605ce5adebb79497d45595a1721a
Alex Ostrovski <aov@matterlabs.dev>
2024-08-15 12:48:00 +0300

    Move VM bins to `bin` directory

--- core/tests/vm-benchmark/src/bin/iai_results_to_prometheus.rs
+++ core/tests/vm-benchmark/src/bin/iai_results_to_prometheus.rs
@@ -9,11 +9,11 @@ fn main() {
 
     vm_benchmark::with_prometheus::with_prometheus(|| {
         for r in results {
-            VM_CACHEGRIND_METRICS.instructions[&r.name.clone()].set(r.instructions as f64);
-            VM_CACHEGRIND_METRICS.l1_accesses[&r.name.clone()].set(r.l1_accesses as f64);
-            VM_CACHEGRIND_METRICS.l2_accesses[&r.name.clone()].set(r.l2_accesses as f64);
-            VM_CACHEGRIND_METRICS.ram_accesses[&r.name.clone()].set(r.ram_accesses as f64);
-            VM_CACHEGRIND_METRICS.cycles[&r.name.clone()].set(r.cycles as f64);
+            VM_CACHEGRIND_METRICS.instructions[&r.name.clone()].set(r.instructions);
+            VM_CACHEGRIND_METRICS.l1_accesses[&r.name.clone()].set(r.l1_accesses);
+            VM_CACHEGRIND_METRICS.l2_accesses[&r.name.clone()].set(r.l2_accesses);
+            VM_CACHEGRIND_METRICS.ram_accesses[&r.name.clone()].set(r.ram_accesses);
+            VM_CACHEGRIND_METRICS.cycles[&r.name.clone()].set(r.cycles);
         }
     })
 }
@@ -22,15 +22,15 @@ fn main() {
 #[metrics(prefix = "vm_cachegrind")]
 pub(crate) struct VmCachegrindMetrics {
     #[metrics(labels = ["benchmark"])]
-    pub instructions: LabeledFamily<String, Gauge<f64>>,
+    pub instructions: LabeledFamily<String, Gauge<u64>>,
     #[metrics(labels = ["benchmark"])]
-    pub l1_accesses: LabeledFamily<String, Gauge<f64>>,
+    pub l1_accesses: LabeledFamily<String, Gauge<u64>>,
     #[metrics(labels = ["benchmark"])]
-    pub l2_accesses: LabeledFamily<String, Gauge<f64>>,
+    pub l2_accesses: LabeledFamily<String, Gauge<u64>>,
     #[metrics(labels = ["benchmark"])]
-    pub ram_accesses: LabeledFamily<String, Gauge<f64>>,
+    pub ram_accesses: LabeledFamily<String, Gauge<u64>>,
     #[metrics(labels = ["benchmark"])]
-    pub cycles: LabeledFamily<String, Gauge<f64>>,
+    pub cycles: LabeledFamily<String, Gauge<u64>>,
 }
 
 #[vise::register]
