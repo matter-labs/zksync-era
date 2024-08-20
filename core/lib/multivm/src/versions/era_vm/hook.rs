31f0252140ebb6af8a5e5e6fc6b6697791c44335
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-08-19 23:23:24 -0300

    Add missing hooks Hook parsing

--- core/lib/multivm/src/versions/era_vm/hook.rs
+++ core/lib/multivm/src/versions/era_vm/hook.rs
@@ -1,15 +1,19 @@
 #[derive(Debug)]
+
 pub(crate) enum Hook {
     AccountValidationEntered,
+    PaymasterValidationEntered,
     AccountValidationExited,
     ValidationStepEnded,
     TxHasEnded,
     DebugLog,
     DebugReturnData,
+    NearCallCatch,
     AskOperatorForRefund,
     NotifyAboutRefund,
     PostResult,
     FinalBatchInfo,
+    PubdataRequested,
 }
 
 impl Hook {
@@ -18,15 +22,18 @@ impl Hook {
     pub fn from_u32(hook: u32) -> Self {
         match hook {
             0 => Hook::AccountValidationEntered,
+            1 => Hook::PaymasterValidationEntered,
             2 => Hook::AccountValidationExited,
             3 => Hook::ValidationStepEnded,
             4 => Hook::TxHasEnded,
             5 => Hook::DebugLog,
             6 => Hook::DebugReturnData,
+            7 => Hook::NearCallCatch,
             8 => Hook::AskOperatorForRefund,
             9 => Hook::NotifyAboutRefund,
             10 => Hook::PostResult,
             11 => Hook::FinalBatchInfo,
+            12 => Hook::PubdataRequested,
             _ => panic!("Unknown hook {}", hook),
         }
     }
