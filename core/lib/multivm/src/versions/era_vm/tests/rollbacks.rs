d4f4fdbf7f91980dc05e5342cd7141cc03519c71
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-08-20 01:07:16 -0300

    Add all remaining tests to era_vm

---
+++ core/lib/multivm/src/versions/era_vm/tests/rollbacks.rs
@@ -0,0 +1,177 @@
+use ethabi::Token;
+use zksync_contracts::{get_loadnext_contract, test_contracts::LoadnextContractExecutionParams};
+use zksync_types::{Execute, U256};
+
+use crate::{
+    era_vm::tests::{
+        tester::{DeployContractsTx, TransactionTestInfo, TxModifier, TxType, VmTesterBuilder},
+        utils::{read_test_contract, sort_execution_state},
+    },
+    interface::TxExecutionMode,
+};
+
+#[test]
+fn test_vm_rollbacks() {
+    let mut vm = VmTesterBuilder::new()
+        .with_empty_in_memory_storage()
+        .with_execution_mode(TxExecutionMode::VerifyExecute)
+        .with_random_rich_accounts(1)
+        .build();
+
+    let mut account = vm.rich_accounts[0].clone();
+    let counter = read_test_contract();
+    let tx_0 = account.get_deploy_tx(&counter, None, TxType::L2).tx;
+    let tx_1 = account.get_deploy_tx(&counter, None, TxType::L2).tx;
+    let tx_2 = account.get_deploy_tx(&counter, None, TxType::L2).tx;
+
+    let mut result_without_rollbacks = vm.execute_and_verify_txs(&vec![
+        TransactionTestInfo::new_processed(tx_0.clone(), false),
+        TransactionTestInfo::new_processed(tx_1.clone(), false),
+        TransactionTestInfo::new_processed(tx_2.clone(), false),
+    ]);
+
+    // reset vm
+    vm.reset_with_empty_storage();
+
+    let mut result_with_rollbacks = vm.execute_and_verify_txs(&vec![
+        TransactionTestInfo::new_rejected(tx_0.clone(), TxModifier::WrongSignatureLength.into()),
+        TransactionTestInfo::new_rejected(tx_0.clone(), TxModifier::WrongMagicValue.into()),
+        TransactionTestInfo::new_rejected(tx_0.clone(), TxModifier::WrongSignature.into()),
+        // The correct nonce is 0, this tx will fail
+        TransactionTestInfo::new_rejected(tx_2.clone(), TxModifier::WrongNonce.into()),
+        // This tx will succeed
+        TransactionTestInfo::new_processed(tx_0.clone(), false),
+        // The correct nonce is 1, this tx will fail
+        TransactionTestInfo::new_rejected(tx_0.clone(), TxModifier::NonceReused.into()),
+        // The correct nonce is 1, this tx will fail
+        TransactionTestInfo::new_rejected(tx_2.clone(), TxModifier::WrongNonce.into()),
+        // This tx will succeed
+        TransactionTestInfo::new_processed(tx_1, false),
+        // The correct nonce is 2, this tx will fail
+        TransactionTestInfo::new_rejected(tx_0.clone(), TxModifier::NonceReused.into()),
+        // This tx will succeed
+        TransactionTestInfo::new_processed(tx_2.clone(), false),
+        // This tx will fail
+        TransactionTestInfo::new_rejected(tx_2, TxModifier::NonceReused.into()),
+        TransactionTestInfo::new_rejected(tx_0, TxModifier::NonceReused.into()),
+    ]);
+    result_with_rollbacks
+        .deduplicated_storage_logs
+        .sort_by(|a, b| {
+            a.key
+                .account()
+                .address()
+                .cmp(&b.key.account().address())
+                .then_with(|| a.key.key().cmp(b.key.key()))
+        });
+
+    sort_execution_state(&mut result_with_rollbacks);
+    sort_execution_state(&mut result_without_rollbacks);
+    assert_eq!(result_without_rollbacks, result_with_rollbacks);
+}
+
+#[test]
+fn test_vm_loadnext_rollbacks() {
+    let mut vm = VmTesterBuilder::new()
+        .with_empty_in_memory_storage()
+        .with_execution_mode(TxExecutionMode::VerifyExecute)
+        .with_random_rich_accounts(1)
+        .build();
+    let mut account = vm.rich_accounts[0].clone();
+
+    let loadnext_contract = get_loadnext_contract();
+    let loadnext_constructor_data = &[Token::Uint(U256::from(100))];
+    let DeployContractsTx {
+        tx: loadnext_deploy_tx,
+        address,
+        ..
+    } = account.get_deploy_tx_with_factory_deps(
+        &loadnext_contract.bytecode,
+        Some(loadnext_constructor_data),
+        loadnext_contract.factory_deps.clone(),
+        TxType::L2,
+    );
+
+    let loadnext_tx_1 = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: address,
+            calldata: LoadnextContractExecutionParams {
+                reads: 100,
+                writes: 100,
+                events: 100,
+                hashes: 500,
+                recursive_calls: 10,
+                deploys: 60,
+            }
+            .to_bytes(),
+            value: Default::default(),
+            factory_deps: vec![],
+        },
+        None,
+    );
+
+    let loadnext_tx_2 = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: address,
+            calldata: LoadnextContractExecutionParams {
+                reads: 100,
+                writes: 100,
+                events: 100,
+                hashes: 500,
+                recursive_calls: 10,
+                deploys: 60,
+            }
+            .to_bytes(),
+            value: Default::default(),
+            factory_deps: vec![],
+        },
+        None,
+    );
+
+    let mut result_without_rollbacks = vm.execute_and_verify_txs(&vec![
+        TransactionTestInfo::new_processed(loadnext_deploy_tx.clone(), false),
+        TransactionTestInfo::new_processed(loadnext_tx_1.clone(), false),
+        TransactionTestInfo::new_processed(loadnext_tx_2.clone(), false),
+    ]);
+
+    // reset vm
+    vm.reset_with_empty_storage();
+
+    let mut result_with_rollbacks = vm.execute_and_verify_txs(&vec![
+        TransactionTestInfo::new_processed(loadnext_deploy_tx.clone(), false),
+        TransactionTestInfo::new_processed(loadnext_tx_1.clone(), true),
+        TransactionTestInfo::new_rejected(
+            loadnext_deploy_tx.clone(),
+            TxModifier::NonceReused.into(),
+        ),
+        TransactionTestInfo::new_processed(loadnext_tx_1, false),
+        TransactionTestInfo::new_processed(loadnext_tx_2.clone(), true),
+        TransactionTestInfo::new_processed(loadnext_tx_2.clone(), true),
+        TransactionTestInfo::new_rejected(loadnext_deploy_tx, TxModifier::NonceReused.into()),
+        TransactionTestInfo::new_processed(loadnext_tx_2, false),
+    ]);
+
+    result_with_rollbacks
+        .deduplicated_storage_logs
+        .sort_by(|a, b| {
+            a.key
+                .account()
+                .address()
+                .cmp(&b.key.account().address())
+                .then_with(|| a.key.key().cmp(b.key.key()))
+        });
+
+    result_without_rollbacks
+        .deduplicated_storage_logs
+        .sort_by(|a, b| {
+            a.key
+                .account()
+                .address()
+                .cmp(&b.key.account().address())
+                .then_with(|| a.key.key().cmp(b.key.key()))
+        });
+
+    sort_execution_state(&mut result_with_rollbacks);
+    sort_execution_state(&mut result_without_rollbacks);
+    assert_eq!(result_without_rollbacks, result_with_rollbacks);
+}
---
+++ core/lib/multivm/src/versions/era_vm/tests/transfer.rs
@@ -0,0 +1,218 @@
+use ethabi::Token;
+use zksync_contracts::{load_contract, read_bytecode};
+use zksync_system_constants::L2_BASE_TOKEN_ADDRESS;
+use zksync_types::{utils::storage_key_for_eth_balance, AccountTreeId, Address, Execute, U256};
+use zksync_utils::u256_to_h256;
+
+use crate::{
+    era_vm::tests::{
+        tester::{get_empty_storage, VmTesterBuilder},
+        utils::get_balance,
+    },
+    interface::{TxExecutionMode, VmExecutionMode, VmInterface},
+};
+
+enum TestOptions {
+    Send(U256),
+    Transfer(U256),
+}
+
+fn test_send_or_transfer(test_option: TestOptions) {
+    let test_bytecode = read_bytecode(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/TransferTest.json",
+    );
+    let recipeint_bytecode = read_bytecode(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/Recipient.json",
+    );
+    let test_abi = load_contract(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/TransferTest.json",
+    );
+
+    let test_contract_address = Address::random();
+    let recipient_address = Address::random();
+
+    let (value, calldata) = match test_option {
+        TestOptions::Send(value) => (
+            value,
+            test_abi
+                .function("send")
+                .unwrap()
+                .encode_input(&[Token::Address(recipient_address), Token::Uint(value)])
+                .unwrap(),
+        ),
+        TestOptions::Transfer(value) => (
+            value,
+            test_abi
+                .function("transfer")
+                .unwrap()
+                .encode_input(&[Token::Address(recipient_address), Token::Uint(value)])
+                .unwrap(),
+        ),
+    };
+
+    let mut storage = get_empty_storage();
+    storage.set_value(
+        storage_key_for_eth_balance(&test_contract_address),
+        u256_to_h256(value),
+    );
+
+    let mut vm = VmTesterBuilder::new()
+        .with_storage(storage)
+        .with_execution_mode(TxExecutionMode::VerifyExecute)
+        .with_deployer()
+        .with_random_rich_accounts(1)
+        .with_custom_contracts(vec![
+            (test_bytecode, test_contract_address, false),
+            (recipeint_bytecode, recipient_address, false),
+        ])
+        .build();
+
+    let account = &mut vm.rich_accounts[0];
+    let tx = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: test_contract_address,
+            calldata,
+            value: U256::zero(),
+            factory_deps: vec![],
+        },
+        None,
+    );
+
+    vm.vm.push_transaction(tx);
+    let tx_result = vm.vm.execute(VmExecutionMode::OneTx);
+    assert!(
+        !tx_result.result.is_failed(),
+        "Transaction wasn't successful"
+    );
+
+    let batch_result = vm.vm.execute(VmExecutionMode::Batch);
+    assert!(!batch_result.result.is_failed(), "Batch wasn't successful");
+
+    let new_recipient_balance = get_balance(
+        AccountTreeId::new(L2_BASE_TOKEN_ADDRESS),
+        &recipient_address,
+        &mut vm.vm.storage,
+        vm.vm.inner.state.storage_changes(),
+    );
+
+    assert_eq!(new_recipient_balance, value);
+}
+
+#[test]
+fn test_send_and_transfer() {
+    test_send_or_transfer(TestOptions::Send(U256::zero()));
+    test_send_or_transfer(TestOptions::Send(U256::from(10).pow(18.into())));
+    test_send_or_transfer(TestOptions::Transfer(U256::zero()));
+    test_send_or_transfer(TestOptions::Transfer(U256::from(10).pow(18.into())));
+}
+
+fn test_reentrancy_protection_send_or_transfer(test_option: TestOptions) {
+    let test_bytecode = read_bytecode(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/TransferTest.json",
+    );
+    let reentrant_recipeint_bytecode = read_bytecode(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/ReentrantRecipient.json",
+    );
+    let test_abi = load_contract(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/TransferTest.json",
+    );
+    let reentrant_recipient_abi = load_contract(
+        "etc/contracts-test-data/artifacts-zk/contracts/transfer/transfer.sol/ReentrantRecipient.json",
+    );
+
+    let test_contract_address = Address::random();
+    let reentrant_recipeint_address = Address::random();
+
+    let (value, calldata) = match test_option {
+        TestOptions::Send(value) => (
+            value,
+            test_abi
+                .function("send")
+                .unwrap()
+                .encode_input(&[
+                    Token::Address(reentrant_recipeint_address),
+                    Token::Uint(value),
+                ])
+                .unwrap(),
+        ),
+        TestOptions::Transfer(value) => (
+            value,
+            test_abi
+                .function("transfer")
+                .unwrap()
+                .encode_input(&[
+                    Token::Address(reentrant_recipeint_address),
+                    Token::Uint(value),
+                ])
+                .unwrap(),
+        ),
+    };
+
+    let mut vm = VmTesterBuilder::new()
+        .with_empty_in_memory_storage()
+        .with_execution_mode(TxExecutionMode::VerifyExecute)
+        .with_deployer()
+        .with_random_rich_accounts(1)
+        .with_custom_contracts(vec![
+            (test_bytecode, test_contract_address, false),
+            (
+                reentrant_recipeint_bytecode,
+                reentrant_recipeint_address,
+                false,
+            ),
+        ])
+        .build();
+
+    // First transaction, the job of which is to warm up the slots for balance of the recipient as well as its storage variable.
+    let account = &mut vm.rich_accounts[0];
+    let tx1 = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: reentrant_recipeint_address,
+            calldata: reentrant_recipient_abi
+                .function("setX")
+                .unwrap()
+                .encode_input(&[])
+                .unwrap(),
+            value: U256::from(1),
+            factory_deps: vec![],
+        },
+        None,
+    );
+
+    vm.vm.push_transaction(tx1);
+    let tx1_result = vm.vm.execute(VmExecutionMode::OneTx);
+    assert!(
+        !tx1_result.result.is_failed(),
+        "Transaction 1 wasn't successful"
+    );
+
+    let tx2 = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: test_contract_address,
+            calldata,
+            value,
+            factory_deps: vec![],
+        },
+        None,
+    );
+
+    vm.vm.push_transaction(tx2);
+    let tx2_result = vm.vm.execute(VmExecutionMode::OneTx);
+    assert!(
+        tx2_result.result.is_failed(),
+        "Transaction 2 should have failed, but it succeeded"
+    );
+
+    let batch_result = vm.vm.execute(VmExecutionMode::Batch);
+    assert!(!batch_result.result.is_failed(), "Batch wasn't successful");
+}
+
+#[test]
+fn test_reentrancy_protection_send_and_transfer() {
+    test_reentrancy_protection_send_or_transfer(TestOptions::Send(U256::zero()));
+    test_reentrancy_protection_send_or_transfer(TestOptions::Send(U256::from(10).pow(18.into())));
+    test_reentrancy_protection_send_or_transfer(TestOptions::Transfer(U256::zero()));
+    test_reentrancy_protection_send_or_transfer(TestOptions::Transfer(
+        U256::from(10).pow(18.into()),
+    ));
+}
