56d385ba9c02f1b379fdcf75420aaec786788561
Lech <88630083+Artemka374@users.noreply.github.com>
2024-08-21 11:32:55 +0300

    fix path of generate-sk

--- zk_toolbox/crates/zk_inception/src/commands/prover/generate_sk.rs
+++ zk_toolbox/crates/zk_inception/src/commands/prover/generate_sk.rs
@@ -19,7 +19,7 @@ pub(crate) async fn run(shell: &Shell) -> anyhow::Result<()> {
         "cargo run --features gpu --release --bin key_generator -- 
             generate-sk all --recompute-if-missing 
             --setup-path=vk_setup_data_generator_server_fri/data 
-            --path={link_to_prover}/vk_setup_data_generator_server_fri/data"
+            --path={link_to_prover}/crates/bin/vk_setup_data_generator_server_fri/data"
     ));
     cmd.run()?;
     spinner.finish();
