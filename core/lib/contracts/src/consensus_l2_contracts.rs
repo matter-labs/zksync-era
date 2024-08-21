0d648a8c56586c311011b47770345c79b5d52377
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-08-21 14:20:43 +0200

    wip abi in the right place

--- core/lib/contracts/src/consensus_l2_contracts.rs
+++ core/lib/contracts/src/consensus_l2_contracts.rs
@@ -1,18 +1,41 @@
-use ethabi::Contract;
+use ethabi::{ethereum_types::{U256},Address,Bytes,Contract,Token};
 
-use crate::{load_contract, read_bytecode, TestContract};
+use crate::{TestContract};
 
-const CONSENSUS_REGISTRY_CONTRACT_FILE: &str =
-    "contracts/l2-contracts/artifacts-zk/contracts/ConsensusRegistry.sol/ConsensusRegistry.json";
+pub struct ConsensusRegistry(Contract);
 
-pub fn load_consensus_registry_contract() -> Contract {
-    load_contract(CONSENSUS_REGISTRY_CONTRACT_FILE)
-}
+impl ConsensusRegistry {
+    const FILE: &str = "contracts/l2-contracts/artifacts-zk/contracts/ConsensusRegistry.sol/ConsensusRegistry.json";
+    
+    pub fn load() -> Self {
+        Self(crate::load_contract(Self::FILE))
+    }
 
-pub fn load_consensus_registry_contract_in_test() -> TestContract {
-    TestContract {
-        bytecode: read_bytecode(CONSENSUS_REGISTRY_CONTRACT_FILE),
-        contract: load_consensus_registry_contract(),
-        factory_deps: vec![],
+    pub fn test_contract(&self) -> TestContract {
+        TestContract {
+            bytecode: crate::read_bytecode(Self::FILE),
+            contract: self.0.clone(),
+            factory_deps: vec![],
+        }
     }
 }
+
+#[derive(Debug, Default)]
+pub struct Attester {
+    pub weight: U256,
+    pub node_owner: Address,
+    pub pub_key: Bytes,
+}
+
+/*
+impl Detokenize for Attester {
+    fn from_tokens(tokens: Vec<Token>) -> Result<Self, Error> {
+        let mut tokens = tokens.into_iter();
+        let next = |field| tokens.next().ok_or_else(|| Error::Other(format!("{field} missing")));
+        Ok(Self {
+            weight: U256::from_token(next("weight")?)?,
+            node_owner: Address::from_token(next("node_owner")?)?,
+            pub_key: Bytes::from_token(next("pub_key")?)?,
+        })
+    }
+}*/
--- core/node/consensus/src/storage/registry_contract/abi.rs
+++ core/node/consensus/src/storage/registry_contract/abi.rs
@@ -6,14 +6,6 @@ use zksync_basic_types::{
 use zksync_consensus_crypto::ByteFmt as _;
 use zksync_consensus_roles::{attester};
 
-#[derive(Debug, Default)]
-#[allow(dead_code)]
-pub struct Attester {
-    pub weight: U256,
-    pub node_owner: Address,
-    pub pub_key: Bytes,
-}
-
 impl Attester {
     pub(crate) fn parse(&self) -> anyhow::Result<attester::WeightedAttester> {
         Ok(attester::WeightedAttester {
@@ -23,14 +15,3 @@ impl Attester {
     }
 }
 
-impl Detokenize for Attester {
-    fn from_tokens(tokens: Vec<Token>) -> Result<Self, Error> {
-        let mut tokens = tokens.into_iter();
-        let next = |field| tokens.next().ok_or_else(|| Error::Other(format!("{field} missing")));
-        Ok(Self {
-            weight: U256::from_token(next("weight")?)?,
-            node_owner: Address::from_token(next("node_owner")?)?,
-            pub_key: Bytes::from_token(next("pub_key")?)?,
-        })
-    }
-}
