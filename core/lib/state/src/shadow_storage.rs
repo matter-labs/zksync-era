7ae2061abfc0a1478cdf7a5b25566b01a1870057
Alex Ostrovski <aov@matterlabs.dev>
2024-08-23 15:33:24 +0300

    Brush up `ShadowStorage`

--- core/lib/state/src/shadow_storage.rs
+++ core/lib/state/src/shadow_storage.rs
@@ -24,19 +24,20 @@ static METRICS: vise::Global<ShadowStorageMetrics> = vise::Global::new();
 /// to_check_storage -- secondary storage, which will verify it's own return values against source_storage
 /// Note that if to_check_storage value is different than source value, execution continues and metrics/ logs are emitted.
 #[derive(Debug)]
-pub struct ShadowStorage<'a> {
-    source_storage: Box<dyn ReadStorage + 'a>,
-    to_check_storage: Box<dyn ReadStorage + 'a>,
-    metrics: &'a ShadowStorageMetrics,
+pub struct ShadowStorage<Ref, Check> {
+    source_storage: Ref,
+    to_check_storage: Check,
+    metrics: &'static ShadowStorageMetrics,
     l1_batch_number: L1BatchNumber,
+    panic_on_divergence: bool,
 }
 
-impl<'a> ShadowStorage<'a> {
+impl<Ref, Check> ShadowStorage<Ref, Check> {
     /// Creates a new storage using the 2 underlying [`ReadStorage`]s, first as source, the second to be checked
     /// against the source.
     pub fn new(
-        source_storage: Box<dyn ReadStorage + 'a>,
-        to_check_storage: Box<dyn ReadStorage + 'a>,
+        source_storage: Ref,
+        to_check_storage: Check,
         l1_batch_number: L1BatchNumber,
     ) -> Self {
         Self {
@@ -44,35 +45,52 @@ impl<'a> ShadowStorage<'a> {
             to_check_storage,
             metrics: &METRICS,
             l1_batch_number,
+            panic_on_divergence: true,
         }
     }
 }
 
-impl ReadStorage for ShadowStorage<'_> {
+impl<Ref: ReadStorage, Check: ReadStorage> ReadStorage for ShadowStorage<Ref, Check> {
     fn read_value(&mut self, key: &StorageKey) -> StorageValue {
-        let source_value = self.source_storage.as_mut().read_value(key);
-        let expected_value = self.to_check_storage.as_mut().read_value(key);
+        let source_value = self.source_storage.read_value(key);
+        let expected_value = self.to_check_storage.read_value(key);
         if source_value != expected_value {
             self.metrics.read_value_mismatch.inc();
-            tracing::error!(
-                "read_value({key:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
-                 to be equal to to_check={expected_value:?}",
-                self.l1_batch_number
-            );
+            if self.panic_on_divergence {
+                panic!(
+                    "read_value({key:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
+                     to be equal to to_check={expected_value:?}",
+                    self.l1_batch_number
+                );
+            } else {
+                tracing::error!(
+                    "read_value({key:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
+                     to be equal to to_check={expected_value:?}",
+                    self.l1_batch_number
+                );
+            }
         }
         source_value
     }
 
     fn is_write_initial(&mut self, key: &StorageKey) -> bool {
-        let source_value = self.source_storage.as_mut().is_write_initial(key);
-        let expected_value = self.to_check_storage.as_mut().is_write_initial(key);
+        let source_value = self.source_storage.is_write_initial(key);
+        let expected_value = self.to_check_storage.is_write_initial(key);
         if source_value != expected_value {
             self.metrics.is_write_initial_mismatch.inc();
-            tracing::error!(
-                "is_write_initial({key:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
-                 to be equal to to_check={expected_value:?}",
-                self.l1_batch_number
-            );
+            if self.panic_on_divergence {
+                panic!(
+                    "is_write_initial({key:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
+                     to be equal to to_check={expected_value:?}",
+                    self.l1_batch_number
+                );
+            } else {
+                tracing::error!(
+                    "is_write_initial({key:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
+                     to be equal to to_check={expected_value:?}",
+                    self.l1_batch_number
+                );
+            }
         }
         source_value
     }
@@ -82,25 +100,41 @@ impl ReadStorage for ShadowStorage<'_> {
         let expected_value = self.to_check_storage.load_factory_dep(hash);
         if source_value != expected_value {
             self.metrics.load_factory_dep_mismatch.inc();
-            tracing::error!(
-                "load_factory_dep({hash:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
-                 to be equal to to_check={expected_value:?}",
-                 self.l1_batch_number
-            );
+            if self.panic_on_divergence {
+                panic!(
+                    "load_factory_dep({hash:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
+                     to be equal to to_check={expected_value:?}",
+                     self.l1_batch_number
+                );
+            } else {
+                tracing::error!(
+                    "load_factory_dep({hash:?}) -- l1_batch_number={:?} -- expected source={source_value:?} \
+                     to be equal to to_check={expected_value:?}",
+                     self.l1_batch_number
+                );
+            }
         }
         source_value
     }
 
     fn get_enumeration_index(&mut self, key: &StorageKey) -> Option<u64> {
-        let source_value = self.source_storage.as_mut().get_enumeration_index(key);
-        let expected_value = self.to_check_storage.as_mut().get_enumeration_index(key);
+        let source_value = self.source_storage.get_enumeration_index(key);
+        let expected_value = self.to_check_storage.get_enumeration_index(key);
         if source_value != expected_value {
-            tracing::error!(
-                "get_enumeration_index({key:?}) -- l1_batch_number={:?} -- \
-                 expected source={source_value:?} to be equal to to_check={expected_value:?}",
-                self.l1_batch_number
-            );
             self.metrics.get_enumeration_index_mismatch.inc();
+            if self.panic_on_divergence {
+                panic!(
+                    "get_enumeration_index({key:?}) -- l1_batch_number={:?} -- \
+                     expected source={source_value:?} to be equal to to_check={expected_value:?}",
+                    self.l1_batch_number
+                );
+            } else {
+                tracing::error!(
+                    "get_enumeration_index({key:?}) -- l1_batch_number={:?} -- \
+                     expected source={source_value:?} to be equal to to_check={expected_value:?}",
+                    self.l1_batch_number
+                );
+            }
         }
         source_value
     }
