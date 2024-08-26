4d9d0d2c9988f1637f93a0d9afb9ba270b0c56c0
IAvecilla <ignacio.avecilla@lambdaclass.com>
2024-08-26 12:38:08 -0300

    Remove unused import

--- core/lib/vm_interface/src/storage/in_memory.rs
+++ core/lib/vm_interface/src/storage/in_memory.rs
@@ -4,7 +4,7 @@ use zksync_types::{
     block::DeployedContract,
     get_code_key, get_deployer_key, get_known_code_key, get_system_context_init_logs,
     system_contracts::{get_evm_simulator_hash, get_system_smart_contracts},
-    L2ChainId, StorageKey, StorageLog, StorageValue, H256, U256,
+    L2ChainId, StorageKey, StorageLog, StorageValue, H256,
 };
 use zksync_utils::u256_to_h256;
 
