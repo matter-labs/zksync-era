8d0bbd7fe03d72bb806ec2b69caa3f5f5cf65561
matias-gonz <maigonzalez@fi.uba.ar>
2024-08-28 10:57:03 -0300

    Remove `get_dals`

--- zk_toolbox/crates/zk_supervisor/src/commands/test/rust.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/test/rust.rs
@@ -1,14 +1,18 @@
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 use anyhow::Context;
 use common::{cmd::Cmd, db::wait_for_db, logger};
 use config::EcosystemConfig;
+use url::Url;
 use xshell::{cmd, Shell};
 
 use super::args::rust::RustArgs;
 use crate::{
     commands::database,
-    dals::get_test_dals,
+    dals::{Dal, CORE_DAL_PATH, PROVER_DAL_PATH},
     messages::{
         MSG_CARGO_NEXTEST_MISSING_ERR, MSG_CHAIN_NOT_FOUND_ERR, MSG_POSTGRES_CONFIG_NOT_FOUND_ERR,
         MSG_RESETTING_TEST_DATABASES, MSG_UNIT_TESTS_RUN_SUCCESS, MSG_USING_CARGO_NEXTEST,
@@ -50,7 +54,18 @@ pub async fn run(shell: &Shell, args: RustArgs) -> anyhow::Result<()> {
             (args.test_server_url.unwrap(), args.test_prover_url.unwrap())
         };
 
-    reset_test_databases(shell, &link_to_code).await?;
+    let dals = vec![
+        Dal {
+            url: Url::from_str(&test_server_url.clone())?,
+            path: CORE_DAL_PATH.to_string(),
+        },
+        Dal {
+            url: Url::from_str(&test_prover_url.clone())?,
+            path: PROVER_DAL_PATH.to_string(),
+        },
+    ];
+
+    reset_test_databases(shell, &link_to_code, dals).await?;
 
     let _dir_guard = shell.push_dir(&link_to_code);
 
@@ -86,7 +101,11 @@ fn nextest_is_installed(shell: &Shell) -> anyhow::Result<bool> {
     Ok(out.contains("cargo-nextest"))
 }
 
-async fn reset_test_databases(shell: &Shell, link_to_code: &Path) -> anyhow::Result<()> {
+async fn reset_test_databases(
+    shell: &Shell,
+    link_to_code: &Path,
+    dals: Vec<Dal>,
+) -> anyhow::Result<()> {
     logger::info(MSG_RESETTING_TEST_DATABASES);
 
     Cmd::new(cmd!(
@@ -100,7 +119,7 @@ async fn reset_test_databases(shell: &Shell, link_to_code: &Path) -> anyhow::Res
     ))
     .run()?;
 
-    for dal in get_test_dals(shell)? {
+    for dal in dals {
         let mut url = dal.url.clone();
         url.set_path("");
         wait_for_db(&url, 3).await?;
