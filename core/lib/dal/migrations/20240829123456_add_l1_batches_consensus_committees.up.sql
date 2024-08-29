f5f93f89792cdc6435a3f7a1b24a602c1f80976d
Grzegorz Prusak <pompon.pompon@gmail.com>
2024-08-29 17:02:03 +0200

    missing files

---
+++ core/lib/dal/migrations/20240829123456_add_l1_batches_consensus_committees.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE l1_batches_consensus_committees (
+  l1_batch_number BIGINT PRIMARY KEY REFERENCES l1_batches (number) ON DELETE CASCADE,
+  attesters JSONB NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+); 
