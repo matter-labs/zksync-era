de90c3c8aadbd9793efe22992aeb2f1b055c4807
matias-gonz <maigonzalez@fi.uba.ar>
2024-08-30 13:08:33 -0300

    Fix default dbs

--- zk_toolbox/crates/zk_supervisor/src/defaults.rs
+++ zk_toolbox/crates/zk_supervisor/src/defaults.rs
@@ -1,2 +1,4 @@
-pub const TEST_DATABASE_SERVER_URL: &str = "postgres://postgres:notsecurepassword@localhost:5433";
-pub const TEST_DATABASE_PROVER_URL: &str = "postgres://postgres:notsecurepassword@localhost:5433";
+pub const TEST_DATABASE_SERVER_URL: &str =
+    "postgres://postgres:notsecurepassword@localhost:5433/zksync_local_test";
+pub const TEST_DATABASE_PROVER_URL: &str =
+    "postgres://postgres:notsecurepassword@localhost:5433/prover_local_test";
