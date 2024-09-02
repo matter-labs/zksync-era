9ed959413649ad86ebb460b3b82b67e66081c6ae
Jacob <encody@users.noreply.github.com>
2024-09-02 21:11:49 +0900

    fix: format sql

--- etc/contracts-test-data/contracts/loadnext/query_write_metrics.sql
+++ etc/contracts-test-data/contracts/loadnext/query_write_metrics.sql
@@ -1,32 +1,51 @@
-with config as (
-    select
-        -- compute metrics over how many of the most recent blocks
-        1000 as limit_miniblocks
-)
-select
-	sum(initial_writes) / sum(l2_tx_count) as avg_initial_writes,
-	(sum(total_writes) - sum(initial_writes)) / sum(l2_tx_count) as avg_repeated_writes
-from (
-	select
-		mb.number as miniblock_number,
-		count(sl.hashed_key) as total_writes,
-		count(distinct sl.hashed_key) filter (
-			where iw.hashed_key is not null
-		) as initial_writes,
-		mb.l2_tx_count as l2_tx_count
-	from
-		miniblocks mb
-		join l1_batches l1b on l1b.number = mb.l1_batch_number
-		join storage_logs sl on sl.miniblock_number = mb.number
-		left join initial_writes iw on iw.hashed_key = sl.hashed_key
-            and iw.l1_batch_number = mb.l1_batch_number
-			and mb.number = (select miniblock_number from storage_logs where hashed_key = sl.hashed_key order by miniblock_number asc limit 1)
-	where
-		mb.l2_tx_count <> 0
-	group by
-		mb.number
-	order by
-		mb.number desc
-	limit
-		(select limit_miniblocks from config)
-) s;
+WITH
+	CONFIG AS (
+		SELECT
+			-- compute metrics over how many of the most recent blocks
+			1000 AS LIMIT_MINIBLOCKS
+	)
+SELECT
+	SUM(INITIAL_WRITES) / SUM(L2_TX_COUNT) AS AVG_INITIAL_WRITES,
+	(SUM(TOTAL_WRITES) - SUM(INITIAL_WRITES)) / SUM(L2_TX_COUNT) AS AVG_REPEATED_WRITES
+FROM
+	(
+		SELECT
+			MB.NUMBER AS MINIBLOCK_NUMBER,
+			COUNT(SL.HASHED_KEY) AS TOTAL_WRITES,
+			COUNT(DISTINCT SL.HASHED_KEY) FILTER (
+				WHERE
+					IW.HASHED_KEY IS NOT NULL
+			) AS INITIAL_WRITES,
+			MB.L2_TX_COUNT AS L2_TX_COUNT
+		FROM
+			MINIBLOCKS MB
+			JOIN L1_BATCHES L1B ON L1B.NUMBER = MB.L1_BATCH_NUMBER
+			JOIN STORAGE_LOGS SL ON SL.MINIBLOCK_NUMBER = MB.NUMBER
+			LEFT JOIN INITIAL_WRITES IW ON IW.HASHED_KEY = SL.HASHED_KEY
+			AND IW.L1_BATCH_NUMBER = MB.L1_BATCH_NUMBER
+			AND MB.NUMBER = (
+				SELECT
+					MINIBLOCK_NUMBER
+				FROM
+					STORAGE_LOGS
+				WHERE
+					HASHED_KEY = SL.HASHED_KEY
+				ORDER BY
+					MINIBLOCK_NUMBER ASC
+				LIMIT
+					1
+			)
+		WHERE
+			MB.L2_TX_COUNT <> 0
+		GROUP BY
+			MB.NUMBER
+		ORDER BY
+			MB.NUMBER DESC
+		LIMIT
+			(
+				SELECT
+					LIMIT_MINIBLOCKS
+				FROM
+					CONFIG
+			)
+	) S;
