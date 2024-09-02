a55c23b3e3b5de61634357d4e3b9de45ce8e6792
EmilLuta <virgil.luta@gmail.com>
2024-09-02 07:45:52 +0200

    WIP

--- prover/crates/bin/circuit_prover/src/lib.rs
+++ prover/crates/bin/circuit_prover/src/lib.rs
@@ -1,5 +1,8 @@
-#![allow(incomplete_features)] // We have to use generic const exprs.
-#![feature(generic_const_exprs)]
-pub use witness_vector_generator::WitnessVectorGenerator;
-
-mod witness_vector_generator;
+// #![allow(incomplete_features)] // We have to use generic const exprs.
+// #![feature(generic_const_exprs)]
+// pub use witness_vector_generator::WitnessVectorGenerator;
+//
+// mod witness_vector_generator;
+// mod prover;
+//
+// mod utils;
