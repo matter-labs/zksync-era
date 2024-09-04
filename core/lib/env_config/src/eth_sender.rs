6260d8fbd9079357f1869efdd60a562e257499f4
tomg10 <lemures64@gmail.com>
2024-09-04 16:37:21 +0200

    fix
    Signed-off-by: tomg10 <lemures64@gmail.com>

--- core/lib/env_config/src/eth_sender.rs
+++ core/lib/env_config/src/eth_sender.rs
@@ -99,6 +99,7 @@ mod tests {
             },
             L1Secrets {
                 l1_rpc_url: "http://127.0.0.1:8545".to_string().parse().unwrap(),
+                gateway_url: Some("http://127.0.0.1:8547".to_string().parse().unwrap()),
             },
         )
     }
@@ -141,6 +142,7 @@ mod tests {
             ETH_WATCH_CONFIRMATIONS_FOR_ETH_EVENT="0"
             ETH_WATCH_ETH_NODE_POLL_INTERVAL="300"
             ETH_CLIENT_WEB3_URL="http://127.0.0.1:8545"
+            ETH_CLIENT_GATEWAY_WEB3_URL="http://127.0.0.1:8547"
 
         "#;
         lock.set_env(config);
