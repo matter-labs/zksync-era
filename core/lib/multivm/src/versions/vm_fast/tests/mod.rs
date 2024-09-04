ab183f917ce017dbaa3a73d074a5eadeb9487a9f
Alex Ostrovski <aov@matterlabs.dev>
2024-09-04 11:28:46 +0300

    Re-enable precompiles unit tests

--- core/lib/multivm/src/versions/vm_fast/tests/mod.rs
+++ core/lib/multivm/src/versions/vm_fast/tests/mod.rs
@@ -11,7 +11,7 @@ mod is_write_initial;
 mod l1_tx_execution;
 mod l2_blocks;
 mod nonce_holder;
-// mod precompiles; FIXME: requires tracers / circuit stats
+mod precompiles;
 // mod prestate_tracer; FIXME: is pre-state tracer still relevant?
 mod refunds;
 mod require_eip712;
