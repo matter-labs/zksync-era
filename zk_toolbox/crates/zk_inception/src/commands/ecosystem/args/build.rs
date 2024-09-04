168a92b8006253fd8ab77c5f210523c07596ff7d
Manuel Mauro <manuel.mauro@protonmail.com>
2024-09-04 12:16:28 +0200

    feat: add l1_rpc_url command option

--- zk_toolbox/crates/zk_inception/src/commands/ecosystem/args/build.rs
+++ zk_toolbox/crates/zk_inception/src/commands/ecosystem/args/build.rs
@@ -3,12 +3,15 @@ use std::{path::PathBuf, str::FromStr};
 use clap::Parser;
 use common::{forge::ForgeScriptArgs, Prompt};
 use serde::{Deserialize, Serialize};
+use url::Url;
 
 use crate::{
     commands::chain::args::genesis::GenesisArgs,
+    defaults::LOCAL_RPC_URL,
     messages::{
         MSG_ECOSYSTEM_CONTRACTS_PATH_INVALID_ERR, MSG_ECOSYSTEM_CONTRACTS_PATH_PROMPT,
-        MSG_GENESIS_ARGS_HELP,
+        MSG_GENESIS_ARGS_HELP, MSG_L1_RPC_URL_HELP, MSG_L1_RPC_URL_INVALID_ERR,
+        MSG_L1_RPC_URL_PROMPT,
     },
 };
 
@@ -22,6 +25,8 @@ pub struct EcosystemArgs {
     /// Path to ecosystem contracts
     #[clap(long)]
     pub ecosystem_contracts_path: Option<PathBuf>,
+    #[clap(long, help = MSG_L1_RPC_URL_HELP)]
+    pub l1_rpc_url: Option<String>,
 }
 
 impl EcosystemArgs {
@@ -48,9 +53,21 @@ impl EcosystemArgs {
             }
         };
 
+        let l1_rpc_url = self.l1_rpc_url.unwrap_or_else(|| {
+            Prompt::new(MSG_L1_RPC_URL_PROMPT)
+                .default(LOCAL_RPC_URL)
+                .validate_with(|val: &String| -> Result<(), String> {
+                    Url::parse(val)
+                        .map(|_| ())
+                        .map_err(|_| MSG_L1_RPC_URL_INVALID_ERR.to_string())
+                })
+                .ask()
+        });
+
         EcosystemArgsFinal {
             build_ecosystem: self.build_ecosystem,
             ecosystem_contracts_path,
+            l1_rpc_url,
         }
     }
 }
@@ -59,6 +76,7 @@ impl EcosystemArgs {
 pub struct EcosystemArgsFinal {
     pub build_ecosystem: bool,
     pub ecosystem_contracts_path: Option<PathBuf>,
+    pub l1_rpc_url: String,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Parser)]
