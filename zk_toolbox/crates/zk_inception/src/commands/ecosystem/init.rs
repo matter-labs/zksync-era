0bc54a0152b1db3f1d7059052ce3892bcf957678
Danil <deniallugo@gmail.com>
2024-09-04 13:20:31 +0200

    fix lint
    Signed-off-by: Danil <deniallugo@gmail.com>

--- zk_toolbox/crates/zk_inception/src/commands/ecosystem/init.rs
+++ zk_toolbox/crates/zk_inception/src/commands/ecosystem/init.rs
@@ -360,6 +360,7 @@ async fn deploy_ecosystem_inner(
 }
 
 fn install_yarn_dependencies(shell: &Shell, link_to_code: &Path) -> anyhow::Result<()> {
+    let _dir_guard = shell.push_dir(link_to_code);
     Ok(Cmd::new(cmd!(shell, "yarn install")).run()?)
 }
 
