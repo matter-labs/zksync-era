1139e029e8bb2abf6011bffacc5e55ab896cc3a3
Igor Aleksanov <popzxc@yandex.ru>
2024-09-05 14:13:02 +0400

    refactor(prover_keystore): Remove cached commitments function (#2805)
    ## What ❔
    
    - Removes `get_cached_commitments` function from keystore, as it didn't
    in fact cache anything.
    - Improve interfaces for commitment generation and checking the
    scheduler vk hash.
    - Generalizes the alignment check.
    - Remove `zksync_types` from `zksync_prover_keystore` deps.
    
    ## Why ❔
    
    Readability.

--- prover/crates/bin/vk_setup_data_generator_server_fri/src/commitment_generator.rs
+++ prover/crates/bin/vk_setup_data_generator_server_fri/src/commitment_generator.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use zksync_prover_keystore::{commitment_utils::generate_commitments, keystore::Keystore};
+use zksync_prover_keystore::keystore::Keystore;
 
 use crate::vk_commitment_helper::{
     get_toml_formatted_value, read_contract_toml, write_contract_toml,
@@ -7,7 +7,9 @@ use crate::vk_commitment_helper::{
 
 pub fn read_and_update_contract_toml(keystore: &Keystore, dryrun: bool) -> anyhow::Result<()> {
     let mut contract_doc = read_contract_toml().context("read_contract_toml()")?;
-    let vk_commitments = generate_commitments(keystore).context("generate_commitments()")?;
+    let vk_commitments = keystore
+        .generate_commitments()
+        .context("generate_commitments()")?;
 
     contract_doc["contracts"]["FRI_RECURSION_LEAF_LEVEL_VK_HASH"] =
         get_toml_formatted_value(vk_commitments.leaf);
