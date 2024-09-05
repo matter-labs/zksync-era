759520415fdc73e7e2b95bff2ac95df948c8c924
Danil <deniallugo@gmail.com>
2024-09-05 18:35:35 +0200

    Fix place
    Signed-off-by: Danil <deniallugo@gmail.com>

--- zk_toolbox/crates/zk_supervisor/src/commands/test/prover.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/test/prover.rs
@@ -14,14 +14,13 @@ use crate::{
 
 pub async fn run(shell: &Shell) -> anyhow::Result<()> {
     let ecosystem = EcosystemConfig::from_file(shell)?;
-    let _dir_guard = shell.push_dir(ecosystem.link_to_code.join("prover"));
     let dals = vec![Dal {
         url: Url::from_str(&TEST_DATABASE_PROVER_URL)?,
         path: PROVER_DAL_PATH.to_string(),
     }];
-
     reset_test_databases(shell, &ecosystem.link_to_code, dals).await?;
 
+    let _dir_guard = shell.push_dir(ecosystem.link_to_code.join("prover"));
     Cmd::new(cmd!(shell, "cargo test --release --workspace --locked"))
         .with_force_run()
         .env("TEST_DATABASE_PROVER_URL", TEST_DATABASE_PROVER_URL)
