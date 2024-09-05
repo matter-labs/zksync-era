ae107002f1d526df084b078cd7f66876eb9aefb0
Patrick Beza <pab@matterlabs.dev>
2024-09-05 11:34:12 +0200

    Fixes

--- core/lib/basic_types/src/tee_types.rs
+++ core/lib/basic_types/src/tee_types.rs
@@ -1,5 +1,3 @@
-use std::{fmt, str::FromStr};
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
@@ -9,23 +7,6 @@ pub enum TeeType {
     Sgx,
 }
 
-impl fmt::Display for TeeType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            TeeType::Sgx => write!(f, "sgx"),
-        }
-    }
-}
-
-impl FromStr for TeeType {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        serde_json::from_str(&format!("\"{}\"", s.to_lowercase()))
-            .map_err(|_| format!("Invalid value for TeeType: {}", s))
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use serde_json;
@@ -33,7 +14,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_deserialize_teetype() {
+    fn test_serialize_teetype() {
         let json_str = "\"sgx\"";
         let tee_type: TeeType = serde_json::from_str(json_str).unwrap();
         assert_eq!(tee_type, TeeType::Sgx);
@@ -45,14 +26,9 @@ mod tests {
     }
 
     #[test]
-    fn test_enumstring_teetype() {
-        assert_eq!(TeeType::from_str("sgx").unwrap(), TeeType::Sgx);
-        assert_eq!(TeeType::from_str("Sgx").unwrap(), TeeType::Sgx);
-        assert_eq!(TeeType::from_str("SGX").unwrap(), TeeType::Sgx);
-    }
-
-    #[test]
-    fn test_display_teetype() {
-        assert_eq!(TeeType::Sgx.to_string(), "sgx");
+    fn test_deserialize_teetype() {
+        let tee_type = TeeType::Sgx;
+        let json_str = serde_json::to_string(&tee_type).unwrap();
+        assert_eq!(json_str, "\"sgx\"");
     }
 }
