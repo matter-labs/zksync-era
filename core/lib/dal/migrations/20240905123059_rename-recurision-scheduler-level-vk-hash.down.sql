0c4e753b7739760b260ef63d83634edcdcec9e12
Igor Aleksanov <popzxc@yandex.ru>
2024-09-06 11:11:35 +0400

    Fix the migration

--- core/lib/dal/migrations/20240905123059_rename-recurision-scheduler-level-vk-hash.down.sql
+++ core/lib/dal/migrations/20240905123059_rename-recurision-scheduler-level-vk-hash.down.sql
@@ -1,2 +1,3 @@
 UPDATE protocol_patches SET recursion_scheduler_level_vk_hash = snark_wrapper_vk_hash WHERE recursion_scheduler_level_vk_hash = ''::bytea;
 ALTER TABLE protocol_patches DROP COLUMN snark_wrapper_vk_hash;
+ALTER TABLE protocol_patches ALTER COLUMN recursion_scheduler_level_vk_hash DROP DEFAULT;
--- prover/crates/lib/prover_dal/migrations/20240905124208_rename-recurision-scheduler-level-vk-hash.up.sql
+++ prover/crates/lib/prover_dal/migrations/20240905124208_rename-recurision-scheduler-level-vk-hash.up.sql
@@ -1,6 +1,8 @@
-ALTER TABLE prover_fri_protocol_versions ADD COLUMN snark_wrapper_vk_hash BYTEA NOT NULL;
+ALTER TABLE prover_fri_protocol_versions ADD COLUMN snark_wrapper_vk_hash BYTEA NOT NULL DEFAULT ''::bytea;
 ALTER TABLE prover_fri_protocol_versions ALTER COLUMN recursion_scheduler_level_vk_hash SET DEFAULT ''::bytea;
 UPDATE prover_fri_protocol_versions SET snark_wrapper_vk_hash = recursion_scheduler_level_vk_hash;
+-- Default was only needed to migrate old rows, we don't want this field to be forgotten by accident after migration.
+ALTER TABLE prover_fri_protocol_versions ALTER COLUMN snark_wrapper_vk_hash DROP DEFAULT;
 
 -- Old column should be removed once the migration is on the mainnet.
 COMMENT ON COLUMN prover_fri_protocol_versions.recursion_scheduler_level_vk_hash IS 'This column is deprecated and will be removed in the future. Use snark_wrapper_vk_hash instead.';
