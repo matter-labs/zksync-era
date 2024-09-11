05661715cbd46a997a01e42b608185c2d161df6e
Jacob <encody@users.noreply.github.com>
2024-09-11 17:23:04 +0900

    chore: consistent i/o for event metrics query

--- etc/contracts-test-data/contracts/loadnext/query_event_metrics.sql
+++ etc/contracts-test-data/contracts/loadnext/query_event_metrics.sql
@@ -1,21 +1,25 @@
 -- calculate distribution of event emissions per transaction
 
-with config as (select 40000000 as start_from_miniblock_number,
-                       10000    as num_miniblocks)
-select stddev_samp(event_count)                                    as stddev,
-       avg(event_count)                                            as avg,
-       percentile_cont(0.00) within group ( order by event_count ) as pct_00,
-       percentile_cont(0.01) within group ( order by event_count ) as pct_01,
-       percentile_cont(0.05) within group ( order by event_count ) as pct_05,
-       percentile_cont(0.25) within group ( order by event_count ) as pct_25,
-       percentile_cont(0.50) within group ( order by event_count ) as pct_50,
-       percentile_cont(0.75) within group ( order by event_count ) as pct_75,
-       percentile_cont(0.95) within group ( order by event_count ) as pct_95,
-       percentile_cont(0.99) within group ( order by event_count ) as pct_99,
-       percentile_cont(1.00) within group ( order by event_count ) as pct_100
-from (select tx.hash, count(ev.*) as event_count
-      from transactions tx
-               left join events ev on ev.tx_hash = tx.hash
-      where ev.miniblock_number >= (select start_from_miniblock_number from config)
-        and ev.miniblock_number < (select start_from_miniblock_number + num_miniblocks from config)
-      group by tx.hash) s;
+with config as (select 40000000                                                     as start_from_miniblock_number,
+                       10000                                                        as miniblock_range,
+                       array [0.00, 0.01, 0.05, 0.25, 0.50, 0.75, 0.95, 0.99, 1.00] as percentiles)
+select stddev,
+       avg,
+       sum,
+       jsonb_object_agg('pct_' || lpad(to_char(percentile * 100, 'FM999'), 3, '0'),
+                        percentile_value) as percentiles_json,
+       array_agg(percentile_value)        as percentiles_array
+from (select stddev_samp(metric)                                        as stddev,
+             avg(metric)                                                as avg,
+             sum(metric)                                                as sum,
+             percentile,
+             percentile_cont(percentile) within group (order by metric) as percentile_value
+      from (select tx.hash, count(ev.*) as metric
+            from transactions tx
+                     left join events ev on ev.tx_hash = tx.hash
+            where ev.miniblock_number >= (select start_from_miniblock_number from config)
+              and ev.miniblock_number < (select start_from_miniblock_number + miniblock_range from config)
+            group by tx.hash) s
+               cross join (select unnest(percentiles) as percentile from config) _s
+      group by percentile) _t
+group by stddev, avg, sum;
