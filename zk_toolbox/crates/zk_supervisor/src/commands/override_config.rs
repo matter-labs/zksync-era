ffd962e34e83cce42554ea289f086d0c6b563fdb
matias-gonz <maigonzalez@fi.uba.ar>
2024-09-12 16:06:50 -0300

    Add override_config

--- zk_toolbox/crates/zk_supervisor/src/commands/override_config.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/override_config.rs
@@ -1,8 +1,14 @@
+use std::path::PathBuf;
+
+use anyhow::Context;
 use clap::Parser;
-use common::Prompt;
+use common::{config::global_config, Prompt};
+use config::{ChainConfig, EcosystemConfig};
 use xshell::Shell;
 
-use crate::messages::{MSG_OVERRIDE_CONFIG_PATH_HELP, MSG_OVERRRIDE_CONFIG_PATH_PROMPT};
+use crate::messages::{
+    MSG_CHAIN_NOT_FOUND_ERR, MSG_OVERRIDE_CONFIG_PATH_HELP, MSG_OVERRRIDE_CONFIG_PATH_PROMPT,
+};
 
 #[derive(Debug, Parser)]
 pub struct OverrideConfigArgs {
@@ -18,6 +24,15 @@ impl OverrideConfigArgs {
 }
 
 pub fn run(shell: &Shell, args: OverrideConfigArgs) -> anyhow::Result<()> {
-    let path = args.get_config_path();
+    let path = args.get_config_path().into();
+    let ecosystem = EcosystemConfig::from_file(shell)?;
+    let chain = ecosystem
+        .load_chain(global_config().chain_name.clone())
+        .context(MSG_CHAIN_NOT_FOUND_ERR)?;
+    override_config(path, &chain)?;
+    Ok(())
+}
+
+pub fn override_config(path: PathBuf, chain: &ChainConfig) -> anyhow::Result<()> {
     Ok(())
 }
