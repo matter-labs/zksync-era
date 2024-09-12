19887ef21a8bbd26977353f8ee277b711850dfd2
Igor Aleksanov <popzxc@yandex.ru>
2024-09-12 16:25:21 +0400

    feat(prover): Optimize setup keys loading (#2847)
    ## What ❔
    
    - Loads setup keys to memory in parallel (for GPU prover only).
    - Refactors a bunch of related code for simplicity.
    
    ## Why ❔
    
    - Locally I've observed load time going from ~30s to ~12s, so ~60%
    improvement for prover start time.
    - Readability & maintainability.
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zk fmt` and `zk lint`.

--- prover/crates/bin/prover_fri/src/gpu_prover_job_processor.rs
+++ prover/crates/bin/prover_fri/src/gpu_prover_job_processor.rs
@@ -8,8 +8,8 @@ pub mod gpu_prover {
         ProverContextConfig,
     };
     use tokio::task::JoinHandle;
-    use zksync_config::configs::{fri_prover_group::FriProverGroupConfig, FriProverConfig};
-    use zksync_env_config::FromEnv;
+    use zksync_config::configs::fri_prover::SetupLoadMode as SetupLoadModeConfig;
+    use zksync_config::configs::FriProverConfig;
     use zksync_object_store::ObjectStore;
     use zksync_prover_dal::{ConnectionPool, ProverDal};
     use zksync_prover_fri_types::{
@@ -341,38 +341,84 @@ pub mod gpu_prover {
         }
     }
 
-    pub fn load_setup_data_cache(
+    #[tracing::instrument(skip_all, fields(setup_load_mode = ?setup_load_mode, specialized_group_id = %specialized_group_id))]
+    pub async fn load_setup_data_cache(
         keystore: &Keystore,
-        config: &FriProverConfig,
+        setup_load_mode: SetupLoadModeConfig,
+        specialized_group_id: u8,
+        circuit_ids: &[CircuitIdRoundTuple],
     ) -> anyhow::Result<SetupLoadMode> {
-        Ok(match config.setup_load_mode {
-            zksync_config::configs::fri_prover::SetupLoadMode::FromDisk => SetupLoadMode::FromDisk,
-            zksync_config::configs::fri_prover::SetupLoadMode::FromMemory => {
+        Ok(match setup_load_mode {
+            SetupLoadModeConfig::FromDisk => SetupLoadMode::FromDisk,
+            SetupLoadModeConfig::FromMemory => {
+                anyhow::ensure!(
+                    !circuit_ids.is_empty(),
+                    "Circuit IDs must be provided when using FromMemory mode"
+                );
                 let mut cache = HashMap::new();
                 tracing::info!(
                     "Loading setup data cache for group {}",
-                    &config.specialized_group_id
+                    &specialized_group_id
                 );
-                let prover_setup_metadata_list = FriProverGroupConfig::from_env()
-                    .context("FriProverGroupConfig::from_env()")?
-                    .get_circuit_ids_for_group_id(config.specialized_group_id)
-                    .context(
-                        "At least one circuit should be configured for group when running in FromMemory mode",
-                    )?;
                 tracing::info!(
                     "for group {} configured setup metadata are {:?}",
-                    &config.specialized_group_id,
-                    prover_setup_metadata_list
+                    &specialized_group_id,
+                    circuit_ids
                 );
-                for prover_setup_metadata in prover_setup_metadata_list {
-                    let key = setup_metadata_to_setup_data_key(&prover_setup_metadata);
-                    let setup_data = keystore
-                        .load_gpu_setup_data_for_circuit_type(key.clone())
-                        .context("load_gpu_setup_data_for_circuit_type()")?;
-                    cache.insert(key, Arc::new(setup_data));
+                // Load each file in parallel. Note that FS access is not necessarily parallel, but
+                // deserialization is (and it's not insignificant, as setup keys are large).
+                // Note: `collect` is important, because iterators are lazy and otherwise we won't actually
+                // spawn threads.
+                let handles: Vec<_> = circuit_ids
+                    .into_iter()
+                    .map(|prover_setup_metadata| {
+                        let keystore = keystore.clone();
+                        let prover_setup_metadata = prover_setup_metadata.clone();
+                        tokio::task::spawn_blocking(move || {
+                            let key = setup_metadata_to_setup_data_key(&prover_setup_metadata);
+                            let setup_data = keystore
+                                .load_gpu_setup_data_for_circuit_type(key.clone())
+                                .context("load_gpu_setup_data_for_circuit_type()")?;
+                            anyhow::Ok((key, Arc::new(setup_data)))
+                        })
+                    })
+                    .collect();
+                for handle in futures::future::join_all(handles).await {
+                    let (key, setup_data) = handle.context("Key loading future panicked")??;
+                    cache.insert(key, setup_data);
                 }
                 SetupLoadMode::FromMemory(cache)
             }
         })
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use zksync_types::basic_fri_types::AggregationRound;
+
+        use super::*;
+
+        #[tokio::test]
+        async fn test_load_setup_data_cache() {
+            tracing_subscriber::fmt::try_init().ok();
+
+            let keystore = Keystore::locate();
+            let mode = SetupLoadModeConfig::FromMemory;
+            let specialized_group_id = 0;
+            let ids: Vec<_> = AggregationRound::ALL_ROUNDS
+                .into_iter()
+                .flat_map(|r| r.circuit_ids())
+                .collect();
+            if !keystore.is_setup_data_present(&setup_metadata_to_setup_data_key(&ids[0])) {
+                // We don't want this test to fail on envs where setup keys are not present.
+                return;
+            }
+
+            let start = Instant::now();
+            let _cache = load_setup_data_cache(&keystore, mode, specialized_group_id, &ids)
+                .await
+                .expect("Unable to load keys");
+            tracing::info!("Cache load time: {:?}", start.elapsed());
+        }
     }
 }
