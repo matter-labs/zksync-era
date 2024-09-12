c7df0ba6d3be6726bf2bd397b5215328f51942a2
Danil <deniallugo@gmail.com>
2024-09-12 18:23:04 +0200

    feat(zk_toolbox): deprecate zk in ci (#2808)
    ## What ❔
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zk fmt` and `zk lint`.
    
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/lib/contracts/src/lib.rs
+++ core/lib/contracts/src/lib.rs
@@ -221,7 +221,9 @@ pub fn known_codes_contract() -> Contract {
 }
 
 /// Reads bytecode from a given path.
-fn read_bytecode_from_path(artifact_path: impl AsRef<Path> + std::fmt::Debug) -> Option<Vec<u8>> {
+pub fn read_bytecode_from_path(
+    artifact_path: impl AsRef<Path> + std::fmt::Debug,
+) -> Option<Vec<u8>> {
     let artifact = read_file_to_json_value(&artifact_path)?;
 
     let bytecode = if let Some(bytecode) = artifact["bytecode"].as_str() {
