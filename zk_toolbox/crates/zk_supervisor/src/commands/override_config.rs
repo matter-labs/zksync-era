ec05822acbf8a04856194c7dd3e42ae311d656a6
matias-gonz <maigonzalez@fi.uba.ar>
2024-09-12 22:24:52 -0300

    Add messages

--- zk_toolbox/crates/zk_supervisor/src/commands/override_config.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/override_config.rs
@@ -2,12 +2,13 @@ use std::path::PathBuf;
 
 use anyhow::Context;
 use clap::Parser;
-use common::{config::global_config, yaml::merge_yaml, Prompt};
+use common::{config::global_config, logger, yaml::merge_yaml, Prompt};
 use config::{ChainConfig, EcosystemConfig};
 use xshell::Shell;
 
 use crate::messages::{
-    MSG_CHAIN_NOT_FOUND_ERR, MSG_OVERRIDE_CONFIG_PATH_HELP, MSG_OVERRRIDE_CONFIG_PATH_PROMPT,
+    msg_overriding_config, MSG_CHAIN_NOT_FOUND_ERR, MSG_OVERRIDE_CONFIG_PATH_HELP,
+    MSG_OVERRIDE_SUCCESS, MSG_OVERRRIDE_CONFIG_PATH_PROMPT,
 };
 
 #[derive(Debug, Parser)]
@@ -29,15 +30,17 @@ pub fn run(shell: &Shell, args: OverrideConfigArgs) -> anyhow::Result<()> {
     let chain = ecosystem
         .load_chain(global_config().chain_name.clone())
         .context(MSG_CHAIN_NOT_FOUND_ERR)?;
-    override_config(path, &chain)?;
+    logger::step(msg_overriding_config(chain.name.clone()));
+    override_config(shell, path, &chain)?;
+    logger::outro(MSG_OVERRIDE_SUCCESS);
     Ok(())
 }
 
-pub fn override_config(path: PathBuf, chain: &ChainConfig) -> anyhow::Result<()> {
+pub fn override_config(shell: &Shell, path: PathBuf, chain: &ChainConfig) -> anyhow::Result<()> {
     let chain_config_path = chain.path_to_general_config();
     let override_config = serde_yaml::from_str(&shell.read_file(path)?)?;
-    let mut chain_config = serde_yaml::from_str(&shell.read_file(chain_config_path)?)?;
-    let diff = merge_yaml(&mut chain_config, override_config, true)?;
+    let mut chain_config = serde_yaml::from_str(&shell.read_file(chain_config_path.clone())?)?;
+    merge_yaml(&mut chain_config, override_config, true)?;
     shell.write_file(chain_config_path, serde_yaml::to_string(&chain_config)?)?;
     Ok(())
 }
