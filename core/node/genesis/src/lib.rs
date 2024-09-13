bd275b9bb118d2eed9dd792ae9277e1007df2a30
Daniyar Itegulov <ditegulov@gmail.com>
2024-09-13 12:22:27 +1000

    fmt

--- core/node/genesis/src/lib.rs
+++ core/node/genesis/src/lib.rs
@@ -15,11 +15,10 @@ use zksync_eth_client::{CallFunctionArgs, EthInterface};
 use zksync_merkle_tree::{domain::ZkSyncTree, TreeInstruction};
 use zksync_multivm::utils::get_max_gas_per_pubdata_byte;
 use zksync_system_constants::PRIORITY_EXPIRATION;
-use zksync_types::fee_model::PubdataIndependentBatchFeeModelInput;
 use zksync_types::{
     block::{BlockGasCount, DeployedContract, L1BatchHeader, L2BlockHasher, L2BlockHeader},
     commitment::{CommitmentInput, L1BatchCommitment},
-    fee_model::BatchFeeInput,
+    fee_model::{BatchFeeInput, PubdataIndependentBatchFeeModelInput},
     protocol_upgrade::decode_set_chain_id_event,
     protocol_version::{L1VerifierConfig, ProtocolSemanticVersion},
     system_contracts::get_system_smart_contracts,
