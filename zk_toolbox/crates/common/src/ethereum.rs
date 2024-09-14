f576837fc0f72fbf73a9c186c90f43d5e29a17f1
Manuel Mauro <manuel.mauro@protonmail.com>
2024-09-14 09:39:48 +0200

    test: increase confirmations only on minting

--- zk_toolbox/crates/common/src/ethereum.rs
+++ zk_toolbox/crates/common/src/ethereum.rs
@@ -47,7 +47,7 @@ pub async fn distribute_eth(
                 .send_transaction(tx, None)
                 .await?
                 // It's safe to set such low number of confirmations and low interval for localhost
-                .confirmations(3)
+                .confirmations(1)
                 .interval(Duration::from_millis(30)),
         );
     }
