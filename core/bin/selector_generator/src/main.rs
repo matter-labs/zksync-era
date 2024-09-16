9d40704bc9ecb5b884f33ca59438a3a724cefb18
Igor Aleksanov <popzxc@yandex.ru>
2024-09-16 17:17:07 +0400

    refactor(selector-generator): Polish the code (#2888)
    ## What ❔
    
    - Changes procedural approach to more idiomatic for rust (introduces
    `Selectors` + `App` structures). `Selectors` are not FS aware, `App` is.
    - Introduces more strong typing -- when both selectors and function
    names are `String`, it's easy to misuse them (I did several times, while
    working on this PR).
    - Removes manual selector calculation and uses `ethabi` for that.
    - Makes the code async.
    - Adds docs for CLI (e.g. to see with `--help` argument).
    - Adds a test for `Selectors`.
    - Reduces number of panics and uses `anyhow` to propagate errors.
    - Adds new selectors (I guess I have more contracts compiled locally).
    
    ## Why ❔
    
    Maintainability and testability, mostly. With the refactoring, it should
    be easier to add new functionality and tests for it.
    Also, the runtime has improved.
    
    Before
    
    ```
    $ time cargo run --bin selector_generator -- contracts etc/selector-generator-data/selectors.json
       Compiling selector_generator v0.1.0 (/home/popzxc/workspace/current/zksync-era/core/bin/selector_generator)
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.54s
         Running `target/debug/selector_generator contracts etc/selector-generator-data/selectors.json`
    Analyzed 401 files. Added 452 selectors (before: 516 after: 968)
    cargo run --bin selector_generator -- contracts   4.48s user 7.02s system 100% cpu 11.486 total
    ```
    
    After
    
    ```
    $ time cargo run --bin selector_generator -- contracts etc/selector-generator-data/selectors.json
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.20s
         Running `target/debug/selector_generator contracts etc/selector-generator-data/selectors.json`
    Analyzed 1087 files. Added 0 selectors (before: 1023 after: 1023)
    cargo run --bin selector_generator -- contracts   1.24s user 0.12s system 100% cpu 1.360 total
    ```
    
    ## Next steps
    
    I would love to see `--check` mode introduced (which checks that 0 new
    entries were added) and this tool is added to CI, so that we continously
    run it and prevent from rotting. @mm-zk leaving it to you.

--- core/bin/selector_generator/src/main.rs
+++ core/bin/selector_generator/src/main.rs
@@ -1,105 +1,33 @@
-use std::{
-    collections::HashMap,
-    fs::{File, OpenOptions},
-    io::{self},
-};
-
+use app::App;
 use clap::Parser;
-use glob::glob;
-use serde::{Deserialize, Serialize};
-use sha3::{Digest, Keccak256};
 
-#[derive(Debug, Serialize, Deserialize)]
-struct ABIEntry {
-    #[serde(rename = "type")]
-    entry_type: String,
-    name: Option<String>,
-    inputs: Option<Vec<ABIInput>>,
-}
+pub(crate) mod app;
+pub(crate) mod selectors;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct ABIInput {
-    #[serde(rename = "type")]
-    input_type: String,
-}
-
+/// Selector generator tool.
+///
+/// Generates a mapping of short (4-byte) function selectors to their corresponding function names.
+///
+/// The generated JSON can be used to lookup function names by their selectors, when interacting
+/// with Ethereum contracts.
 #[derive(Debug, Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about, long_about)]
 struct Cli {
+    /// Path to the directory with JSON files containing ABI.
+    /// All JSON files in this directory will be processed.
     contracts_dir: String,
+    /// Path to the output file.
+    /// The file will contain the list of function selectors.
+    /// If the file already exists, new selectors will be appended to it.
     output_file: String,
 }
 
-/// Computes solidity selector for a given method and arguments.
-fn compute_selector(name: &str, inputs: &[ABIInput]) -> String {
-    let signature = format!(
-        "{}({})",
-        name,
-        inputs
-            .iter()
-            .map(|i| i.input_type.clone())
-            .collect::<Vec<_>>()
-            .join(",")
-    );
-    let mut hasher = Keccak256::new();
-    hasher.update(signature);
-    format!("{:x}", hasher.finalize())[..8].to_string()
-}
-
-/// Analyses all the JSON files, looking for 'abi' entries, and then computing the selectors for them.
-fn process_files(directory: &str, output_file: &str) -> io::Result<()> {
-    let mut selectors: HashMap<String, String> = match File::open(output_file) {
-        Ok(file) => serde_json::from_reader(file).unwrap_or_default(),
-        Err(_) => HashMap::new(),
-    };
-    let selectors_before = selectors.len();
-    let mut analyzed_files = 0;
-
-    for entry in glob(&format!("{}/**/*.json", directory)).expect("Failed to read glob pattern") {
-        match entry {
-            Ok(path) => {
-                let file_path = path.clone();
-                let file = File::open(path)?;
-                let json: Result<serde_json::Value, _> = serde_json::from_reader(file);
-
-                if let Ok(json) = json {
-                    if let Some(abi) = json.get("abi").and_then(|v| v.as_array()) {
-                        analyzed_files += 1;
-                        for item in abi {
-                            let entry: ABIEntry = serde_json::from_value(item.clone()).unwrap();
-                            if entry.entry_type == "function" {
-                                if let (Some(name), Some(inputs)) = (entry.name, entry.inputs) {
-                                    let selector = compute_selector(&name, &inputs);
-                                    selectors.entry(selector).or_insert(name);
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    eprintln!("Error parsing file: {:?} - ignoring.", file_path)
-                }
-            }
-            Err(e) => eprintln!("Error reading file: {:?}", e),
-        }
-    }
-    println!(
-        "Analyzed {} files. Added {} selectors (before: {} after: {})",
-        analyzed_files,
-        selectors.len() - selectors_before,
-        selectors_before,
-        selectors.len()
-    );
-
-    let file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(output_file)?;
-    serde_json::to_writer_pretty(file, &selectors)?;
-    Ok(())
-}
-
-fn main() -> io::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
-    process_files(&args.contracts_dir, &args.output_file)
+    let mut app = App::load(args.output_file).await?;
+    app.process_files(&args.contracts_dir).await?;
+    app.report();
+    app.save().await?;
+    Ok(())
 }
