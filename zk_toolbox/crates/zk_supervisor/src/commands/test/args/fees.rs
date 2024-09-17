3028221cba848d2b940c94bad65c4aba305e1544
Manuel Mauro <manuel.mauro@protonmail.com>
2024-09-17 16:40:31 +0200

    refactor: simplify code

--- zk_toolbox/crates/zk_supervisor/src/commands/test/args/fees.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/test/args/fees.rs
@@ -1,12 +1,10 @@
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 
-use crate::messages::{MSG_NO_DEPS_HELP, MSG_TESTS_EXTERNAL_NODE_HELP};
+use crate::messages::MSG_NO_DEPS_HELP;
 
 #[derive(Debug, Serialize, Deserialize, Parser)]
 pub struct FeesArgs {
-    #[clap(short, long, help = MSG_TESTS_EXTERNAL_NODE_HELP)]
-    pub external_node: bool,
     #[clap(short, long, help = MSG_NO_DEPS_HELP)]
     pub no_deps: bool,
 }
--- zk_toolbox/crates/zk_supervisor/src/commands/test/fees.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/test/fees.rs
@@ -1,28 +1,16 @@
-use std::path::PathBuf;
-
-use anyhow::Context;
 use common::{cmd::Cmd, config::global_config, logger};
 use config::EcosystemConfig;
 use xshell::{cmd, Shell};
 
 use super::{
     args::integration::IntegrationArgs,
-    utils::{
-        build_contracts, install_and_build_dependencies, TestWallets, TEST_WALLETS_PATH,
-        TS_INTEGRATION_PATH,
-    },
+    utils::{build_contracts, install_and_build_dependencies, TS_INTEGRATION_PATH},
 };
-use crate::messages::{
-    msg_integration_tests_run, MSG_CHAIN_NOT_FOUND_ERR, MSG_DESERIALIZE_TEST_WALLETS_ERR,
-    MSG_INTEGRATION_TESTS_RUN_SUCCESS,
-};
+use crate::messages::{msg_integration_tests_run, MSG_INTEGRATION_TESTS_RUN_SUCCESS};
 
 pub async fn run(shell: &Shell, args: IntegrationArgs) -> anyhow::Result<()> {
     let ecosystem_config = EcosystemConfig::from_file(shell)?;
 
-    let chain_config = ecosystem_config
-        .load_chain(global_config().chain_name.clone())
-        .context(MSG_CHAIN_NOT_FOUND_ERR)?;
     shell.change_dir(ecosystem_config.link_to_code.join(TS_INTEGRATION_PATH));
 
     logger::info(msg_integration_tests_run(args.external_node));
@@ -32,17 +20,8 @@ pub async fn run(shell: &Shell, args: IntegrationArgs) -> anyhow::Result<()> {
         install_and_build_dependencies(shell, &ecosystem_config)?;
     }
 
-    let wallets_path: PathBuf = ecosystem_config.link_to_code.join(TEST_WALLETS_PATH);
-    let wallets: TestWallets = serde_json::from_str(shell.read_file(&wallets_path)?.as_ref())
-        .context(MSG_DESERIALIZE_TEST_WALLETS_ERR)?;
-
-    wallets
-        .init_test_wallet(&ecosystem_config, &chain_config)
-        .await?;
-
     let mut command = cmd!(shell, "yarn jest -- fees.test.ts")
-        .env("CHAIN_NAME", ecosystem_config.current_chain())
-        .env("MASTER_WALLET_PK", wallets.get_test_pk(&chain_config)?);
+        .env("CHAIN_NAME", ecosystem_config.current_chain());
 
     if args.external_node {
         command = command.env("EXTERNAL_NODE", format!("{:?}", args.external_node))
