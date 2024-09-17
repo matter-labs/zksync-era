f9343847fe621f68e1849f74c23ba8c65bcb310d
Manuel Mauro <manuel.mauro@protonmail.com>
2024-09-17 15:35:16 +0200

    style: lint

--- zk_toolbox/crates/zk_supervisor/src/commands/test/args/fees.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/test/args/fees.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 
-use crate::messages::{MSG_NO_DEPS_HELP, MSG_TESTS_EXTERNAL_NODE_HELP, MSG_TEST_PATTERN_HELP};
+use crate::messages::{MSG_NO_DEPS_HELP, MSG_TESTS_EXTERNAL_NODE_HELP};
 
 #[derive(Debug, Serialize, Deserialize, Parser)]
 pub struct FeesArgs {
