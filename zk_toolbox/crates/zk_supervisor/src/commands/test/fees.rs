5b816e14b710f6c56d2733f72618452ab2828289
Manuel Mauro <manuel.mauro@protonmail.com>
2024-09-18 09:50:26 +0200

    refactor: remove unused arg

--- zk_toolbox/crates/zk_supervisor/src/commands/test/fees.rs
+++ zk_toolbox/crates/zk_supervisor/src/commands/test/fees.rs
@@ -3,29 +3,28 @@ use config::EcosystemConfig;
 use xshell::{cmd, Shell};
 
 use super::{
-    args::integration::IntegrationArgs,
+    args::fees::FeesArgs,
     utils::{build_contracts, install_and_build_dependencies, TS_INTEGRATION_PATH},
 };
 use crate::messages::{msg_integration_tests_run, MSG_INTEGRATION_TESTS_RUN_SUCCESS};
 
-pub async fn run(shell: &Shell, args: IntegrationArgs) -> anyhow::Result<()> {
+pub async fn run(shell: &Shell, args: FeesArgs) -> anyhow::Result<()> {
     let ecosystem_config = EcosystemConfig::from_file(shell)?;
-
     shell.change_dir(ecosystem_config.link_to_code.join(TS_INTEGRATION_PATH));
 
-    logger::info(msg_integration_tests_run(args.external_node));
-
     if !args.no_deps {
+        logger::info("Installing dependencies");
         build_contracts(shell, &ecosystem_config)?;
         install_and_build_dependencies(shell, &ecosystem_config)?;
     }
 
+    logger::info(format!(
+        "Running fees tests on chain: {}",
+        ecosystem_config.current_chain()
+    ));
+
     let mut command = cmd!(shell, "yarn jest -- fees.test.ts")
         .env("CHAIN_NAME", ecosystem_config.current_chain());
-
-    if args.external_node {
-        command = command.env("EXTERNAL_NODE", format!("{:?}", args.external_node))
-    }
 
     if global_config().verbose {
         command = command.env(
