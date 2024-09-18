20442f65a1e23ac92298e6235ccc3e4c987abc00
Alex Ostrovski <slowli@users.noreply.github.com>
2024-09-18 11:39:13 +0300

    test: Investigate L1 "nonce too low" errors (#2848)
    ## What ❔
    
    - Resolves "nonce too low" errors on L1 by retrying transaction
    requests. This is a temporary measure until a new version of `reth` is
    released.
    - Reduces spammy logs for integration tests. Enables verbose output for
    integration tests in CI.
    - Fixes some other issues with tests (e.g., a data race in the snapshot
    recovery test).
    
    ## Why ❔
    
    Makes CI more stable.
    
    ## Checklist
    
    - [x] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [x] Tests for the changes have been added / updated.
    - [x] Code has been formatted via `zk fmt` and `zk lint`.

---
+++ core/tests/ts-integration/src/jest-setup/env.ts
@@ -0,0 +1,14 @@
+import NodeEnvironment from 'jest-environment-node';
+import type { EnvironmentContext, JestEnvironmentConfig } from '@jest/environment';
+
+export default class IntegrationTestEnvironment extends NodeEnvironment {
+    constructor(config: JestEnvironmentConfig, context: EnvironmentContext) {
+        super(config, context);
+    }
+
+    override async setup() {
+        await super.setup();
+        // Provide access to raw console in order to produce less cluttered debug messages
+        this.global.rawWriteToConsole = console.log;
+    }
+}
