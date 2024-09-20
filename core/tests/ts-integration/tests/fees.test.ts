a12bf21511d85054fb39ee6a0a0ee0fe2c9e4e2f
Manuel Mauro <manuel.mauro@protonmail.com>
2024-09-20 16:18:50 +0200

    fix: fix operations in beforeAll step

--- core/tests/ts-integration/tests/fees.test.ts
+++ core/tests/ts-integration/tests/fees.test.ts
@@ -31,22 +31,22 @@ const MAX_GAS_PER_PUBDATA = 50_000n;
 // For CI we use only 2 gas prices to not slow it down too much.
 const L1_GAS_PRICES_TO_TEST = process.env.CI
     ? [
-        5_000_000_000n, // 5 gwei
-        10_000_000_000n // 10 gwei
-    ]
+          5_000_000_000n, // 5 gwei
+          10_000_000_000n // 10 gwei
+      ]
     : [
-        1_000_000_000n, // 1 gwei
-        5_000_000_000n, // 5 gwei
-        10_000_000_000n, // 10 gwei
-        25_000_000_000n, // 25 gwei
-        50_000_000_000n, // 50 gwei
-        100_000_000_000n, // 100 gwei
-        200_000_000_000n, // 200 gwei
-        400_000_000_000n, // 400 gwei
-        800_000_000_000n, // 800 gwei
-        1_000_000_000_000n, // 1000 gwei
-        2_000_000_000_000n // 2000 gwei
-    ];
+          1_000_000_000n, // 1 gwei
+          5_000_000_000n, // 5 gwei
+          10_000_000_000n, // 10 gwei
+          25_000_000_000n, // 25 gwei
+          50_000_000_000n, // 50 gwei
+          100_000_000_000n, // 100 gwei
+          200_000_000_000n, // 200 gwei
+          400_000_000_000n, // 400 gwei
+          800_000_000_000n, // 800 gwei
+          1_000_000_000_000n, // 1000 gwei
+          2_000_000_000_000n // 2000 gwei
+      ];
 
 // Unless `RUN_FEE_TEST` is provided, skip the test suit
 const testFees = process.env.RUN_FEE_TEST ? describe : describe.skip;
@@ -90,12 +90,6 @@ testFees('Test fees', function () {
             iter += 1;
         }
 
-        mainNode = await mainNodeSpawner.spawnMainNode();
-
-        alice = testMaster.mainAccount();
-        tokenDetails = testMaster.environment().erc20Token;
-        aliceErc20 = new ethers.Contract(tokenDetails.l1Address, zksync.utils.IERC20, alice.ethWallet());
-
         if (!fileConfig.loadFromFile) {
             ethClientWeb3Url = process.env.ETH_CLIENT_WEB3_URL!;
             apiWeb3JsonRpcHttpUrl = process.env.API_WEB3_JSON_RPC_HTTP_URL!;
@@ -132,6 +126,12 @@ testFees('Test fees', function () {
             apiWeb3JsonRpcHttpUrl,
             baseTokenAddress
         });
+
+        mainNode = await mainNodeSpawner.spawnMainNode();
+
+        alice = testMaster.mainAccount();
+        tokenDetails = testMaster.environment().erc20Token;
+        aliceErc20 = new ethers.Contract(tokenDetails.l1Address, zksync.utils.IERC20, alice.ethWallet());
     });
 
     test('Test fees', async () => {
