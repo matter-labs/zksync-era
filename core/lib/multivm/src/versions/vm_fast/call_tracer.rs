c81a71de08b251589bdfabcd30cf457ffa10fa66
Joonatan Saarhelo <joon.saar@gmail.com>
2024-09-23 11:59:49 +0200

    start implementing vm2 call tracer

---
+++ core/lib/multivm/src/versions/vm_fast/call_tracer.rs
@@ -0,0 +1,79 @@
+use zksync_vm2::interface::{
+    CallframeInterface, Opcode, OpcodeType, ShouldStop, StateInterface, Tracer,
+};
+use zksync_vm_interface::Call;
+
+#[derive(Debug, Clone, Default)]
+pub struct CallTracer {
+    stack: Vec<FarcallAndNearCallCount>,
+
+    current_stack_depth: usize,
+    max_stack_depth: usize,
+
+    max_near_calls: usize,
+}
+
+#[derive(Debug, Clone)]
+struct FarcallAndNearCallCount {
+    farcall: Call,
+    near_calls_after: usize,
+}
+
+impl CallTracer {
+    pub fn result(&self) -> Vec<Call> {
+        self.stack.iter().map(|x| x.farcall.clone()).collect()
+    }
+}
+
+impl Tracer for CallTracer {
+    fn after_instruction<OP: OpcodeType, S: StateInterface>(
+        &mut self,
+        state: &mut S,
+    ) -> ShouldStop {
+        match OP::VALUE {
+            Opcode::FarCall(_) => {
+                self.current_stack_depth += 1;
+                self.max_stack_depth = self.max_stack_depth.max(self.current_stack_depth);
+            }
+            Opcode::NearCall => {
+                self.current_stack_depth += 1;
+                self.max_stack_depth = self.max_stack_depth.max(self.current_stack_depth);
+
+                if let Some(frame) = self.stack.last_mut() {
+                    frame.near_calls_after += 1;
+                    self.max_near_calls = self.max_near_calls.max(frame.near_calls_after);
+                }
+            }
+            Opcode::Ret(_) => {
+                self.current_stack_depth -= 1;
+
+                let Some(mut current_call) = self.stack.pop() else {
+                    return ShouldStop::Continue;
+                };
+
+                if current_call.near_calls_after == 0 {
+                    current_call.farcall.gas_used = current_call
+                        .farcall
+                        .parent_gas
+                        .saturating_sub(state.current_frame().gas() as u64);
+
+                    // TODO save return value
+
+                    // If there is a parent call, push the current call to it
+                    // Otherwise, put the current call back on the stack, because it's the top level call
+                    if let Some(parent_call) = self.stack.last_mut() {
+                        parent_call.farcall.calls.push(current_call.farcall);
+                    } else {
+                        self.stack.push(current_call);
+                    }
+                } else {
+                    current_call.near_calls_after -= 1;
+                    self.stack.push(current_call);
+                }
+            }
+            _ => {}
+        }
+
+        ShouldStop::Continue
+    }
+}
---
+++ core/lib/multivm/src/versions/vm_fast/tests/call_tracer.rs
@@ -0,0 +1,84 @@
+use zksync_types::{Address, Execute};
+use zksync_vm_interface::InspectExecutionMode;
+
+use crate::{
+    interface::{TxExecutionMode, VmInterface},
+    versions::testonly::{read_test_contract, ContractToDeploy, VmTester, VmTesterBuilder},
+    vm_fast::{call_tracer::CallTracer, Vm},
+    vm_latest::constants::BATCH_COMPUTATIONAL_GAS_LIMIT,
+};
+
+/* TODO
+// This test is ultra slow, so it's ignored by default.
+#[test]
+#[ignore]
+fn test_max_depth() {
+    let contract = read_max_depth_contract();
+    let address = Address::random();
+    let mut vm = VmTesterBuilder::new(HistoryEnabled)
+        .with_empty_in_memory_storage()
+        .with_random_rich_accounts(1)
+        .with_deployer()
+        .with_bootloader_gas_limit(BATCH_COMPUTATIONAL_GAS_LIMIT)
+        .with_execution_mode(TxExecutionMode::VerifyExecute)
+        .with_custom_contracts(vec![(contract, address, true)])
+        .build();
+
+    let account = &mut vm.rich_accounts[0];
+    let tx = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: address,
+            calldata: vec![],
+            value: Default::default(),
+            factory_deps: None,
+        },
+        None,
+    );
+
+    let result = Arc::new(OnceCell::new());
+    let call_tracer = CallTracer::new(result.clone()).into_tracer_pointer();
+    vm.vm.push_transaction(tx);
+    let res = vm.vm.inspect(call_tracer.into(), VmExecutionMode::OneTx);
+    assert!(result.get().is_some());
+    assert!(res.result.is_failed());
+}
+*/
+
+#[test]
+fn test_basic_behavior() {
+    let bytecode = read_test_contract();
+    let address = Address::random();
+    let mut vm: VmTester<Vm<_, CallTracer>> = VmTesterBuilder::new()
+        .with_empty_in_memory_storage()
+        .with_rich_accounts(1)
+        .with_bootloader_gas_limit(BATCH_COMPUTATIONAL_GAS_LIMIT)
+        .with_execution_mode(TxExecutionMode::VerifyExecute)
+        .with_custom_contracts(vec![ContractToDeploy::new(bytecode, address)])
+        .build();
+
+    let increment_by_6_calldata =
+        "7cf5dab00000000000000000000000000000000000000000000000000000000000000006";
+
+    let account = &mut vm.rich_accounts[0];
+    let tx = account.get_l2_tx_for_execute(
+        Execute {
+            contract_address: Some(address),
+            calldata: hex::decode(increment_by_6_calldata).unwrap(),
+            value: Default::default(),
+            factory_deps: vec![],
+        },
+        None,
+    );
+
+    let mut call_tracer = CallTracer::default();
+    vm.vm.push_transaction(tx);
+    let res = vm.vm.inspect(&mut call_tracer, InspectExecutionMode::OneTx);
+
+    let call_tracer_result = call_tracer.result();
+
+    assert_eq!(call_tracer_result.len(), 1);
+    // Expect that there are a plenty of subcalls underneath.
+    let subcall = &call_tracer_result[0].calls;
+    assert!(subcall.len() > 10);
+    assert!(!res.result.is_failed());
+}
