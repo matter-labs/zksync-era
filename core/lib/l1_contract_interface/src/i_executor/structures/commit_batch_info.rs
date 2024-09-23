7fe2466c214b540b7fb64b0ba57dac068125c105
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-09-23 17:06:46 +0200

    fix lint

--- core/lib/l1_contract_interface/src/i_executor/structures/commit_batch_info.rs
+++ core/lib/l1_contract_interface/src/i_executor/structures/commit_batch_info.rs
@@ -19,7 +19,6 @@ use crate::{
 /// These are used by the L1 Contracts to indicate what DA layer is used for pubdata
 const PUBDATA_SOURCE_CALLDATA: u8 = 0;
 const PUBDATA_SOURCE_BLOBS: u8 = 1;
-const PUBDATA_SOURCE_CUSTOM: u8 = 2;
 
 /// Encoding for `CommitBatchInfo` from `IExecutor.sol` for a contract running in rollup mode.
 #[derive(Debug)]
