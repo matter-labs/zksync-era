ba321b474174e65495ea425e9e4c623e73b1a68d
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-09-23 15:47:11 +0200

    remove bad change

--- core/node/genesis/src/lib.rs
+++ core/node/genesis/src/lib.rs
@@ -361,20 +361,20 @@ pub async fn ensure_genesis_state(
                 "expected_rollup_last_leaf_index",
             ))?;
 
-    // if expected_root_hash != root_hash {
-    //     return Err(GenesisError::RootHash(expected_root_hash, root_hash));
-    // }
+    if expected_root_hash != root_hash {
+        return Err(GenesisError::RootHash(expected_root_hash, root_hash));
+    }
 
-    // if expected_commitment != commitment {
-    //     return Err(GenesisError::Commitment(expected_commitment, commitment));
-    // }
+    if expected_commitment != commitment {
+        return Err(GenesisError::Commitment(expected_commitment, commitment));
+    }
 
-    // if expected_rollup_last_leaf_index != rollup_last_leaf_index {
-    //     return Err(GenesisError::LeafIndexes(
-    //         expected_rollup_last_leaf_index,
-    //         rollup_last_leaf_index,
-    //     ));
-    // }
+    if expected_rollup_last_leaf_index != rollup_last_leaf_index {
+        return Err(GenesisError::LeafIndexes(
+            expected_rollup_last_leaf_index,
+            rollup_last_leaf_index,
+        ));
+    }
 
     tracing::info!("genesis is complete");
     transaction.commit().await?;
