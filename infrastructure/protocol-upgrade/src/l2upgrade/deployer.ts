21ef547348173f3c006be2276be2c8f406c3b18d
Alex Ostrovski <aov@matterlabs.dev>
2024-09-27 14:54:46 +0300

    Revert changes in config files

--- infrastructure/protocol-upgrade/src/l2upgrade/deployer.ts
+++ infrastructure/protocol-upgrade/src/l2upgrade/deployer.ts
@@ -8,7 +8,6 @@ export async function callSystemContractDeployer(
     nonce: string,
     bootloader: boolean,
     defaultAA: boolean,
-    evmSimulator: boolean,
     systemContracts: boolean,
     file: string
 ) {
@@ -20,9 +19,6 @@ export async function callSystemContractDeployer(
     }
     if (defaultAA) {
         argsString += ' --default-aa';
-    }
-    if (evmSimulator) {
-        argsString += '--evm-simulator';
     }
     if (systemContracts) {
         argsString += ' --system-contracts';
