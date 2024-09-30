802b4e17e24e45aa113972dc97ce607e84881c49
Alexander Melnikov <sanekmelnikov@gmail.com>
2024-09-30 09:34:34 -0600

    feat(zk_inception): Add ecosystem ports scanner (#2849)
    ## What ❔
    Added `EcosystemPortsScanner` tool to scan ecosystem directory config
    and find all values for ports.
    Returns `EcosystemPorts` object with ports found and custom description
    of a service.
    `EcosystemPorts` also contains methods to: check whether the port is
    assigned, allocate a new port in range, etc.
    
    ## Why ❔
    With multiple chains and number of services (like explorer, etc.)
    growing - it is useful to have a tool (`EcosystemPorts`) that can be
    used to allocate new ports, thus, lowering the chance of port conflicts
    within created configuration.
    
    ## Checklist
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zk fmt` and `zk lint`.
    
    ---------
    
    Co-authored-by: Matías Ignacio González <maigonzalez@fi.uba.ar>

--- zk_toolbox/crates/zk_inception/src/commands/chain/args/init.rs
+++ zk_toolbox/crates/zk_inception/src/commands/chain/args/init.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use clap::Parser;
 use common::{forge::ForgeScriptArgs, Prompt};
 use config::ChainConfig;
@@ -13,34 +11,9 @@ use crate::{
     defaults::LOCAL_RPC_URL,
     messages::{
         MSG_DEPLOY_PAYMASTER_PROMPT, MSG_GENESIS_ARGS_HELP, MSG_L1_RPC_URL_HELP,
-        MSG_L1_RPC_URL_INVALID_ERR, MSG_L1_RPC_URL_PROMPT, MSG_PORT_OFFSET_HELP,
+        MSG_L1_RPC_URL_INVALID_ERR, MSG_L1_RPC_URL_PROMPT, MSG_NO_PORT_REALLOCATION_HELP,
     },
 };
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct PortOffset(u16);
-
-impl PortOffset {
-    pub fn from_chain_id(chain_id: u16) -> Self {
-        Self((chain_id - 1) * 100)
-    }
-}
-
-impl FromStr for PortOffset {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.parse::<u16>()
-            .map(PortOffset)
-            .map_err(|_| "Invalid port offset".to_string())
-    }
-}
-
-impl From<PortOffset> for u16 {
-    fn from(port_offset: PortOffset) -> Self {
-        port_offset.0
-    }
-}
 
 #[derive(Debug, Clone, Serialize, Deserialize, Parser)]
 pub struct InitArgs {
@@ -55,8 +28,8 @@ pub struct InitArgs {
     pub deploy_paymaster: Option<bool>,
     #[clap(long, help = MSG_L1_RPC_URL_HELP)]
     pub l1_rpc_url: Option<String>,
-    #[clap(long, help = MSG_PORT_OFFSET_HELP)]
-    pub port_offset: Option<PortOffset>,
+    #[clap(long, help = MSG_NO_PORT_REALLOCATION_HELP, default_value = "false", default_missing_value = "true", num_args = 0..=1)]
+    pub no_port_reallocation: bool,
 }
 
 impl InitArgs {
@@ -86,10 +59,7 @@ impl InitArgs {
             genesis_args: self.genesis_args.fill_values_with_prompt(config),
             deploy_paymaster,
             l1_rpc_url,
-            port_offset: self
-                .port_offset
-                .unwrap_or(PortOffset::from_chain_id(config.id as u16))
-                .into(),
+            no_port_reallocation: self.no_port_reallocation,
         }
     }
 }
@@ -100,5 +70,5 @@ pub struct InitArgsFinal {
     pub genesis_args: GenesisArgsFinal,
     pub deploy_paymaster: bool,
     pub l1_rpc_url: String,
-    pub port_offset: u16,
+    pub no_port_reallocation: bool,
 }
