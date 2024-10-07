03a696de9f812fa068cc6ef7df699f79e0bdf38c
Manuel Mauro <manuel.mauro@protonmail.com>
2024-10-07 16:36:15 +0200

    fix: make run_on_all_chains a scipt instead of a bash function

--- .github/scripts/run_on_all_chains.sh
+++ .github/scripts/run_on_all_chains.sh
@@ -1,44 +1,42 @@
 #! /bin/bash
 
-run_on_all_chains() {
-    # Disable immediate exit on non-zero status
-    set +e
+# Disable immediate exit on non-zero status
+set +e
 
-    local command=$1
-    local chain_list=$2
-    local log_list=$3
-    IFS=',' read -r -a chains <<< "$chain_list"
-    IFS=',' read -r -a logs <<< "$log_list"
-    local pids=()
-    local statuses=()
+command=$1
+chain_list=$2
+log_list=$3
+IFS=',' read -r -a chains <<< "$chain_list"
+IFS=',' read -r -a logs <<< "$log_list"
+pids=()
+statuses=()
 
-    # Start background processes
-    for i in "${!chains[@]}"; do
-        eval "$command --chain ${chains[$i]} &> ${logs[$i]}/${chains[$i]}.log" &
-        pids+=($!)
-    done
+# Start background processes
+for i in "${!chains[@]}"; do
+    eval "$command --chain ${chains[$i]} &> ${logs[$i]}/${chains[$i]}.log" &
+    pids+=($!)
+done
 
-    # Wait for all processes to complete and capture their exit statuses
-    for i in "${!pids[@]}"; do
-        wait ${pids[$i]}
-        statuses[$i]=$?
-    done
+# Wait for all processes to complete and capture their exit statuses
+for i in "${!pids[@]}"; do
+    wait ${pids[$i]}
+    statuses[$i]=$?
+done
 
-    # Check exit statuses and set overall status
-    local overall_status=0
+# Check exit statuses and set overall status
+overall_status=0
 
-    for i in "${!statuses[@]}"; do
-        if [ ${statuses[$i]} -ne 0 ]; then
-            overall_status=1
-            echo "Chain ${chains[$i]} failed with status ${statuses[$i]}"
-        else
-            echo "Chain ${chains[$i]} succeeded"
-        fi
-    done
+for i in "${!statuses[@]}"; do
+    if [ ${statuses[$i]} -ne 0 ]; then
+        overall_status=1
+        echo "Chain ${chains[$i]} failed with status ${statuses[$i]}"
+    else
+        echo "Chain ${chains[$i]} succeeded"
+    fi
+done
 
-    # Re-enable immediate exit on non-zero status
-    set -e
+# Re-enable immediate exit on non-zero status
+set -e
 
-    # Exit with overall status
-    return $overall_status
-}
+# Exit with overall status
+exit $overall_status
