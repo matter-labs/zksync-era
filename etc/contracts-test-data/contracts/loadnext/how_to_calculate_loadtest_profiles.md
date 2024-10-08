37029c7aad8c1701b8eb409cb7f8cdb724d213ea
Jacob Lindahl <encody@users.noreply.github.com>
2024-10-09 00:57:47 +0900

    feat: compensation numbers in loadtest contract

--- etc/contracts-test-data/contracts/loadnext/how_to_calculate_loadtest_profiles.md
+++ etc/contracts-test-data/contracts/loadnext/how_to_calculate_loadtest_profiles.md
@@ -1,11 +1,15 @@
 # Calculating loadtest profiles
 
-Use the SQL scripts in this folder (`query_*.sql`) to calculate the characteristics of transactions within a miniblock range.
+Use the SQL scripts in this directory to calculate the characteristics of transactions within a miniblock range.
 
 Calculate `CONTRACT_EXECUTION_PARAMS` as follows:
 
 - `light`: all zeroes.
-- `realistic`: 75th percentile.
+- `realistic`: median (50th percentile).
 - `heavy`: generally use 2.5&times; the values in the 99th percentile. However, some operations are even less frequent than that (e.g. contract deployments). At the time of writing, contract deployments is set to 5.
 
 Metrics may be averaged across different block ranges to calculate a more holistic "characteristic."
+
+## Compensating for implicit activity
+
+The mere act of executing a transaction entails some ancillary activity on the network. For example, some events are emitted when tokens are transferred for gas payments. Thus, the loadtest contract compensates for some of this implicit activity by subtracting a previously-observed baseline from the requested target. The compensatory numbers are derived by running the loadtest with the configuration set to all zeroes (like the `light` configuration), and then running the SQL queries to collect the metrics, and using the median (50th percentile, rounded _down_) as the compensation for the given metric. All configurations where the configured target is below the compensation number will act identically, explicitly producing no additional activity (but of course, we cannot go below the activity baseline).
--- etc/contracts-test-data/contracts/loadnext/loadnext_contract.sol
+++ etc/contracts-test-data/contracts/loadnext/loadnext_contract.sol
@@ -4,12 +4,22 @@ pragma solidity ^0.8.0;
 pragma abicoder v2;
 
 contract LoadnextContract {
+    uint internal constant COMPENSATE_READS = 155;
+    uint internal constant COMPENSATE_REPEATED_WRITES = 3; // observed: 3.2
+    uint internal constant COMPENSATE_INITIAL_WRITES = 0; // observed: 0.1
+    uint internal constant COMPENSATE_EVENTS = 6;
+
     event Event(uint val);
     uint[] readArray;
     uint[] writeArray;
 
+    function saturatingSubtract(uint a, uint b) internal pure returns(uint) {
+        return b > a ? 0 : a - b;
+    }
+
     constructor(uint reads) {
-        for (uint i = 0; i < reads; i++) {
+        uint _reads = saturatingSubtract(reads, COMPENSATE_READS);
+        for (uint i = 0; i < _reads; i++) {
             readArray.push(i);
         }
     }
@@ -36,24 +46,30 @@ contract LoadnextContract {
                 );
         }
 
-        require(repeatedWrites <= readArray.length);
+        // apply compensation from observed baseline
+        uint _reads = saturatingSubtract(reads, COMPENSATE_READS);
+        uint _repeatedWrites = saturatingSubtract(repeatedWrites, COMPENSATE_REPEATED_WRITES);
+        uint _initialWrites = saturatingSubtract(initialWrites, COMPENSATE_INITIAL_WRITES);
+        uint _events = saturatingSubtract(events, COMPENSATE_EVENTS);
+
+        require(_repeatedWrites <= readArray.length);
         uint sum = 0;
 
         // Somehow use result of storage read for compiler to not optimize this place.
-        for (uint i = 0; i < repeatedWrites; i++) {
+        for (uint i = 0; i < _repeatedWrites; i++) {
             uint value = readArray[i];
             sum += value;
             readArray[i] = value + 1;
         }
-        for (uint i = repeatedWrites; i < reads; i++) {
+        for (uint i = _repeatedWrites; i < _reads; i++) {
             sum += readArray[i];
         }
 
-        for (uint i = 0; i < initialWrites; i++) {
+        for (uint i = 0; i < _initialWrites; i++) {
             writeArray.push(i);
         }
 
-        for (uint i = 0; i < events; i++) {
+        for (uint i = 0; i < _events; i++) {
             emit Event(i);
         }
 
