fc9f7d4439d6b40237d9944c287bd738fa472360
vibhurajeev <vibhurajeev@gmail.com>
2024-10-09 18:37:38 +0400

    fix(protobuf_config/da_client): minor cleanup

--- core/lib/protobuf_config/src/da_client.rs
+++ core/lib/protobuf_config/src/da_client.rs
@@ -15,50 +15,55 @@ impl ProtoRepr for proto::DataAvailabilityClient {
         let config = required(&self.config).context("config")?;
 
         let client = match config {
-            proto::data_availability_client::Config::Avail(conf) => Avail(AvailConfig {
-                api_node_url: if conf.gas_relay_mode.unwrap_or(false) {
-                    None
-                } else {
-                    Some(
-                        required(&conf.api_node_url)
-                            .context("api_node_url")?
+            proto::data_availability_client::Config::Avail(conf) => {
+                Avail(if conf.gas_relay_mode.unwrap_or(false) {
+                    AvailConfig {
+                        api_node_url: None,
+                        bridge_api_url: required(&conf.bridge_api_url)
+                            .context("bridge_api_url")?
                             .clone(),
-                    )
-                },
-                bridge_api_url: required(&conf.bridge_api_url)
-                    .context("bridge_api_url")?
-                    .clone(),
-                app_id: if conf.gas_relay_mode.unwrap_or(false) {
-                    None
+                        app_id: None,
+                        timeout: *required(&conf.timeout).context("timeout")? as usize,
+                        max_retries: *required(&conf.max_retries).context("max_retries")? as usize,
+                        gas_relay_mode: conf
+                            .gas_relay_mode
+                            .context("gas_relay_mode")
+                            .unwrap_or(false),
+                        // if gas_relay_mode is true, then we need to set the gas_relay_api_url and gas_relay_api_key
+                        gas_relay_api_url: Some(
+                            required(&conf.gas_relay_api_url)
+                                .context("gas_relay_api_url")?
+                                .clone(),
+                        ),
+                        gas_relay_api_key: Some(
+                            required(&conf.gas_relay_api_key)
+                                .context("gas_relay_api_key")?
+                                .clone(),
+                        ),
+                    }
                 } else {
-                    Some(*required(&conf.app_id).context("app_id")?)
-                },
-                timeout: *required(&conf.timeout).context("timeout")? as usize,
-                max_retries: *required(&conf.max_retries).context("max_retries")? as usize,
-                gas_relay_mode: conf
-                    .gas_relay_mode
-                    .context("gas_relay_mode")
-                    .unwrap_or(false),
-                // if gas_relay_mode is true, then we need to set the gas_relay_api_url and gas_relay_api_key
-                gas_relay_api_url: if conf.gas_relay_mode.unwrap_or(false) {
-                    Some(
-                        required(&conf.gas_relay_api_url)
-                            .context("gas_relay_api_url")?
+                    AvailConfig {
+                        api_node_url: Some(
+                            required(&conf.api_node_url)
+                                .context("api_node_url")?
+                                .clone(),
+                        ),
+                        bridge_api_url: required(&conf.bridge_api_url)
+                            .context("bridge_api_url")?
                             .clone(),
-                    )
-                } else {
-                    None
-                },
-                gas_relay_api_key: if conf.gas_relay_mode.unwrap_or(false) {
-                    Some(
-                        required(&conf.gas_relay_api_key)
-                            .context("gas_relay_api_key")?
-                            .clone(),
-                    )
-                } else {
-                    None
-                },
-            }),
+                        app_id: Some(*required(&conf.app_id).context("app_id")?),
+                        timeout: *required(&conf.timeout).context("timeout")? as usize,
+                        max_retries: *required(&conf.max_retries).context("max_retries")? as usize,
+                        gas_relay_mode: conf
+                            .gas_relay_mode
+                            .context("gas_relay_mode")
+                            .unwrap_or(false),
+                        // if gas_relay_mode is not true, then the gas_relay_api_url and gas_relay_api_key are not required
+                        gas_relay_api_url: None,
+                        gas_relay_api_key: None,
+                    }
+                })
+            }
             proto::data_availability_client::Config::ObjectStore(conf) => {
                 ObjectStore(object_store_proto::ObjectStore::read(conf)?)
             }
