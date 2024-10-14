f42469bf122f0364d0a5d455ce285e54216d9ad6
Alex Ostrovski <aov@matterlabs.dev>
2024-10-14 12:34:28 +0300

    Sketch `yab` benchmarking

--- core/tests/vm-benchmark/benches/iai.rs
+++ core/tests/vm-benchmark/benches/iai.rs
@@ -1,35 +1,28 @@
-use iai::black_box;
-use vm_benchmark::{BenchmarkingVm, BenchmarkingVmFactory, Bytecode, Fast, Legacy};
+use vm_benchmark::{BenchmarkingVm, BenchmarkingVmFactory, Fast, Legacy, BYTECODES};
+use yab::{Bencher, BenchmarkId};
 
-fn run_bytecode<VM: BenchmarkingVmFactory>(name: &str) {
-    let tx = Bytecode::get(name).deploy_tx();
-    black_box(BenchmarkingVm::<VM>::default().run_transaction(&tx));
+fn benchmarks_for_vm<VM: BenchmarkingVmFactory>(bencher: &mut Bencher) {
+    bencher.bench(
+        BenchmarkId::new("init", VM::LABEL.as_str()),
+        BenchmarkingVm::<VM>::default,
+    );
+
+    for bytecode in BYTECODES {
+        let tx = bytecode.deploy_tx();
+        bencher.bench_with_capture(
+            BenchmarkId::new(bytecode.name, VM::LABEL.as_str()),
+            |capture| {
+                let mut vm = BenchmarkingVm::<VM>::default();
+                capture.measure(|| vm.run_transaction(yab::black_box(&tx)));
+            },
+        );
+    }
 }
 
-macro_rules! make_functions_and_main {
-    ($($file:ident => $legacy_name:ident,)+) => {
-        $(
-        fn $file() {
-            run_bytecode::<Fast>(stringify!($file));
-        }
-
-        fn $legacy_name() {
-            run_bytecode::<Legacy>(stringify!($file));
-        }
-        )+
-
-        iai::main!($($file, $legacy_name,)+);
-    };
+fn benchmarks(bencher: &mut Bencher) {
+    // FIXME: integrate reporting here
+    benchmarks_for_vm::<Fast>(bencher);
+    benchmarks_for_vm::<Legacy>(bencher);
 }
 
-make_functions_and_main!(
-    access_memory => access_memory_legacy,
-    call_far => call_far_legacy,
-    decode_shl_sub => decode_shl_sub_legacy,
-    deploy_simple_contract => deploy_simple_contract_legacy,
-    finish_eventful_frames => finish_eventful_frames_legacy,
-    write_and_decode => write_and_decode_legacy,
-    event_spam => event_spam_legacy,
-    slot_hash_collision => slot_hash_collision_legacy,
-    heap_read_write => heap_read_write_legacy,
-);
+yab::main!(benchmarks);
