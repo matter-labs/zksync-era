e7964effe0f2a94276f61ef0d9d317339b39b6fc
Alex Ostrovski <aov@matterlabs.dev>
2024-10-15 18:57:57 +0300

    Remove more unused files

--- core/lib/multivm/src/versions/vm_latest/tests/tester/mod.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) use vm_tester::{get_empty_storage, InMemoryStorageView, VmTester, VmTesterBuilder};
-pub(crate) use zksync_test_account::{Account, DeployContractsTx, TxType};
-
-pub(crate) use self::inner_state::VmInstanceInnerState; // FIXME
-
-mod inner_state;
-mod transaction_test_info;
-mod vm_tester;
