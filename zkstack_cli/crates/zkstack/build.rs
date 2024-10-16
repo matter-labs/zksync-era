12f16299851d299cccec6cbdc58983d939a84fd6
Manuel Mauro <manuel.mauro@protonmail.com>
2024-10-16 14:32:15 +0200

    feat: copy completion files at build time

--- zkstack_cli/crates/zkstack/build.rs
+++ zkstack_cli/crates/zkstack/build.rs
@@ -1,12 +1,58 @@
 use std::path::PathBuf;
 
+use dirs::{config_local_dir, home_dir};
 use ethers::contract::Abigen;
+use eyre::ContextCompat;
 
 fn main() -> eyre::Result<()> {
     let outdir = PathBuf::from(std::env::var("OUT_DIR")?).canonicalize()?;
     Abigen::new("ConsensusRegistry", "abi/ConsensusRegistry.json")?
         .generate()?
         .write_to_file(outdir.join("consensus_registry_abi.rs"))?;
+
+    //Crate name
+    let crate_name = env!("CARGO_PKG_NAME");
+
+    // Create local config directory
+    let local_config_dir = config_local_dir().unwrap().join(crate_name);
+    std::fs::create_dir_all(&local_config_dir)?;
+
+    // Array of supported shells
+    let shells = ["bash", "zsh"];
+
+    // Copy completion files
+    let completion_dir = local_config_dir.join("completion");
+    std::fs::create_dir_all(&completion_dir)?;
+
+    for shell in &shells {
+        let completion_file = format!("_{}_{}", crate_name, shell);
+        std::fs::copy(
+            format!("completion/{}", &completion_file),
+            completion_dir.join(&completion_file),
+        )?;
+
+        // Source the completion file inside .{shell}rc
+        let shell_rc = home_dir()
+            .context("missing home directory")?
+            .join(format!(".{}rc", shell));
+
+        if shell_rc.exists() {
+            let shell_rc_content = std::fs::read_to_string(&shell_rc)?;
+
+            if !shell_rc_content.contains("# zkstack completion") {
+                let completion_path = completion_dir.join(&completion_file);
+                let completion_path = completion_path.to_str().unwrap();
+
+                std::fs::write(
+                    shell_rc,
+                    format!(
+                        "{}\n# zkstack completion\nsource \"{}\"\n\n",
+                        shell_rc_content, completion_path
+                    ),
+                )?;
+            }
+        }
+    }
 
     zksync_protobuf_build::Config {
         input_root: "src/commands/consensus/proto".into(),
