b59dd1c7989e139cad7e3c6d160667c8b1edfd20
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-10-16 15:59:31 +0200

    new upgrade system contracts

--- core/lib/types/src/abi.rs
+++ core/lib/types/src/abi.rs
@@ -427,7 +427,7 @@ impl GatewayUpgradeEncodedInput {
     /// Returns an error if token doesn't match the `schema()`.
     pub fn decode(token: Token) -> anyhow::Result<Self> {
         let tokens = token.into_tuple().context("not a tuple")?;
-        anyhow::ensure!(tokens.len() == 7);
+        anyhow::ensure!(tokens.len() == 6);
         let mut t = tokens.into_iter();
         let mut next = || t.next().unwrap();
 
