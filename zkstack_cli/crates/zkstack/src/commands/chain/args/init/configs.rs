e4fd78776b241de566a40579dbc766da338a6e10
Manuel Mauro <manuel.mauro@protonmail.com>
2024-10-17 15:57:15 +0200

    test: check no_port_reallocation flag

--- zkstack_cli/crates/zkstack/src/commands/chain/args/init/configs.rs
+++ zkstack_cli/crates/zkstack/src/commands/chain/args/init/configs.rs
@@ -24,7 +24,6 @@ pub struct InitConfigsArgs {
     pub genesis_args: GenesisArgs,
     #[clap(long, help = MSG_L1_RPC_URL_HELP)]
     pub l1_rpc_url: Option<String>,
-    // TODO: test this
     #[clap(long, help = MSG_NO_PORT_REALLOCATION_HELP)]
     pub no_port_reallocation: bool,
 }
--- zkstack_cli/crates/zkstack/src/commands/chain/args/init/mod.rs
+++ zkstack_cli/crates/zkstack/src/commands/chain/args/init/mod.rs
@@ -29,7 +29,6 @@ pub struct InitArgs {
     pub deploy_paymaster: Option<bool>,
     #[clap(long, help = MSG_L1_RPC_URL_HELP)]
     pub l1_rpc_url: Option<String>,
-    // TODO: test this
     #[clap(long, help = MSG_NO_PORT_REALLOCATION_HELP)]
     pub no_port_reallocation: bool,
 }
