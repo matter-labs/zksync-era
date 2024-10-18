fad450310af95dc0597f17a32e3b1c30b4505ae7
Daniyar Itegulov <di@matterlabs.dev>
2024-10-19 00:47:33 +1100

    fix(external-node): delete empty unsealed batch on EN initialization (#3125)
    ## What ❔
    
    This PR reverts #3088 as I have realized it is going to be very hard to
    make this fix work by going in that direction. Basically initializing
    with an empty unsealed batch causes a lot of issues and the existing
    state keeper/external IO flow heavily relies on us having at least one
    at the start to initialize correctly. Will leave more context in the
    comments.
    
    Feel free to review individual commits to not see revert changelog.
    
    ## Why ❔
    
    This bug causes EN to panic
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [x] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [x] Tests for the changes have been added / updated.
    - [x] Documentation comments have been added / updated.
    - [x] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.

--- core/node/node_sync/src/external_io.rs
+++ core/node/node_sync/src/external_io.rs
@@ -155,6 +155,14 @@ impl StateKeeperIO for ExternalIO {
                 )
             })?;
         let Some(mut pending_l2_block_header) = pending_l2_block_header else {
+            tracing::info!(
+                l1_batch_number = %cursor.l1_batch,
+                "No pending L2 blocks found; pruning unsealed batch if exists as we need at least one L2 block to initialize"
+            );
+            storage
+                .blocks_dal()
+                .delete_unsealed_l1_batch(cursor.l1_batch - 1)
+                .await?;
             return Ok((cursor, None));
         };
 
