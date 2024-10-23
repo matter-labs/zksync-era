7c59c3c5d3b08cc2c23f8de473fcf10d64c01075
Daniyar Itegulov <di@matterlabs.dev>
2024-10-23 18:29:48 +1100

    reduce logs

--- core/lib/merkle_tree/src/storage/rocksdb.rs
+++ core/lib/merkle_tree/src/storage/rocksdb.rs
@@ -209,12 +209,14 @@ impl Database for RocksDBWrapper {
         let raw_nodes = self.raw_nodes(keys).into_iter().zip(keys);
 
         let nodes = raw_nodes.map(|(maybe_node, (key, is_leaf))| {
-            println!(
-                "Loading raw node: key={}, is_leaf={}, found={:?}",
-                hex::encode(key.to_db_key()),
-                is_leaf,
-                maybe_node.is_some(),
-            );
+            if maybe_node.is_none() {
+                println!(
+                    "Loading raw node: key={}, is_leaf={}, found={:?}",
+                    hex::encode(key.to_db_key()),
+                    is_leaf,
+                    maybe_node.is_some(),
+                );
+            }
             maybe_node
                 .map(|raw_node| Self::deserialize_node(&raw_node, key, *is_leaf))
                 .transpose()
