f4c9a412223aad4b7005c3c19be2775bd843f983
Joonatan Saarhelo <joon.saar@gmail.com>
2024-10-23 16:20:48 +0200

    long comment explaining validation rules' purpose

--- core/lib/multivm/src/versions/vm_fast/validation_tracer.rs
+++ core/lib/multivm/src/versions/vm_fast/validation_tracer.rs
@@ -5,6 +5,30 @@ use zksync_vm_interface::tracer::ViolatedValidationRule;
 
 use super::vm::TracerExt;
 
+/// Account abstraction exposes a chain to denial of service attacks because someone who fails to
+/// authenticate does not pay for the failed transaction. Otherwise people could empty other's
+/// wallets for free!
+///
+/// If some address repeatedly posts transactions that validate during preliminary checks but fail
+/// to validate during the actual execution, that address is considered a spammer. However, when
+/// the spam comes from multiple addresses, that doesn't work.
+///
+/// We want to ensure that a spammer has to pay for every account that fails validation. This is
+/// achieved by limiting what the code of a custom account is allowed to do. If we allowed access
+/// to things like time, a validation that fails in the sequencer could be crafted for free, so we
+/// don't.
+///
+/// However, we want to give access to storage. A spammer has to pay for changing storage but
+/// could change just one storage slot to invalidate transactions from many accounts. To prevent
+/// that, we make sure that the storage slots accessed by different accounts are disjoint by only
+/// allowing access to storage in the account itself and slots derived from the account's address.
+///
+/// Our rules are an extension of the rules are outlined in EIP-7562.
+///
+/// This tracer enforces the rules by checking what the code does at runtime, even though the
+/// properties checked are supposed to always hold for a well-written custom account. Proving
+/// that a contract adheres to the rules ahead of time would be challenging or even impossible,
+/// considering that contracts that the code depends on may get upgraded.
 #[derive(Debug, Default)]
 pub struct ValidationTracer {
     out_of_gas: bool,
