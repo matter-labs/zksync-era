9df63c22d598f9103720ee105100b506cf990590
Joonatan Saarhelo <joon.saar@gmail.com>
2024-11-14 13:18:30 +0100

    reduce repetition

--- core/lib/multivm/src/versions/testonly/account_validation_rules.rs
+++ core/lib/multivm/src/versions/testonly/account_validation_rules.rs
@@ -1,13 +1,10 @@
 use assert_matches::assert_matches;
-use zksync_types::{
-    fee::Fee, l2::L2Tx, transaction_request::TransactionRequest, u256_to_h256, AccountTreeId,
-    Address, Eip712Domain, L2ChainId, StorageKey, U256,
-};
+use zksync_types::{u256_to_h256, AccountTreeId, Address, StorageKey};
 use zksync_vm_interface::tracer::ViolatedValidationRule;
 
 use super::{
-    get_empty_storage, read_validation_test_contract, tester::VmTesterBuilder, ContractToDeploy,
-    TestedVm, TestedVmForValidation,
+    get_empty_storage, read_validation_test_contract, require_eip712::make_aa_transaction,
+    tester::VmTesterBuilder, ContractToDeploy, TestedVm, TestedVmForValidation,
 };
 use crate::interface::TxExecutionMode;
 
@@ -54,40 +51,8 @@ fn test_rule<VM: TestedVm + TestedVmForValidation>(rule: u32) -> Option<Violated
 
     let private_account = vm.rich_accounts[0].clone();
 
-    // Use account abstraction
-    let chain_id: u32 = 270;
-
-    let tx_712 = L2Tx::new(
-        Some(beneficiary_address),
-        vec![],
-        private_account.nonce,
-        Fee {
-            gas_limit: U256::from(1000000000),
-            max_fee_per_gas: U256::from(1000000000),
-            max_priority_fee_per_gas: U256::from(1000000000),
-            gas_per_pubdata_limit: U256::from(1000000000),
-        },
-        aa_address,
-        U256::from(28374938),
-        vec![],
-        Default::default(),
-    );
-
-    let mut transaction_request: TransactionRequest = tx_712.into();
-    transaction_request.chain_id = Some(chain_id.into());
-
-    let domain = Eip712Domain::new(L2ChainId::from(chain_id));
-    let signature = private_account
-        .get_pk_signer()
-        .sign_typed_data(&domain, &transaction_request)
-        .unwrap();
-    let encoded_tx = transaction_request.get_signed_bytes(&signature).unwrap();
-
-    let (aa_txn_request, aa_hash) =
-        TransactionRequest::from_bytes(&encoded_tx, L2ChainId::from(chain_id)).unwrap();
-
-    let mut l2_tx = L2Tx::from_request(aa_txn_request, 100000, false).unwrap();
-    l2_tx.set_input(encoded_tx, aa_hash);
-
-    vm.vm.run_validation(l2_tx, 55)
+    vm.vm.run_validation(
+        make_aa_transaction(aa_address, beneficiary_address, &private_account),
+        55,
+    )
 }
