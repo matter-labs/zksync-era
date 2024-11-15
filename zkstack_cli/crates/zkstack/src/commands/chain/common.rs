48490c15773e5e8da57ddabc1366dacb5a1da696
matias-gonz <maigonzalez@fi.uba.ar>
2024-11-15 13:03:31 -0300

    Refactor distribute_eth and mint_base_token

--- zkstack_cli/crates/zkstack/src/commands/chain/common.rs
+++ zkstack_cli/crates/zkstack/src/commands/chain/common.rs
@@ -13,33 +13,36 @@ pub async fn distribute_eth(
     chain_config: &ChainConfig,
     l1_rpc_url: String,
 ) -> anyhow::Result<()> {
-    if chain_config.wallet_creation == WalletCreation::Localhost
-        && chain_config.l1_network == L1Network::Localhost
+    if chain_config.wallet_creation != WalletCreation::Localhost
+        || chain_config.l1_network != L1Network::Localhost
     {
-        let spinner = Spinner::new(MSG_DISTRIBUTING_ETH_SPINNER);
-        let wallets = ecosystem_config.get_wallets()?;
-        let chain_wallets = chain_config.get_wallets_config()?;
-        let mut addresses = vec![
-            chain_wallets.operator.address,
-            chain_wallets.blob_operator.address,
-            chain_wallets.governor.address,
-        ];
-        if let Some(deployer) = chain_wallets.deployer {
-            addresses.push(deployer.address)
-        }
-        if let Some(setter) = chain_wallets.token_multiplier_setter {
-            addresses.push(setter.address)
-        }
-        common::ethereum::distribute_eth(
-            wallets.operator,
-            addresses,
-            l1_rpc_url,
-            chain_config.l1_network.chain_id(),
-            AMOUNT_FOR_DISTRIBUTION_TO_WALLETS,
-        )
-        .await?;
-        spinner.finish();
+        return Ok(());
     }
+
+    let spinner = Spinner::new(MSG_DISTRIBUTING_ETH_SPINNER);
+    let wallets = ecosystem_config.get_wallets()?;
+    let chain_wallets = chain_config.get_wallets_config()?;
+    let mut addresses = vec![
+        chain_wallets.operator.address,
+        chain_wallets.blob_operator.address,
+        chain_wallets.governor.address,
+    ];
+    if let Some(deployer) = chain_wallets.deployer {
+        addresses.push(deployer.address);
+    }
+    if let Some(setter) = chain_wallets.token_multiplier_setter {
+        addresses.push(setter.address);
+    }
+    common::ethereum::distribute_eth(
+        wallets.operator,
+        addresses,
+        l1_rpc_url,
+        chain_config.l1_network.chain_id(),
+        AMOUNT_FOR_DISTRIBUTION_TO_WALLETS,
+    )
+    .await?;
+    spinner.finish();
+
     Ok(())
 }
 
@@ -48,27 +51,30 @@ pub async fn mint_base_token(
     chain_config: &ChainConfig,
     l1_rpc_url: String,
 ) -> anyhow::Result<()> {
-    if chain_config.wallet_creation == WalletCreation::Localhost
-        && chain_config.l1_network == L1Network::Localhost
-        && chain_config.base_token != BaseToken::eth()
+    if chain_config.wallet_creation != WalletCreation::Localhost
+        || chain_config.l1_network != L1Network::Localhost
+        || chain_config.base_token == BaseToken::eth()
     {
-        let spinner = Spinner::new(MSG_MINT_BASE_TOKEN_SPINNER);
-        let wallets = ecosystem_config.get_wallets()?;
-        let chain_wallets = chain_config.get_wallets_config()?;
-        let base_token = &chain_config.base_token;
-        let addresses = vec![wallets.governor.address, chain_wallets.governor.address];
-        let amount = AMOUNT_FOR_DISTRIBUTION_TO_WALLETS * base_token.nominator as u128
-            / base_token.denominator as u128;
-        common::ethereum::mint_token(
-            wallets.governor,
-            base_token.address,
-            addresses,
-            l1_rpc_url,
-            chain_config.l1_network.chain_id(),
-            amount,
-        )
-        .await?;
-        spinner.finish();
+        return Ok(());
     }
+
+    let spinner = Spinner::new(MSG_MINT_BASE_TOKEN_SPINNER);
+    let wallets = ecosystem_config.get_wallets()?;
+    let chain_wallets = chain_config.get_wallets_config()?;
+    let base_token = &chain_config.base_token;
+    let addresses = vec![wallets.governor.address, chain_wallets.governor.address];
+    let amount = AMOUNT_FOR_DISTRIBUTION_TO_WALLETS * base_token.nominator as u128
+        / base_token.denominator as u128;
+    common::ethereum::mint_token(
+        wallets.governor,
+        base_token.address,
+        addresses,
+        l1_rpc_url,
+        chain_config.l1_network.chain_id(),
+        amount,
+    )
+    .await?;
+    spinner.finish();
+
     Ok(())
 }
