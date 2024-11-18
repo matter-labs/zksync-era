0c366dd9f0eb112976e0c5c1ae18308d37d754c0
kelemeno <kl@matterlabs.dev>
2024-11-18 08:35:13 +0000

    small fixes

--- .github/scripts/interop.sh
+++ .github/scripts/interop.sh
@@ -1,16 +1,16 @@
-sudo rm -rf ./volumes && zk_supervisor clean containers && zk_inception up -o false
+sudo rm -rf ./volumes && zkstack dev clean containers && zkstack up -o false
 
-zk_inception ecosystem init --deploy-paymaster --deploy-erc20 \
+zkstack ecosystem init --deploy-paymaster --deploy-erc20 \
             --deploy-ecosystem --l1-rpc-url=http://localhost:8545 \
             --server-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
             --server-db-name=zksync_server_localhost_era \
-            --prover-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
-            --prover-db-name=zksync_prover_localhost_era \
             --ignore-prerequisites --observability=false --skip-submodules-checkout \
             --chain era # --no-port-reallocation
+        #     --prover-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
+        #     --prover-db-name=zksync_prover_localhost_era \
             # --skip-contract-compilation-override \
 
-zk_inception chain create \
+zkstack chain create \
         --chain-name second \
         --chain-id 505 \
         --prover-mode no-proofs \
@@ -22,19 +22,19 @@ zk_inception chain create \
         --set-as-default false \
         --ignore-prerequisites --skip-submodules-checkout --skip-contract-compilation-override
 
-zk_inception chain init \
+zkstack chain init \
             --deploy-paymaster \
             --l1-rpc-url=http://localhost:8545 \
             --server-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
             --server-db-name=zksync_server_localhost_gateway \
-            --prover-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
-            --prover-db-name=zksync_prover_localhost_gateway \
             --chain second --skip-submodules-checkout --no-port-reallocation
+        #     --prover-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
+        #     --prover-db-name=zksync_prover_localhost_gateway \
 
-zk_inception server --ignore-prerequisites --chain era &> ./rollup.log & 
+zkstack server --ignore-prerequisites --chain era &> ./rollup.log & 
 
-zk_inception server --ignore-prerequisites --chain second &> ./second.log & 
+zkstack server --ignore-prerequisites --chain second &> ./second.log & 
 
 # sleep 20
 
-# zk_supervisor test integration -t "Interop" --no-deps --ignore-prerequisites --chain era
+# zkstack test integration -t "Interop" --no-deps --ignore-prerequisites --chain era
--- core/lib/types/src/system_contracts.rs
+++ core/lib/types/src/system_contracts.rs
@@ -28,7 +28,7 @@ use crate::{
 pub const TX_NONCE_INCREMENT: U256 = U256([1, 0, 0, 0]); // 1
 pub const DEPLOYMENT_NONCE_INCREMENT: U256 = U256([0, 0, 1, 0]); // 2^128
 
-static SYSTEM_CONTRACT_LIST: [(&str, &str, Address, ContractLanguage); 33] = [
+static SYSTEM_CONTRACT_LIST: [(&str, &str, Address, ContractLanguage); 36] = [
     (
         "",
         "AccountCodeStorage",
