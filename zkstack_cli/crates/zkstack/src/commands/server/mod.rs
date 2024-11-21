9d72c07958eb8f209d0fe7920b45a9bf9652bcaf
Manuel Mauro <manuel.mauro@protonmail.com>
2024-11-21 15:46:48 +0100

    refactor: create module for run subcommand

--- zkstack_cli/crates/zkstack/src/commands/server/mod.rs
+++ zkstack_cli/crates/zkstack/src/commands/server/mod.rs
@@ -1,22 +1,17 @@
 use anyhow::Context;
 use build::build_server;
-use common::{
-    logger,
-    server::{Server, ServerMode},
-};
-use config::{
-    traits::FileConfigWithDefaultName, ChainConfig, ContractsConfig, EcosystemConfig,
-    GeneralConfig, GenesisConfig, SecretsConfig, WalletsConfig,
-};
+use config::EcosystemConfig;
+use run::run_server;
 use wait::wait_for_server;
 use xshell::Shell;
 
 use crate::{
-    commands::args::{RunServerArgs, ServerArgs, ServerCommand},
-    messages::{MSG_CHAIN_NOT_INITIALIZED, MSG_FAILED_TO_RUN_SERVER_ERR, MSG_STARTING_SERVER},
+    commands::args::{ServerArgs, ServerCommand},
+    messages::MSG_CHAIN_NOT_INITIALIZED,
 };
 
 mod build;
+mod run;
 mod wait;
 
 pub async fn run(shell: &Shell, args: ServerArgs) -> anyhow::Result<()> {
@@ -30,35 +25,4 @@ pub async fn run(shell: &Shell, args: ServerArgs) -> anyhow::Result<()> {
         ServerCommand::Build => build_server(&chain_config, shell),
         ServerCommand::Wait(args) => wait_for_server(args, &chain_config).await,
     }
-}
-
-fn run_server(
-    args: RunServerArgs,
-    chain_config: &ChainConfig,
-    shell: &Shell,
-) -> anyhow::Result<()> {
-    logger::info(MSG_STARTING_SERVER);
-    let server = Server::new(
-        args.components.clone(),
-        chain_config.link_to_code.clone(),
-        args.uring,
-    );
-
-    let mode = if args.genesis {
-        ServerMode::Genesis
-    } else {
-        ServerMode::Normal
-    };
-    server
-        .run(
-            shell,
-            mode,
-            GenesisConfig::get_path_with_base_path(&chain_config.configs),
-            WalletsConfig::get_path_with_base_path(&chain_config.configs),
-            GeneralConfig::get_path_with_base_path(&chain_config.configs),
-            SecretsConfig::get_path_with_base_path(&chain_config.configs),
-            ContractsConfig::get_path_with_base_path(&chain_config.configs),
-            vec![],
-        )
-        .context(MSG_FAILED_TO_RUN_SERVER_ERR)
 }
---
+++ zkstack_cli/crates/zkstack/src/commands/server/run.rs
@@ -0,0 +1,46 @@
+use anyhow::Context;
+use common::{
+    logger,
+    server::{Server, ServerMode},
+};
+use config::{
+    traits::FileConfigWithDefaultName, ChainConfig, ContractsConfig, GeneralConfig, GenesisConfig,
+    SecretsConfig, WalletsConfig,
+};
+use xshell::Shell;
+
+use crate::{
+    commands::args::RunServerArgs,
+    messages::{MSG_FAILED_TO_RUN_SERVER_ERR, MSG_STARTING_SERVER},
+};
+
+pub(super) fn run_server(
+    args: RunServerArgs,
+    chain_config: &ChainConfig,
+    shell: &Shell,
+) -> anyhow::Result<()> {
+    logger::info(MSG_STARTING_SERVER);
+    let server = Server::new(
+        args.components.clone(),
+        chain_config.link_to_code.clone(),
+        args.uring,
+    );
+
+    let mode = if args.genesis {
+        ServerMode::Genesis
+    } else {
+        ServerMode::Normal
+    };
+    server
+        .run(
+            shell,
+            mode,
+            GenesisConfig::get_path_with_base_path(&chain_config.configs),
+            WalletsConfig::get_path_with_base_path(&chain_config.configs),
+            GeneralConfig::get_path_with_base_path(&chain_config.configs),
+            SecretsConfig::get_path_with_base_path(&chain_config.configs),
+            ContractsConfig::get_path_with_base_path(&chain_config.configs),
+            vec![],
+        )
+        .context(MSG_FAILED_TO_RUN_SERVER_ERR)
+}
