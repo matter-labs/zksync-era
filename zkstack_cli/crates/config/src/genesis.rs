b9d60f5e3054a6a70c830824c09021ceec366c02
Vladislav Volosnikov <Volosnikov.apmath@gmail.com>
2024-12-11 15:06:09 +0100

    Fix genesis without EVM emulator

--- zkstack_cli/crates/config/src/genesis.rs
+++ zkstack_cli/crates/config/src/genesis.rs
@@ -20,14 +20,6 @@ pub fn update_from_chain_config(
     // TODO(EVM-676): for now, the settlement layer is always the same as the L1 network
     genesis.l1_chain_id = L1ChainId(config.l1_network.chain_id());
     genesis.l1_batch_commit_data_generator_mode = config.l1_batch_commit_data_generator_mode;
-    genesis.evm_emulator_hash = if config.evm_emulator {
-        Some(genesis.evm_emulator_hash.context(
-            "impossible to initialize a chain with EVM emulator: the template genesis config \
-             does not contain EVM emulator hash",
-        )?)
-    } else {
-        None
-    };
     Ok(())
 }
 
