5a4f53f109ad4e040e2a3e72b28e1941d35edf50
Lech <88630083+Artemka374@users.noreply.github.com>
2024-12-13 12:32:03 +0200

    fix zkstack

--- zkstack_cli/crates/zkstack/src/commands/chain/args/init/mod.rs
+++ zkstack_cli/crates/zkstack/src/commands/chain/args/init/mod.rs
@@ -35,7 +35,7 @@ pub struct InitArgs {
     pub l1_rpc_url: Option<String>,
     #[clap(long, help = MSG_NO_PORT_REALLOCATION_HELP)]
     pub no_port_reallocation: bool,
-    #[clap(long, default_value_t = "true")]
+    #[clap(long, default_value_t = true)]
     pub update_submodules: bool,
     #[clap(long, help = MSG_DEV_ARG_HELP)]
     pub dev: bool,
