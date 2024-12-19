d2ae607f7d526efd784b5e143d2d6bd92d90ebbd
Stanislav Breadless <stanislavbezkor@gmail.com>
2024-12-19 12:27:57 +0100

    fix mistake

--- core/lib/multivm/src/versions/testonly/refunds.rs
+++ core/lib/multivm/src/versions/testonly/refunds.rs
@@ -78,7 +78,7 @@ pub(crate) fn test_predetermined_refunded_gas<VM: TestedVm>() {
         current_state_without_predefined_refunds.events
     );
 
-    assert_ne!(
+    assert_eq!(
         current_state_with_predefined_refunds.user_l2_to_l1_logs,
         current_state_without_predefined_refunds.user_l2_to_l1_logs
     );
@@ -140,7 +140,7 @@ pub(crate) fn test_predetermined_refunded_gas<VM: TestedVm>() {
         current_state_without_predefined_refunds.user_l2_to_l1_logs
     );
 
-    assert_eq!(
+    assert_ne!(
         current_state_with_changed_predefined_refunds.system_logs,
         current_state_without_predefined_refunds.system_logs
     );
