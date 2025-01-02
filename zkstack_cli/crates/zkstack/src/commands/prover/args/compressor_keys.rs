1a297bedd226c56fc2ba02dc54d79129a271a1eb
Artem Fomiuk <88630083+Artemka374@users.noreply.github.com>
2025-01-02 15:28:07 +0200

    feat: FFLONK support for compressor (#3359)
    ## What ❔
    
    Enables support of FFLONK compression
    * Compressor now can be run with `--flonk=true` flag to run compressor
    in FFLONK mode(or by running `zkstack prover run --component compressor
    --mode=fflonk`), default mode is still PLONK
    * Final proof is now an enum: it can be either
    `L1BatchProofForL1::Fflonk` or `L1BatchProofForL1::Plonk` containing the
    proof inside
    * We are now making use out of `compression_mode` environment variable -
    FFLONK compressor can be run in 5 modes - 5th one is the most effective,
    so default value is 5.
    * VK setup data generator is updated to generate setup and verification
    keys for FFLONK as well.
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zk fmt` and `zk lint`.
    
    ---------
    
    Co-authored-by: Daniyar Itegulov <ditegulov@gmail.com>
    Co-authored-by: Igor Aleksanov <popzxc@yandex.ru>
    Co-authored-by: zksync-admin-bot2 <91326834+zksync-admin-bot2@users.noreply.github.com>

--- zkstack_cli/crates/zkstack/src/commands/prover/args/compressor_keys.rs
+++ zkstack_cli/crates/zkstack/src/commands/prover/args/compressor_keys.rs
@@ -1,22 +1,57 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use common::Prompt;
+use strum::EnumIter;
 
 use crate::messages::MSG_SETUP_COMPRESSOR_KEY_PATH_PROMPT;
 
 #[derive(Debug, Clone, Parser, Default)]
 pub struct CompressorKeysArgs {
     #[clap(long)]
-    pub path: Option<String>,
+    pub plonk_path: Option<String>,
+    #[clap(long)]
+    pub fflonk_path: Option<String>,
+    #[clap(long, default_value = "plonk")]
+    pub compressor_type: CompressorType,
+}
+
+#[derive(Debug, Clone, ValueEnum, EnumIter, strum::Display, PartialEq, Eq, Default)]
+pub enum CompressorType {
+    Fflonk,
+    #[default]
+    Plonk,
+    All,
 }
 
 impl CompressorKeysArgs {
-    pub fn fill_values_with_prompt(self, default: &str) -> CompressorKeysArgs {
-        let path = self.path.unwrap_or_else(|| {
-            Prompt::new(MSG_SETUP_COMPRESSOR_KEY_PATH_PROMPT)
-                .default(default)
-                .ask()
-        });
+    pub fn fill_values_with_prompt(
+        self,
+        default_plonk_path: &str,
+        default_fflonk_path: &str,
+    ) -> CompressorKeysArgs {
+        let plonk_path = if self.compressor_type != CompressorType::Fflonk {
+            Some(self.plonk_path.unwrap_or_else(|| {
+                Prompt::new(MSG_SETUP_COMPRESSOR_KEY_PATH_PROMPT)
+                    .default(default_plonk_path)
+                    .ask()
+            }))
+        } else {
+            None
+        };
 
-        CompressorKeysArgs { path: Some(path) }
+        let fflonk_path = if self.compressor_type != CompressorType::Plonk {
+            Some(self.fflonk_path.unwrap_or_else(|| {
+                Prompt::new(MSG_SETUP_COMPRESSOR_KEY_PATH_PROMPT)
+                    .default(default_fflonk_path)
+                    .ask()
+            }))
+        } else {
+            None
+        };
+
+        CompressorKeysArgs {
+            plonk_path,
+            fflonk_path,
+            ..self
+        }
     }
 }
