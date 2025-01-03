66020ceb4809611f14846cda797a4f64d294629b
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-01-03 10:53:14 +0100

    add todos

--- zkstack_cli/crates/zkstack/src/commands/chain/convert_to_gateway.rs
+++ zkstack_cli/crates/zkstack/src/commands/chain/convert_to_gateway.rs
@@ -283,7 +283,9 @@ async fn gateway_governance_whitelisting(
     .await?
     .governance_l2_tx_hash;
 
+    // TOOD(EVM-931): adapt for more generic functionality from the rest of zkstack
     if !with_broadcast {
+        // TODO(EVM-930): the path below relies explicitly on chain id 9.
         shell.copy_file(
             config.link_to_code.join("contracts/l1-contracts/broadcast/GatewayPreparation.s.sol/9/dry-run/932d9a4d-latest.json"),
             config.link_to_code.join(GATEWAY_GOVERNANCE_TX_PATH1),
@@ -313,7 +315,9 @@ async fn gateway_governance_whitelisting(
     .await?
     .governance_l2_tx_hash;
 
+    // TOOD(EVM-931): adapt for more generic functionality from the rest of zkstack
     if !with_broadcast {
+        // TODO(EVM-930): the path below relies explicitly on chain id 9.
         shell.copy_file(
             config.link_to_code.join("contracts/l1-contracts/broadcast/GatewayPreparation.s.sol/9/dry-run/e518d36a-latest.json"),
             config.link_to_code.join(GATEWAY_GOVERNANCE_TX_PATH1),
@@ -341,7 +345,9 @@ async fn gateway_governance_whitelisting(
     .await?
     .governance_l2_tx_hash;
 
+    // TOOD(EVM-931): adapt for more generic functionality from the rest of zkstack
     if !with_broadcast {
+        // TODO(EVM-930): the path below relies explicitly on chain id 9.
         shell.copy_file(
             config.link_to_code.join("contracts/l1-contracts/broadcast/GatewayPreparation.s.sol/9/dry-run/98b2aab7-latest.json"),
             config.link_to_code.join(GATEWAY_GOVERNANCE_TX_PATH1),
@@ -372,7 +378,9 @@ async fn gateway_governance_whitelisting(
     .await?
     .governance_l2_tx_hash;
 
+    // TOOD(EVM-931): adapt for more generic functionality from the rest of zkstack
     if !with_broadcast {
+        // TODO(EVM-930): the path below relies explicitly on chain id 9.
         shell.copy_file(
             config.link_to_code.join("contracts/l1-contracts/broadcast/GatewayPreparation.s.sol/9/dry-run/b620eb4c-latest.json"),
             config.link_to_code.join(GATEWAY_GOVERNANCE_TX_PATH1),
