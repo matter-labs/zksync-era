338433a81fdb734d7fec620adb83ec53544e3c50
Alex Ostrovski <aov@matterlabs.dev>
2025-01-08 18:04:30 +0200

    Add more address derivation logic for deployments

--- core/lib/types/src/tx/execute.rs
+++ core/lib/types/src/tx/execute.rs
@@ -1,6 +1,6 @@
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
-use zksync_basic_types::bytecode::BytecodeHash;
+use zksync_basic_types::{address_to_h256, bytecode::BytecodeHash, web3::keccak256};
 use zksync_system_constants::CONTRACT_DEPLOYER_ADDRESS;
 
 use crate::{
@@ -88,6 +88,29 @@ impl EIP712TypedStructure for Execute {
     }
 }
 
+const CREATE_PARAMS: &[ethabi::ParamType] = &[
+    ethabi::ParamType::FixedBytes(32),
+    ethabi::ParamType::FixedBytes(32),
+    ethabi::ParamType::Bytes,
+];
+const CREATE_ACC_PARAMS: &[ethabi::ParamType] = &[
+    ethabi::ParamType::FixedBytes(32),
+    ethabi::ParamType::FixedBytes(32),
+    ethabi::ParamType::Bytes,
+    ethabi::ParamType::Uint(8),
+];
+
+// TODO (SMA-1608): We should not re-implement the ABI parts in different places, instead have the ABI available
+//  from the `zksync_contracts` crate.
+static CREATE_FUNCTION: Lazy<[u8; 4]> =
+    Lazy::new(|| ethabi::short_signature("create", CREATE_PARAMS));
+static CREATE2_FUNCTION: Lazy<[u8; 4]> =
+    Lazy::new(|| ethabi::short_signature("create2", CREATE_PARAMS));
+static CREATE_ACC_FUNCTION: Lazy<[u8; 4]> =
+    Lazy::new(|| ethabi::short_signature("createAccount", CREATE_ACC_PARAMS));
+static CREATE2_ACC_FUNCTION: Lazy<[u8; 4]> =
+    Lazy::new(|| ethabi::short_signature("create2Account", CREATE_ACC_PARAMS));
+
 impl Execute {
     pub fn calldata(&self) -> &[u8] {
         &self.calldata
@@ -99,25 +122,12 @@ impl Execute {
         contract_hash: H256,
         constructor_input: Vec<u8>,
     ) -> Vec<u8> {
-        // TODO (SMA-1608): We should not re-implement the ABI parts in different places, instead have the ABI available
-        //  from the `zksync_contracts` crate.
-        static FUNCTION_SIGNATURE: Lazy<[u8; 4]> = Lazy::new(|| {
-            ethabi::short_signature(
-                "create",
-                &[
-                    ethabi::ParamType::FixedBytes(32),
-                    ethabi::ParamType::FixedBytes(32),
-                    ethabi::ParamType::Bytes,
-                ],
-            )
-        });
         let params = ethabi::encode(&[
             ethabi::Token::FixedBytes(salt.as_bytes().to_vec()),
             ethabi::Token::FixedBytes(contract_hash.as_bytes().to_vec()),
             ethabi::Token::Bytes(constructor_input),
         ]);
-
-        FUNCTION_SIGNATURE.iter().copied().chain(params).collect()
+        CREATE_FUNCTION.iter().copied().chain(params).collect()
     }
 
     /// Creates an instance for deploying the specified bytecode without additional dependencies. If necessary,
@@ -148,5 +158,90 @@ impl Execute {
             value,
             factory_deps: vec![],
         }
+    }
+}
+
+/// Deployment params for a `ContractDeployer.{create2, create2Account}` call.
+#[derive(Debug)]
+pub struct Create2DeploymentParams {
+    pub salt: H256,
+    pub bytecode_hash: H256,
+    pub raw_constructor_input: Vec<u8>,
+}
+
+impl Create2DeploymentParams {
+    /// Assumes tokens have expected shape.
+    fn from_tokens(tokens: Vec<ethabi::Token>) -> Self {
+        let mut tokens = tokens.into_iter();
+        // Salt is the first token. `unwrap()`s are safe because of the successful decoding.
+        let salt = tokens.next().unwrap();
+        let salt = H256::from_slice(&salt.into_fixed_bytes().unwrap());
+        // Bytecode hash is the second token.
+        let bytecode_hash = tokens.next().unwrap();
+        let bytecode_hash = H256::from_slice(&bytecode_hash.into_fixed_bytes().unwrap());
+        // Raw constructor input is the 3rd token.
+        let raw_constructor_input = tokens.next().unwrap();
+        let raw_constructor_input = raw_constructor_input.into_bytes().unwrap();
+
+        Self {
+            salt,
+            bytecode_hash,
+            raw_constructor_input,
+        }
+    }
+
+    /// Pre-calculates the address of the to-be-deployed EraVM contract (via CREATE2).
+    // FIXME: test that this is correct
+    pub fn derive_address(&self, sender: Address) -> Address {
+        let prefix_bytes = keccak256("zksyncCreate2".as_bytes());
+        let address_bytes = address_to_h256(&sender);
+
+        let mut bytes = [0u8; 160];
+        bytes[..32].copy_from_slice(&prefix_bytes);
+        bytes[32..64].copy_from_slice(address_bytes.as_bytes());
+        bytes[64..96].copy_from_slice(self.salt.as_bytes());
+        bytes[96..128].copy_from_slice(self.bytecode_hash.as_bytes());
+        bytes[128..].copy_from_slice(&keccak256(&self.raw_constructor_input));
+
+        Address::from_slice(&keccak256(&bytes)[12..])
+    }
+}
+
+/// Deployment params encoding various canonical ways to deploy EraVM contracts.
+#[derive(Debug)]
+pub enum DeploymentParams {
+    Create,
+    Create2(Create2DeploymentParams),
+    CreateAccount,
+    Create2Account(Create2DeploymentParams),
+}
+
+impl DeploymentParams {
+    pub fn decode(calldata: &[u8]) -> anyhow::Result<Option<Self>> {
+        if calldata.len() < 4 {
+            return Ok(None);
+        }
+        let (short_signature, token_data) = calldata.split_at(4);
+        Ok(match short_signature {
+            sig if sig == *CREATE_FUNCTION => {
+                ethabi::decode(CREATE_PARAMS, token_data)?;
+                Some(Self::Create)
+            }
+            sig if sig == *CREATE2_FUNCTION => {
+                let tokens = ethabi::decode(CREATE_PARAMS, token_data)?;
+                Some(Self::Create2(Create2DeploymentParams::from_tokens(tokens)))
+            }
+            sig if sig == *CREATE_ACC_FUNCTION => {
+                ethabi::decode(CREATE_ACC_PARAMS, token_data)?;
+                Some(Self::CreateAccount)
+            }
+            sig if sig == *CREATE2_ACC_FUNCTION => {
+                let tokens = ethabi::decode(CREATE_ACC_PARAMS, token_data)?;
+                Some(Self::Create2Account(Create2DeploymentParams::from_tokens(
+                    tokens,
+                )))
+            }
+            _ => None,
+        })
     }
 }
