8688c34b467e3ba6a1ebc77097aa73aec395f958
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-01-17 17:53:12 +0100

    remove tokenize

--- core/lib/l1_contract_interface/src/i_executor/methods/execute_batches.rs
+++ core/lib/l1_contract_interface/src/i_executor/methods/execute_batches.rs
@@ -6,7 +6,7 @@ use zksync_types::{
 
 use crate::{
     i_executor::structures::{StoredBatchInfo, SUPPORTED_ENCODING_VERSION},
-    Tokenizable, Tokenize,
+    Tokenizable,
 };
 
 /// Input required to encode `executeBatches` call.
@@ -21,49 +21,10 @@ impl ExecuteBatches {
     // of the underlying chain.
     // However, we can send batches with older protocol versions just by changing the encoding.
     // This makes the migration simpler.
-    pub fn encode_for_eth_tx(self, chain_protocol_version: ProtocolVersionId) -> Vec<Token> {
+    pub fn encode_for_eth_tx(&self, chain_protocol_version: ProtocolVersionId) -> Vec<Token> {
         let internal_protocol_version = self.l1_batches[0].header.protocol_version.unwrap();
 
         if internal_protocol_version.is_pre_gateway() && chain_protocol_version.is_pre_gateway() {
-            vec![Token::Array(
-                self.l1_batches
-                    .iter()
-                    .map(|batch| StoredBatchInfo::from(batch).into_token())
-                    .collect(),
-            )]
-        } else {
-            let encoded_data = encode(&[
-                Token::Array(
-                    self.l1_batches
-                        .iter()
-                        .map(|batch| StoredBatchInfo::from(batch).into_token())
-                        .collect(),
-                ),
-                Token::Array(
-                    self.priority_ops_proofs
-                        .iter()
-                        .map(|proof| proof.into_token())
-                        .collect(),
-                ),
-            ]);
-            let execute_data = [[SUPPORTED_ENCODING_VERSION].to_vec(), encoded_data]
-                .concat()
-                .to_vec();
-
-            vec![
-                Token::Uint(self.l1_batches[0].header.number.0.into()),
-                Token::Uint(self.l1_batches.last().unwrap().header.number.0.into()),
-                Token::Bytes(execute_data),
-            ]
-        }
-    }
-}
-
-impl Tokenize for &ExecuteBatches {
-    fn into_tokens(self) -> Vec<Token> {
-        let protocol_version = self.l1_batches[0].header.protocol_version.unwrap();
-
-        if protocol_version.is_pre_gateway() {
             vec![Token::Array(
                 self.l1_batches
                     .iter()
