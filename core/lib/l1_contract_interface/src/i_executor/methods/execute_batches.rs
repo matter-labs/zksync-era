03b05ec479ab4593bf84c64fbe86a76704558e02
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-01-17 17:43:23 +0100

    fix execute encoding for transactions

--- core/lib/l1_contract_interface/src/i_executor/methods/execute_batches.rs
+++ core/lib/l1_contract_interface/src/i_executor/methods/execute_batches.rs
@@ -1,6 +1,7 @@
 use zksync_types::{
     commitment::{L1BatchWithMetadata, PriorityOpsMerkleProof},
     ethabi::{encode, Token},
+    ProtocolVersionId,
 };
 
 use crate::{
@@ -13,6 +14,49 @@ use crate::{
 pub struct ExecuteBatches {
     pub l1_batches: Vec<L1BatchWithMetadata>,
     pub priority_ops_proofs: Vec<PriorityOpsMerkleProof>,
+}
+
+impl ExecuteBatches {
+    // The encodings of `ExecuteBatches` operations are different depending on the protocol version
+    // of the underlying chain.
+    // However, we can send batches with older protocol versions just by changing the encoding.
+    // This makes the migration simpler.
+    pub fn encode_for_eth_tx(self, chain_protocol_version: ProtocolVersionId) -> Vec<Token> {
+        let internal_protocol_version = self.l1_batches[0].header.protocol_version.unwrap();
+
+        if internal_protocol_version.is_pre_gateway() && chain_protocol_version.is_pre_gateway() {
+            vec![Token::Array(
+                self.l1_batches
+                    .iter()
+                    .map(|batch| StoredBatchInfo::from(batch).into_token())
+                    .collect(),
+            )]
+        } else {
+            let encoded_data = encode(&[
+                Token::Array(
+                    self.l1_batches
+                        .iter()
+                        .map(|batch| StoredBatchInfo::from(batch).into_token())
+                        .collect(),
+                ),
+                Token::Array(
+                    self.priority_ops_proofs
+                        .iter()
+                        .map(|proof| proof.into_token())
+                        .collect(),
+                ),
+            ]);
+            let execute_data = [[SUPPORTED_ENCODING_VERSION].to_vec(), encoded_data]
+                .concat()
+                .to_vec();
+
+            vec![
+                Token::Uint(self.l1_batches[0].header.number.0.into()),
+                Token::Uint(self.l1_batches.last().unwrap().header.number.0.into()),
+                Token::Bytes(execute_data),
+            ]
+        }
+    }
 }
 
 impl Tokenize for &ExecuteBatches {
