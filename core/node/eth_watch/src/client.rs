c6796c9b66a5b819f43121179e85ffd0770fe969
Stanislav Breadless <stanislavbezkor@gmail.com>
2025-01-20 18:52:03 +0100

    lint

--- core/node/eth_watch/src/client.rs
+++ core/node/eth_watch/src/client.rs
@@ -15,7 +15,7 @@ use zksync_types::{
     abi::ZkChainSpecificUpgradeData,
     api::{ChainAggProof, Log},
     ethabi::{self, decode, encode, Contract, ParamType},
-    web3::{keccak256, BlockId, BlockNumber, CallRequest, Filter, FilterBuilder},
+    web3::{keccak256, BlockId, BlockNumber, Filter, FilterBuilder},
     Address, L1BatchNumber, L2ChainId, SLChainId, H256, L2_NATIVE_TOKEN_VAULT_ADDRESS,
     SHARED_BRIDGE_ETHER_TOKEN_ADDRESS, U256, U64,
 };
