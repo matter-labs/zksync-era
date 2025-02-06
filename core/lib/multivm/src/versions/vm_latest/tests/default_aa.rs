4179711c82c210e0a1236c37a2a97fb9311ef820
Alex Ostrovski <slowli@users.noreply.github.com>
2025-02-06 18:11:11 +0200

    fix(api): Change `contractAddress` assignment for transaction receipts (#3452)
    ## What ❔
    
    Changes how `contractAddress` is assigned for transaction receipts. With
    these changes, it is assigned for EVM deployment transactions (ones with
    `to == None`) and EraVM deployments (i.e., calls to
    `ContractDeployer.{create, create2, createAccount, create2Account}`)
    regardless of whether the transaction succeeds.
    
    ## Why ❔
    
    Improves EVM compatibility.
    
    ## Checklist
    
    - [x] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [x] Tests for the changes have been added / updated.
    - [x] Documentation comments have been added / updated.
    - [x] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.

--- core/lib/multivm/src/versions/vm_latest/tests/default_aa.rs
+++ core/lib/multivm/src/versions/vm_latest/tests/default_aa.rs
@@ -1,9 +1,14 @@
 use crate::{
-    versions::testonly::default_aa::test_default_aa_interaction,
+    versions::testonly::default_aa::{test_default_aa_interaction, test_permissive_aa_works},
     vm_latest::{HistoryEnabled, Vm},
 };
 
 #[test]
 fn default_aa_interaction() {
     test_default_aa_interaction::<Vm<_, HistoryEnabled>>();
+}
+
+#[test]
+fn permissive_aa_works() {
+    test_permissive_aa_works::<Vm<_, HistoryEnabled>>();
 }
