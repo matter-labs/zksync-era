97afdedb23a68a2369ede606cacfd281b8162b26
Alex Ostrovski <aov@matterlabs.dev>
2025-02-07 10:14:16 +0200

    Remove DB migration

--- core/lib/dal/migrations/20250131090112_remove_tx_initiator_address_from_events.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE events ADD COLUMN tx_initiator_address BYTEA NOT NULL DEFAULT '\x0000000000000000000000000000000000000000'::bytea;
-CREATE INDEX events_tx_initiator_address_idx ON events (tx_initiator_address);
