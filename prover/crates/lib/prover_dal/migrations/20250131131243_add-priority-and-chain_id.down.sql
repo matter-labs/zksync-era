e5ded710d79fb76c711ea3946cafc61ff6eb9034
Lech <88630083+Artemka374@users.noreply.github.com>
2025-02-11 14:20:37 +0200

    add indexes to migration

--- prover/crates/lib/prover_dal/migrations/20250131131243_add-priority-and-chain_id.down.sql
+++ prover/crates/lib/prover_dal/migrations/20250131131243_add-priority-and-chain_id.down.sql
@@ -6,3 +6,11 @@ ALTER TABLE scheduler_witness_jobs_fri DROP COLUMN priority;
 ALTER TABLE proof_compression_jobs_fri DROP COLUMN priority;
 ALTER TABLE prover_jobs_fri DROP COLUMN priority;
 ALTER TABLE prover_jobs_fri_archive DROP COLUMN priority;
+
+DROP INDEX IF EXISTS idx_witness_inputs_fri_priority;
+DROP INDEX IF EXISTS idx_leaf_aggregation_witness_jobs_fri_priority;
+DROP INDEX IF EXISTS idx_node_aggregation_witness_jobs_fri_priority;
+DROP INDEX IF EXISTS idx_recursion_tip_witness_jobs_fri_priority;
+DROP INDEX IF EXISTS idx_scheduler_witness_jobs_fri_priority;
+DROP INDEX IF EXISTS idx_proof_compression_jobs_fri_priority;
+DROP INDEX IF EXISTS idx_prover_jobs_fri_priority;
