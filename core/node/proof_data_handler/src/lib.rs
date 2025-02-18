0976f03500f9d0027f9ffcd7dd088ff6deddeb7c
Lech <88630083+Artemka374@users.noreply.github.com>
2025-02-18 13:32:28 +0200

    delete proof data submitter

--- core/node/proof_data_handler/src/lib.rs
+++ core/node/proof_data_handler/src/lib.rs
@@ -4,12 +4,8 @@ mod tests;
 mod errors;
 mod metrics;
 mod middleware;
-mod proof_data_submitter;
 mod rpc_client;
 mod tee_proof_api;
 
-pub use proof_data_submitter::{
-    proof_data_processor::ProofGenerationDataProcessor, ProofGenerationDataSubmitter,
-};
 pub use rpc_client::{processor::ProofDataProcessor, RpcClient};
 pub use tee_proof_api::{RequestProcessor, TeeProofDataHandler};
