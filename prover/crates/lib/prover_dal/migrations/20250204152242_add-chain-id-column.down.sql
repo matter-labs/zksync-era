f062b95a25e9836dc3bf70c5c2d0bcb83c471280
Lech <88630083+Artemka374@users.noreply.github.com>
2025-02-19 13:32:31 +0200

    fix unique indexes

--- prover/crates/lib/prover_dal/migrations/20250204152242_add-chain-id-column.down.sql
+++ prover/crates/lib/prover_dal/migrations/20250204152242_add-chain-id-column.down.sql
@@ -8,6 +8,16 @@ ALTER TABLE recursion_tip_witness_jobs_fri ADD CONSTRAINT recursion_tip_witness_
 ALTER TABLE scheduler_witness_jobs_fri ADD CONSTRAINT scheduler_witness_jobs_fri_pkey PRIMARY KEY (l1_batch_number);
 ALTER TABLE proof_compression_jobs_fri ADD CONSTRAINT proof_compression_jobs_fri_pkey PRIMARY KEY (l1_batch_number);
 
+ALTER TABLE leaf_aggregation_witness_jobs_fri DROP CONSTRAINT IF EXISTS leaf_aggregation_witness_jobs_fri_composite_index;
+CREATE UNIQUE INDEX leaf_aggregation_witness_jobs_fri_composite_index ON leaf_aggregation_witness_jobs_fri (l1_batch_number, circuit_id);
+
+ALTER TABLE node_aggregation_witness_jobs_fri DROP CONSTRAINT IF EXISTS node_aggregation_witness_jobs_fri_composite_index;
+CREATE UNIQUE INDEX node_aggregation_witness_jobs_fri_composite_index ON node_aggregation_witness_jobs_fri (l1_batch_number, circuit_id, depth);
+
+ALTER TABLE prover_jobs_fri DROP CONSTRAINT IF EXISTS prover_jobs_fri_composite_index;
+CREATE UNIQUE INDEX prover_jobs_fri_composite_index ON prover_jobs_fri (l1_batch_number, aggregation_round, circuit_id, depth, sequence_number);
+
+
 ALTER TABLE witness_inputs_fri DROP COLUMN chain_id;
 ALTER TABLE leaf_aggregation_witness_jobs_fri DROP COLUMN chain_id;
 ALTER TABLE node_aggregation_witness_jobs_fri DROP COLUMN chain_id;
