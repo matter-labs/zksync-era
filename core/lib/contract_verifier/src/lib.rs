42ec013691dc4b83458a377af474ab4403517946
Vasyl Ivanchuk <vasyl.ivanchuk@gmail.com>
2025-02-24 14:43:49 +0200

    fix: formatting issues

--- core/lib/contract_verifier/src/lib.rs
+++ core/lib/contract_verifier/src/lib.rs
@@ -599,7 +599,9 @@ impl ContractVerifier {
                     )
                     .await?;
                 if self.etherscan_verifier_enabled {
-                    tracing::debug!("Created etherscan verification request with id = {request_id}");
+                    tracing::debug!(
+                        "Created etherscan verification request with id = {request_id}"
+                    );
                     transaction
                         .etherscan_verification_dal()
                         .add_verification_request(request_id)
