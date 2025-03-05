901ca0ed0996a87e62bc788cf3d91bc4d6ed78b6
kelemeno <kl@matterlabs.dev>
2025-03-05 12:42:47 +0000

    lint

--- core/node/eth_watch/src/client.rs
+++ core/node/eth_watch/src/client.rs
@@ -17,8 +17,8 @@ use zksync_types::{
     ethabi::{decode, Contract, ParamType},
     utils::encode_ntv_asset_id,
     web3::{BlockId, BlockNumber, Filter, FilterBuilder},
-    Address, L1BatchNumber, L2ChainId, SLChainId, H256,L1_MESSENGER_ADDRESS, SHARED_BRIDGE_ETHER_TOKEN_ADDRESS, U256,
-    U64,
+    Address, L1BatchNumber, L2ChainId, SLChainId, H256, L1_MESSENGER_ADDRESS,
+    SHARED_BRIDGE_ETHER_TOKEN_ADDRESS, U256, U64,
 };
 use zksync_web3_decl::{
     client::{Network, L2},
