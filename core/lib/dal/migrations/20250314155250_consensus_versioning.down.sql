b3c7714ff2f19c2ef8981bc40d148d3e05a33763
Bruno França <bruno@franca.xyz>
2025-03-24 17:20:02 +0000

    migration

--- core/lib/dal/migrations/20250314155250_consensus_versioning.down.sql
+++ core/lib/dal/migrations/20250314155250_consensus_versioning.down.sql
@@ -1,3 +1,2 @@
 ALTER TABLE miniblocks_consensus ALTER COLUMN certificate SET NOT NULL;
 ALTER TABLE miniblocks_consensus DROP COLUMN versioned_certificate;
-ALTER TABLE consensus_replica_state ADD COLUMN genesis JSONB;
--- core/lib/dal/migrations/20250314155250_consensus_versioning.up.sql
+++ core/lib/dal/migrations/20250314155250_consensus_versioning.up.sql
@@ -1,3 +1,2 @@
-ALTER TABLE consensus_replica_state DROP COLUMN genesis;
 ALTER TABLE miniblocks_consensus ADD COLUMN versioned_certificate JSONB NULL;
 ALTER TABLE miniblocks_consensus ALTER COLUMN certificate DROP NOT NULL;
