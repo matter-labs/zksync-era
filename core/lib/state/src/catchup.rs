88e794c16076af23b0e6b43bd3b49fdae6090a87
perekopskiy <mikeson.dp@gmail.com>
2025-03-24 17:49:17 +0200

    move sleep out of else

--- core/lib/state/src/catchup.rs
+++ core/lib/state/src/catchup.rs
@@ -342,9 +342,8 @@ mod tests {
 
             if builder.l1_batch_number().await == Some(L1BatchNumber(3)) {
                 break;
-            } else {
-                tokio::time::sleep(Duration::from_millis(10)).await;
             }
+            tokio::time::sleep(Duration::from_millis(10)).await;
         }
     }
 
