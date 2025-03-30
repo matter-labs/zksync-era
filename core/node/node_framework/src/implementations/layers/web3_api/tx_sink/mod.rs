601cfc475fdd1e23bbb675bf346359ab27805271
Roman Hiden <romeo8881@gmail.com>
2025-03-30 20:36:26 +0300

    removed oboslete file

--- core/node/node_framework/src/implementations/layers/web3_api/tx_sink/mod.rs
+++ core/node/node_framework/src/implementations/layers/web3_api/tx_sink/mod.rs
@@ -1,6 +1,5 @@
 pub use self::{master_pool_sink::MasterPoolSinkLayer, proxy_sink::ProxySinkLayer};
 
-pub mod allow_list_task;
 pub mod master_pool_sink;
 pub mod proxy_sink;
 pub mod whitelisted_master_pool_sink_layer;
