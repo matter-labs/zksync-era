cb299cedfcfe6afdba59ce3691c7e4b22f43a923
Alex Ostrovski <aov@matterlabs.dev>
2025-04-04 12:34:12 +0300

    Refactor integration tests command; add --evm switch

--- zkstack_cli/crates/zkstack/src/commands/dev/commands/test/args/integration.rs
+++ zkstack_cli/crates/zkstack/src/commands/dev/commands/test/args/integration.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use serde::{Deserialize, Serialize};
 
 use crate::commands::dev::messages::{
-    MSG_NO_DEPS_HELP, MSG_TESTS_EXTERNAL_NODE_HELP, MSG_TEST_PATTERN_HELP,
+    MSG_EVM_TESTS_HELP, MSG_NO_DEPS_HELP, MSG_TESTS_EXTERNAL_NODE_HELP, MSG_TEST_PATTERN_HELP,
 };
 
 #[derive(Debug, Serialize, Deserialize, Parser)]
@@ -11,6 +11,8 @@ pub struct IntegrationArgs {
     pub external_node: bool,
     #[clap(short, long, help = MSG_NO_DEPS_HELP)]
     pub no_deps: bool,
+    #[clap(long, help = MSG_EVM_TESTS_HELP)]
+    pub evm: bool,
     #[clap(short, long, help = MSG_TEST_PATTERN_HELP, allow_hyphen_values(true))]
     pub test_pattern: Option<String>,
 }
