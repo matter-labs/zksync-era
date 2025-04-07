a36ec2294522595140458f4977db3643da34b4d7
Lech <88630083+Artemka374@users.noreply.github.com>
2025-04-07 20:58:45 +0200

    fix test

--- core/lib/prover_interface/tests/job_serialization.rs
+++ core/lib/prover_interface/tests/job_serialization.rs
@@ -125,61 +125,63 @@ fn test_proof_request_serialization() {
     let encoded_json = r#"{
         "Proof": {
             "inner": {
-                "aggregation_result_coords": [
-                    [
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                "Plonk": {
+                    "aggregation_result_coords": [
+                        [
+                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                        ],
+                        [
+                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                        ],
+                        [
+                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                        ],
+                        [
+                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                        ]
                     ],
-                    [
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                    ],
-                    [
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                    ],
-                    [
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-                    ]
-                ],
-                "scheduler_proof": {
-                    "n": 0,
-                    "inputs": [],
-                    "state_polys_commitments": [],
-                    "witness_polys_commitments": [],
-                    "copy_permutation_grand_product_commitment": {
-                        "x": [ 0, 0, 0, 0 ],
-                        "y": [ 1, 0, 0, 0 ],
-                        "infinity": true
+                    "scheduler_proof": {
+                        "n": 0,
+                        "inputs": [],
+                        "state_polys_commitments": [],
+                        "witness_polys_commitments": [],
+                        "copy_permutation_grand_product_commitment": {
+                            "x": [ 0, 0, 0, 0 ],
+                            "y": [ 1, 0, 0, 0 ],
+                            "infinity": true
+                        },
+                        "lookup_s_poly_commitment": null,
+                        "lookup_grand_product_commitment": null,
+                        "quotient_poly_parts_commitments": [],
+                        "state_polys_openings_at_z": [],
+                        "state_polys_openings_at_dilations": [],
+                        "witness_polys_openings_at_z": [],
+                        "witness_polys_openings_at_dilations": [],
+                        "gate_setup_openings_at_z": [],
+                        "gate_selectors_openings_at_z": [],
+                        "copy_permutation_polys_openings_at_z": [],
+                        "copy_permutation_grand_product_opening_at_z_omega": [ 0, 0, 0, 0 ],
+                        "lookup_s_poly_opening_at_z_omega": null,
+                        "lookup_grand_product_opening_at_z_omega": null,
+                        "lookup_t_poly_opening_at_z": null,
+                        "lookup_t_poly_opening_at_z_omega": null,
+                        "lookup_selector_poly_opening_at_z": null,
+                        "lookup_table_type_poly_opening_at_z": null,
+                        "quotient_poly_opening_at_z": [ 0, 0, 0, 0 ],
+                        "linearization_poly_opening_at_z": [ 0, 0, 0, 0 ],
+                        "opening_proof_at_z": {
+                            "x": [ 0, 0, 0, 0 ],
+                            "y": [ 1, 0, 0, 0 ],
+                            "infinity": true
+                        },
+                        "opening_proof_at_z_omega": {
+                            "x": [ 0, 0, 0, 0 ],
+                            "y": [ 1, 0, 0, 0 ],
+                            "infinity": true
+                        }
                     },
-                    "lookup_s_poly_commitment": null,
-                    "lookup_grand_product_commitment": null,
-                    "quotient_poly_parts_commitments": [],
-                    "state_polys_openings_at_z": [],
-                    "state_polys_openings_at_dilations": [],
-                    "witness_polys_openings_at_z": [],
-                    "witness_polys_openings_at_dilations": [],
-                    "gate_setup_openings_at_z": [],
-                    "gate_selectors_openings_at_z": [],
-                    "copy_permutation_polys_openings_at_z": [],
-                    "copy_permutation_grand_product_opening_at_z_omega": [ 0, 0, 0, 0 ],
-                    "lookup_s_poly_opening_at_z_omega": null,
-                    "lookup_grand_product_opening_at_z_omega": null,
-                    "lookup_t_poly_opening_at_z": null,
-                    "lookup_t_poly_opening_at_z_omega": null,
-                    "lookup_selector_poly_opening_at_z": null,
-                    "lookup_table_type_poly_opening_at_z": null,
-                    "quotient_poly_opening_at_z": [ 0, 0, 0, 0 ],
-                    "linearization_poly_opening_at_z": [ 0, 0, 0, 0 ],
-                    "opening_proof_at_z": {
-                        "x": [ 0, 0, 0, 0 ],
-                        "y": [ 1, 0, 0, 0 ],
-                        "infinity": true
-                    },
-                    "opening_proof_at_z_omega": {
-                        "x": [ 0, 0, 0, 0 ],
-                        "y": [ 1, 0, 0, 0 ],
-                        "infinity": true
-                    }
-                },
-                "protocol_version": "0.25.10"
+                    "protocol_version": "0.25.10"
+                }
             }
         }
     }"#;
