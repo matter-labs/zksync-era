d749338009bec7b03a96f50fe728d277b0984edc
Bruno França <bruno@franca.xyz>
2025-04-14 19:01:09 +0100

    finished work on cli crate

--- zkstack_cli/crates/zkstack/src/utils/consensus.rs
+++ zkstack_cli/crates/zkstack/src/utils/consensus.rs
@@ -4,73 +4,60 @@ use zkstack_cli_config::{raw::PatchedConfig, ChainConfig};
 use zksync_consensus_crypto::{Text, TextFmt};
 use zksync_consensus_roles::{node, validator};
 
-#[derive(Debug, Clone)]
-pub struct ConsensusSecretKeys {
-    validator_key: validator::SecretKey,
-    node_key: node::SecretKey,
-}
+pub(crate) fn read_validator_committee_yaml(
+    raw_yaml: serde_yaml::Value,
+) -> anyhow::Result<(validator::Committee, Vec<Validator>)> {
+    let file: SetValidatorCommitteeFile =
+        serde_yaml::from_value(raw_yaml).context("invalid validator committee file format")?;
 
-pub struct ConsensusPublicKeys {
-    validator_key: validator::PublicKey,
-    node_key: node::PublicKey,
-}
+    let mut committee_validators = Vec::with_capacity(file.validators.len());
+    let mut validators = Vec::with_capacity(file.validators.len());
 
-pub fn generate_consensus_keys() -> ConsensusSecretKeys {
-    ConsensusSecretKeys {
-        validator_key: validator::SecretKey::generate(),
-        node_key: node::SecretKey::generate(),
+    for v in &file.validators {
+        let key: validator::PublicKey = Text::new(&v.key).decode().context("key")?;
+
+        committee_validators.push(validator::WeightedValidator {
+            key: key.clone(),
+            weight: v.weight,
+        });
+
+        validators.push(Validator {
+            key,
+            pop: Text::new(&v.pop).decode().context("pop")?,
+            weight: v.weight,
+        });
     }
+
+    let committee = validator::Committee::new(committee_validators.into_iter())?;
+
+    Ok((committee, validators))
 }
 
-fn get_consensus_public_keys(consensus_keys: &ConsensusSecretKeys) -> ConsensusPublicKeys {
-    ConsensusPublicKeys {
-        validator_key: consensus_keys.validator_key.public(),
-        node_key: consensus_keys.node_key.public(),
-    }
+/// This is the file that contains the validator committee.
+/// It is used to set the validator committee in the consensus registry.
+#[derive(Debug, Deserialize)]
+pub(crate) struct SetValidatorCommitteeFile {
+    validators: Vec<ValidatorWithPopYaml>,
 }
 
-/// Mirrors key–address pair used in the consensus config.
-#[derive(Debug, Serialize)]
-pub(crate) struct KeyAndAddress {
-    pub key: String,
-    pub addr: String,
-}
-
+/// This represents a validator with its proof of possession in a YAML file.
+/// The proof of possession is necessary because we don't trust the validator to provide
+/// a valid key.
 #[derive(Debug, Serialize, Deserialize)]
-struct Weighted {
+struct ValidatorWithPopYaml {
     key: String,
+    pop: String,
     weight: u64,
 }
 
-impl Weighted {
-    fn new(key: String, weight: u64) -> Self {
-        Self { key, weight }
-    }
-}
-
-pub(crate) fn read_validator_committee_yaml(
-    raw_yaml: serde_yaml::Value,
-) -> anyhow::Result<validator::Committee> {
-    #[derive(Debug, Deserialize)]
-    struct SetValidatorCommitteeFile {
-        validators: Vec<Weighted>,
-    }
-
-    let file: SetValidatorCommitteeFile =
-        serde_yaml::from_value(raw_yaml).context("invalid validator committee format")?;
-    let validators: Vec<_> = file
-        .validators
-        .iter()
-        .enumerate()
-        .map(|(i, v)| {
-            Ok(validator::WeightedValidator {
-                key: Text::new(&v.key).decode().context("key").context(i)?,
-                weight: v.weight,
-            })
-        })
-        .collect::<anyhow::Result<_>>()
-        .context("validators")?;
-    validator::Committee::new(validators).context("Committee::new()")
+/// This represents a validator in the consensus registry.
+/// The proof of possession is necessary because we don't trust the validator to provide
+/// a valid key.
+#[derive(Debug)]
+pub(crate) struct Validator {
+    pub(crate) key: validator::PublicKey,
+    pub(crate) pop: validator::ProofOfPossession,
+    pub(crate) weight: u64,
 }
 
 pub fn set_genesis_specs(
@@ -78,8 +65,7 @@ pub fn set_genesis_specs(
     chain_config: &ChainConfig,
     consensus_keys: &ConsensusSecretKeys,
 ) -> anyhow::Result<()> {
-    let public_keys = get_consensus_public_keys(consensus_keys);
-    let validator_key = public_keys.validator_key.encode();
+    let validator_key = consensus_keys.validator_key.public().encode();
     let leader = validator_key.clone();
 
     general.insert(
@@ -89,7 +75,10 @@ pub fn set_genesis_specs(
     general.insert("consensus.genesis_spec.protocol_version", 1_u64)?;
     general.insert_yaml(
         "consensus.genesis_spec.validators",
-        [Weighted::new(validator_key, 1)],
+        [WeightedValidatorYaml {
+            key: validator_key,
+            weight: 1,
+        }],
     )?;
     general.insert("consensus.genesis_spec.leader", leader)?;
     Ok(())
@@ -111,4 +100,32 @@ pub fn node_public_key(secret_key: &str) -> anyhow::Result<String> {
         .decode()
         .context("invalid node key format")?;
     Ok(secret_key.public().encode())
+}
+
+#[derive(Debug, Clone)]
+pub struct ConsensusSecretKeys {
+    validator_key: validator::SecretKey,
+    node_key: node::SecretKey,
+}
+
+impl ConsensusSecretKeys {
+    pub fn generate() -> Self {
+        Self {
+            validator_key: validator::SecretKey::generate(),
+            node_key: node::SecretKey::generate(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct WeightedValidatorYaml {
+    key: String,
+    weight: u64,
+}
+
+/// Mirrors key–address pair used in the consensus config.
+#[derive(Debug, Serialize)]
+pub(crate) struct KeyAndAddress {
+    pub key: String,
+    pub addr: String,
 }
