bf345d7fb0dd7aec8338e1abb95c7c74d898a7b9
perekopskiy <mikeson.dp@gmail.com>
2025-04-17 16:13:19 +0300

    allow shut_down during genesis recovery

--- core/tests/recovery-test/tests/genesis-recovery.test.ts
+++ core/tests/recovery-test/tests/genesis-recovery.test.ts
@@ -163,7 +163,7 @@ describe('genesis recovery', () => {
 
             if (!reorgDetectorSucceeded) {
                 const status = health.components.reorg_detector?.status;
-                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready']);
+                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready', 'shut_down']);
                 const details = health.components.reorg_detector?.details;
                 if (status === 'ready' && details !== undefined) {
                     console.log('Received reorg detector health details', details);
@@ -175,7 +175,7 @@ describe('genesis recovery', () => {
 
             if (!consistencyCheckerSucceeded) {
                 const status = health.components.consistency_checker?.status;
-                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready']);
+                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready', 'shut_down']);
                 const details = health.components.consistency_checker?.details;
                 if (status === 'ready' && details !== undefined) {
                     console.log('Received consistency checker health details', details);
@@ -260,7 +260,7 @@ describe('genesis recovery', () => {
 
             if (!reorgDetectorSucceeded) {
                 const status = health.components.reorg_detector?.status;
-                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready']);
+                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready', 'shut_down']);
                 const details = health.components.reorg_detector?.details;
                 if (status === 'ready' && details !== undefined) {
                     console.log('Received reorg detector health details', details);
@@ -272,7 +272,7 @@ describe('genesis recovery', () => {
 
             if (!consistencyCheckerSucceeded) {
                 const status = health.components.consistency_checker?.status;
-                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready']);
+                expect(status).to.be.oneOf([undefined, 'not_ready', 'ready', 'shut_down']);
                 const details = health.components.consistency_checker?.details;
                 if (status === 'ready' && details !== undefined) {
                     console.log('Received consistency checker health details', details);
