5243379bb75e119e80e00beeeda5db6cf39ed67a
zkzoomer <sergio@zkzoomer.io>
2025-05-12 12:05:10 +0200

    fixes

--- core/node/eth_watch/src/event_processors/appended_chain_batch_root.rs
+++ core/node/eth_watch/src/event_processors/appended_chain_batch_root.rs
@@ -247,7 +247,10 @@ impl BatchRootProcessor {
     ) -> Result<L2BlockNumber, EventProcessorError> {
         let eth_tx_id = storage
             .eth_sender_dal()
-            .get_last_sent_eth_tx_id(l1_batch_number, AggregatedActionType::Execute)
+            .get_last_sent_successfully_eth_tx_id_by_batch_and_op(
+                l1_batch_number,
+                AggregatedActionType::Execute,
+            )
             .await;
 
         let tx = storage
