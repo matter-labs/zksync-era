0bc1e46d710aea254f8005993ffca345b5aa3868
Yury Akudovich <ya@matterlabs.dev>
2025-05-12 15:44:49 +0200

    fix(prover): Check pods list for stale ones every `pod_check_interval` (#3982)
    ## What ❔
    
    Check pods list for stale ones every `pod_check_interval`.
    By default checks every hour.
    Add metric `autoscaler_stale_pods`.
    
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    Sometimes watcher doesn't receive pod removal event, the pods stacks in
    status until reboot and this breaks future scheduling.
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- The `Why` has to be clear to non-Matter Labs entities running their
    own ZK Chain -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Is this a breaking change?
    - [ ] Yes
    - [x] No
    
    ## Operational changes
    <!-- Any config changes? Any new flags? Any changes to any scripts? -->
    <!-- Please add anything that non-Matter Labs entities running their own
    ZK Chain may need to know -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [x] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [x] Documentation comments have been added / updated.
    - [x] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.
    
    ref zkd-2681

--- prover/crates/bin/prover_autoscaler/src/config.rs
+++ prover/crates/bin/prover_autoscaler/src/config.rs
@@ -37,6 +37,12 @@ pub struct ProverAutoscalerAgentConfig {
     /// If dry-run enabled don't do any k8s updates, just report success.
     #[serde(default = "ProverAutoscalerAgentConfig::default_dry_run")]
     pub dry_run: bool,
+    /// Interval for periodic pod checks against the K8s API to remove stale pods.
+    #[serde(
+        with = "humantime_serde",
+        default = "ProverAutoscalerAgentConfig::default_pod_check_interval"
+    )]
+    pub pod_check_interval: Duration,
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
@@ -171,6 +177,10 @@ impl ProverAutoscalerAgentConfig {
 
     pub fn default_dry_run() -> bool {
         true
+    }
+
+    pub fn default_pod_check_interval() -> Duration {
+        Duration::from_secs(3600) // Default to 1 hour
     }
 }
 
