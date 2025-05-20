d55a49f2b1aba5ba4c26dee4c359ffde1703d1d9
0xValera <55665573+0xValera@users.noreply.github.com>
2025-05-20 20:27:55 +0200

    chore: bump contracts + genesis (#4045)
    ## What ❔
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- The `Why` has to be clear to non-Matter Labs entities running their
    own ZK Chain -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Is this a breaking change?
    - [ ] Yes
    - [ ] No
    
    ## Operational changes
    <!-- Any config changes? Any new flags? Any changes to any scripts? -->
    <!-- Please add anything that non-Matter Labs entities running their own
    ZK Chain may need to know -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.

--- infrastructure/scripts/interop.sh
+++ infrastructure/scripts/interop.sh
@@ -1,26 +1,81 @@
-#!/bin/bash
-# to use this properly comment out the section that fails the node if the bootloader has is wrong in genesis/src/lib.rs
-
-
 zkstack dev clean containers && zkstack up -o false
 zkstack dev contracts
 
+zkstack ecosystem init --deploy-paymaster --deploy-erc20 \
+    --deploy-ecosystem --l1-rpc-url=http://localhost:8545 \
+    --server-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
+    --server-db-name=zksync_server_localhost_era \
+    --ignore-prerequisites --observability=false \
+    --chain era \
+    --update-submodules false
+    
 zkstack dev generate-genesis
 
-zkstack ecosystem init --dev --observability=false \
+zkstack ecosystem init --deploy-paymaster --deploy-erc20 \
+    --deploy-ecosystem --l1-rpc-url=http://localhost:8545 \
+    --server-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
+    --server-db-name=zksync_server_localhost_era \
+    --ignore-prerequisites --observability=false \
+    --chain era \
     --update-submodules false
 
-zkstack chain convert-to-gateway --chain gateway --ignore-prerequisites
+zkstack chain create \
+        --chain-name validium \
+        --chain-id 260 \
+        --prover-mode no-proofs \
+        --wallet-creation localhost \
+        --l1-batch-commit-data-generator-mode rollup \
+        --base-token-address 0x0000000000000000000000000000000000000001 \
+        --base-token-price-nominator 1 \
+        --base-token-price-denominator 1 \
+        --set-as-default false \
+        --evm-emulator false \
+        --ignore-prerequisites --update-submodules false 
+
+zkstack chain init \
+            --deploy-paymaster \
+            --l1-rpc-url=http://localhost:8545 \
+            --server-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
+            --server-db-name=zksync_server_localhost_second \
+            --chain validium --update-submodules false
+
+zkstack chain create \
+        --chain-name gateway \
+        --chain-id 506 \
+        --prover-mode no-proofs \
+        --wallet-creation localhost \
+        --l1-batch-commit-data-generator-mode rollup \
+        --base-token-address 0x0000000000000000000000000000000000000001 \
+        --base-token-price-nominator 1 \
+        --base-token-price-denominator 1 \
+        --set-as-default false \
+        --evm-emulator false \
+        --ignore-prerequisites --update-submodules false 
+
+zkstack chain init \
+            --deploy-paymaster \
+            --l1-rpc-url=http://localhost:8545 \
+            --server-db-url=postgres://postgres:notsecurepassword@localhost:5432 \
+            --server-db-name=zksync_server_localhost_gateway \
+            --chain gateway --update-submodules false
+
+
+zkstack chain gateway convert-to-gateway --chain gateway --ignore-prerequisites
 zkstack server --ignore-prerequisites --chain gateway &> ./gateway.log & 
 
 zkstack server wait --ignore-prerequisites --verbose --chain gateway
-zkstack chain migrate-to-gateway --chain era --gateway-chain-name gateway
-# zkstack chain migrate-to-gateway --chain second --gateway-chain-name gateway
 
- zkstack server --ignore-prerequisites --chain era &> ./rollup.log &
+sleep 10
 
-#  zkstack server --ignore-prerequisites --chain second &> ./second.log &
+zkstack chain gateway migrate-to-gateway --chain era --gateway-chain-name gateway
+zkstack chain gateway migrate-to-gateway --chain validium --gateway-chain-name gateway
 
-# sleep 20
+zkstack server --ignore-prerequisites --chain era &> ./era.log & 
+zkstack server --ignore-prerequisites --chain validium &> ./validium.log & 
 
-# zkstack dev test integration -t "L1 ERC20" --no-deps --ignore-prerequisites --chain era
+
+# Runs interop integration test between era-validium in parallel
+mkdir -p zlogs
+./bin/run_on_all_chains.sh "zkstack dev test integration -t 'L1 ERC20' --verbose" \
+            "era,validium" zlogs/ \
+            'era:--evm' 'validium:--evm'
