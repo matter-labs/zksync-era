c1a84f9fc0f9f4c6e170daa93e75abf052d77b69
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2025-05-20 14:24:33 -0300

    Update config

--- core/lib/config/src/configs/da_client/eigenda.rs
+++ core/lib/config/src/configs/da_client/eigenda.rs
@@ -1,34 +1,55 @@
 use serde::Deserialize;
+use serde::Serialize;
 use zksync_basic_types::{secrets::PrivateKey, url::SensitiveUrl, Address};
-
-pub const EIGENDA_V1_CLIENT_NAME: &str = "V1";
-pub const EIGENDA_V2_CLIENT_NAME: &str = "V2";
+use smart_config::{
+    de::{Delimited, FromSecretString, Optional, Serde, WellKnown},
+    DescribeConfig, DeserializeConfig,
+};
 
 /// Describes the different ways a polynomial may be represented
-#[derive(Debug, Clone, Copy, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Deserialize, PartialEq, Eq, Default, Serialize)]
 pub enum PolynomialForm {
     /// Coeff is short for polynomial "coefficient form".
     /// The field elements represent the coefficients of the polynomial.
+    #[default]
+    #[serde(rename = "coeff")]
     Coeff,
     /// Eval is short for polynomial "evaluation form".
     /// The field elements represent the evaluation of the polynomial at roots of unity.
+    #[serde(rename = "eval")]
     Eval,
+}
+
+impl PolynomialForm {
+    pub const fn as_str(self) -> &'static str {
+        match self {
+            Self::Coeff => "coeff",
+            Self::Eval => "eval",
+        }
+    }
+}
+
+impl WellKnown for PolynomialForm {
+    type Deserializer = Serde![str];
+    const DE: Self::Deserializer = Serde![str];
 }
 
 /// The source of the points used for dispersing the blobs.
 /// It can be either a path to a local file or a URL.
-#[derive(Clone, Debug, PartialEq, Deserialize)]
+#[derive(Clone, Debug, PartialEq, DescribeConfig, DeserializeConfig, Deserialize)]
+#[config(tag = "source")]
 pub enum PointsSource {
     /// Path to a local file
-    Path(String),
+    Path { path: String },
     /// g1_url, g2_url
-    Url((String, String)),
+    Url { g1_url: String, g2_url: String },
 }
 
 /// The EigenDA client has two versions: V1 and V2.
 /// The V1 client is the original EigenDA client, while the V2 client is a new version.
 /// This enum is used to differentiate between the two versions.
-#[derive(Clone, Debug, PartialEq, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Deserialize, DescribeConfig, DeserializeConfig)]
+#[config(tag = "version")]
 pub enum VersionSpecificConfig {
     /// The EigenDA V1 client
     V1(V1Config),
@@ -37,29 +58,33 @@ pub enum VersionSpecificConfig {
 }
 
 /// Configuration fields unique of the EigenDA V1 client.
-#[derive(Clone, Debug, PartialEq, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Deserialize, DescribeConfig, DeserializeConfig)]
 pub struct V1Config {
     /// Block height needed to reach in order to consider the blob finalized
     /// a value less or equal to 0 means that the disperser will not wait for finalization
+    #[config(default_t = 0)]
     pub settlement_layer_confirmation_depth: u32,
     /// Address of the service manager contract
     pub eigenda_svc_manager_address: Address,
     /// Wait for the blob to be finalized before returning the response
+    #[config(default_t = true)]
     pub wait_for_finalization: bool,
     /// Points source
+    #[config(nest)]
     pub points_source: PointsSource,
     /// Custom quorum numbers
     pub custom_quorum_numbers: Vec<u8>,
 }
 
 /// Configuration fields unique of the EigenDA V2 client.
-#[derive(Clone, Debug, PartialEq, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Deserialize, DescribeConfig, DeserializeConfig)]
 pub struct V2Config {
     /// Address of the EigenDA cert verifier
     pub cert_verifier_addr: Address,
     /// Blob version
     pub blob_version: u16,
     /// Polynomial form to disperse the blobs
+    #[serde(default)]
     pub polynomial_form: PolynomialForm,
 }
 
@@ -67,22 +92,26 @@ pub struct V2Config {
 ///
 /// This configuration is meant to be used by both the EigenDA V1 and V2 client.
 /// The M0 stands for Milestone 0, an insecure integration, where the dispersal is not verified.
-#[derive(Clone, Debug, PartialEq, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Deserialize, DescribeConfig, DeserializeConfig)]
 pub struct EigenDAConfig {
     // Shared fields between V1 and V2
     /// URL of the Disperser RPC server
     pub disperser_rpc: String,
     /// URL of the Ethereum RPC server
+    #[config(secret, with = Optional(Serde![str]))]
     pub eigenda_eth_rpc: Option<SensitiveUrl>,
     /// Authenticated dispersal
+    #[config(default_t = true)]
     pub authenticated: bool,
     /// Config specific to each version
+    #[config(nest)]
     pub version_specific: VersionSpecificConfig,
 }
 
 /// Configuration for the EigenDA secrets.
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, DescribeConfig, DeserializeConfig)]
 pub struct EigenDASecrets {
     /// Private key used for dispersing the blobs
+    #[config(with = FromSecretString)]
     pub private_key: PrivateKey,
 }
