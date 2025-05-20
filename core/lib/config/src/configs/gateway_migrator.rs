e5b35e226b08617f1de7bd2cf61731087bf8eefb
Danil <deniallugo@gmail.com>
2025-05-20 09:33:52 +0200

    Remove backward compatibility
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/lib/config/src/configs/gateway_migrator.rs
+++ core/lib/config/src/configs/gateway_migrator.rs
@@ -1,12 +1,12 @@
 use std::time::Duration;
 
-use smart_config::{metadata::TimeUnit, DescribeConfig, DeserializeConfig};
+use smart_config::{DescribeConfig, DeserializeConfig};
 
 /// Configuration for the Gateway Migrator crate.
 #[derive(Debug, Clone, PartialEq, DescribeConfig, DeserializeConfig)]
 #[config(derive(Default))]
 pub struct GatewayMigratorConfig {
     /// How often we want to poll the Ethereum node.
-    #[config(default_t = Duration::from_secs(12), with = TimeUnit::Millis, rename = "eth_node_poll_interval_ms")]
+    #[config(default_t = Duration::from_secs(12))]
     pub eth_node_poll_interval: Duration,
 }
