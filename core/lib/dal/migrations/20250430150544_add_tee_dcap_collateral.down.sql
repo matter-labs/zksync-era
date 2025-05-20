a40cc77cb514995e34e641185217fad3e0bf33e5
Lucille L. Blumire <lucy@llblumire.co.uk>
2025-05-20 13:39:09 +0100

    feat: add locking mechanism for dcap collateral updater
    Signed-off-by: Lucille L. Blumire <lucy@llblumire.co.uk>

--- core/lib/dal/migrations/20250430150544_add_tee_dcap_collateral.down.sql
+++ core/lib/dal/migrations/20250430150544_add_tee_dcap_collateral.down.sql
@@ -1,3 +1,4 @@
 DROP TABLE IF EXISTS tee_dcap_collateral_certs;
 DROP TABLE IF EXISTS tee_dcap_collateral;
 DROP TYPE IF EXISTS tee_dcap_collateral_kind;
+DROP TABLE IF EXISTS chain_update_delay;
--- core/lib/dal/src/tee_dcap_collateral_dal.rs
+++ core/lib/dal/src/tee_dcap_collateral_dal.rs
@@ -1,7 +1,33 @@
-use chrono::NaiveDateTime;
+use chrono::{DateTime, Utc};
 use zksync_db_connection::{connection::Connection, error::DalResult, instrument::InstrumentExt};
 
 use crate::Core;
+
+/// The status of a specific piece of collateral that is stored on chain
+pub enum TeeDcapCollateralInfo {
+    /// The collateral provided matches what is present in the database and on chain
+    Matches,
+    /// The collateral provided does not match what is in the database, the receipient of this data
+    /// has a responsibility to update the chain by the time specified.
+    UpdateChainBy(DateTime<Utc>),
+    /// The collateral provided does not match what is in the database, another agent has already
+    /// been tasked with updating the chain by the time specified, at which point a retry may be
+    /// necessary.
+    PendingUpdateBy(DateTime<Utc>),
+    /// No record of the collateral on the chain is present, it must be created
+    RecordMissing,
+}
+
+#[derive(sqlx::Type, Debug, Copy, Clone)]
+#[sqlx(type_name = "tee_dcap_collateral_kind", rename_all = "snake_case")]
+pub enum TeeDcapCollateralKind {
+    RootCrl,
+    PckCrl,
+    SgxTcbInfoJson,
+    TdxTcbInfoJson,
+    SgxQeIdentityJson,
+    TdxQeIdentityJson,
+}
 
 #[derive(Debug)]
 pub struct TeeDcapCollateralDal<'a, 'c> {
@@ -13,25 +39,84 @@ impl TeeDcapCollateralDal<'_, '_> {
         serial_number: &[u8],
         issuer: &str,
         sha256: &[u8],
-    ) -> DalResult<bool> {
-        Ok(sqlx::query_scalar!(
+    ) -> DalResult<TeeDcapCollateralInfo> {
+        let mut tx = self.storage.start_transaction().await?;
+        if let Some(record) = sqlx::query!(
             r#"
-                SELECT count(serial_number) FROM tee_dcap_collateral_certs
-                WHERE serial_number = $1 
+            SELECT
+                sha256 = $3 AND not_after < transaction_timestamp() AS "sha_matches",
+                CASE
+                    WHEN update_guard_expires IS NOT NULL
+                        THEN update_guard_expires < transaction_timestamp()
+                    ELSE
+                        NULL
+                END AS "guard_expired",
+                update_guard_expires
+            FROM tee_dcap_collateral_certs
+            WHERE
+                serial_number = $1
                 AND issuer = $2
-                AND sha256 = $3
-                AND transaction_timestamp() < not_after
             "#,
             serial_number,
             issuer,
-            sha256
+            sha256,
         )
         .instrument("tee_dcap_collateral_cert_is_current")
         .with_arg("serial_number", &serial_number)
         .with_arg("issuer", &issuer)
-        .fetch_one(self.storage)
+        .fetch_optional(&mut tx)
         .await?
-        .is_some_and(|n| n > 0))
+        {
+            match (record.guard_expired, record.sha_matches) {
+                (None, Some(true)) => {
+                    // No record of the guard exists, and the sha matches
+                    Ok(TeeDcapCollateralInfo::Matches)
+                }
+                (Some(true), _) | (None, Some(false)) => {
+                    // The guard either does not exist, or has expired due to the previous caller
+                    // responsible for updating it taking too long to do so. In either case, the
+                    // chain needs to be provided the new collateral and the database updated, and
+                    // it is the callers responsibility to do this.
+                    let guard_expires = sqlx::query_scalar!(
+                        r#"
+                            UPDATE tee_dcap_collateral_certs
+                            SET update_guard_expires = transaction_timestamp() + (
+                                SELECT delay FROM chain_update_delay WHERE id = 1
+                            )
+                            WHERE serial_number = $1
+                            AND issuer = $2
+                            RETURNING update_guard_expires
+                        "#,
+                        serial_number,
+                        issuer
+                    )
+                    .instrument("tee_dcap_collateral_cert_lock")
+                    .with_arg("serial_number", &serial_number)
+                    .with_arg("issuer", &issuer)
+                    .fetch_one(&mut tx)
+                    .await?
+                    .unwrap(); // value is not null as it was just set
+                    Ok(TeeDcapCollateralInfo::UpdateChainBy(guard_expires))
+                }
+                (Some(false), _) => {
+                    // The guard has not yet expired, a previous caller has been tasked with
+                    // updating the information on the chain and reflecting this in the database
+                    // upon completion.
+                    Ok(TeeDcapCollateralInfo::PendingUpdateBy(
+                        record.update_guard_expires.unwrap(),
+                    ))
+                }
+                (_, None) => {
+                    // Case cannot occur as a NOT NULL field cannot produce a null equality check
+                    // unless checked with null, and there is no possibility to pass in a nullish
+                    // sha256.
+                    unreachable!()
+                }
+            }
+        } else {
+            // No record of cert, create record and inform caller they need to put this record on the chain.
+            Ok(TeeDcapCollateralInfo::RecordMissing)
+        }
     }
 
     pub async fn cert_updated(
@@ -39,18 +124,19 @@ impl TeeDcapCollateralDal<'_, '_> {
         serial_number: &[u8],
         issuer: &str,
         sha256: &[u8],
-        not_after: NaiveDateTime,
+        not_after: DateTime<Utc>,
     ) -> DalResult<()> {
         let query = sqlx::query!(
             r#"
             INSERT INTO tee_dcap_collateral_certs
-            (serial_number, issuer, sha256, not_after, updated)
-            VALUES ($1, $2, $3, $4, transaction_timestamp())
+            (serial_number, issuer, sha256, not_after, updated, update_guard_expires)
+            VALUES ($1, $2, $3, $4, transaction_timestamp(), NULL)
             ON CONFLICT (serial_number, issuer)
             DO UPDATE SET
             sha256 = $3,
             not_after = $4,
-            updated = transaction_timestamp()
+            updated = transaction_timestamp(),
+            update_guard_expires = NULL
             "#,
             serial_number,
             issuer,
@@ -66,165 +152,108 @@ impl TeeDcapCollateralDal<'_, '_> {
         Ok(())
     }
 
-    pub async fn root_crl_is_current(&mut self, sha256: &[u8]) -> DalResult<bool> {
-        Ok(sqlx::query_scalar!(
+    pub async fn field_is_current(
+        &mut self,
+        kind: TeeDcapCollateralKind,
+        sha256: &[u8],
+    ) -> DalResult<TeeDcapCollateralInfo> {
+        let mut tx = self.storage.start_transaction().await?;
+        if let Some(record) = sqlx::query!(
             r#"
-                SELECT count(kind) FROM tee_dcap_collateral
-                WHERE kind = 'root_crl'
-                AND sha256 = $1
-                AND transaction_timestamp() < not_after
+            SELECT
+                sha256 = $2 AND not_after < transaction_timestamp() AS "sha_matches",
+                CASE
+                    WHEN update_guard_expires IS NOT NULL
+                        THEN update_guard_expires < transaction_timestamp()
+                    ELSE
+                        NULL
+                END AS "guard_expired",
+                update_guard_expires
+            FROM tee_dcap_collateral
+            WHERE kind = $1
             "#,
-            sha256
+            kind as _,
+            sha256,
         )
-        .instrument("tee_dcap_collateral_root_crl_is_current")
-        .fetch_one(self.storage)
+        .instrument("tee_dcap_collateral_field_is_current")
+        .with_arg("field", &kind)
+        .fetch_optional(&mut tx)
         .await?
-        .is_some_and(|n| n > 0))
+        {
+            match (record.guard_expired, record.sha_matches) {
+                (None, Some(true)) => {
+                    // No record of the guard exists, and the sha matches
+                    Ok(TeeDcapCollateralInfo::Matches)
+                }
+                (Some(true), _) | (None, Some(false)) => {
+                    // The guard either does not exist, or has expired due to the previous caller
+                    // responsible for updating it taking too long to do so. In either case, the
+                    // chain needs to be provided the new collateral and the database updated, and
+                    // it is the callers responsibility to do this.
+                    let guard_expires = sqlx::query_scalar!(
+                        r#"
+                            UPDATE tee_dcap_collateral
+                            SET update_guard_expires = transaction_timestamp() + (
+                                SELECT delay FROM chain_update_delay WHERE id = 1
+                            )
+                            WHERE kind = $1
+                            RETURNING update_guard_expires
+                        "#,
+                        kind as _
+                    )
+                    .instrument("tee_dcap_collateral_field_lock")
+                    .with_arg("kind", &kind)
+                    .fetch_one(&mut tx)
+                    .await?
+                    .unwrap(); // value is not null as it was just set
+                    Ok(TeeDcapCollateralInfo::UpdateChainBy(guard_expires))
+                }
+                (Some(false), _) => {
+                    // The guard has not yet expired, a previous caller has been tasked with
+                    // updating the information on the chain and reflecting this in the database
+                    // upon completion.
+                    Ok(TeeDcapCollateralInfo::PendingUpdateBy(
+                        record.update_guard_expires.unwrap(),
+                    ))
+                }
+                (_, None) => {
+                    // Case cannot occur as a NOT NULL field cannot produce a null equality check
+                    // unless checked with null, and there is no possibility to pass in a nullish
+                    // sha256.
+                    unreachable!()
+                }
+            }
+        } else {
+            // No record of cert, create record and inform caller they need to put this record on the chain.
+            Ok(TeeDcapCollateralInfo::RecordMissing)
+        }
     }
 
-    pub async fn root_crl_updated(
+    pub async fn field_updated(
         &mut self,
+        kind: TeeDcapCollateralKind,
         sha256: &[u8],
-        not_after: NaiveDateTime,
+        not_after: DateTime<Utc>,
     ) -> DalResult<()> {
         let query = sqlx::query!(
             r#"
-            INSERT INTO tee_dcap_collateral (kind, sha256, not_after, updated)
-            VALUES ('root_crl', $1, $2, transaction_timestamp())
+            INSERT INTO tee_dcap_collateral
+            (kind, sha256, not_after, updated, update_guard_expires)
+            VALUES ($1, $2, $3, transaction_timestamp(), NULL)
             ON CONFLICT (kind)
             DO UPDATE SET
-            sha256 = $1,
-            not_after = $2,
-            updated = transaction_timestamp()
+            sha256 = $2,
+            not_after = $3,
+            updated = transaction_timestamp(),
+            update_guard_expires = NULL
             "#,
+            kind as _,
             sha256,
             not_after
         );
         query
-            .instrument("tee_dcap_collateral_root_crl_updated")
-            .execute(self.storage)
-            .await?;
-        Ok(())
-    }
-
-    pub async fn pck_crl_is_current(&mut self, sha256: &[u8]) -> DalResult<bool> {
-        Ok(sqlx::query_scalar!(
-            r#"
-                SELECT count(kind) FROM tee_dcap_collateral
-                WHERE kind = 'pck_crl'
-                AND sha256 = $1
-                AND transaction_timestamp() < not_after
-            "#,
-            sha256
-        )
-        .instrument("tee_dcap_collateral_pck_crl_is_current")
-        .fetch_one(self.storage)
-        .await?
-        .is_some_and(|n| n > 0))
-    }
-
-    pub async fn pck_crl_updated(
-        &mut self,
-        sha256: &[u8],
-        not_after: NaiveDateTime,
-    ) -> DalResult<()> {
-        let query = sqlx::query!(
-            r#"
-            INSERT INTO tee_dcap_collateral (kind, sha256, not_after, updated)
-            VALUES ('pck_crl', $1, $2, transaction_timestamp())
-            ON CONFLICT (kind)
-            DO UPDATE SET
-            sha256 = $1,
-            not_after = $2,
-            updated = transaction_timestamp()
-            "#,
-            sha256,
-            not_after
-        );
-        query
-            .instrument("tee_dcap_collateral_pck_crl_updated")
-            .execute(self.storage)
-            .await?;
-        Ok(())
-    }
-
-    pub async fn tcb_info_json_is_current(&mut self, sha256: &[u8]) -> DalResult<bool> {
-        Ok(sqlx::query_scalar!(
-            r#"
-                SELECT count(kind) FROM tee_dcap_collateral
-                WHERE kind = 'tcb_info_json'
-                AND sha256 = $1
-                AND transaction_timestamp() < not_after
-            "#,
-            sha256
-        )
-        .instrument("tee_dcap_collateral_tcb_info_json_is_current")
-        .fetch_one(self.storage)
-        .await?
-        .is_some_and(|n| n > 0))
-    }
-
-    pub async fn tcb_info_json_updated(
-        &mut self,
-        sha256: &[u8],
-        not_after: NaiveDateTime,
-    ) -> DalResult<()> {
-        let query = sqlx::query!(
-            r#"
-            INSERT INTO tee_dcap_collateral (kind, sha256, not_after, updated)
-            VALUES ('tcb_info_json', $1, $2, transaction_timestamp())
-            ON CONFLICT (kind)
-            DO UPDATE SET
-            sha256 = $1,
-            not_after = $2,
-            updated = transaction_timestamp()
-            "#,
-            sha256,
-            not_after
-        );
-        query
-            .instrument("tee_dcap_collateral_tcb_info_json_updated")
-            .execute(self.storage)
-            .await?;
-        Ok(())
-    }
-
-    pub async fn qe_identity_json_is_current(&mut self, sha256: &[u8]) -> DalResult<bool> {
-        Ok(sqlx::query_scalar!(
-            r#"
-                SELECT count(kind) FROM tee_dcap_collateral
-                WHERE kind = 'qe_identity_json'
-                AND sha256 = $1
-                AND transaction_timestamp() < not_after
-            "#,
-            sha256
-        )
-        .instrument("tee_dcap_collateral_qe_identity_json_is_current")
-        .fetch_one(self.storage)
-        .await?
-        .is_some_and(|n| n > 0))
-    }
-
-    pub async fn qe_identity_json_updated(
-        &mut self,
-        sha256: &[u8],
-        not_after: NaiveDateTime,
-    ) -> DalResult<()> {
-        let query = sqlx::query!(
-            r#"
-            INSERT INTO tee_dcap_collateral (kind, sha256, not_after, updated)
-            VALUES ('qe_identity_json', $1, $2, transaction_timestamp())
-            ON CONFLICT (kind)
-            DO UPDATE SET
-            sha256 = $1,
-            not_after = $2,
-            updated = transaction_timestamp()
-            "#,
-            sha256,
-            not_after
-        );
-        query
-            .instrument("tee_dcap_collateral_qe_identity_json_updated")
+            .instrument("tee_dcap_collateral_updated")
+            .with_arg("kind", &kind)
             .execute(self.storage)
             .await?;
         Ok(())
