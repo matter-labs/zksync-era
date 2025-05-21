83a3e2d691392a7478ab5a2d5acc673da33301b5
tomg10 <lemures64@gmail.com>
2025-05-21 22:50:34 +0200

    small fix

--- zkstack_cli/crates/config/src/private_proxy_compose.rs
+++ zkstack_cli/crates/config/src/private_proxy_compose.rs
@@ -6,8 +6,7 @@ use std::{
 use serde_json::json;
 use url::Url;
 use xshell::{cmd, Shell};
-use zkstack_cli_common::docker::adjust_localhost_for_docker;
-use zkstack_cli_common::logger;
+use zkstack_cli_common::{docker::adjust_localhost_for_docker, logger};
 
 use crate::{
     consts::{LOCAL_CHAINS_PATH, LOCAL_CONFIGS_PATH},
