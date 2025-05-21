224fafd97a824e9572b76a56a5545b803935b10f
Bruno França <bruno@franca.xyz>
2025-05-21 16:07:56 +0100

    registry

--- core/node/consensus/src/registry/mod.rs
+++ core/node/consensus/src/registry/mod.rs
@@ -58,43 +58,95 @@ impl Registry {
         }
     }
 
-    /// It tries to get a pending validator committee from the consensus registry contract.
-    /// Returns `None` if there's no pending committee.
-    /// If a pending committee exists, returns a tuple of (Committee, commit_block_number).
-    pub async fn get_pending_validator_committee(
+    /// It gets a current validator committee from the consensus registry contract.
+    /// Returns a tuple of (Schedule, commit_block_number).
+    pub async fn get_current_validator_schedule(
         &self,
         ctx: &ctx::Ctx,
         address: Option<Address>,
         sealed_block_number: validator::BlockNumber,
-    ) -> ctx::Result<Option<(validator::Committee, validator::BlockNumber)>> {
+    ) -> ctx::Result<(validator::Schedule, validator::BlockNumber)> {
+        self.get_schedule(ctx, address, sealed_block_number, true)
+            .await
+            .context("get_schedule()")?
+            .ok_or(anyhow::anyhow!("failed to get current validator schedule").into())
+    }
+
+    /// It tries to get a pending validator committee from the consensus registry contract.
+    /// Returns `None` if there's no pending committee.
+    /// If a pending committee exists, returns a tuple of (Schedule, commit_block_number).
+    pub async fn get_pending_validator_schedule(
+        &self,
+        ctx: &ctx::Ctx,
+        address: Option<Address>,
+        sealed_block_number: validator::BlockNumber,
+    ) -> ctx::Result<Option<(validator::Schedule, validator::BlockNumber)>> {
+        self.get_schedule(ctx, address, sealed_block_number, false)
+            .await
+    }
+
+    async fn get_schedule(
+        &self,
+        ctx: &ctx::Ctx,
+        address: Option<Address>,
+        sealed_block_number: validator::BlockNumber,
+        cur_schedule_flag: bool,
+    ) -> ctx::Result<Option<(validator::Schedule, validator::BlockNumber)>> {
         let Some(address) = address else {
             return Ok(None);
         };
 
-        let raw = match self
-            .vm
-            .call(
-                ctx,
-                sealed_block_number,
-                address,
-                self.contract.call(abi::GetNextValidatorCommittee),
-            )
-            .await
-        {
-            Ok(raw) => raw,
-            // TODO: If there's no pending committee, the call will fail with a revert.
-            // However, we should differentiate between no pending committee and an error.
-            Err(_) => return Ok(None),
+        let (validators, leader_selection) = if cur_schedule_flag {
+            self.vm
+                .call(
+                    ctx,
+                    sealed_block_number,
+                    address,
+                    self.contract.call(abi::GetValidatorCommittee),
+                )
+                .await
+                .wrap("vm.call()")?
+        } else {
+            match self
+                .vm
+                .call(
+                    ctx,
+                    sealed_block_number,
+                    address,
+                    self.contract.call(abi::GetNextValidatorCommittee),
+                )
+                .await
+            {
+                Ok(raw) => raw,
+                // TODO: only possible revert is if there's no pending committee, so ignore it
+                Err(_) => return Ok(None),
+            }
         };
 
-        // TODO: Check that it isn't empty?
-        let mut validators = vec![];
-        for a in raw {
-            validators.push(decode_weighted_validator(&a).context("decode_weighted_validator()")?);
+        // If there are no validators in the committee, error. However, this should never happen
+        // as the contract checks that there is at least one leader validator before committing
+        // a new committee. So this is just a sanity check.
+        if validators.is_empty() {
+            return Err(anyhow::anyhow!("no validators in the committee").into());
         }
 
-        let committee =
-            validator::Committee::new(validators.into_iter()).context("Committee::new()")?;
+        let mut validator_infos = vec![];
+        for a in validators {
+            validator_infos
+                .push(decode_weighted_validator(&a).context("decode_weighted_validator()")?);
+        }
+
+        let leader_selection = validator::LeaderSelection {
+            frequency: leader_selection.frequency,
+            mode: if leader_selection.weighted {
+                validator::LeaderSelectionMode::Weighted
+            } else {
+                validator::LeaderSelectionMode::RoundRobin
+            },
+        };
+
+        let schedule = validator::Schedule::new(validator_infos.into_iter(), leader_selection)
+            .context("Schedule::new()")?;
 
         // Get the validators commit block
         let commit_block_uint = self
@@ -110,6 +162,6 @@ impl Registry {
 
         let commit_block = validator::BlockNumber(commit_block_uint.as_u64());
 
-        Ok(Some((committee, commit_block)))
+        Ok(Some((schedule, commit_block)))
     }
 }
