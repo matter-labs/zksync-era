a47dbcfe65b08857a34bf3faf66d22d984fe9d25
tomg10 <lemures64@gmail.com>
2025-05-22 19:06:22 +0200

    comment out one of the tests

--- core/tests/ts-integration/src/jest-setup/add-matchers.ts
+++ core/tests/ts-integration/src/jest-setup/add-matchers.ts
@@ -5,12 +5,3 @@ import * as fail from '../matchers/fail';
 expect.extend(ethPrimitives);
 expect.extend(transaction);
 expect.extend(fail);
-
-beforeAll(() => {
-    // Add custom serializer for BigInt
-    expect.addSnapshotSerializer({
-        test: (val) => typeof val === 'bigint',
-        // @ts-ignore
-        print: (val) => `${val.toString()}n`
-    });
-});
