14733910e348f494238a42e2e96ed8098425f1b8
perekopskiy <mikeson.dp@gmail.com>
2025-05-28 17:01:16 +0300

    remove unused metric; update yarn.lock

--- core/node/state_keeper/src/metrics.rs
+++ core/node/state_keeper/src/metrics.rs
@@ -94,9 +94,6 @@ pub struct StateKeeperMetrics {
     /// The time it takes for state keeper to wait for tx execution result from batch executor.
     #[metrics(buckets = Buckets::LATENCIES)]
     pub execute_tx_outer_time: Histogram<Duration>,
-    /// The time it takes for one iteration of the main loop in `process_l1_batch`.
-    #[metrics(buckets = Buckets::LATENCIES)]
-    pub process_l1_batch_loop_iteration: Histogram<Duration>,
     /// The time it takes for one iteration of the main loop in `process_block`.
     #[metrics(buckets = Buckets::LATENCIES)]
     pub process_block_loop_iteration: Histogram<Duration>,
