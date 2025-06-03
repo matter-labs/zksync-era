0a043d9701c6e9499d5efc30fca4d2e73717e2e2
zkzoomer <sergio@zkzoomer.io>
2025-06-03 14:18:13 +0200

    use single zksync-ethers

--- core/tests/ts-integration/src/test-master.ts
+++ core/tests/ts-integration/src/test-master.ts
@@ -1,5 +1,4 @@
-// import * as zksync from 'zksync-ethers';
-import * as zksync from 'zksync-ethers-interop-support';
+import * as zksync from 'zksync-ethers';
 import * as ethers from 'ethers';
 import { TestEnvironment, TestContext } from './types';
 import { claimEtherBack } from './context-owner';
