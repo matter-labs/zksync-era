4d7bb6fd3ba0f27ddbc8ff706f753eea9ab2d016
zkzoomer <sergio@zkzoomer.io>
2025-06-05 12:56:04 +0200

    rename to InteropMode

--- core/node/api_server/src/web3/namespaces/zks.rs
+++ core/node/api_server/src/web3/namespaces/zks.rs
@@ -7,8 +7,8 @@ use zksync_shared_resources::tree::TreeApiError;
 use zksync_system_constants::DEFAULT_L2_TX_GAS_PER_PUBDATA_BYTE;
 use zksync_types::{
     api::{
-        state_override::StateOverride, BlockDetails, BridgeAddresses, L1BatchDetails,
-        L2ToL1LogProof, LogProofTarget, Proof, ProtocolVersion, StorageProof, TransactionDetails,
+        state_override::StateOverride, BlockDetails, BridgeAddresses, InteropMode, L1BatchDetails,
+        L2ToL1LogProof, Proof, ProtocolVersion, StorageProof, TransactionDetails,
     },
     fee::Fee,
     fee_model::{FeeParams, PubdataIndependentBatchFeeModelInput},
@@ -263,7 +263,7 @@ impl ZksNamespace {
         l1_batch_number: L1BatchNumber,
         index_in_filtered_logs: usize,
         log_filter: impl Fn(&L2ToL1Log) -> bool,
-        log_proof_target: Option<LogProofTarget>,
+        interop_mode: Option<InteropMode>,
     ) -> Result<Option<L2ToL1LogProof>, Web3Error> {
         let all_l1_logs_in_batch = storage
             .blocks_web3_dal()
@@ -327,39 +327,35 @@ impl ZksNamespace {
         };
 
         let (batch_proof_len, batch_chain_proof, is_final_node) =
-        // if we provide the GW chain id, we don't want to extend to L1.
-        if log_proof_target == Some(LogProofTarget::Chain)  {
-            // Serve a proof to the L2 batch's ChainBatchRoot
-            (0, Vec::new(), true)
-        } else if sl_chain_id.0 != self.state.api_config.l1_chain_id.0 {
-            let batch_chain_proof = if log_proof_target == Some(LogProofTarget::GatewayMessageRoot) {
-                // Serve a proof to Gateway's MessageRoot
-                storage
-                    .blocks_dal()
-                    .get_gw_interop_batch_chain_merkle_path(l1_batch_number)
-                    .await
-                    .map_err(DalError::generalize)
-            } else {
-                // Serve a proof to Gateway's ChainBatchRoot
-                storage
-                    .blocks_dal()
-                    .get_l1_batch_chain_merkle_path(l1_batch_number)
-                    .await
-                    .map_err(DalError::generalize)
-            };
+            if sl_chain_id.0 != self.state.api_config.l1_chain_id.0 {
+                let batch_chain_proof = if interop_mode == Some(InteropMode::ProofBasedGateway) {
+                    // Serve a proof to Gateway's MessageRoot
+                    storage
+                        .blocks_dal()
+                        .get_gw_interop_batch_chain_merkle_path(l1_batch_number)
+                        .await
+                        .map_err(DalError::generalize)
+                } else {
+                    // Serve a proof to Gateway's ChainBatchRoot, used for withdrawals
+                    storage
+                        .blocks_dal()
+                        .get_l1_batch_chain_merkle_path(l1_batch_number)
+                        .await
+                        .map_err(DalError::generalize)
+                };
 
-            if let Ok(Some(batch_chain_proof)) = batch_chain_proof {
-                (
-                    batch_chain_proof.batch_proof_len,
-                    batch_chain_proof.proof,
-                    false,
-                )
+                if let Ok(Some(batch_chain_proof)) = batch_chain_proof {
+                    (
+                        batch_chain_proof.batch_proof_len,
+                        batch_chain_proof.proof,
+                        false,
+                    )
+                } else {
+                    return Ok(None);
+                }
             } else {
-                return Ok(None);
-            }
-        } else {
-            (0, Vec::new(), true)
-        };
+                (0, Vec::new(), true)
+            };
 
         let proof = {
             let mut metadata = [0u8; 32];
@@ -387,11 +383,11 @@ impl ZksNamespace {
         &self,
         tx_hash: H256,
         index: Option<usize>,
-        log_proof_target: Option<LogProofTarget>,
+        interop_mode: Option<InteropMode>,
     ) -> Result<Option<L2ToL1LogProof>, Web3Error> {
         if let Some(handler) = &self.state.l2_l1_log_proof_handler {
             return handler
-                .get_l2_to_l1_log_proof(tx_hash, index, log_proof_target)
+                .get_l2_to_l1_log_proof(tx_hash, index, interop_mode)
                 .rpc_context("get_l2_to_l1_log_proof")
                 .await
                 .map_err(Into::into);
@@ -420,7 +416,7 @@ impl ZksNamespace {
                 l1_batch_number,
                 index.unwrap_or(0),
                 |log| log.tx_number_in_block == l1_batch_tx_index,
-                log_proof_target,
+                interop_mode,
             )
             .await?;
         Ok(log_proof)
