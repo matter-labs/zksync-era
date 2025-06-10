bf7d58d4e9948f04b5c8d9f39b44c5435a3b6bf4
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-06-10 12:11:07 -0500

    fix: address issue with zkstack dev generate-genesis cmd

--- zkstack_cli/crates/zkstack/src/commands/dev/commands/genesis.rs
+++ zkstack_cli/crates/zkstack/src/commands/dev/commands/genesis.rs
@@ -19,8 +19,9 @@ pub(crate) async fn run(shell: &Shell) -> anyhow::Result<()> {
     let spinner = Spinner::new(MSG_GENESIS_FILE_GENERATION_STARTED);
     let secrets_path = chain.path_to_secrets_config().canonicalize().unwrap();
     let dal = get_core_dal(shell, None).await?;
-    reset_database(shell, ecosystem.link_to_code, dal).await?;
-    let _dir = shell.push_dir("core");
+    reset_database(shell, ecosystem.link_to_code.clone(), dal).await?;
+    let core_path = ecosystem.link_to_code.clone().join("core");
+    let _dir = shell.push_dir(&core_path);
     Cmd::new(cmd!(shell,"cargo run --package genesis_generator --bin genesis_generator -- --config-path={secrets_path}")).run()?;
     spinner.finish();
     Ok(())
