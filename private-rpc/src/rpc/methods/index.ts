9e8b4c077d1d29bc3e3ebe61dd1ea1e27b336837
Tomasz Grześkiewicz <lemures64@gmail.com>
2025-06-10 14:43:51 +0200

    fix(private-rpc): fix discrepancies Between Private RPC and Block Explorer on eth_getTransactionReceipt (#4137)

--- private-rpc/src/rpc/methods/index.ts
+++ private-rpc/src/rpc/methods/index.ts
@@ -1,7 +1,6 @@
 export * from './eth_getBlockByNumber';
 export * from './eth_getBlockReceipts';
 export * from './eth_getLogs';
-export * from './eth_getTransactionReceipt';
 export * from './eth_newFilter';
 export * from './who-am-i';
 export * from './zks_getRawBlockTransactions';
