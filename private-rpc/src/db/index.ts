a061f66b9fbe3954a94463bda6c8b7885a5b6f80
tomg10 <lemures64@gmail.com>
2025-06-11 15:45:46 +0200

    move private-rpc inside prividium directory

--- private-rpc/src/db/index.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { env } from '@/env';
-
-export const db = drizzle(env.DATABASE_URL);
-export type DB = typeof db;
--- private-rpc/src/db/schema.ts
+++
@@ -1,8 +0,0 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
-import { addressRow } from './custom-types';
-
-export const usersTable = pgTable('users', {
-  id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-  token: varchar('token', { length: 255 }).unique().notNull(),
-  address: addressRow('address').notNull(),
-});
