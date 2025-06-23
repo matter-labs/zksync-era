39f667768203d37aecabe86f724e8340f438c875
zkzoomer <sergio@zkzoomer.io>
2025-06-23 11:09:12 +0200

    define InteropBatchRootProcessor

--- core/lib/dal/migrations/202601016134308_message_roots.up.sql
+++ core/lib/dal/migrations/202601016134308_message_roots.up.sql
@@ -14,4 +14,5 @@ ALTER TABLE miniblocks
     ADD COLUMN interop_roots_assigned BOOLEAN NOT NULL DEFAULT FALSE;
 
 -- postgres doesn't allow dropping enum variant, so nothing is done in down.sql
+ALTER TYPE event_type ADD VALUE 'ChainBatchRootInterop';
 ALTER TYPE event_type ADD VALUE 'InteropRoot';
