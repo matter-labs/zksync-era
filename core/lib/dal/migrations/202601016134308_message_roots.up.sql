415ced9ee0c0dc61b2f29701788ed31f5c86104c
zkzoomer <sergio@zkzoomer.io>
2025-06-26 16:16:11 +0200

    rm interop_roots_assigned

--- core/lib/dal/migrations/202601016134308_message_roots.up.sql
+++ core/lib/dal/migrations/202601016134308_message_roots.up.sql
@@ -10,9 +10,6 @@ CREATE TABLE IF NOT EXISTS interop_roots (
 ALTER TABLE l1_batches
     ADD COLUMN batch_chain_merkle_path_until_msg_root BYTEA;
 
-ALTER TABLE miniblocks
-    ADD COLUMN interop_roots_assigned BOOLEAN NOT NULL DEFAULT FALSE;
-
 -- postgres doesn't allow dropping enum variant, so nothing is done in down.sql
 ALTER TYPE event_type ADD VALUE 'ChainBatchRootInterop';
 ALTER TYPE event_type ADD VALUE 'InteropRoot';
