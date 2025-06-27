ffa9b77a4c1b3ed576d42b772a958d345c5f1289
Alex Ostrovski <aov@matterlabs.dev>
2025-06-27 10:25:36 +0300

    Test `SyncStateUpdater`

--- core/node/node_sync/src/node/sync_state_updater.rs
+++ core/node/node_sync/src/node/sync_state_updater.rs
@@ -83,6 +83,7 @@ impl WiringLayer for SyncStateUpdaterLayer {
                 sync_state,
                 connection_pool,
                 main_node_client: input.main_node_client,
+                update_interval: SyncStateUpdater::DEFAULT_UPDATE_INTERVAL,
             }),
         })
     }
@@ -93,10 +94,11 @@ pub struct SyncStateUpdater {
     sync_state: SyncState,
     connection_pool: ConnectionPool<Core>,
     main_node_client: Box<DynClient<L2>>,
+    update_interval: Duration,
 }
 
 impl SyncStateUpdater {
-    const UPDATE_INTERVAL: Duration = Duration::from_secs(10);
+    const DEFAULT_UPDATE_INTERVAL: Duration = Duration::from_secs(10);
 
     async fn step(&self) -> anyhow::Result<()> {
         let local_block = self
@@ -119,11 +121,11 @@ impl SyncStateUpdater {
         let main_node_block = match fetch_result {
             Ok(block) => block,
             Err(err) if err.is_retryable() => {
-                tracing::warn!(%err, "Failed fetching latest block number from main node, will retry after {:?}", Self::UPDATE_INTERVAL);
+                tracing::warn!(%err, "Failed fetching latest block number from main node, will retry after {:?}", self.update_interval);
                 return Ok(());
             }
             Err(err) => {
-                return Err(err).context("Fatal error fetching latest block number from main node")
+                return Err(err).context("Fatal error fetching latest block number from main node");
             }
         };
 
@@ -143,7 +145,7 @@ impl Task for SyncStateUpdater {
     async fn run(self: Box<Self>, mut stop_receiver: StopReceiver) -> anyhow::Result<()> {
         while !*stop_receiver.0.borrow_and_update() {
             self.step().await?;
-            tokio::time::timeout(Self::UPDATE_INTERVAL, stop_receiver.0.changed())
+            tokio::time::timeout(self.update_interval, stop_receiver.0.changed())
                 .await
                 .ok();
         }
@@ -165,7 +167,7 @@ impl Task for SyncStateMetricsTask {
         while !*stop_receiver.0.borrow() {
             tokio::select! {
                 _ = self.0.changed() => {
-                    let data = self.0.borrow_and_update().clone();
+                    let data = *self.0.borrow_and_update();
                     let (is_synced, lag) = data.lag();
                     EN_METRICS.synced.set(is_synced.into());
                     if let Some(lag) = lag {
@@ -177,5 +179,94 @@ impl Task for SyncStateMetricsTask {
         }
         tracing::info!("Stop signal received, shutting down");
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+
+    use zksync_node_genesis::{insert_genesis_batch, GenesisParams};
+    use zksync_types::{L2BlockNumber, U64};
+    use zksync_web3_decl::{client::MockClient, jsonrpsee::core::ClientError};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn sync_state_updater_basics() {
+        let pool = ConnectionPool::test_pool().await;
+        let mut storage = pool.connection().await.unwrap();
+        insert_genesis_batch(&mut storage, &GenesisParams::mock())
+            .await
+            .unwrap();
+
+        let sync_state = SyncState::default();
+        let main_node_client = MockClient::builder(L2::default())
+            .method("eth_blockNumber", move || Ok(U64::from(42)))
+            .build();
+        let (stop_sender, stop_receiver) = watch::channel(false);
+
+        let updater = Box::new(SyncStateUpdater {
+            sync_state: sync_state.clone(),
+            connection_pool: pool.clone(),
+            main_node_client: Box::new(main_node_client),
+            update_interval: Duration::from_millis(10),
+        });
+        let task_handle = tokio::spawn(updater.run(StopReceiver(stop_receiver)));
+
+        let sync_state = *sync_state
+            .subscribe()
+            .wait_for(|state| state.main_node_block().is_some() && state.local_block().is_some())
+            .await
+            .unwrap();
+
+        assert!(!task_handle.is_finished());
+        assert_eq!(sync_state.local_block(), Some(L2BlockNumber(0)));
+        assert_eq!(sync_state.main_node_block(), Some(L2BlockNumber(42)));
+
+        // Check graceful shutdown.
+        stop_sender.send_replace(true);
+        task_handle.await.unwrap().unwrap();
+    }
+
+    #[tokio::test]
+    async fn sync_state_updater_handles_transient_errors() {
+        let pool = ConnectionPool::test_pool().await;
+        let mut storage = pool.connection().await.unwrap();
+        insert_genesis_batch(&mut storage, &GenesisParams::mock())
+            .await
+            .unwrap();
+
+        let sync_state = SyncState::default();
+        let request_count = AtomicUsize::new(0);
+        let main_node_client = MockClient::builder(L2::default())
+            .method("eth_blockNumber", move || {
+                // Emulate transient connectivity issues
+                if request_count.fetch_add(1, Ordering::Relaxed) < 10 {
+                    Err(ClientError::RequestTimeout)
+                } else {
+                    Ok(U64::from(1))
+                }
+            })
+            .build();
+        let (_stop_sender, stop_receiver) = watch::channel(false);
+
+        let updater = Box::new(SyncStateUpdater {
+            sync_state: sync_state.clone(),
+            connection_pool: pool.clone(),
+            main_node_client: Box::new(main_node_client),
+            update_interval: Duration::from_millis(10),
+        });
+        let task_handle = tokio::spawn(updater.run(StopReceiver(stop_receiver)));
+
+        let sync_state = *sync_state
+            .subscribe()
+            .wait_for(|state| state.main_node_block().is_some() && state.local_block().is_some())
+            .await
+            .unwrap();
+
+        assert!(!task_handle.is_finished());
+        assert_eq!(sync_state.local_block(), Some(L2BlockNumber(0)));
+        assert_eq!(sync_state.main_node_block(), Some(L2BlockNumber(1)));
     }
 }
