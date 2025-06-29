58afbf08d4970e4fe4f01f26d290995b27243354
tomg10 <lemures64@gmail.com>
2025-06-29 02:18:26 +0200

    two test types

--- core/tests/highlevel-test-tools/src/index.ts
+++ core/tests/highlevel-test-tools/src/index.ts
@@ -5,9 +5,11 @@ export {
 } from './create-chain';
 
 export { executeCommand, executeBackgroundCommand } from './execute-command';
-export { startServer, killServer, ServerHandle } from './start-server';
+export { startServer, ServerHandle } from './start-server';
 export { StepTimer, cleanTimingLog, displayTimingLog, getTimingStats, getStepTimer, getAllGlobalTimers } from './timing-tools';
 export { FileMutex, cleanHistoricalLogs, cleanTestChains, cleanMutexLockFiles } from './file-mutex';
 export { runIntegrationTests } from './run-integration-tests';
 export { generateLoad } from './generate-load';
 export { getRpcUrl, queryJsonRpc, getL1BatchNumber, getL1BatchDetails } from './rpc-utils';
+export { waitForAllBatchesToBeExecuted } from './wait-for-batches';
+export { CHAIN_TYPES } from './chain-types';
---
+++ core/tests/highlevel-test-tools/tests/fees-test.test.ts
@@ -0,0 +1,24 @@
+import {describe, it} from 'vitest';
+import {
+  type ChainType,
+  createChain,
+  runIntegrationTests,
+  waitForAllBatchesToBeExecuted,
+  CHAIN_TYPES
+} from '../src';
+import {feesTest} from "../src/run-integration-tests";
+
+describe('Fees Test', () => {
+  it.concurrent.each<ChainType>(CHAIN_TYPES)('for %s chain', async (chainType) => {
+    const { chainName, serverHandle } = await createChain(chainType);
+
+    await runIntegrationTests(chainName, 'ETH token checks');
+
+    await waitForAllBatchesToBeExecuted(chainName);
+
+    await serverHandle.kill();
+
+    await feesTest(chainName)
+
+  }, 3600 * 1000);
+});
