cc052b50e7aac54b6fb65dac796900b94f631959
tomg10 <lemures64@gmail.com>
2025-06-30 00:13:41 +0200

    one chainType for now

--- core/tests/highlevel-test-tools/src/chain-types.ts
+++ core/tests/highlevel-test-tools/src/chain-types.ts
@@ -4,8 +4,8 @@ import { type ChainType } from './create-chain';
  * Array of all supported chain types for testing
  */
 export const ALL_CHAIN_TYPES: ChainType[] = [
-  'consensus',
-  // 'validium',
+  //'consensus',
+   'validium',
   // 'da_migration',
   // 'custom_token'
 ]; 
