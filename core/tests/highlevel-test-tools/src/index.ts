d01d57820ba0de977a57ce14170774547c79ff22
tomg10 <lemures64@gmail.com>
2025-06-29 23:13:18 +0200

    more confirmations

--- core/tests/highlevel-test-tools/src/index.ts
+++ core/tests/highlevel-test-tools/src/index.ts
@@ -6,9 +6,9 @@ export {
 
 export { executeCommand, executeBackgroundCommand } from './execute-command';
 export { startServer, ServerHandle } from './start-server';
-export { initExternalNode, runExternalNode } from './start-external-node';
+export { initExternalNode, runExternalNode, ExternalNodeHandle } from './start-external-node';
 export { FileMutex, cleanHistoricalLogs, cleanTestChains, cleanMutexLockFiles } from './file-mutex';
-export { runIntegrationTests, feesTest, genesisRecoveryTest, snapshotsRecoveryTest } from './run-integration-tests';
+export { runIntegrationTests, feesTest, revertTest, genesisRecoveryTest, snapshotsRecoveryTest } from './run-integration-tests';
 export { generateLoad } from './generate-load';
 export { getRpcUrl, queryJsonRpc, getL1BatchNumber, getL1BatchDetails } from './rpc-utils';
 export { waitForAllBatchesToBeExecuted, generateRealisticLoad } from './wait-for-batches';
