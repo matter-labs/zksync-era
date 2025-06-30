49c1a5147f0df2c9aa0c055fedd3713b807132e5
zkzoomer <sergio@zkzoomer.io>
2025-06-30 14:57:55 +0200

    fix: various draft-v29 fixes (#4254)
    ## What ❔
    
    Includes:
    - [x] Removal of `BatchOrBlockNumber`
    - [x] Removal of `received_timestamp`
    - [x] Recent major VM changes from
    https://github.com/matter-labs/zksync-era/pull/4221
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- The `Why` has to be clear to non-Matter Labs entities running their
    own ZK Chain -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Is this a breaking change?
    - [ ] Yes
    - [ ] No
    
    ## Operational changes
    <!-- Any config changes? Any new flags? Any changes to any scripts? -->
    <!-- Please add anything that non-Matter Labs entities running their own
    ZK Chain may need to know -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.

--- core/node/api_server/src/web3/backend_jsonrpsee/namespaces/unstable.rs
+++ core/node/api_server/src/web3/backend_jsonrpsee/namespaces/unstable.rs
@@ -3,7 +3,6 @@ use zksync_types::{
         ChainAggProof, DataAvailabilityDetails, GatewayMigrationStatus, L1ToL2TxsStatus, TeeProof,
         TransactionDetailedResult, TransactionExecutionInfo,
     },
-    block::BatchOrBlockNumber,
     tee_types::TeeType,
     web3, L1BatchNumber, L2BlockNumber, L2ChainId, H256,
 };
@@ -40,7 +39,7 @@ impl UnstableNamespaceServer for UnstableNamespace {
         batch_number: L1BatchNumber,
         chain_id: L2ChainId,
     ) -> RpcResult<Option<ChainAggProof>> {
-        self.get_chain_log_proof_impl(BatchOrBlockNumber::BatchNumber(batch_number), chain_id)
+        self.get_chain_log_proof_impl(batch_number, chain_id)
             .await
             .map_err(|err| self.current_method().map_err(err))
     }
@@ -50,7 +49,7 @@ impl UnstableNamespaceServer for UnstableNamespace {
         block_number: L2BlockNumber,
         chain_id: L2ChainId,
     ) -> RpcResult<Option<ChainAggProof>> {
-        self.get_chain_log_proof_impl(BatchOrBlockNumber::BlockNumber(block_number), chain_id)
+        self.get_chain_log_proof_until_msg_root_impl(block_number, chain_id)
             .await
             .map_err(|err| self.current_method().map_err(err))
     }
