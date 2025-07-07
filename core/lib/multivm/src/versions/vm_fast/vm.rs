31f5ffadca1a508628f674775e04b4815e5a1abc
zkzoomer <sergio@zkzoomer.io>
2025-07-07 12:45:19 +0200

    Revert "rm insert_fictive_l2_block"
    This reverts commit 7e5e4cba320ba56316cd8c9f6ffeaea3e0ad284e.

--- core/lib/multivm/src/versions/vm_fast/vm.rs
+++ core/lib/multivm/src/versions/vm_fast/vm.rs
@@ -578,7 +578,7 @@ where
                     // set fictive l2 block
                     let new_block_config = self.bootloader_state.get_new_block_config();
                     let txs_index = self.bootloader_state.free_tx_index();
-                    let l2_block = self.bootloader_state.last_l2_block();
+                    let l2_block = self.bootloader_state.insert_fictive_l2_block();
                     let mut memory = vec![];
                     apply_l2_block(
                         &mut memory,
