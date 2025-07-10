7e8306a8022dec7e1f5eff90bef649a191d1abb2
zkzoomer <sergio@zkzoomer.io>
2025-07-10 12:25:26 +0200

    fix: fix interop integration tests (#4300)
    ## What ❔
    
    CI for interop integration tests broke recently as a result of the
    recent CI rework in:
    https://github.com/matter-labs/zksync-era/pull/4255. This PR defines a
    new (optional) secondary chain for integration tests, to be used for
    testing interop behavior, which defaults to the chain the tests are
    being run on.
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- The `Why` has to be clear to non-Matter Labs entities running their
    own ZK Chain -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Is this a breaking change?
    - [ ] Yes
    - [ ] No
    
    ## Operational changes
    <!-- Any config changes? Any new flags? Any changes to any scripts? -->
    <!-- Please add anything that non-Matter Labs entities running their own
    ZK Chain may need to know -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.

--- core/tests/highlevel-test-tools/tests/en-integration-test.test.ts
+++ core/tests/highlevel-test-tools/tests/en-integration-test.test.ts
@@ -5,6 +5,9 @@ import { enIntegrationTests } from '../src/run-integration-tests';
 describe('External Node Integration tests Test', () => {
     it(`for ${TESTED_CHAIN_TYPE} chain`, async () => {
         const testChain = await createChainAndStartServer(TESTED_CHAIN_TYPE, 'External Node Integration tests Test');
+        // Define some chain B used for interop tests
+        const secondChainType = TESTED_CHAIN_TYPE === 'era' ? 'validium' : 'era';
+        const testSecondChain = await createChainAndStartServer(secondChainType, 'External Node Integration tests');
 
         await testChain.generateRealisticLoad();
 
@@ -14,6 +17,6 @@ describe('External Node Integration tests Test', () => {
 
         await testChain.runExternalNode();
 
-        await enIntegrationTests(testChain.chainName);
+        await enIntegrationTests(testChain.chainName, testSecondChain.chainName);
     });
 });
