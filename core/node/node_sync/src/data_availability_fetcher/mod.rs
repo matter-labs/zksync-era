443601652d1536fc9cbbe4dc2c538f111317be90
dimazhornyk <dima.zhornyk@gmail.com>
2025-07-17 16:49:57 +0200

    simplify

--- core/node/node_sync/src/data_availability_fetcher/mod.rs
+++ core/node/node_sync/src/data_availability_fetcher/mod.rs
@@ -322,12 +322,12 @@ impl DataAvailabilityFetcher {
             .update(Health::from(HealthStatus::Ready));
         let mut last_updated_l1_batch = None;
 
-        self.last_scanned_batch = L1BatchNumber(
-            self.determine_first_batch_to_scan(stop_receiver.clone())
-                .await?
-                .0
-                .saturating_sub(1), // set the last scanned batch to the one before the first one to scan
-        );
+        self.last_scanned_batch = self
+            .determine_first_batch_to_scan(stop_receiver.clone())
+            .await?
+            .0
+            .saturating_sub(1) // set the last scanned batch to the one before the first one to scan
+            .into();
 
         self.drop_entries_without_inclusion_data().await?;
 
