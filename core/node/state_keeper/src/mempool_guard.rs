906bdd5b41d2dfb5d098b86d497c585dfd83e1b7
Lech <88630083+Artemka374@users.noreply.github.com>
2025-07-22 14:50:47 +0200

    fmt

--- core/node/state_keeper/src/mempool_guard.rs
+++ core/node/state_keeper/src/mempool_guard.rs
@@ -6,7 +6,9 @@ use std::{
 use tokio::sync::{Mutex as TokioMutex, MutexGuard};
 use zksync_dal::{Connection, Core, CoreDal};
 use zksync_mempool::{AdvanceInput, L2TxFilter, MempoolInfo, MempoolStore};
-use zksync_types::{Address, Nonce, PriorityOpId, ProtocolVersionId, Transaction, TransactionTimeRangeConstraint};
+use zksync_types::{
+    Address, Nonce, PriorityOpId, ProtocolVersionId, Transaction, TransactionTimeRangeConstraint,
+};
 
 use super::metrics::StateKeeperGauges;
 
