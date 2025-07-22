e1afcf6edac799ed9dec06e5ff8cf8c8e3cb643b
EmilLuta <evl@matterlabs.dev>
2025-07-22 10:40:32 +0000

    add artifacts

--- fflonk_test/src/main.rs
+++ fflonk_test/src/main.rs
@@ -6,20 +6,20 @@ use zksync_object_store::{FileBackedObjectStore, ObjectStore};
 use zksync_prover_fri_types::FriProofWrapper;
 use zksync_prover_keystore::{compressor::load_all_resources, keystore::Keystore};
 
-fn setup_crs_keys(config: &FriProofCompressorConfig) {
-    let universal_setup_path = ;
-    let universal_setup_download_url = ;
-    download_initial_setup_keys_if_not_present(
-        &config.universal_setup_path,
-        &config.universal_setup_download_url,
-    );
-    std::env::set_var("COMPACT_CRS_FILE", &config.universal_setup_path);
-}
+// fn setup_crs_keys(config: &FriProofCompressorConfig) {
+//     let universal_setup_path = ;
+//     let universal_setup_download_url = ;
+//     download_initial_setup_keys_if_not_present(
+//         &config.universal_setup_path,
+//         &config.universal_setup_download_url,
+//     );
+//     std::env::set_var("COMPACT_CRS_FILE", &config.universal_setup_path);
+// }
 
 
 #[tokio::main]
 async fn main() {
-
+    // setup_crs_keys();
 
     let keystore = Arc::new(Keystore::locate().with_setup_path(Some(PathBuf::from("../prover/data/keys"))));
     load_all_resources(&keystore, true);
