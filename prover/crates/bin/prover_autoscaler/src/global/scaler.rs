20bc4a8bee67a7896e37117493d16ad0c7de258f
Danil <deniallugo@gmail.com>
2025-08-12 11:25:50 +0200

    feat(en): Cache remote config for en (#4367)
    ## What ❔
    
    <!-- What are the changes this PR brings about? -->
    <!-- Example: This PR adds a PR template to the repo. -->
    <!-- (For bigger PRs adding more context is appreciated) -->
    
    ## Why ❔
    
    <!-- Why are these changes done? What goal do they contribute to? What
    are the principles behind them? -->
    <!-- The `Why` has to be clear to non-Matter Labs entities running their
    own ZK Chain -->
    <!-- Example: PR templates ensure PR reviewers, observers, and future
    iterators are in context about the evolution of repos. -->
    
    ## Is this a breaking change?
    - [ ] Yes
    - [ ] No
    
    ## Operational changes
    <!-- Any config changes? Any new flags? Any changes to any scripts? -->
    <!-- Please add anything that non-Matter Labs entities running their own
    ZK Chain may need to know -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.
    
    ---------
    
    Signed-off-by: Danil <deniallugo@gmail.com>

--- prover/crates/bin/prover_autoscaler/src/global/scaler.rs
+++ prover/crates/bin/prover_autoscaler/src/global/scaler.rs
@@ -1623,7 +1623,7 @@ mod tests {
         };
 
         assert_eq!(
-            scaler.calculate(&"prover".into(), 2 * 1500 + 1 * 3000 - 1500, &clusters),
+            scaler.calculate(&"prover".into(), 2 * 1500 + 3000 - 1500, &clusters),
             [
                 (
                     PoolKey {
@@ -1665,7 +1665,7 @@ mod tests {
             "Override priority: H100 in foo, then L4 in bar"
         );
         assert_eq!(
-            scaler2.calculate(&"prover".into(), 2 * 1500 + 1 * 3000 - 1500, &clusters),
+            scaler2.calculate(&"prover".into(), 2 * 1500 + 3000 - 1500, &clusters),
             [
                 (
                     PoolKey {
@@ -1687,7 +1687,7 @@ mod tests {
         );
 
         assert_eq!(
-            scaler.calculate(&"prover".into(), 0 * 1500 + 0 * 3000, &clusters_h100),
+            scaler.calculate(&"prover".into(), 0 * 3000, &clusters_h100),
             [
                 (
                     PoolKey {
