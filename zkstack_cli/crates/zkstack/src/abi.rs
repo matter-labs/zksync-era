c4565677eb1481aa808e264f05e77ae1474fb608
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-08-15 12:17:19 -0500

    chore: run cargo fmt

--- zkstack_cli/crates/zkstack/src/abi.rs
+++ zkstack_cli/crates/zkstack/src/abi.rs
@@ -10,6 +10,7 @@ abigen!(
     function ctmAssetIdFromChainId(uint256)(bytes32)
     function baseTokenAssetId(uint256)(bytes32)
     function chainTypeManager(uint256)(address)
+    function chainAssetHandler() external view returns (address)
 ]"
 );
 
@@ -36,6 +37,95 @@ abigen!(
 ]"
 );
 
+abigen!(
+    ChainTypeManagerUpgradeFnAbi,
+    r#"[
+      {
+        "type": "function",
+        "name": "upgradeChainFromVersion",
+        "stateMutability": "nonpayable",
+        "inputs": [
+          { "name": "version", "type": "uint256" },
+          {
+            "name": "cfg",
+            "type": "tuple",
+            "components": [
+              {
+                "name": "validators",
+                "type": "tuple[]",
+                "components": [
+                  { "name": "addr", "type": "address" },
+                  { "name": "weight", "type": "uint8" },
+                  { "name": "active", "type": "bool" },
+                  { "name": "selectors", "type": "bytes4[]" }
+                ]
+              },
+              { "name": "admin", "type": "address" },
+              { "name": "data", "type": "bytes" }
+            ]
+          }
+        ],
+        "outputs": []
+      }
+    ]"#
+);
+
+abigen!(
+    DiamondCutAbi,
+    r#"[
+      {
+        "type": "function",
+        "name": "diamondCut",
+        "stateMutability": "nonpayable",
+        "inputs": [
+          {
+            "name": "_diamondCut",
+            "type": "tuple",
+            "components": [
+              {
+                "name": "facetCuts",
+                "type": "tuple[]",
+                "components": [
+                  { "name": "facet", "type": "address" },
+                  { "name": "action", "type": "uint8" },
+                  { "name": "isFreezable", "type": "bool" },
+                  { "name": "selectors", "type": "bytes4[]" }
+                ]
+              },
+              { "name": "initAddress", "type": "address" },
+              { "name": "initCalldata", "type": "bytes" }
+            ]
+          }
+        ],
+        "outputs": []
+      }
+    ]"#
+);
+
+abigen!(
+    ChainAdminOwnableAbi,
+    r#"[
+      {
+        "type": "function",
+        "name": "multicall",
+        "stateMutability": "payable",
+        "inputs": [
+          {
+            "name": "_calls",
+            "type": "tuple[]",
+            "components": [
+              { "name": "target", "type": "address" },
+              { "name": "value",  "type": "uint256" },
+              { "name": "data",   "type": "bytes" }
+            ]
+          },
+          { "name": "_requireSuccess", "type": "bool" }
+        ],
+        "outputs": []
+      }
+    ]"#
+);
+
 // "validators" is from pre-v29, used for backward compatibility. Will be removed once v29 is released.
 abigen!(
     ValidatorTimelockAbi,
@@ -47,5 +137,12 @@ abigen!(
     function PROVER_ROLE()(bytes32)
     function EXECUTOR_ROLE()(bytes32)
     function validators(uint256 _chainId, address _validator)(bool)
+    ]"
+);
+
+abigen!(
+    IChainAssetHandlerAbi,
+    r"[
+    event MigrationStarted(uint256 indexed chainId, bytes32 indexed assetId, uint256 indexed settlementLayerChainId)
     ]"
 );
