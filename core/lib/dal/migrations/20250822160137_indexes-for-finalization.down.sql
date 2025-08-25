f8ad261a244a6ddb104850da6af7d1f6f88c9ebf
Danil <deniallugo@gmail.com>
2025-08-25 13:06:29 +0200

    Update indexes based on abstract experiments
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/lib/dal/migrations/20250822160137_indexes-for-finalization.down.sql
+++ core/lib/dal/migrations/20250822160137_indexes-for-finalization.down.sql
@@ -1,1 +1,6 @@
-DROP INDEX IF EXISTS eth_txs_finalized_tx;
+DROP INDEX IF EXIST eth_txs_history_finalized;
+DROP INDEX IF EXIST eth_txs_history_non_finalized;
+DROP INDEX IF EXIST eth_txs_non_gateway_index;
+DROP INDEX IF EXIST eth_txs_gateway_index;
+DROP INDEX IF EXIST miniblocks_precommit_txs;
+CREATE INDEX IF NOT EXISTS eth_precommit_tx_id_miniblocks ON miniblocks (eth_precommit_tx_id);
