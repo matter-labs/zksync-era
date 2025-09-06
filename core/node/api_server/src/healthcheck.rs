929c56a66b6abc350f6c7b1071891259555ae84b
mm-zk <mm@matterlabs.dev>
2025-09-06 11:20:09 +0200

    some change to trigger CI

--- core/node/api_server/src/healthcheck.rs
+++ core/node/api_server/src/healthcheck.rs
@@ -31,7 +31,7 @@ async fn run_server_inner(
 ) -> anyhow::Result<()> {
     tracing::info!(
         ?bind_address,
-        "Starting healthcheck server with checks {app_health_check:?}"
+        "Starting healthcheck server with checks some {app_health_check:?}"
     );
 
     app_health_check.expose_metrics();
