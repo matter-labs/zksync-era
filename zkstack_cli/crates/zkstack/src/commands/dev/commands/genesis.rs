37e94e92a36f5fbbbd4b132278ae14ff7abb6d0a
Danil <deniallugo@gmail.com>
2025-09-09 13:50:20 +0200

    Fix genesis
    Signed-off-by: Danil <deniallugo@gmail.com>

--- zkstack_cli/crates/zkstack/src/commands/dev/commands/genesis.rs
+++ zkstack_cli/crates/zkstack/src/commands/dev/commands/genesis.rs
@@ -9,11 +9,15 @@ use crate::commands::dev::{
 
 pub(crate) async fn run(shell: &Shell) -> anyhow::Result<()> {
     let chain = ZkStackConfig::current_chain(shell)?;
+    dbg!(shell.current_dir());
     let spinner = Spinner::new(MSG_GENESIS_FILE_GENERATION_STARTED);
     let secrets_path = chain.path_to_secrets_config().canonicalize().unwrap();
     let dal = get_core_dal(shell, None).await?;
-    reset_database(shell, chain.link_to_code, dal).await?;
+    reset_database(shell, chain.link_to_code.clone(), dal).await?;
+    shell.change_dir(chain.link_to_code);
+    dbg!(shell.current_dir());
     let _dir = shell.push_dir("core");
+    dbg!(shell.current_dir());
     Cmd::new(cmd!(shell,"cargo run --package genesis_generator --bin genesis_generator -- --config-path={secrets_path}")).run()?;
     spinner.finish();
     Ok(())
