e5386faddb2ba045f883215327758fac10b12bc0
mm <mm@matterlabs.dev>
2025-09-10 11:44:33 +0200

    feat(zkstack): added 2 new wallets for prove & execute

--- zkstack_cli/crates/config/src/forge_interface/register_chain/input.rs
+++ zkstack_cli/crates/config/src/forge_interface/register_chain/input.rs
@@ -59,6 +59,9 @@ pub struct ChainL1Config {
     pub validium_mode: bool,
     pub validator_sender_operator_commit_eth: Address,
     pub validator_sender_operator_blobs_eth: Address,
+    /// Additional validators that can be used for prove & execute (when these are handled by different entities).
+    pub validator_sender_prove_operator: Option<Address>,
+    pub validator_sender_execute_operator: Option<Address>,
     pub base_token_gas_price_multiplier_nominator: u64,
     pub base_token_gas_price_multiplier_denominator: u64,
     pub governance_security_council_address: Address,
@@ -121,6 +124,14 @@ impl RegisterChainL1Config {
                     == L1BatchCommitmentMode::Validium,
                 validator_sender_operator_commit_eth: wallets_config.operator.address,
                 validator_sender_operator_blobs_eth: wallets_config.blob_operator.address,
+                validator_sender_prove_operator: wallets_config
+                    .prove_operator
+                    .as_ref()
+                    .map(|w| w.address),
+                validator_sender_execute_operator: wallets_config
+                    .execute_operator
+                    .as_ref()
+                    .map(|w| w.address),
                 allow_evm_emulator: chain_config.evm_emulator,
             },
             owner_address: wallets_config.governor.address,
