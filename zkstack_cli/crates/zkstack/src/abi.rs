7460ad5440f2cb692a00345464372edc66c98da8
kelemeno <kl@matterlabs.dev>
2025-12-10 09:36:09 +0000

    fix import interface from contracts

--- zkstack_cli/crates/zkstack/src/abi.rs
+++ zkstack_cli/crates/zkstack/src/abi.rs
@@ -2,74 +2,74 @@ use ethers::contract::abigen;
 
 abigen!(
     BridgehubAbi,
-    "../../../contracts/l1-contracts/out/L1Bridgehub.sol/L1Bridgehub.json",
+    "../../../contracts/l1-contracts/zkstack-out/L1Bridgehub.sol/L1Bridgehub.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     ZkChainAbi,
-    "../../../contracts/l1-contracts/out/IZKChain.sol/IZKChain.json",
+    "../../../contracts/l1-contracts/zkstack-out/IZKChain.sol/IZKChain.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     ChainTypeManagerAbi,
-    "../../../contracts/l1-contracts/out/IChainTypeManagerForZKStack.sol/IChainTypeManagerForZKStack.json",
+    "../../../contracts/l1-contracts/zkstack-out/IChainTypeManagerForZKStack.sol/IChainTypeManagerForZKStack.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     ValidatorTimelockAbi,
-    "../../../contracts/l1-contracts/out/IValidatorTimelockForZKStack.sol/IValidatorTimelockForZKStack.json",
+    "../../../contracts/l1-contracts/zkstack-out/IValidatorTimelockForZKStack.sol/IValidatorTimelockForZKStack.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     IChainAssetHandlerAbi,
-    "../../../contracts/l1-contracts/out/IChainAssetHandler.sol/IChainAssetHandler.json",
+    "../../../contracts/l1-contracts/zkstack-out/IChainAssetHandler.sol/IChainAssetHandler.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 // Using IChainTypeManager for the upgradeChainFromVersion function
 abigen!(
     ChainTypeManagerUpgradeFnAbi,
-    "../../../contracts/l1-contracts/out/IChainTypeManager.sol/IChainTypeManager.json",
+    "../../../contracts/l1-contracts/zkstack-out/IChainTypeManager.sol/IChainTypeManager.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 // Using IAdmin for the diamondCut function
 abigen!(
     DiamondCutAbi,
-    "../../../contracts/l1-contracts/out/IAdmin.sol/IAdmin.json",
+    "../../../contracts/l1-contracts/zkstack-out/IAdmin.sol/IAdmin.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     ChainAdminOwnableAbi,
-    "../../../contracts/l1-contracts/out/IChainAdminOwnable.sol/IChainAdminOwnable.json",
+    "../../../contracts/l1-contracts/zkstack-out/IChainAdminOwnable.sol/IChainAdminOwnable.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     IRegisterZKChainAbi,
-    "../../../contracts/l1-contracts/out/IRegisterZKChain.sol/IRegisterZKChain.json",
+    "../../../contracts/l1-contracts/zkstack-out/IRegisterZKChain.sol/IRegisterZKChain.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     IDeployL2ContractsAbi,
-    "../../../contracts/l1-contracts/out/IDeployL2Contracts.sol/IDeployL2Contracts.json",
+    "../../../contracts/l1-contracts/zkstack-out/IDeployL2Contracts.sol/IDeployL2Contracts.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     DeployPaymasterAbi,
-    "../../../contracts/l1-contracts/out/DeployPaymaster.s.sol/DeployPaymaster.json",
+    "../../../contracts/l1-contracts/zkstack-out/DeployPaymaster.s.sol/DeployPaymaster.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
 abigen!(
     GatewayVotePreparationAbi,
-    "../../../contracts/l1-contracts/out/GatewayVotePreparation.s.sol/GatewayVotePreparation.json",
+    "../../../contracts/l1-contracts/zkstack-out/GatewayVotePreparation.s.sol/GatewayVotePreparation.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
