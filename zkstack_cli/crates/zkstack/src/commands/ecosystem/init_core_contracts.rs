1635ba449e2226fdd9bda322de95ce353ee8a008
kelemeno <kl@matterlabs.dev>
2026-01-27 14:39:10 +0000

    remove old ownerships

--- zkstack_cli/crates/zkstack/src/commands/ecosystem/init_core_contracts.rs
+++ zkstack_cli/crates/zkstack/src/commands/ecosystem/init_core_contracts.rs
@@ -113,18 +113,6 @@ pub async fn deploy_ecosystem(
     .await?;
     spinner.finish();
 
-    accept_owner(
-        shell,
-        ecosystem_config.path_to_foundry_scripts_for_ctm(vm_option),
-        contracts_config.l1.governance_addr,
-        &ecosystem_config.get_wallets()?.governor,
-        contracts_config
-            .core_ecosystem_contracts
-            .bridgehub_proxy_addr,
-        &forge_args,
-        l1_rpc_url.clone(),
-    )
-    .await?;
     accept_admin(
         shell,
         ecosystem_config.path_to_foundry_scripts_for_ctm(vm_option),
@@ -133,33 +121,6 @@ pub async fn deploy_ecosystem(
         contracts_config
             .core_ecosystem_contracts
             .bridgehub_proxy_addr,
-        &forge_args,
-        l1_rpc_url.clone(),
-    )
-    .await?;
-
-    // Note, that there is no admin in L1 asset router, so we do
-    // need to accept it
-    accept_owner(
-        shell,
-        ecosystem_config.path_to_foundry_scripts_for_ctm(vm_option),
-        contracts_config.l1.governance_addr,
-        &ecosystem_config.get_wallets()?.governor,
-        contracts_config.bridges.shared.l1_address,
-        &forge_args,
-        l1_rpc_url.clone(),
-    )
-    .await?;
-
-    accept_owner(
-        shell,
-        ecosystem_config.path_to_foundry_scripts_for_ctm(vm_option),
-        contracts_config.l1.governance_addr,
-        &ecosystem_config.get_wallets()?.governor,
-        contracts_config
-            .core_ecosystem_contracts
-            .stm_deployment_tracker_proxy_addr
-            .context("stm_deployment_tracker_proxy_addr")?,
         &forge_args,
         l1_rpc_url.clone(),
     )
