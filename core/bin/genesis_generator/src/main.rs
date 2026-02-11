5649a67a83cb2ec869ce32628c1a1b75a3a187a2
Danil <deniallugo@gmail.com>
2026-02-11 10:53:39 +0100

    Update genesis
    Signed-off-by: Danil <deniallugo@gmail.com>

--- core/bin/genesis_generator/src/main.rs
+++ core/bin/genesis_generator/src/main.rs
@@ -74,10 +74,7 @@ async fn generate_new_config(
 
     let base_system_contracts = BaseSystemContracts::load_from_disk().hashes();
     let mut updated_genesis = GenesisConfig {
-        protocol_version: Some(ProtocolSemanticVersion {
-            minor: ProtocolVersionId::latest(),
-            patch: 0.into(), // genesis generator proposes some new valid config, so patch 0 works here.
-        }),
+        protocol_version: genesis_config.protocol_version,
         genesis_root_hash: None,
         rollup_last_leaf_index: None,
         genesis_commitment: None,
