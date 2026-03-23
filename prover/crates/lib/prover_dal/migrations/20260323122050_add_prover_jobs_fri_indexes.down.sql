cc81d65349d4060a550903d9f06600970d30eab9
dimazhornyk <dima.zhornyk@gmail.com>
2026-03-23 13:34:12 +0100

    feat(prover): add indexes for prover_jobs_fri table

---
+++ prover/crates/lib/prover_dal/migrations/20260323122050_add_prover_jobs_fri_indexes.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_prover_jobs_fri_active_stats;
+
+DROP INDEX IF EXISTS  idx_prover_jobs_fri_unproved;
+
+DROP INDEX IF EXISTS  idx_prover_jobs_fri_attempts_check;
+
+DROP INDEX IF EXISTS  idx_prover_jobs_fri_archive_candidates;
