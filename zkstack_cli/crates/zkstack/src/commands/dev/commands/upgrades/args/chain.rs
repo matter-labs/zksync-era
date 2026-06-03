7ae4a42c0fcfa3114f266b5e6d7454488cbbe053
Dima Zhornyk <55756184+dimazhornyk@users.noreply.github.com>
2026-06-03 14:14:48 +0200

    fix(zkstack): upgrade command (#4840)
    ## What ❔
    
    - remove GW-related  params
    - remove `state-transition`
    
    ## Why ❔
    
    `zkstack` was expecting the fields that are no longer relevant
    
    ## Is this a breaking change?
    - [ ] Yes
    - [x] No
    
    ## Operational changes
    <!-- Any config changes? Any new flags? Any changes to any scripts? -->
    <!-- Please add anything that non-Matter Labs entities running their own
    ZK Chain may need to know -->
    
    ## Checklist
    
    <!-- Check your PR fulfills the following items. -->
    <!-- For draft PRs check the boxes as you complete them. -->
    
    - [ ] PR title corresponds to the body of PR (we generate changelog
    entries from PRs).
    - [ ] Tests for the changes have been added / updated.
    - [ ] Documentation comments have been added / updated.
    - [ ] Code has been formatted via `zkstack dev fmt` and `zkstack dev
    lint`.

--- zkstack_cli/crates/zkstack/src/commands/dev/commands/upgrades/args/chain.rs
+++ zkstack_cli/crates/zkstack/src/commands/dev/commands/upgrades/args/chain.rs
@@ -16,11 +16,9 @@ pub struct DefaultChainUpgradeArgs {
 pub struct ChainUpgradeParams {
     pub upgrade_description_path: Option<String>,
     pub chain_id: Option<u64>,
-    pub gw_chain_id: Option<u64>,
     pub l1_gas_price: Option<u64>,
     pub l1_rpc_url: Option<String>,
     pub l2_rpc_url: Option<String>,
-    pub gw_rpc_url: Option<String>,
     pub server_upgrade_timestamp: Option<u64>,
     #[clap(long, default_missing_value = "true")]
     pub dangerous_no_cross_check: Option<bool>,
@@ -52,27 +50,12 @@ impl ChainUpgradeParams {
                     .as_secs(),
             ),
         );
-        self.gw_rpc_url = Some(
-            self.gw_rpc_url
-                .unwrap_or("http://localhost:3250".to_string()),
-        );
 
         self.l1_rpc_url = Some(
             self.l1_rpc_url
                 .unwrap_or("http://localhost:8545".to_string()),
         );
 
-        self.gw_rpc_url = if let Some(url) = self.gw_rpc_url {
-            Some(url)
-        } else {
-            chain_config
-                .get_secrets_config()
-                .await?
-                .gateway_rpc_url()
-                .ok()
-        };
-
-        self.gw_chain_id = Some(self.gw_chain_id.unwrap_or(506));
         self.l1_gas_price = Some(self.l1_gas_price.unwrap_or(100000));
         self.l2_rpc_url = if let Some(url) = self.l2_rpc_url {
             Some(url)
@@ -87,7 +70,6 @@ impl ChainUpgradeParams {
 pub struct UpgradeArgsInner {
     pub chain_id: u64,
     pub l1_rpc_url: String,
-    pub gw_rpc_url: Option<String>,
 }
 
 impl From<DefaultChainUpgradeArgs> for UpgradeArgsInner {
@@ -95,7 +77,6 @@ impl From<DefaultChainUpgradeArgs> for UpgradeArgsInner {
         Self {
             chain_id: value.params.chain_id.unwrap(),
             l1_rpc_url: value.params.l1_rpc_url.clone().unwrap(),
-            gw_rpc_url: value.params.gw_rpc_url.clone(),
         }
     }
 }
@@ -105,7 +86,6 @@ impl From<ChainUpgradeParams> for UpgradeArgsInner {
         Self {
             chain_id: value.chain_id.unwrap(),
             l1_rpc_url: value.l1_rpc_url.clone().unwrap(),
-            gw_rpc_url: value.gw_rpc_url.clone(),
         }
     }
 }
